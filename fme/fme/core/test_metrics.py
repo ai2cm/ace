fd1e52172bca5d3084a1bfc8a0b05e1dee6bc4ed
Gideon Dresdner <gideond@allenai.org>
2023-04-18 10:27:53 -0700

    parameterized tests boilerplate

--- fme/fme/core/test_metrics.py
+++ fme/fme/core/test_metrics.py
@@ -1,5 +1,28 @@
+import pytest
 import metrics
 
+variables = ['variable1', 'variable2', 'variable3']
+times = [1, 2, 4, 8, 16]
+grid_yts = [45, 90, 180]
+grid_xts = [45, 90, 180]
 
-def test_global_mean_bias():
-    assert False
+test_parameters = [(variable, time, grid_yt, grid_xt) for variable in variables
+                   for time in times for grid_yt in grid_yts for grid_xt in grid_xts]
+
+
+@pytest.mark.parametrize("variable, time, grid_yt, grid_xt", test_parameters)
+def test_weighted_global_mean_bias(variable, time, grid_yt, grid_xt):
+    result = metrics.weighted_global_mean_bias(variable, time, grid_yt, grid_xt)
+    assert isinstance(result, float), "The result should be a float"
+
+
+@pytest.mark.parametrize("variable, time, grid_yt, grid_xt", test_parameters)
+def test_weighted_time_mean_bias(variable, time, grid_yt, grid_xt):
+    result = metrics.weighted_time_mean_bias(variable, time, grid_yt, grid_xt)
+    assert isinstance(result, float), "The result should be a float"
+
+
+@pytest.mark.parametrize("variable, time, grid_yt, grid_xt", test_parameters)
+def test_weighted_global_time_rmse(variable, time, grid_yt, grid_xt):
+    result = metrics.weighted_global_time_rmse(variable, time, grid_yt, grid_xt)
+    assert isinstance(result, float), "The result should be a float"
