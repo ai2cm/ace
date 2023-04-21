c1f8ab7e43a9e720cab35c1745a0f034884159a5
Gideon Dresdner <gideond@allenai.org>
2023-04-21 13:25:16 -0700

    directory structure

---
+++ fme/fme/core/__init__.py
@@ -0,0 +1,15 @@
+from metrics import (
+    lat_cell_centers,
+    spherical_area_weights,
+    weighted_mean,
+    weighted_mean_bias,
+    root_mean_squared_error,
+)
+
+__all__ = [
+    'lat_cell_centers',
+    'spherical_area_weights',
+    'weighted_mean',
+    'weighted_mean_bias',
+    'root_mean_squared_error',
+]
