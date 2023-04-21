e669606fa23bc51482ec7cb9fc5e31bcd3f7665b
Gideon Dresdner <gideond@allenai.org>
2023-04-21 12:37:45 -0700

    wip

--- fme/fme/core/metrics.py
+++ fme/fme/core/metrics.py
@@ -48,6 +48,10 @@ def weighted_mean(
     """
     if weights is None:
         return tensor.mean(dim=dim)
+    
+    a = (tensor * weights).sum(dim=dim)
+    b = weights.sum(dim=dim)
+    print(f"{a=}, {b=}")
     return (tensor * weights).sum(dim=dim) / weights.sum(dim=dim)
 
 
