4fc497eca5ed2b4d10b2b51bdf9347828f7b2a5f
Jeremy McGibbon <mcgibbon@uw.edu>
2023-07-13 11:11:07 -0400

    Allow standalone inference on more than one sample (#178)
    This PR updates the standalone inference so it can be configured and run
    on more than one sample.
    
    Functionally there are no other changes, though because of
    confusing/hacky ways the APIs were previously used, it should be noted
    that aggregators are no longer used to store any netCDF data during
    inference. These were previously only used to store ensemble-mean
    values, but only on a single value so that it corresponded to raw data.
    Because of this previous (and now removed) restriction, no user
    functionality has changed. Saving ensemble-mean [time, lat, lon] metrics
    is left to a future PR.
    
    Instead, raw data is stored directly by appending to netCDF using
    netcdf4-python.
    
    No changes are made to inference during training, which is still
    hard-coded to run on a single ensemble member. Updating this is also
    left for a future PR.

---
+++ fme/fme/fcn_training/inference/test_data_writer.py
@@ -0,0 +1,95 @@
+from fme.fcn_training.inference.data_writer import DataWriter
+import pytest
+import torch
+import numpy as np
+from typing import NamedTuple
+from netCDF4 import Dataset
+
+
+class TestDataWriter:
+    class VariableMetadata(NamedTuple):
+        units: str
+        long_name: str
+
+    @pytest.fixture
+    def sample_metadata(self):
+        return {
+            "temp": self.VariableMetadata(units="K", long_name="Temperature"),
+            "humidity": self.VariableMetadata(units="%", long_name="Relative Humidity"),
+        }
+
+    @pytest.fixture
+    def sample_target_data(self):
+        data = {
+            # sample, time, lat, lon
+            "temp": torch.rand((2, 3, 4, 5)),
+            "humidity": torch.rand((2, 3, 4, 5)),  # input-only variable
+            "pressure": torch.rand(
+                (2, 3, 4, 5)
+            ),  # Extra variable for which there's no metadata
+        }
+        return data
+
+    @pytest.fixture
+    def sample_prediction_data(self):
+        data = {
+            # sample, time, lat, lon
+            "temp": torch.rand((2, 3, 4, 5)),
+            "pressure": torch.rand(
+                (2, 3, 4, 5)
+            ),  # Extra variable for which there's no metadata
+        }
+        return data
+
+    def test_append_batch(
+        self, sample_metadata, sample_target_data, sample_prediction_data, tmp_path
+    ):
+        filename = tmp_path / "test.nc"
+        n_samples = 4
+        writer = DataWriter(
+            str(filename), n_samples=n_samples, metadata=sample_metadata
+        )
+        writer.append_batch(
+            sample_target_data, sample_prediction_data, start_timestep=0, start_sample=0
+        )
+        writer.append_batch(
+            sample_target_data, sample_prediction_data, start_timestep=0, start_sample=2
+        )
+        writer.append_batch(
+            sample_target_data, sample_prediction_data, start_timestep=2, start_sample=0
+        )
+        writer.append_batch(
+            sample_target_data, sample_prediction_data, start_timestep=2, start_sample=2
+        )
+
+        # Open the file again and check the data
+        dataset = Dataset(filename, "r")
+        assert set(dataset.variables.keys()) == set(sample_target_data.keys()).union(
+            {"source"}
+        )
+        assert (
+            dataset.variables["source"][:] == np.array(["target", "prediction"])
+        ).all()
+
+        for var_name in sample_target_data.keys():
+            var_data = dataset.variables[var_name][:]
+            assert var_data.shape == (2, 4, 5, 4, 5)  # source, sample, time, lat, lon
+            assert not np.isnan(var_data[0, :]).any(), "unexpected NaNs in target data"
+            if var_name in sample_prediction_data:
+                assert not np.isnan(
+                    var_data[1, :]
+                ).any(), "unexpected NaNs in prediction data"
+
+            if var_name in sample_metadata:
+                assert (
+                    dataset.variables[var_name].units == sample_metadata[var_name].units
+                )
+                assert (
+                    dataset.variables[var_name].long_name
+                    == sample_metadata[var_name].long_name
+                )
+            else:
+                assert not hasattr(dataset.variables[var_name], "units")
+                assert not hasattr(dataset.variables[var_name], "long_name")
+
+        dataset.close()
