d1b329430c46566f98fbbe891d4db43cd99390f1
Oliver Watt-Meyer <oliwm@vulcan.com>
2023-07-18 22:02:32 +0000

    actually lint...

--- projects/data_process/compute_dataset_fv3gfs_main.sh
+++ projects/data_process/compute_dataset_fv3gfs_main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-argo submit compute_dataset_fv3gfs_argo_workflow.yaml -p python_script="$(< compute_dataset_fv3gfs.py)" 
+argo submit compute_dataset_fv3gfs_argo_workflow.yaml -p python_script="$(< compute_dataset_fv3gfs.py)"
