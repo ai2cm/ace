a93bf6d59c5bea60485680cbc2484f6fe012e3b9
Jeremy McGibbon <mcgibbon@uw.edu>
2023-07-26 11:34:12 -0400

    Add config to interpolate prescribed and generated data (#198)
    We have gridcells with fractional ocean area coverage. In these cells it
    may be more appropriate to interpolate between generated and prescribed
    surface temperature values, rather than replacing them completely, to
    give values which play better with the spherical harmonic transform.
    
    This PR adds a configuration option allowing this to be done, assuming
    the mask variable is 1 where values should be prescribed and 0 where
    they should not. It maintains backwards compatibility with the existing
    code, and is disabled by default.

--- fme/fme/core/prescriber.py
+++ fme/fme/core/prescriber.py
@@ -6,11 +6,29 @@ import torch
 @dataclasses.dataclass
 class PrescriberConfig:
     """Configuration for overwriting the predictions of 'prescribed_name' by the target
-    values in the region where 'mask_name' == 'mask_value'."""
+    values in the region where 'mask_name' == 'mask_value'.
+
+    Attributes:
+        prescribed_name: Name of the variable to be overwritten.
+        mask_name: Name of the mask variable.
+        mask_value: Value of the mask variable in the region to be overwritten.
+        interpolate: Whether to interpolate linearly between the generated and target
+            values in the masked region, where 0 means keep the generated values and
+            1 means replace completely with the target values. Requires mask_value
+            be set to 1.
+    """
 
     prescribed_name: str
     mask_name: str
     mask_value: int
+    interpolate: bool = False
+
+    def __post_init__(self):
+        if self.interpolate and self.mask_value != 1:
+            raise ValueError(
+                "Interpolation requires mask_value to be 1, but it is set to "
+                f"{self.mask_value}."
+            )
 
     def build(self, in_names: List[str], out_names: List[str]):
         if not (self.prescribed_name in in_names and self.prescribed_name in out_names):
@@ -22,6 +40,7 @@ class PrescriberConfig:
             prescribed_name=self.prescribed_name,
             mask_name=self.mask_name,
             mask_value=self.mask_value,
+            interpolate=self.interpolate,
         )
 
 
@@ -30,10 +49,17 @@ class Prescriber:
     Responsible for overwriting model predictions by target data in masked regions.
     """
 
-    def __init__(self, prescribed_name: str, mask_name: str, mask_value: int):
+    def __init__(
+        self,
+        prescribed_name: str,
+        mask_name: str,
+        mask_value: int,
+        interpolate: bool = False,
+    ):
         self.prescribed_name = prescribed_name
         self.mask_name = mask_name
         self.mask_value = mask_value
+        self.interpolate = interpolate
 
     def __call__(
         self,
@@ -41,12 +67,26 @@ class Prescriber:
         gen_norm: Dict[str, torch.Tensor],
         target_norm: Dict[str, torch.Tensor],
     ):
-        # overwrite specified generated variable in given mask region
-        prescribed_gen = torch.where(
-            torch.round(data[self.mask_name]).to(int) == self.mask_value,
-            target_norm[self.prescribed_name],
-            gen_norm[self.prescribed_name],
-        )
+        """
+        Args:
+            data: Dictionary of data containing the mask variable.
+            gen_norm: Dictionary of generated data.
+            target_norm: Dictionary of target data.
+        """
+        if self.interpolate:
+            mask = data[self.mask_name]
+            # 0 keeps the generated values, 1 replaces completely with the target values
+            prescribed_gen = (
+                mask * target_norm[self.prescribed_name]
+                + (1 - mask) * gen_norm[self.prescribed_name]
+            )
+        else:
+            # overwrite specified generated variable in given mask region
+            prescribed_gen = torch.where(
+                torch.round(data[self.mask_name]).to(int) == self.mask_value,
+                target_norm[self.prescribed_name],
+                gen_norm[self.prescribed_name],
+            )
         return {**gen_norm, self.prescribed_name: prescribed_gen}
 
     def get_state(self):
@@ -54,17 +94,23 @@ class Prescriber:
             "prescribed_name": self.prescribed_name,
             "mask_name": self.mask_name,
             "mask_value": self.mask_value,
+            "interpolate": self.interpolate,
         }
 
     def load_state(self, state):
         self.prescribed_name = state["prescribed_name"]
         self.mask_name = state["mask_name"]
         self.mask_value = state["mask_value"]
+        interpolate = state.get("interpolate", False)
+        self.interpolate = interpolate
 
     @classmethod
     def from_state(cls, state) -> "Prescriber":
         return Prescriber(
-            state["prescribed_name"], state["mask_name"], state["mask_value"]
+            state["prescribed_name"],
+            state["mask_name"],
+            state["mask_value"],
+            state.get("interpolate", False),
         )
 
 
