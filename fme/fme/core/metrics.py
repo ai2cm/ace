6d7a74dfead4662730b7b111c29f3be4539673e5
Gideon Dresdner <gideond@allenai.org>
2023-07-27 15:51:58 -0400

    Add the dry air mass metric  (#208)
    First attempt at incorporating the dry air mass computation as a new
    metric. Correctness is verified in [this
    notebook](https://github.com/ai2cm/explore/blob/4ffaca70a59af3eebaabf5457e3261614bd19171/gideond/full-model-emulation/2023-07-21-dry-air-mass.ipynb).

--- fme/fme/core/metrics.py
+++ fme/fme/core/metrics.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Optional, Union
+from typing import Iterable, Optional, Tuple, Union
 from typing_extensions import TypeAlias
 import numpy as np
 
@@ -178,3 +178,53 @@ def time_and_global_mean_bias(
         predicted_time_mean - truth_time_mean, weights=weights, dim=spatial_dims
     )
     return result
+
+
+def global_dry_air_mass(
+    specific_total_water: torch.Tensor,
+    surface_pressure: torch.Tensor,
+    sigma_grid_offsets_ak: torch.Tensor,
+    sigma_grid_offsets_bk: torch.Tensor,
+    area: torch.Tensor,
+    spatial_dims: Optional[Tuple[int, int]] = (0, 1),
+) -> torch.Tensor:
+    """Computes the global dry air mass in kg/m/s^2.
+
+    Args:
+        specific_total_water (lat, lon, vertical_level), (kg/kg)
+        surface_pressure: (lat, lon), (Pa)
+        sigma_grid_offsets_ak: Sorted sigma grid offsets ak, (vertical_level + 1,)
+        sigma_grid_offsets_bk: Sorted sigma grid offsets bk, (vertical_level + 1,)
+        area: Area weights of each grid cell (m^2), (lat, lon)
+        spatial_dims: Tuple indicating the spatial (lat, lon) dimensions of the
+            input tensors.
+
+    Returns:
+        Integrated global dry air mass (1,), (kg)
+    """
+
+    num_levels = len(sigma_grid_offsets_ak) - 1
+
+    if (
+        num_levels != len(sigma_grid_offsets_bk) - 1
+        or num_levels != specific_total_water.shape[-1]
+    ):
+        raise ValueError(
+            (
+                "Number of vertical levels in ak, bk, and specific_total_water must"
+                "be the same."
+            )
+        )
+
+    ak, bk = sigma_grid_offsets_ak, sigma_grid_offsets_bk
+
+    pressure_thickness = ((ak + (surface_pressure.unsqueeze(-1) * bk))).diff(
+        dim=-1
+    )  # Pa
+    water_path = torch.sum(
+        pressure_thickness * specific_total_water, axis=-1
+    )  # Pa * kg / kg = Pa  # type: ignore
+
+    gravity = 9.80665  # m / s^2, valued used by FV3GFS
+    dry_air_mass = 1 / gravity * (surface_pressure - water_path)  # kg / m^2
+    return (dry_air_mass * area).sum(axis=spatial_dims)
