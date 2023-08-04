ai2cm/ace

commit 8c5e6c0675d117d6b62ccdd4b2fc63eeffd6c69b

Add lat and lon coords to inference output (#231)
Resolves #228.

Adds `horizontal_coordinates` information to the `GriddedDataset` as the
interface to retrieve latitude and longitude from the input data.

fme/fme/fcn_training/utils/data_loader_xarray.py
@@ -5,7 +5,7 @@ import logging
 import xarray as xr
 from glob import glob
 from typing import Mapping, Optional, Tuple
-from .data_typing import Dataset, VariableMetadata
+from .data_typing import Dataset, HorizontalCoordinates, VariableMetadata
 from .data_loader_params import DataLoaderParams
 from .data_requirements import DataRequirements
 from .data_loader_netcdf4 import get_sigma_coordinates
@@ -59,6 +59,11 @@ class XarrayDataset(Dataset):
         lons, lats = get_lons_and_lats(first_dataset)
         self._area_weights = metrics.spherical_area_weights(lats, len(lons))
         self._sigma_coordinates = get_sigma_coordinates(first_dataset)
+        self._horizontal_coordinates = HorizontalCoordinates(lat=lats, lon=lons)
+
+    @property
+    def horizontal_coordinates(self) -> HorizontalCoordinates:
+        return self._horizontal_coordinates
 
     def _get_metadata(self, ds):
         result = {}

fme/fme/fcn_training/inference/data_writer.py
@@ -13,16 +13,22 @@ class DataWriter:
     VariableMetadata = namedtuple("VariableMetadata", ["units", "long_name"])
 
     def __init__(
-        self, filename: str, n_samples: int, metadata: Mapping[str, VariableMetadata]
+        self,
+        filename: str,
+        n_samples: int,
+        metadata: Mapping[str, VariableMetadata],
+        coords: Mapping[str, np.ndarray],
     ):
         """
         Args:
             filename: Path to the netCDF file to write to.
             n_samples: Number of samples to write to the file.
             metadata: Metadata for each variable to be written to the file.
+            coords: Coordinate data to be written to the file.
         """
         self.filename = filename
         self.metadata = metadata
+        self.coords = coords
         self.dataset = Dataset(filename, "w", format="NETCDF4")
         self.dataset.createDimension("source", 2)
         self.dataset.createDimension("timestep", None)  # unlimited dimension
@@ -50,12 +56,18 @@ class DataWriter:
         """
         if self._n_lat is None:
             self._n_lat = target[next(iter(target.keys()))].shape[-2]
-            self.dataset.createDimension("n_lat", self._n_lat)
+            self.dataset.createDimension("lat", self._n_lat)
+            if "lat" in self.coords:
+                self.dataset.createVariable("lat", "f4", ("lat",))
+                self.dataset.variables["lat"][:] = self.coords["lat"]
         if self._n_lon is None:
             self._n_lon = target[next(iter(target.keys()))].shape[-1]
-            self.dataset.createDimension("n_lon", self._n_lon)
+            self.dataset.createDimension("lon", self._n_lon)
+            if "lon" in self.coords:
+                self.dataset.createVariable("lon", "f4", ("lon",))
+                self.dataset.variables["lon"][:] = self.coords["lon"]
 
-        dims = ("source", "sample", "timestep", "n_lat", "n_lon")
+        dims = ("source", "sample", "timestep", "lat", "lon")
         for variable_name in set(target.keys()).union(prediction.keys()):
             # define the variable if it doesn't exist
             if variable_name not in self.dataset.variables:

fme/fme/fcn_training/inference/inference.py
@@ -149,6 +149,7 @@ def main(
             filename=output_netcdf_filename,
             n_samples=n_samples,
             metadata=validation.metadata,
+            coords=validation.coords,
         )
     else:
         writer = NullDataWriter()

fme/fme/fcn_training/inference/test_data_writer.py
@@ -47,7 +47,10 @@ class TestDataWriter:
         filename = tmp_path / "test.nc"
         n_samples = 4
         writer = DataWriter(
-            str(filename), n_samples=n_samples, metadata=sample_metadata
+            str(filename),
+            n_samples=n_samples,
+            metadata=sample_metadata,
+            coords={"lat": np.arange(4), "lon": np.arange(5)},
         )
         writer.append_batch(
             sample_target_data, sample_prediction_data, start_timestep=0, start_sample=0
@@ -65,7 +68,7 @@ class TestDataWriter:
         # Open the file again and check the data
         dataset = Dataset(filename, "r")
         assert set(dataset.variables.keys()) == set(sample_target_data.keys()).union(
-            {"source"}
+            {"source", "lat", "lon"}
         )
         assert (
             dataset.variables["source"][:] == np.array(["target", "prediction"])
@@ -100,7 +103,10 @@ class TestDataWriter:
         filename = tmp_path / "test.nc"
         n_samples = 4
         writer = DataWriter(
-            str(filename), n_samples=n_samples, metadata=sample_metadata
+            str(filename),
+            n_samples=n_samples,
+            metadata=sample_metadata,
+            coords={"lat": np.arange(4), "lon": np.arange(5)},
         )
         writer.append_batch(
             sample_target_data, sample_prediction_data, start_timestep=0, start_sample=0

fme/fme/fcn_training/inference/test_inference.py
@@ -117,3 +117,5 @@ def test_inference_plus_one_model(tmp_path: pathlib.Path, use_prediction_data: b
             prediction_ds["x"].isel(timestep=i).values + 1,
             prediction_ds["x"].isel(timestep=i + 1).values,
         )
+    assert "lat" in prediction_ds.coords
+    assert "lon" in prediction_ds.coords

fme/fme/fcn_training/utils/data_loader_multifiles.py
@@ -111,6 +111,7 @@ def _get_ensemble_dataset(
     ensemble.metadata = metadatas[0]  # type: ignore
     ensemble.area_weights = datasets[0].area_weights  # type: ignore
     ensemble.sigma_coordinates = datasets[0].sigma_coordinates  # type: ignore
+    ensemble.horizontal_coordinates = datasets[0].horizontal_coordinates  # type: ignore
     return ensemble
 
 
@@ -188,4 +189,5 @@ def get_data_loader(
         area_weights=dataset.area_weights,
         sampler=sampler,
         sigma_coordinates=dataset.sigma_coordinates,
+        horizontal_coordinates=dataset.horizontal_coordinates,
     )

fme/fme/fcn_training/utils/data_loader_netcdf4.py
@@ -3,7 +3,6 @@ import os
 from typing import Mapping, Optional
 
 import netCDF4
-from torch.utils.data import Dataset
 import torch
 import xarray as xr
 
@@ -12,7 +11,12 @@ from fme.core import metrics
 from .data_loader_params import DataLoaderParams
 from .data_requirements import DataRequirements
 from .data_utils import load_series_data, get_lons_and_lats
-from .data_typing import SigmaCoordinates, VariableMetadata
+from .data_typing import (
+    SigmaCoordinates,
+    HorizontalCoordinates,
+    VariableMetadata,
+    Dataset,
+)
 from fme.core.device import get_device
 
 
@@ -43,7 +47,7 @@ class NetCDF4Dataset(Dataset):
         logging.info(f"Opening data at {self.full_path}")
         self.ds = netCDF4.MFDataset(self.full_path)
         self.ds.set_auto_mask(False)
-        self.metadata = self._compute_variable_metadata()  # Note: requires `self.ds`
+        self._metadata = self._compute_variable_metadata()  # Note: requires `self.ds`
         self.n_samples_total = len(self.ds.variables["time"][:]) - self.n_steps + 1
         if params.n_samples is not None:
             if params.n_samples > self.n_samples_total:
@@ -56,8 +60,29 @@ class NetCDF4Dataset(Dataset):
         self.window_time_slice = window_time_slice
         self.lons, self.lats = get_lons_and_lats(self.ds)
         self._log_files_stats()
-        self.area_weights = metrics.spherical_area_weights(self.lats, len(self.lons))
-        self.sigma_coordinates = get_sigma_coordinates(self.ds)
+
+        self._area_weights = metrics.spherical_area_weights(self.lats, len(self.lons))
+        self._sigma_coordinates = get_sigma_coordinates(self.ds)
+        self._horizontal_coordinates = HorizontalCoordinates(
+            lat=torch.as_tensor(self.lats, device=get_device()),
+            lon=torch.as_tensor(self.lons, device=get_device()),
+        )
+
+    @property
+    def area_weights(self):
+        return self._area_weights
+
+    @property
+    def sigma_coordinates(self):
+        return self._sigma_coordinates
+
+    @property
+    def horizontal_coordinates(self):
+        return self._horizontal_coordinates
+
+    @property
+    def metadata(self):
+        return self._metadata
 
     def _log_files_stats(self):
         img_shape_y = len(self.lats)

fme/fme/fcn_training/utils/data_typing.py
@@ -3,6 +3,7 @@ from collections import namedtuple
 from typing import Mapping, Optional
 import dataclasses
 
+import numpy as np
 import torch
 
 
@@ -24,6 +25,28 @@ class SigmaCoordinates:
 
     ak: torch.Tensor
     bk: torch.Tensor
+
+    @property
+    def coords(self) -> Mapping[str, np.ndarray]:
+        return {"ak": self.ak.cpu().numpy(), "bk": self.bk.cpu().numpy()}
+
+
+@dataclasses.dataclass
+class HorizontalCoordinates:
+    """
+    Defines a (latitude, longitude) grid.
+
+    Attributes:
+        lat: 1-dimensional tensor of latitudes
+        lon: 1-dimensional tensor of longitudes
+    """
+
+    lat: torch.Tensor
+    lon: torch.Tensor
+
+    @property
+    def coords(self) -> Mapping[str, np.ndarray]:
+        return {"lat": self.lat.cpu().numpy(), "lon": self.lon.cpu().numpy()}
 
 
 @dataclasses.dataclass
@@ -52,7 +75,15 @@ class GriddedData:
     metadata: Mapping[str, VariableMetadata]
     area_weights: torch.Tensor
     sigma_coordinates: SigmaCoordinates
+    horizontal_coordinates: HorizontalCoordinates
     sampler: Optional[torch.utils.data.Sampler] = None
+
+    @property
+    def coords(self) -> Mapping[str, np.ndarray]:
+        return {
+            **self.horizontal_coordinates.coords,
+            **self.sigma_coordinates.coords,
+        }
 
 
 class Dataset(torch.utils.data.Dataset, abc.ABC):
@@ -62,6 +93,10 @@ class Dataset(torch.utils.data.Dataset, abc.ABC):
 
     @abc.abstractproperty
     def area_weights(self) -> torch.Tensor:
+        ...
+
+    @abc.abstractproperty
+    def horizontal_coordinates(self) -> HorizontalCoordinates:
         ...
 
     @abc.abstractproperty

commit 0921ac95ba10a398b0b319bf2281d7ef8afc1f14

Fix torch tensor versus numpy array issue (#237)
Fixes a bug introduced in #231 (HorizontalCoordinates assumes the
lat/lon coordinates are torch tensors).

fme/fme/fcn_training/utils/data_loader_xarray.py
@@ -5,6 +5,7 @@ import logging
 import xarray as xr
 from glob import glob
 from typing import Mapping, Optional, Tuple
+import fme
 from .data_typing import Dataset, HorizontalCoordinates, VariableMetadata
 from .data_loader_params import DataLoaderParams
 from .data_requirements import DataRequirements
@@ -59,7 +60,10 @@ class XarrayDataset(Dataset):
         lons, lats = get_lons_and_lats(first_dataset)
         self._area_weights = metrics.spherical_area_weights(lats, len(lons))
         self._sigma_coordinates = get_sigma_coordinates(first_dataset)
-        self._horizontal_coordinates = HorizontalCoordinates(lat=lats, lon=lons)
+        self._horizontal_coordinates = HorizontalCoordinates(
+            lat=torch.as_tensor(lats, device=fme.get_device()),
+            lon=torch.as_tensor(lons, device=fme.get_device()),
+        )
 
     @property
     def horizontal_coordinates(self) -> HorizontalCoordinates: