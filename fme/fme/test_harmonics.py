622c7eafcaf85cf6cb3497e451335a87e6eee11f
Gideon Dresdner <gideond@allenai.org>
2023-09-05 15:19:05 -0400

    Enable multistep autoregressive training (#317)
    This PR enables multistep autoregressive training. This is important to
    improve performance and compare to existing works.
    
    Changes:
    
    - Removes `NotImplementedError` raised when `n_forward_steps` is not
    one.
    - Modified tests, e.g. `n_forward_steps` is now specified in mock
    configs.
    - `sht_fix.py` is a fork of torch_harmonics's `sht.py` with
    modifications that can be applied by changing the hard-coded variable
    `USE_FIX`. This turned out to be necessary to get multistep
    autoregressive training working on multiple processes (e.g. GPUs).
    
    Resolves https://github.com/ai2cm/full-model/issues/291

--- fme/fme/test_harmonics.py
+++ fme/fme/test_harmonics.py
@@ -12,22 +12,22 @@ def test_constant_field(nlat, nlon, grid, constant):
     """Tests that the SHT of a constant field has a single non-zero wavenumber,
     the first one.
     """
-    constant_field = torch.tensor(constant, dtype=torch.float64).repeat(nlat, nlon)
+    constant_field = torch.tensor(constant).repeat(nlat, nlon)
 
     sht = harmonics.RealSHT(nlat, nlon, grid=grid)
 
     coeffs = sht(constant_field).ravel()
-    zero = torch.zeros(1, dtype=torch.complex128)
+    zero = torch.zeros(1, dtype=torch.complex64)
 
     assert not torch.isclose(coeffs[0], zero)
-    assert torch.all(torch.isclose(zero, coeffs[1:]))
+    assert torch.all(torch.isclose(zero, coeffs[1:], atol=1e-6))
 
 
 def _roundtrip(field, grid):
     nlat, nlon = field.shape
     sht = harmonics.RealSHT(nlat, nlon, grid=grid)
     isht = harmonics.InverseRealSHT(nlat, nlon, grid=grid)
-    return isht(sht(field.type(torch.double)))
+    return isht(sht(field))
 
 
 @pytest.mark.parametrize("seed", [0, 1])
@@ -37,4 +37,4 @@ def test_roundtrip(nlat, nlon, seed, grid="legendre-gauss"):
     torch.manual_seed(seed)
     random_field = torch.randn(nlat, nlon)
     proj = _roundtrip(random_field, grid)
-    assert torch.all(torch.isclose(proj, _roundtrip(proj, grid)))
+    assert torch.all(torch.isclose(proj, _roundtrip(proj, grid), atol=1e-6))
