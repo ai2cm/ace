ai2cm/ace

commit 45e50c1803ee1b7d22b1cd32e13ac7c6c09580a2

Refactor optimization config outside of stepper config (#318)
This PR factors out optimization from the stepper. This is will be
helpful when building the fine-tuning system which will re-use an
existing stepper while swapping out a different optimizer (e.g. with a
smaller learning rate).

Changes:
- The configuration for the optimization is now a top-level block within
`TrainConfig` rather than nested within the `stepper` block. This will
be reflected in a [PR to
vcm-control-workflow](https://github.com/ai2cm/vcm-workflow-control/pull/177).
- `run_on_batch` now requires an `Optimization` object as an argument.
Rather than using a `train` flag to enable/disable training, training is
now enabled or disabled via the null object pattern, e.g. by using
`Optimization` or `NullOptimization` respectively.
- The `Optimization` object is checkpointed alongside the stepper.
- `max_epochs` is now passed to the scheduler when it is built and
stored as a private attribute.

Resolves #310

fme/fme/core/optimization.py
@@ -16,6 +16,7 @@ class Optimization:
         parameters,
         optimizer_type: Literal["Adam", "FusedAdam"],
         lr: float,
+        max_epochs: int,
         scheduler: SchedulerConfig,
         enable_automatic_mixed_precision: bool,
         kwargs: Mapping[str, Any],
@@ -31,12 +32,16 @@ class Optimization:
             self.gscaler: Optional[amp.GradScaler] = amp.GradScaler()
         else:
             self.gscaler = None
-        self.scheduler = scheduler.build(self.optimizer)
+        self.scheduler = scheduler.build(self.optimizer, max_epochs)
 
     @contextlib.contextmanager
     def autocast(self):
         with amp.autocast(enabled=self.gscaler is not None):
             yield
+
+    @property
+    def learning_rate(self) -> float:
+        return self.optimizer.param_groups[0]["lr"]
 
     def set_mode(self, module: nn.Module):
         """
@@ -124,6 +129,7 @@ class OptimizationConfig:
             parameters=parameters,
             optimizer_type=self.optimizer_type,
             lr=self.lr,
+            max_epochs=max_epochs,
             scheduler=self.scheduler,
             enable_automatic_mixed_precision=self.enable_automatic_mixed_precision,
             kwargs=self.kwargs,
@@ -141,6 +147,10 @@ class NullOptimization:
     @contextlib.contextmanager
     def autocast(self):
         yield
+
+    @property
+    def learning_rate(self) -> float:
+        return float("nan")
 
     def step_scheduler(self, valid_loss: float):
         return

fme/fme/core/stepper.py
@@ -20,7 +20,7 @@ from fme.core.packer import Packer
 from fme.core.prescriber import NullPrescriber, Prescriber, PrescriberConfig
 from fme.fcn_training.registry import ModuleSelector
 
-from .optimization import NullOptimization, Optimization, OptimizationConfig
+from .optimization import NullOptimization, Optimization
 
 
 @dataclasses.dataclass
@@ -29,7 +29,6 @@ class SingleModuleStepperConfig:
     in_names: List[str]
     out_names: List[str]
     normalization: Union[NormalizationConfig, FromStateNormalizer]
-    optimization: Optional[OptimizationConfig] = None
     prescriber: Optional[PrescriberConfig] = None
     loss: LossConfig = dataclasses.field(default_factory=lambda: LossConfig())
 
@@ -41,21 +40,19 @@ class SingleModuleStepperConfig:
             n_timesteps=n_forward_steps + 1,
         )
 
+    def get_state(self):
+        return dataclasses.asdict(self)
+
     def get_stepper(
         self,
         shapes: Dict[str, Tuple[int, ...]],
-        max_epochs: int,
-        area: torch.Tensor,
+        area: Optional[torch.Tensor],
     ):
         return SingleModuleStepper(
             config=self,
             data_shapes=shapes,
-            max_epochs=max_epochs,
             area=area,
         )
-
-    def get_state(self):
-        return dataclasses.asdict(self)
 
     @classmethod
     def from_state(cls, state) -> "SingleModuleStepperConfig":
@@ -130,15 +127,12 @@ class SingleModuleStepper:
         self,
         config: SingleModuleStepperConfig,
         data_shapes: Dict[str, Tuple[int, ...]],
-        max_epochs: int,
         area: torch.Tensor,
     ):
         """
         Args:
             config: The configuration.
             data_shapes: The shapes of the data.
-            max_epochs: The maximum number of epochs. Used when constructing
-                certain learning rate schedulers, if applicable.
             area: (n_lat, n_lon) array containing relative gridcell area,
                 in any units including unitless.
         """
@@ -160,16 +154,6 @@ class SingleModuleStepper:
         ).to(get_device())
         self.data_shapes = data_shapes
         self._config = config
-        self._max_epochs = max_epochs
-        if config.optimization is not None:
-            self.optimization: Union[
-                Optimization, NullOptimization
-            ] = config.optimization.build(
-                parameters=self.module.parameters(), max_epochs=max_epochs
-            )
-        else:
-            self.optimization = NullOptimization()
-
         self._no_optimization = NullOptimization()
 
         if dist.is_distributed():
@@ -202,20 +186,10 @@ class SingleModuleStepper:
         """
         return [self.module]
 
-    def step_scheduler(self, valid_loss: float):
-        """
-        Step the scheduler.
-
-        Args:
-            valid_loss: The validation loss. Used in schedulers which change the
-                learning rate based on whether the validation loss is decreasing.
-        """
-        self.optimization.step_scheduler(valid_loss)
-
     def run_on_batch(
         self,
         data: Dict[str, torch.Tensor],
-        train: bool,
+        optimization: Union[Optimization, NullOptimization],
         n_forward_steps: int = 1,
         aggregator: Optional[OneStepAggregator] = None,
     ) -> SteppedData:
@@ -224,7 +198,8 @@ class SingleModuleStepper:
 
         Args:
             data: The batch data of shape [n_sample, n_timesteps, n_channels, n_x, n_y].
-            train: Whether to train the model.
+            optimization: The optimization class to use for updating the module.
+                Use `NullOptimization` to disable training.
             n_forward_steps: The number of timesteps to run the model for.
             aggregator: The data aggregator.
 
@@ -238,15 +213,6 @@ class SingleModuleStepper:
             ] = NullAggregator()
         else:
             non_none_aggregator = aggregator
-        if train:
-            if self.optimization is None:
-                raise ValueError(
-                    "Cannot train without an optimizer, "
-                    "should be passed at initialization."
-                )
-            optimization: Union[Optimization, NullOptimization] = self.optimization
-        else:
-            optimization = self._no_optimization
 
         device = get_device()
         device_data = {
@@ -272,42 +238,36 @@ class SingleModuleStepper:
         """
         return {
             "module": self.module.state_dict(),
-            "optimization": self.optimization.get_state(),
             "normalizer": self.normalizer.get_state(),
             "in_packer": self.in_packer.get_state(),
             "out_packer": self.out_packer.get_state(),
             "data_shapes": self.data_shapes,
-            "max_epochs": self._max_epochs,
             "config": self._config.get_state(),
             "prescriber": self.prescriber.get_state(),
         }
 
-    def load_state(self, state, load_optimizer: bool = True):
+    def load_state(self, state):
         """
         Load the state of the stepper.
 
         Args:
             state: The state to load.
-            load_optimizer: Whether to load the optimizer state.
         """
         if "module" in state:
             self.module.load_state_dict(state["module"])
-        if load_optimizer and "optimization" in state:
-            self.optimization.load_state(state["optimization"])
         self.in_packer = Packer.from_state(state["in_packer"])
         self.out_packer = Packer.from_state(state["out_packer"])
         self.prescriber.load_state(state["prescriber"])
 
     @classmethod
-    def from_state(
-        cls, state, area: torch.Tensor, load_optimizer: bool = True
-    ) -> "SingleModuleStepper":
+    def from_state(cls, state, area: torch.Tensor) -> "SingleModuleStepper":
         """
         Load the state of the stepper.
 
         Args:
             state: The state to load.
-            load_optimizer: Whether to load the optimizer state.
+            area: (n_lat, n_lon) array containing relative gridcell area, in any
+                units including unitless.
 
         Returns:
             The stepper.
@@ -317,10 +277,9 @@ class SingleModuleStepper:
         stepper = cls(
             config=SingleModuleStepperConfig.from_state(config),
             data_shapes=state["data_shapes"],
-            max_epochs=state["max_epochs"],
             area=area,
         )
-        stepper.load_state(state, load_optimizer=load_optimizer)
+        stepper.load_state(state)
         return stepper
 
 

fme/fme/core/scheduler.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Any, Mapping, Optional
+from typing import Any, MutableMapping, Optional
 
 import torch.optim.lr_scheduler
 
@@ -16,14 +16,21 @@ class SchedulerConfig:
     """
 
     type: Optional[str] = None
-    kwargs: Mapping[str, Any] = dataclasses.field(default_factory=dict)
+    kwargs: MutableMapping[str, Any] = dataclasses.field(default_factory=dict)
 
-    def build(self, optimizer) -> Optional[torch.optim.lr_scheduler._LRScheduler]:
+    def build(
+        self, optimizer, max_epochs
+    ) -> Optional[torch.optim.lr_scheduler._LRScheduler]:
         """
         Build the scheduler.
         """
         if self.type is None:
             return None
-        else:
-            scheduler_class = getattr(torch.optim.lr_scheduler, self.type)
-            return scheduler_class(optimizer=optimizer, **self.kwargs)
+
+        # work-around so we don't need to specify T_max
+        # in the yaml file for this scheduler
+        if self.type == "CosineAnnealingLR" and "T_max" not in self.kwargs:
+            self.kwargs["T_max"] = max_epochs
+
+        scheduler_class = getattr(torch.optim.lr_scheduler, self.type)
+        return scheduler_class(optimizer=optimizer, **self.kwargs)

fme/fme/core/test_optimization.py
@@ -43,6 +43,7 @@ def test_optimization_reload(
         parameters=model.parameters(),
         optimizer_type=optimizer_type,
         lr=lr,
+        max_epochs=max_epochs,
         scheduler=scheduler_config,
         enable_automatic_mixed_precision=enable_amp,
         kwargs={},
@@ -70,6 +71,7 @@ def test_optimization_reload(
         parameters=model.parameters(),
         optimizer_type=optimizer_type,
         lr=lr,
+        max_epochs=max_epochs,
         scheduler=scheduler_config,
         enable_automatic_mixed_precision=enable_amp,
         kwargs={},

fme/fme/core/test_scheduler.py
@@ -5,14 +5,16 @@ from fme.core.scheduler import SchedulerConfig
 
 def test_default_gives_none():
     optimizer = torch.optim.Adam(params=[torch.nn.Parameter()])
-    assert SchedulerConfig().build(optimizer) is None
+    max_epochs = 42
+    assert SchedulerConfig().build(optimizer, max_epochs) is None
 
 
 def test_build():
     config = SchedulerConfig(type="StepLR", kwargs={"step_size": 1})
     # define dummy parameters for optimizer
     optimizer = torch.optim.Adam(params=[torch.nn.Parameter()])
-    scheduler = config.build(optimizer)
+    max_epochs = 42
+    scheduler = config.build(optimizer, max_epochs)
     assert isinstance(scheduler, torch.optim.lr_scheduler.StepLR)
     assert scheduler.step_size == 1
     assert scheduler.optimizer is optimizer

fme/fme/core/test_stepper.py
@@ -5,7 +5,7 @@ import torch
 
 import fme
 from fme.core.normalizer import NormalizationConfig, StandardNormalizer
-from fme.core.optimization import OptimizationConfig
+from fme.core.optimization import NullOptimization
 from fme.core.packer import Packer
 from fme.core.prescriber import NullPrescriber, Prescriber, PrescriberConfig
 from fme.core.stepper import (
@@ -51,7 +51,6 @@ def test_stepper_config_all_names_property(
         in_names=in_names,
         out_names=out_names,
         normalization=MagicMock(),
-        optimization=MagicMock(),
         prescriber=prescriber_config,
     )
     # check there are no duplications
@@ -219,11 +218,6 @@ def test_reloaded_stepper_gives_same_prediction():
         ),
         in_names=["a", "b"],
         out_names=["a", "b"],
-        optimization=OptimizationConfig(
-            optimizer_type="Adam",
-            lr=0.001,
-            enable_automatic_mixed_precision=False,
-        ),
         normalization=NormalizationConfig(
             means={"a": 0.0, "b": 0.0},
             stds={"a": 1.0, "b": 1.0},
@@ -235,7 +229,6 @@ def test_reloaded_stepper_gives_same_prediction():
     }
     stepper = config.get_stepper(
         shapes=shapes,
-        max_epochs=1,
         area=None,  # not actually used in this test
     )
     area = torch.ones((5, 5), device=fme.get_device())
@@ -243,12 +236,12 @@ def test_reloaded_stepper_gives_same_prediction():
     data, _ = get_data(["a", "b"], n_samples=5, n_time=2)
     first_result = stepper.run_on_batch(
         data=data,
-        train=False,
+        optimization=NullOptimization(),
         n_forward_steps=1,
     )
     second_result = new_stepper.run_on_batch(
         data=data,
-        train=False,
+        optimization=NullOptimization(),
         n_forward_steps=1,
     )
     assert torch.allclose(first_result.loss, second_result.loss)

fme/fme/fcn_training/inference/inference.py
@@ -31,9 +31,7 @@ def _load_stepper_config(checkpoint_file: str) -> SingleModuleStepperConfig:
 
 def _load_stepper(checkpoint_file: str, area: torch.Tensor) -> SingleModuleStepper:
     checkpoint = torch.load(checkpoint_file, map_location=fme.get_device())
-    stepper = SingleModuleStepper.from_state(
-        checkpoint["stepper"], area=area, load_optimizer=False
-    )
+    stepper = SingleModuleStepper.from_state(checkpoint["stepper"], area=area)
     return stepper
 
 

fme/fme/fcn_training/inference/loop.py
@@ -10,6 +10,7 @@ from fme.core.aggregator.inference.main import InferenceAggregator
 from fme.core.data_loading.typing import GriddedData, SigmaCoordinates
 from fme.core.device import get_device
 from fme.core.normalizer import StandardNormalizer
+from fme.core.optimization import NullOptimization
 from fme.core.stepper import SteppedData
 
 from .data_writer import DataWriter, NullDataWriter
@@ -217,7 +218,7 @@ def run_inference(
                 batch_manager.apply_initial_condition(data)
                 stepped = stepper.run_on_batch(
                     data,
-                    train=False,
+                    NullOptimization(),
                     n_forward_steps=forward_steps_in_memory,
                 )
                 _inference_internal_loop(

commit 0800931c44523616bfb6a2d6ed8bac5d34297683

Restore inference backwards compatibility (#326)
We did not realize it at the time, but backwards compatibility for
inference was broken by #318 because the SingleModuleStepperConfig is
loaded by inference, and was changed in a backwards-incompatible way.
We've created issue #325 to address this. This PR restores backwards
compatibility for this specific case.

Changes:
- Added DisabledOptimizationConfig to SingleModuleStepperConfig which
loads configuration from older models that include an
OptimizationConfig, but does not allow that configuration to be used to
build an optimizer.

- [ ] Tests added (none needed)

fme/fme/core/optimization.py
@@ -102,6 +102,33 @@ class Optimization:
 
 
 @dataclasses.dataclass
+class DisabledOptimizationConfig:
+    """
+    Configuration for optimization, kept only for backwards compatibility when
+    loading configuration. Cannot be used to build, will raise an exception.
+
+    Attributes:
+        optimizer_type: The type of optimizer to use.
+        lr: The learning rate.
+        kwargs: Additional keyword arguments to pass to the optimizer.
+        enable_automatic_mixed_precision: Whether to use automatic mixed
+            precision.
+        scheduler: The type of scheduler to use. If none is given, no scheduler
+            will be used.
+    """
+
+    def build(self, parameters, max_epochs: int) -> Optimization:
+        raise RuntimeError("Cannot build DisabledOptimizationConfig")
+
+    def get_state(self) -> Mapping[str, Any]:
+        return dataclasses.asdict(self)
+
+    @classmethod
+    def from_state(cls, state: Mapping[str, Any]) -> "DisabledOptimizationConfig":
+        return cls(**state)
+
+
+@dataclasses.dataclass
 class OptimizationConfig:
     """
     Configuration for optimization.

fme/fme/core/stepper.py
@@ -20,7 +20,7 @@ from fme.core.packer import Packer
 from fme.core.prescriber import NullPrescriber, Prescriber, PrescriberConfig
 from fme.fcn_training.registry import ModuleSelector
 
-from .optimization import NullOptimization, Optimization
+from .optimization import DisabledOptimizationConfig, NullOptimization, Optimization
 
 
 @dataclasses.dataclass
@@ -29,6 +29,7 @@ class SingleModuleStepperConfig:
     in_names: List[str]
     out_names: List[str]
     normalization: Union[NormalizationConfig, FromStateNormalizer]
+    optimization: Optional[DisabledOptimizationConfig] = None
     prescriber: Optional[PrescriberConfig] = None
     loss: LossConfig = dataclasses.field(default_factory=lambda: LossConfig())
 