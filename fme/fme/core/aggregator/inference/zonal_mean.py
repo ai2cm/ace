ai2cm/ace

commit 25937ed87d851410f18ea9ea74acb4c8024f9344

Output inference diagnostics as netcdf for paper-ready figures (#333)
We need to make pretty plots for our paper and wandb isn't intended for
this. This PR outputs much of the raw data (e.g. maps) that are sent to
wandb in netcdf form so that it can be slurped up and piped to custom
plotting scripts.

Changes:
- Refactors `time_mean.py` to facilitate code re-use between
`get_dataset` and `get_logs`.
- Passes coords to `zonal_mean.py` so that outputs have the correct
components.
- Adds `get_datasets` method to `InferenceAggregator` so that all the
xarray Datasets can be passed up to the top-level for saving.
- Writes each aggregators' dataset to disk in netcdf format.

[x] Tests added

Resolves #298

---------

Co-authored-by: Oliver Watt-Meyer <oliverwatt@gmail.com>

fme/fme/core/aggregator/inference/zonal_mean.py
@@ -1,6 +1,9 @@
+import dataclasses
 from typing import Dict, Mapping, Optional
 
+import numpy as np
 import torch
+from wandb import Image
 
 from fme.core.data_loading.typing import VariableMetadata
 from fme.core.device import get_device
@@ -10,6 +13,24 @@ from fme.core.wandb import WandB
 from .image_scaling import scale_image
 
 wandb = WandB.get_instance()
+
+import xarray as xr
+
+
+@dataclasses.dataclass
+class _RawData:
+    datum: np.ndarray
+    caption: str
+    metadata: VariableMetadata
+
+    def get_image(self):
+        # images are y, x from upper left corner
+        # data is time, lat
+        # we want lat on y-axis (increasing upward) and time on x-axis
+        # so transpose and flip along lat axis
+        datum = np.flip(self.datum.transpose(), axis=0)
+        datum = scale_image(datum)
+        return wandb.Image(self.datum, caption=self.caption)
 
 
 class ZonalMeanAggregator:
@@ -90,30 +111,59 @@ class ZonalMeanAggregator:
             self._gen_data[name][:, time_slice, :] += tensor.mean(dim=lon_dim)
         self._n_batches[:, time_slice, :] += 1
 
-    def get_logs(self, label: str) -> Dict[str, torch.Tensor]:
+    def _get_data(self) -> Dict[str, _RawData]:
         if self._gen_data is None or self._target_data is None:
             raise RuntimeError("No data recorded")
         sample_dim = 0
-        logs = {}
+        data: Dict[str, _RawData] = {}
         for name in self._gen_data.keys():
-            zonal_means = {}
-            gen = self._dist.reduce_mean(self._gen_data[name] / self._n_batches)
-            zonal_means["gen"] = gen.mean(dim=sample_dim).cpu()
-            error = self._dist.reduce_mean(
-                (self._gen_data[name] - self._target_data[name]) / self._n_batches
+            gen = (
+                self._dist.reduce_mean(self._gen_data[name] / self._n_batches)
+                .mean(sample_dim)
+                .cpu()
+                .numpy()
             )
-            zonal_means["error"] = error.mean(dim=sample_dim).cpu()
-            for key, data in zonal_means.items():
-                caption = self._get_caption(key, name, data)
-                # images are y, x from upper left corner
-                # data is time, lat
-                # we want lat on y-axis (increasing upward) and time on x-axis
-                # so transpose and flip along lat axis
-                data = data.t().flip(dims=[0])
-                data = scale_image(data.cpu().numpy())
-                wandb_image = wandb.Image(data, caption=caption)
-                logs[f"{label}/{key}/{name}"] = wandb_image
+            error = (
+                self._dist.reduce_mean(
+                    (self._gen_data[name] - self._target_data[name]) / self._n_batches
+                )
+                .mean(sample_dim)
+                .cpu()
+                .numpy()
+            )
+
+            metadata = self._metadata.get(name, VariableMetadata("unknown_units", name))
+            data[f"gen/{name}"] = _RawData(
+                datum=gen,
+                caption=self._get_caption("gen", name, gen),
+                # generated data is not considered to have units
+                metadata=VariableMetadata(units="", long_name=metadata.long_name),
+            )
+            data[f"error/{name}"] = _RawData(
+                datum=error,
+                caption=self._get_caption("error", name, error),
+                metadata=metadata,
+            )
+
+        return data
+
+    def get_logs(self, label: str) -> Dict[str, Image]:
+        logs = {}
+        data = self._get_data()
+        for key, datum in data.items():
+            logs[f"{label}/{key}"] = datum.get_image()
         return logs
+
+    def get_dataset(self) -> xr.Dataset:
+        data = {
+            k.replace("/", "-"): xr.DataArray(
+                v.datum, dims=("forecast_step", "lat"), attrs=v.metadata._asdict()
+            )
+            for k, v in self._get_data().items()
+        }
+
+        ret = xr.Dataset(data)
+        return ret
 
     def _get_caption(self, caption_key: str, varname: str, data: torch.Tensor) -> str:
         if varname in self._metadata:

fme/fme/core/aggregator/inference/main.py
@@ -1,6 +1,7 @@
-from typing import Dict, List, Mapping, Optional, Protocol, Union
+from typing import Dict, Iterable, List, Mapping, Optional, Protocol, Union
 
 import torch
+import xarray as xr
 from wandb import Table
 
 from fme.core.data_loading.typing import SigmaCoordinates, VariableMetadata
@@ -30,6 +31,10 @@ class _Aggregator(Protocol):
 
     @torch.no_grad()
     def get_logs(self, label: str):
+        ...
+
+    @torch.no_grad()
+    def get_dataset(self) -> xr.Dataset:
         ...
 
 
@@ -74,14 +79,20 @@ class InferenceAggregator:
                 target="denorm",
                 n_timesteps=n_timesteps,
                 dist=dist,
+                metadata=metadata,
             ),
             "mean_norm": MeanAggregator(
                 area_weights,
                 target="norm",
                 n_timesteps=n_timesteps,
                 dist=dist,
+                metadata=metadata,
             ),
-            "time_mean": TimeMeanAggregator(area_weights, dist=dist, metadata=metadata),
+            "time_mean": TimeMeanAggregator(
+                area_weights,
+                dist=dist,
+                metadata=metadata,
+            ),
         }
         if record_step_20:
             self._aggregators["mean_step_20"] = OneStepMeanAggregator(
@@ -96,7 +107,9 @@ class InferenceAggregator:
             )
         if log_zonal_mean_images:
             self._aggregators["zonal_mean"] = ZonalMeanAggregator(
-                n_timesteps=n_timesteps, dist=dist, metadata=metadata
+                n_timesteps=n_timesteps,
+                dist=dist,
+                metadata=metadata,
             )
 
     @torch.no_grad()
@@ -147,6 +160,24 @@ class InferenceAggregator:
         from tables into a list of dictionaries.
         """
         return to_inference_logs(self.get_logs(label=label))
+
+    @torch.no_grad()
+    def get_datasets(
+        self, aggregator_whitelist: Optional[Iterable[str]] = None
+    ) -> Dict[str, xr.Dataset]:
+        """
+        Args:
+            aggregator_whitelist: aggregator names to include in the output. If
+                None, return all the datasets associated with all aggregators.
+        """
+        datasets = (
+            (name, agg.get_dataset()) for name, agg in self._aggregators.items()
+        )
+        if aggregator_whitelist is not None:
+            filter_ = set(aggregator_whitelist)
+            return {name: ds for name, ds in datasets if name in filter_}
+
+        return {name: ds for name, ds in datasets}
 
 
 def to_inference_logs(

fme/fme/core/aggregator/inference/reduced.py
@@ -1,16 +1,31 @@
-from typing import Dict, Literal, Mapping, Optional, Protocol
+import dataclasses
+from typing import Dict, List, Literal, Mapping, Optional, Protocol
 
 import numpy as np
 import torch
 import xarray as xr
 
 from fme.core import metrics
+from fme.core.data_loading.typing import VariableMetadata
 from fme.core.device import get_device
 from fme.core.distributed import Distributed
 from fme.core.metrics import Dimension
 from fme.core.wandb import WandB
 
 wandb = WandB.get_instance()
+
+
+@dataclasses.dataclass
+class _SeriesData:
+    metric_name: str
+    var_name: str
+    data: np.ndarray
+
+    def get_wandb_key(self) -> str:
+        return f"{self.metric_name}/{self.var_name}"
+
+    def get_xarray_key(self) -> str:
+        return f"{self.metric_name}-{self.var_name}"
 
 
 def get_gen_shape(gen_data: Mapping[str, torch.Tensor]):
@@ -137,6 +152,7 @@ class MeanAggregator:
         target: Literal["norm", "denorm"],
         n_timesteps: int,
         dist: Optional[Distributed] = None,
+        metadata: Optional[Mapping[str, VariableMetadata]] = None,
     ):
         self._area_weights = area_weights
         self._variable_metrics: Optional[Dict[str, Dict[str, MeanMetric]]] = None
@@ -144,10 +160,15 @@ class MeanAggregator:
         self._shape_y = None
         self._target = target
         self._n_timesteps = n_timesteps
+
         if dist is None:
             self._dist = Distributed.get_instance()
         else:
             self._dist = dist
+        if metadata is None:
+            self._metadata: Mapping[str, VariableMetadata] = {}
+        else:
+            self._metadata = metadata
 
     def _get_variable_metrics(self, gen_data: Mapping[str, torch.Tensor]):
         if self._variable_metrics is None:
@@ -219,26 +240,21 @@ class MeanAggregator:
                     i_time_start=i_time_start,
                 )
 
-    def _get_series_data(self) -> Dict[str, np.ndarray]:
-        """
-        Returns a dictionary of series data, where each key is a metric name.
-
-        Used as a helper, as we present this series data in different ways.
-        """
+    def _get_series_data(self) -> List[_SeriesData]:
+        """Converts internally stored variable_metrics to a list."""
         if self._variable_metrics is None:
             raise ValueError("No batches have been recorded.")
-        series_tensors: Dict[str, torch.Tensor] = {}
+        data: List[_SeriesData] = []
         for metric in self._variable_metrics:
             for key in self._variable_metrics[metric]:
-                series_tensors[f"{metric}/{key}"] = self._variable_metrics[metric][
-                    key
-                ].get()
-        series_arrays: Dict[str, np.ndarray] = {}
-        for key in sorted(series_tensors.keys()):
-            series_arrays[key] = (
-                self._dist.reduce_mean(series_tensors[key].detach()).cpu().numpy()
-            )
-        return series_arrays
+                arr = self._variable_metrics[metric][key].get().detach()
+                datum = _SeriesData(
+                    metric_name=metric,
+                    var_name=key,
+                    data=self._dist.reduce_mean(arr).cpu().numpy(),
+                )
+                data.append(datum)
+        return data
 
     @torch.no_grad()
     def get_logs(self, label: str):
@@ -249,26 +265,29 @@ class MeanAggregator:
             label: Label to prepend to all log keys.
         """
         logs = {}
-        series_data = self._get_series_data()
+        series_data: Dict[str, np.ndarray] = {
+            datum.get_wandb_key(): datum.data for datum in self._get_series_data()
+        }
         table = data_to_table(series_data)
         logs[f"{label}/series"] = table
         return logs
 
     @torch.no_grad()
-    def get_dataset(self, label: str) -> xr.Dataset:
+    def get_dataset(self) -> xr.Dataset:
         """
         Returns a dataset representation of the logs.
         """
-        if len(label) > 0:
-            label = f"{label}_"
-        series_data = self._get_series_data()
-        series_data = {
-            key.replace("/", "-"): value for key, value in series_data.items()
-        }
         data_vars = {}
-        for key in series_data:
-            data_vars[f"{label}{key}"] = (["forecast_step"], series_data[key])
-        coords = {"forecast_step": np.arange(len(series_data[key]))}
+        for datum in self._get_series_data():
+            metadata = self._metadata.get(
+                datum.var_name, VariableMetadata("unknown_units", datum.var_name)
+            )
+            data_vars[datum.get_xarray_key()] = xr.DataArray(
+                datum.data, dims=["forecast_step"], attrs=metadata._asdict()
+            )
+
+        n_forecast_steps = len(next(iter(data_vars.values())))
+        coords = {"forecast_step": np.arange(n_forecast_steps)}
         return xr.Dataset(data_vars=data_vars, coords=coords)
 
 

fme/fme/core/aggregator/inference/test_video.py
@@ -83,23 +83,23 @@ def test_video_data(offsets: np.ndarray):
         aggregator.record_batch(
             loss=0, target_data=target_window, gen_data=gen_window, i_time_start=i_start
         )
-    data = aggregator._get_data("video")
-    assert data["video/bias/a"].target is None
-    assert data["video/min_err/a"].target is None
-    assert data["video/max_err/a"].target is None
-    assert data["video/a"].gen.shape[0] == len(offsets)
-    assert data["video/a"].target.shape[0] == len(offsets)
-    assert data["video/bias/a"].gen.shape[0] == len(offsets)
-    assert data["video/min_err/a"].gen.shape[0] == len(offsets)
-    assert data["video/max_err/a"].gen.shape[0] == len(offsets)
+    data = aggregator._get_data()
+    assert data["bias/a"].target is None
+    assert data["min_err/a"].target is None
+    assert data["max_err/a"].target is None
+    assert data["a"].gen.shape[0] == len(offsets)
+    assert data["a"].target.shape[0] == len(offsets)
+    assert data["bias/a"].gen.shape[0] == len(offsets)
+    assert data["min_err/a"].gen.shape[0] == len(offsets)
+    assert data["max_err/a"].gen.shape[0] == len(offsets)
     for i, offset_i in enumerate(offsets):
         np.testing.assert_allclose(
-            data["video/a"].gen[i, :], data["video/a"].target[i, :] + offset_i
+            data["a"].gen[i, :], data["a"].target[i, :] + offset_i
         )
-        np.testing.assert_allclose(data["video/bias/a"].gen[i, :], offset_i)
-        np.testing.assert_allclose(data["video/min_err/a"].gen[i, :], offset_i)
-        np.testing.assert_allclose(data["video/max_err/a"].gen[i, :], offset_i)
-        np.testing.assert_allclose(data["video/gen_var/a"].gen[i, :], 1.0)
+        np.testing.assert_allclose(data["bias/a"].gen[i, :], offset_i)
+        np.testing.assert_allclose(data["min_err/a"].gen[i, :], offset_i)
+        np.testing.assert_allclose(data["max_err/a"].gen[i, :], offset_i)
+        np.testing.assert_allclose(data["gen_var/a"].gen[i, :], 1.0)
 
 
 @pytest.mark.parametrize(
@@ -123,13 +123,13 @@ def test_video_data_without_extended_videos(offsets: np.ndarray):
         aggregator.record_batch(
             loss=0, target_data=target_window, gen_data=gen_window, i_time_start=i_start
         )
-    data = aggregator._get_data("video")
+    data = aggregator._get_data()
     assert len(data) == 1
-    assert data["video/a"].gen.shape[0] == len(offsets)
-    assert data["video/a"].target.shape[0] == len(offsets)
+    assert data["a"].gen.shape[0] == len(offsets)
+    assert data["a"].target.shape[0] == len(offsets)
     for i, offset_i in enumerate(offsets):
         np.testing.assert_allclose(
-            data["video/a"].gen[i, :], data["video/a"].target[i, :] + offset_i
+            data["a"].gen[i, :], data["a"].target[i, :] + offset_i
         )
 
 
@@ -172,40 +172,40 @@ def test_video_data_values_on_random_inputs(n_batches: int):
                 ),
                 i_time_start=i_start,
             )
-    data = aggregator._get_data("video")
-    assert data["video/bias/a"].target is None
-    assert data["video/rmse/a"].target is None
-    assert data["video/min_err/a"].target is None
-    assert data["video/max_err/a"].target is None
-    assert data["video/a"].gen.shape[0] == len(offsets)
-    assert data["video/a"].target.shape[0] == len(offsets)
-    assert data["video/bias/a"].gen.shape[0] == len(offsets)
-    assert data["video/rmse/a"].gen.shape[0] == len(offsets)
-    assert data["video/min_err/a"].gen.shape[0] == len(offsets)
-    assert data["video/max_err/a"].gen.shape[0] == len(offsets)
+    data = aggregator._get_data()
+    assert data["bias/a"].target is None
+    assert data["rmse/a"].target is None
+    assert data["min_err/a"].target is None
+    assert data["max_err/a"].target is None
+    assert data["a"].gen.shape[0] == len(offsets)
+    assert data["a"].target.shape[0] == len(offsets)
+    assert data["bias/a"].gen.shape[0] == len(offsets)
+    assert data["rmse/a"].gen.shape[0] == len(offsets)
+    assert data["min_err/a"].gen.shape[0] == len(offsets)
+    assert data["max_err/a"].gen.shape[0] == len(offsets)
     np.testing.assert_allclose(
-        data["video/a"].gen.cpu().numpy(), gen["a"].mean(dim=0).cpu().numpy()
+        data["a"].gen.cpu().numpy(), gen["a"].mean(dim=0).cpu().numpy()
     )
     np.testing.assert_allclose(
-        data["video/a"].target.cpu().numpy(), target["a"].mean(dim=0).cpu().numpy()
+        data["a"].target.cpu().numpy(), target["a"].mean(dim=0).cpu().numpy()
     )
     np.testing.assert_allclose(
-        data["video/bias/a"].gen.cpu().numpy(),
+        data["bias/a"].gen.cpu().numpy(),
         (gen["a"] - target["a"]).mean(dim=0).cpu().numpy(),
     )
     np.testing.assert_allclose(
-        data["video/rmse/a"].gen.cpu().numpy(),
+        data["rmse/a"].gen.cpu().numpy(),
         ((gen["a"] - target["a"]) ** 2).mean(dim=0).sqrt().cpu().numpy(),
     )
     np.testing.assert_allclose(
-        data["video/min_err/a"].gen.cpu().numpy(),
+        data["min_err/a"].gen.cpu().numpy(),
         (gen["a"] - target["a"]).min(dim=0)[0].cpu().numpy(),
     )
     np.testing.assert_allclose(
-        data["video/max_err/a"].gen.cpu().numpy(),
+        data["max_err/a"].gen.cpu().numpy(),
         (gen["a"] - target["a"]).max(dim=0)[0].cpu().numpy(),
     )
     np.testing.assert_allclose(
-        data["video/gen_var/a"].gen.cpu().numpy(),
+        data["gen_var/a"].gen.cpu().numpy(),
         (gen["a"].var(dim=0) / target["a"].var(dim=0)).cpu().numpy(),
     )

fme/fme/core/aggregator/inference/time_mean.py
@@ -1,4 +1,5 @@
-from typing import Dict, Mapping, MutableMapping, Optional
+import dataclasses
+from typing import Dict, List, Mapping, MutableMapping, Optional, Union
 
 import numpy as np
 import torch
@@ -12,6 +13,37 @@ from fme.core.wandb import WandB
 from .image_scaling import scale_image
 
 wandb = WandB.get_instance()
+
+
+@dataclasses.dataclass
+class _TargetGenPair:
+    name: str
+    target: torch.Tensor
+    gen: torch.Tensor
+
+    def bias(self):
+        return self.gen - self.target
+
+    def rmse(self, weights: torch.Tensor) -> float:
+        ret = float(
+            metrics.root_mean_squared_error(
+                predicted=self.gen,
+                truth=self.target,
+                weights=weights,
+            )
+            .cpu()
+            .numpy()
+        )
+        return ret
+
+    def weighted_mean_bias(self, weights: torch.Tensor) -> float:
+        return float(
+            metrics.weighted_mean_bias(
+                predicted=self.gen, truth=self.target, weights=weights
+            )
+            .cpu()
+            .numpy()
+        )
 
 
 def get_gen_shape(gen_data: Mapping[str, torch.Tensor]):
@@ -107,41 +139,42 @@ class TimeMeanAggregator:
         # same length
         self._n_batches += 1
 
-    @torch.no_grad()
-    def get_logs(self, label: str):
-        """
-        Returns logs as can be reported to WandB.
-
-        Args:
-            label: Label to prepend to all log keys.
-        """
+    def _get_target_gen_pairs(self) -> List[_TargetGenPair]:
         if self._n_batches == 0 or self._gen_data is None or self._target_data is None:
             raise ValueError("No data recorded.")
-        logs = {}
+
+        ret = []
         for name in self._gen_data.keys():
             gen = self._dist.reduce_mean(self._gen_data[name] / self._n_batches)
             target = self._dist.reduce_mean(self._target_data[name] / self._n_batches)
-            images = {"bias_map": gen - target, "gen_map": gen}
-            for key, data in images.items():
-                logs[f"{key}/{name}"] = _make_image(
-                    caption=self._get_caption(key, name, data),
-                    data=data,
-                )
-            logs[f"rmse/{name}"] = float(
-                metrics.root_mean_squared_error(
-                    predicted=gen, truth=target, weights=self._area_weights
-                )
-                .cpu()
-                .numpy()
+            ret.append(_TargetGenPair(gen=gen, target=target, name=name))
+        return ret
+
+    @torch.no_grad()
+    def get_logs(self, label: str) -> Dict[str, Union[float, torch.Tensor]]:
+        logs = {}
+        preds = self._get_target_gen_pairs()
+        bias_map_key, gen_map_key = "bias_map", "gen_map"
+        for pred in preds:
+            logs.update(
+                {
+                    f"{bias_map_key}/{pred.name}": _make_image(
+                        self._get_caption(bias_map_key, pred.name, pred.bias()),
+                        pred.bias(),
+                    ),
+                    f"{gen_map_key}/{pred.name}": _make_image(
+                        self._get_caption(gen_map_key, pred.name, pred.gen), pred.gen
+                    ),
+                    f"rmse/{pred.name}": pred.rmse(weights=self._area_weights),
+                    f"bias/{pred.name}": pred.weighted_mean_bias(
+                        weights=self._area_weights
+                    ),
+                }
             )
-            logs[f"bias/{name}"] = float(
-                metrics.weighted_mean_bias(
-                    predicted=gen, truth=target, weights=self._area_weights
-                )
-                .cpu()
-                .numpy()
-            )
-        return {f"{label}/{key}": logs[key] for key in logs}
+
+        if len(label) != 0:
+            return {f"{label}/{key}": logs[key] for key in logs}
+        return logs
 
     def _get_caption(self, key: str, name: str, data: torch.Tensor) -> str:
         if name in self._metadata:
@@ -153,14 +186,28 @@ class TimeMeanAggregator:
         caption += f" vmin={data.min():.4g}, vmax={data.max():.4g}."
         return caption
 
-    @torch.no_grad()
-    def get_dataset(self, label: str) -> xr.Dataset:
-        logs = self.get_logs(label=label)
-        logs = {key.replace("/", "-"): logs[key] for key in logs}
-        data_vars = {}
-        for key, value in logs.items():
-            data_vars[key] = xr.DataArray(value)
-        return xr.Dataset(data_vars=data_vars)
+    def get_dataset(self) -> xr.Dataset:
+        data = {}
+        preds = self._get_target_gen_pairs()
+        dims = ("lat", "lon")
+        for pred in preds:
+            bias_metadata = self._metadata.get(
+                pred.name, VariableMetadata(units="unknown_units", long_name=pred.name)
+            )._asdict()
+            gen_metadata = VariableMetadata(units="", long_name=pred.name)._asdict()
+            data.update(
+                {
+                    f"bias_map-{pred.name}": xr.DataArray(
+                        pred.bias().cpu(), dims=dims, attrs=bias_metadata
+                    ),
+                    f"gen_map-{pred.name}": xr.DataArray(
+                        pred.gen.cpu(),
+                        dims=dims,
+                        attrs=gen_metadata,
+                    ),
+                }
+            )
+        return xr.Dataset(data)
 
 
 def _make_image(

fme/fme/core/aggregator/inference/video.py
@@ -359,14 +359,14 @@ class VideoAggregator:
         Args:
             label: Label to prepend to all log keys.
         """
-        data = self._get_data(label=label)
+        data = self._get_data()
         videos = {}
-        for label, d in data.items():
-            videos[label] = d.make_video()
+        for sub_label, d in data.items():
+            videos[f"{label}/{sub_label}"] = d.make_video()
         return videos
 
     @torch.no_grad()
-    def _get_data(self, label: str) -> Mapping[str, _MaybePairedVideoData]:
+    def _get_data(self) -> Mapping[str, _MaybePairedVideoData]:
         """
         Returns video data as can be reported to WandB.
 
@@ -389,7 +389,7 @@ class VideoAggregator:
                 return None
 
         for name in gen_data:
-            video_data[f"{label}/{name}"] = _MaybePairedVideoData(
+            video_data[name] = _MaybePairedVideoData(
                 caption=self._get_caption(name),
                 gen=gen_data[name],
                 target=target_data[name],
@@ -397,7 +397,7 @@ class VideoAggregator:
                 long_name=f"ensemble mean of {get_long_name(name)}",
             )
             if self._enable_extended_videos:
-                video_data[f"{label}/bias/{name}"] = _MaybePairedVideoData(
+                video_data[f"bias/{name}"] = _MaybePairedVideoData(
                     caption=(f"prediction - target for {name}"),
                     gen=gen_data[name] - target_data[name],
                     units=get_units(name),
@@ -406,14 +406,14 @@ class VideoAggregator:
         if self._error_data is not None:
             data = self._error_data.get()
             for name in data.rmse:
-                video_data[f"{label}/rmse/{name}"] = _MaybePairedVideoData(
+                video_data[f"rmse/{name}"] = _MaybePairedVideoData(
                     caption=f"RMSE over ensemble for {name}",
                     gen=data.rmse[name],
                     units=get_units(name),
                     long_name=f"root mean squared error of {get_long_name(name)}",
                 )
             for name in data.min_err:
-                video_data[f"{label}/min_err/{name}"] = _MaybePairedVideoData(
+                video_data[f"min_err/{name}"] = _MaybePairedVideoData(
                     caption=f"Min across ensemble members of min error for {name}",
                     gen=data.min_err[name],
                     units=get_units(name),
@@ -422,7 +422,7 @@ class VideoAggregator:
                     ),
                 )
             for name in data.max_err:
-                video_data[f"{label}/max_err/{name}"] = _MaybePairedVideoData(
+                video_data[f"max_err/{name}"] = _MaybePairedVideoData(
                     caption=f"Max across ensemble members of max error for {name}",
                     gen=data.max_err[name],
                     units=get_units(name),
@@ -433,7 +433,7 @@ class VideoAggregator:
         if self._variance_data is not None:
             gen_data, target_data = self._variance_data.get()
             for name in gen_data:
-                video_data[f"{label}/gen_var/{name}"] = _MaybePairedVideoData(
+                video_data[f"gen_var/{name}"] = _MaybePairedVideoData(
                     caption=(
                         f"Variance of gen data for {name} "
                         "as fraction of target variance"
@@ -452,7 +452,7 @@ class VideoAggregator:
         """
         Return video data as an xarray Dataset.
         """
-        data = self._get_data(label="")
+        data = self._get_data()
         video_data = {}
         for label, d in data.items():
             label = label.strip("/").replace("/", "_")  # remove leading slash

commit 1b9d15982cf9de2468557703d92c1f3ccb5ea826

overwrote flipped and transposed with original (#367)
This PR fixes a bug introduced in #333 where the zonal mean wandb image
was no longer being aligned properly with time on the x-axis.

Resolves #366

fme/fme/core/aggregator/inference/zonal_mean.py
@@ -30,7 +30,7 @@ class _RawData:
         # so transpose and flip along lat axis
         datum = np.flip(self.datum.transpose(), axis=0)
         datum = scale_image(datum)
-        return wandb.Image(self.datum, caption=self.caption)
+        return wandb.Image(datum, caption=self.caption)
 
 
 class ZonalMeanAggregator: