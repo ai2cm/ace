6683a005ef15873347cbccc66fcf6f83ca3e9302
Jeremy McGibbon <mcgibbon@uw.edu>
2023-11-07 10:50:59 -0500

    Simplify typing on ClimateData (#400)
    Includes some simplifications to the typing of ClimateData which made it
    easier to implement #339.
    
    Instead of returning None types to handle missing variables, exceptions
    are raised. This makes it much easier to statically type equations using
    those variables, instead of checking for None everywhere.
    
    A special `__getattr__` method to get arbitrary data from the underlying
    values is only used in tests of that method and never in the training
    code, so it was removed. Using it would have required dynamic typing,
    which we don't allow in our code.
    
    Changes:
    - ClimateData tensor attributes are no longer Optional, instead a
    KeyError is raised if they are missing
    - `ClimateData.__getattr__` override is removed
    - removed `required_names` argument from `derived_variables.register`,
    as its purpose now handled automatically by raised KeyErrors if
    variables are missing.
    - Added a test of `compute_derived_quantities`
    
    - [x] Tests added

--- fme/fme/fcn_training/inference/test_derived_variables.py
+++ fme/fme/fcn_training/inference/test_derived_variables.py
@@ -1,13 +1,18 @@
 import pytest
 import torch
 
+from fme.core.data_loading.typing import SigmaCoordinates
 from fme.core.stepper import SteppedData
 
-from .derived_variables import DerivedVariableRegistryEntry, _compute_derived_variable
+from .derived_variables import (
+    DerivedVariableRegistryEntry,
+    _compute_derived_variable,
+    compute_derived_quantities,
+)
 
 
 def test_compute_derived_variable():
-    fake_data = {"a": torch.tensor([1.0]), "b": torch.tensor([2.0])}
+    fake_data = {"PRESsfc": torch.tensor([1.0])}
     data = SteppedData(
         metrics={"loss": torch.tensor(0.0)},
         gen_data=fake_data,
@@ -17,7 +22,7 @@ def test_compute_derived_variable():
     )
     sigma_coordinates = None
     derived_variable = DerivedVariableRegistryEntry(
-        func=lambda data, _: data.a + data.b, required_names=["a", "b"]
+        func=lambda data, _: 3.0 * data.surface_pressure
     )
     output_data = _compute_derived_variable(
         data, sigma_coordinates, "c", derived_variable
@@ -27,7 +32,7 @@ def test_compute_derived_variable():
 
 
 def test_compute_derived_variable_raises_value_error_when_overwriting():
-    fake_data = {"a": torch.tensor([1.0]), "b": torch.tensor([2.0])}
+    fake_data = {"PRESsfc": torch.tensor([1.0])}
     data = SteppedData(
         metrics={"loss": torch.tensor(0.0)},
         gen_data=fake_data,
@@ -37,7 +42,36 @@ def test_compute_derived_variable_raises_value_error_when_overwriting():
     )
     sigma_coordinates = None
     derived_variable = DerivedVariableRegistryEntry(
-        func=lambda data, _: data.a + data.b, required_names=["a", "b"]
+        func=lambda data, _: 3.0 * data.surface_pressure
     )
     with pytest.raises(ValueError):
-        _compute_derived_variable(data, sigma_coordinates, "b", derived_variable)
+        _compute_derived_variable(data, sigma_coordinates, "PRESsfc", derived_variable)
+
+
+def test_compute_derived_quantities():
+    torch.manual_seed(0)
+    fake_data = {
+        "PRESsfc": 10.0 + torch.rand(2, 3, 4, 8),
+        "specific_total_water_0": torch.rand(2, 3, 4, 8),
+        "specific_total_water_1": torch.rand(2, 3, 4, 8),
+    }
+    data = SteppedData(
+        metrics={"loss": torch.tensor(0.0)},
+        gen_data=fake_data,
+        target_data=fake_data,
+        gen_data_norm=fake_data,
+        target_data_norm=fake_data,
+    )
+    sigma_coordinates = SigmaCoordinates(
+        ak=torch.tensor([0.0, 0.5, 0.0]),
+        bk=torch.tensor([0.0, 0.5, 1.0]),
+    )
+    out_data = compute_derived_quantities(data, sigma_coordinates)
+    for name in (
+        "total_water_path",
+        "surface_pressure_due_to_dry_air",
+    ):
+        assert name in out_data.gen_data
+        assert name in out_data.target_data
+        assert out_data.gen_data[name].shape == (2, 3, 4, 8)
+        assert out_data.target_data[name].shape == (2, 3, 4, 8)
