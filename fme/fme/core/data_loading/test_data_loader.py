f3b29135ad8b1ddf61b356788fae4381cdae7a2a
Gideon Dresdner <gideond@allenai.org>
2023-11-14 08:30:08 -0500

    Repeat dataloader for long inference (#397)
    This PR adds an option to repeat the validation dataset so that we can
    run long inference runs for testing long term stability of trained FME
    models.
    
    Changes:
    - Changes the way that the xarray dataloader indexes into the netcdf
    files on disk. Instead of using time stamps, it uses time indexes
    - Adds an `n_repeats` option to the validation data loader.
    
    - [x] Tests added
    
    ---------
    
    Co-authored-by: Oliver Watt-Meyer <oliverwatt@gmail.com>

--- fme/fme/core/data_loading/test_data_loader.py
+++ fme/fme/core/data_loading/test_data_loader.py
@@ -79,7 +79,7 @@ def test_ensemble_loader(tmp_path, num_ensemble_members=3):
         _create_dataset_on_disk(ic_path)
         netcdfs.append(ic_path / "data")
 
-    params = DataLoaderParams(tmp_path, "ensemble_xarray", 1, 0, None)
+    params = DataLoaderParams(tmp_path, "ensemble_xarray", 1, 0, 1)
     window_timesteps = 2  # 1 initial condition and 1 step forward
     requirements = DataRequirements(["foo"], window_timesteps)
 
@@ -94,7 +94,7 @@ def test_ensemble_loader(tmp_path, num_ensemble_members=3):
 def test_xarray_loader(tmp_path):
     """Checks that sigma coordinates are present."""
     _create_dataset_on_disk(tmp_path)
-    params = DataLoaderParams(tmp_path, "xarray", 1, 0, None)
+    params = DataLoaderParams(tmp_path, "xarray", 1, 0, 1)
     window_timesteps = 2  # 1 initial condition and 1 step forward
     requirements = DataRequirements(["foo"], window_timesteps)
     data = get_data_loader(params, True, requirements)  # type: ignore
