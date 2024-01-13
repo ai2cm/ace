62d2fab9f16889df1b06630fcbbdd6c521020c4e
Elynn Wu <elynn510@gmail.com>
2024-01-12 16:46:43 -0800

    added time-mean rmse across all channels

--- fme/fme/core/aggregator/inference/time_mean.py
+++ fme/fme/core/aggregator/inference/time_mean.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Dict, List, Mapping, MutableMapping, Optional, Union
+from typing import Dict, List, Literal, Mapping, MutableMapping, Optional, Union
 
 import matplotlib.pyplot as plt
 import torch
@@ -220,3 +220,136 @@ class TimeMeanAggregator:
                 }
             )
         return xr.Dataset(data)
+
+
+class TimeMeanScalarMetricAggregator:
+    """Scalar statistics on the time-mean state.
+
+    This aggregator is currently used to keep track of
+    time-mean RMSE across all channels.
+    In the future, we can extend it to keep track of other scalar metrics.
+    """
+
+    def __init__(
+        self,
+        area_weights: torch.Tensor,
+        target: Literal["norm", "denorm"],
+        dist: Optional[Distributed] = None,
+        metadata: Optional[Mapping[str, VariableMetadata]] = None,
+    ):
+        """
+        Args:
+            area_weights: Area weights for each grid cell.
+            target: Whether to compute metrics on the normalized or denormalized data
+            dist: Distributed object to use for communication.
+            metadata: Mapping of variable names their metadata that will
+                used in generating logged image captions.
+        """
+        self._area_weights = area_weights
+        self._target = target
+        if dist is None:
+            self._dist = Distributed.get_instance()
+        else:
+            self._dist = dist
+        if metadata is None:
+            self._metadata: Mapping[str, VariableMetadata] = {}
+        else:
+            self._metadata = metadata
+
+        # Dictionaries of tensors of shape [n_lat, n_lon] represnting time means
+        self._target_data: Optional[Dict[str, torch.Tensor]] = None
+        self._gen_data: Optional[Dict[str, torch.Tensor]] = None
+        self._target_data_norm = None
+        self._gen_data_norm = None
+        self._n_batches = 0
+
+    @staticmethod
+    def _add_or_initialize_time_mean(
+        maybe_dict: Optional[MutableMapping[str, torch.Tensor]],
+        new_data: Mapping[str, torch.Tensor],
+        ignore_initial: bool = False,
+    ) -> Dict[str, torch.Tensor]:
+        sample_dim = 0
+        time_dim = 1
+        if ignore_initial:
+            time_slice = slice(1, None)
+        else:
+            time_slice = slice(0, None)
+        if maybe_dict is None:
+            d: Dict[str, torch.Tensor] = {
+                name: tensor[:, time_slice].mean(dim=time_dim).mean(dim=sample_dim)
+                for name, tensor in new_data.items()
+            }
+        else:
+            d = dict(maybe_dict)
+            for name, tensor in new_data.items():
+                d[name] += tensor[:, time_slice].mean(dim=time_dim).mean(dim=sample_dim)
+        return d
+
+    @torch.no_grad()
+    def record_batch(
+        self,
+        loss: float,
+        target_data: Mapping[str, torch.Tensor],
+        gen_data: Mapping[str, torch.Tensor],
+        target_data_norm: Mapping[str, torch.Tensor],
+        gen_data_norm: Mapping[str, torch.Tensor],
+        i_time_start: int = 0,
+    ):
+        if self._target == "norm":
+            target_data = target_data_norm
+            gen_data = gen_data_norm
+        ignore_initial = i_time_start == 0
+        self._target_data = self._add_or_initialize_time_mean(
+            self._target_data, target_data, ignore_initial
+        )
+        self._gen_data = self._add_or_initialize_time_mean(
+            self._gen_data, gen_data, ignore_initial
+        )
+
+        # we can ignore time slicing and just treat segments as though they're
+        # different batches, because we can assume all time segments have the
+        # same length
+        self._n_batches += 1
+
+    def _get_target_gen_pairs(self) -> List[_TargetGenPair]:
+        if self._n_batches == 0 or self._gen_data is None or self._target_data is None:
+            raise ValueError("No data recorded.")
+
+        ret = []
+        for name in self._gen_data.keys():
+            gen = self._dist.reduce_mean(self._gen_data[name] / self._n_batches)
+            target = self._dist.reduce_mean(self._target_data[name] / self._n_batches)
+            ret.append(_TargetGenPair(gen=gen, target=target, name=name))
+        return ret
+
+    @torch.no_grad()
+    def get_logs(self, label: str) -> Dict[str, Union[float, torch.Tensor]]:
+        logs = {}
+        preds = self._get_target_gen_pairs()
+        rmse_all_channels = {}
+        for pred in preds:
+            rmse_all_channels[pred.name] = pred.rmse(weights=self._area_weights)
+        logs.update(
+            {
+                f"rmse/all_channels": sum(rmse_all_channels.values())
+                / len(rmse_all_channels),
+            }
+        )
+        if len(label) != 0:
+            return {f"{label}/{key}": logs[key] for key in logs}
+        return logs
+
+    def get_dataset(self) -> xr.Dataset:
+        preds = self._get_target_gen_pairs()
+        rmse_all_channels = {}
+        for pred in preds:
+            rmse_all_channels[pred.name] = pred.rmse(weights=self._area_weights)
+        all_channel_rmse = sum(rmse_all_channels.values()) / len(rmse_all_channels)
+        return xr.Dataset(
+            data_vars={
+                "rmse_all_channels": xr.DataArray(
+                    all_channel_rmse, attrs={"units": "-"}
+                )
+            }
+        )
