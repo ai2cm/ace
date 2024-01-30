7054177dd846fb1fdec8142f7f3f9fb43a4f1476
Jeremy McGibbon <mcgibbon@uw.edu>
2024-01-30 13:58:35 -0500

    Add BuilderWithWeights (#444)
    This PR adds a module builder BuilderWithWeights which creates a module
    by using another builder and then partially or fully overwriting its
    weights with values from a saved SingleModuleStepper. If the saved
    weights are smaller, the weights are inserted at the start of each axis.
    
    Changes:
    - Added BuilderWithWeights
    - Removed unused num_blocks parameter from SFNO configuration, to avoid
    accidentally setting it and thinking it does something (as was being
    done in our tests)
    
    - [x] Tests added

--- fme/fme/fcn_training/test_registry.py
+++ fme/fme/fcn_training/test_registry.py
@@ -1,10 +1,34 @@
 """The registry also performs configuration set up so it needs to be tested."""
+import copy
+from typing import Iterable, Tuple
 
+import pytest
+import torch
+
+from fme.core.data_loading.data_typing import SigmaCoordinates
+from fme.core.device import get_device
+from fme.core.normalizer import FromStateNormalizer
+from fme.core.stepper import SingleModuleStepperConfig
 from fme.fcn_training import registry
 
 
+class MockModule(torch.nn.Module):
+    def __init__(self, param_shapes: Iterable[Tuple[int, ...]]):
+        super().__init__()
+        for i, shape in enumerate(param_shapes):
+            setattr(self, f"param{i}", torch.nn.Parameter(torch.randn(shape)))
+
+
+class MockModuleBuilder:
+    def __init__(self, param_shapes: Iterable[Tuple[int, ...]]):
+        self.param_shapes = param_shapes
+
+    def build(self, n_in_channels, n_out_channels, img_shape):
+        return MockModule(self.param_shapes)
+
+
 def test_sfno_builder():
-    """Make sure that the monkey patch is going through. Not that this is note
+    """Make sure that the monkey patch is going through. Note that this is not
     testing whether the modulus code runs the code correctly."""
 
     builder = registry.NET_REGISTRY["SphericalFourierNeuralOperatorNet"]()
@@ -15,3 +39,327 @@ def test_sfno_builder():
         sfno_net.trans_down.grid == "legendre-gauss"
         and sfno_net.itrans_up.grid == "legendre-gauss"
     ), "The grid should be set to legendre-gauss"
+
+
+def test_builder_with_weights_loads_same_state(tmpdir):
+    sfno_config_data = {
+        "type": "SphericalFourierNeuralOperatorNet",
+        "config": {
+            "num_layers": 2,
+            "embed_dim": 3,
+        },
+    }
+    stepper_config_data = {
+        "builder": sfno_config_data,
+        "in_names": ["x"],
+        "out_names": ["x"],
+        "normalization": FromStateNormalizer(
+            state={
+                "means": {"x": torch.randn(1)},
+                "stds": {"x": torch.randn(1)},
+            }
+        ),
+    }
+    area = torch.ones((1, 16, 32)).to(get_device())
+    sigma_coordinates = SigmaCoordinates(ak=torch.arange(7), bk=torch.arange(7)).to(
+        get_device()
+    )
+    stepper_config = SingleModuleStepperConfig.from_state(stepper_config_data)
+    stepper = stepper_config.get_stepper(
+        img_shape=(16, 32),
+        area=area,
+        sigma_coordinates=sigma_coordinates,
+    )
+    torch.save(
+        {
+            "stepper": stepper.get_state(),
+        },
+        str(tmpdir / "weights.ckpt"),
+    )
+    with_builder_config = {
+        "type": "BuilderWithWeights",
+        "config": {
+            "module": sfno_config_data,
+            "weights_path": str(tmpdir / "weights.ckpt"),
+            "allow_missing_parameters": False,
+        },
+    }
+    with_builder_stepper_config_data = {
+        "builder": with_builder_config,
+        "in_names": ["x"],
+        "out_names": ["x"],
+        "normalization": FromStateNormalizer(
+            state={
+                "means": {"x": torch.randn(1)},
+                "stds": {"x": torch.randn(1)},
+            }
+        ),
+    }
+    with_builder_stepper = SingleModuleStepperConfig.from_state(
+        with_builder_stepper_config_data
+    ).get_stepper(
+        img_shape=(16, 32),
+        area=area,
+        sigma_coordinates=sigma_coordinates,
+    )
+    assert_same_state(
+        with_builder_stepper.module.state_dict(),
+        stepper.module.state_dict(),
+        allow_larger=False,
+    )
+
+
+def assert_same_state(state1, state2, allow_larger: bool, same_keys: bool = True):
+    """
+    Assert that two states are the same.
+
+    If allow_larger is True then the states
+    may have different dimension lengths, and if they do then the initial slice
+    along that dimension will be compared.
+
+    Args:
+        state1: The first state.
+        state2: The second state.
+        allow_larger: Whether the states can have different dimension lengths.
+        same_keys: Whether the states must have the same keys.
+    """
+    if same_keys:
+        assert state1.keys() == state2.keys()
+    same_state = []
+    zero_state = []
+    different_state = []
+    for key in state1:
+        if key in state2:
+            if allow_larger:
+                s = [
+                    slice(0, min(s1, s2))
+                    for s1, s2 in zip(state1[key].shape, state2[key].shape)
+                ]
+            else:
+                s = [slice(0, None) for _ in state1[key].shape]
+            if torch.allclose(state1[key][s], state2[key][s]):
+                if torch.all(state1[key] == 0):
+                    zero_state.append(key)
+                else:
+                    same_state.append(key)
+            else:
+                different_state.append(key)
+    assert len(same_state) > 0, "No parameters were the same between the two states."
+    assert len(different_state) == 0, (
+        f"Parameters {different_state} were different between "
+        f"the two states, only {same_state} had the same non-zero state."
+    )
+
+
+@pytest.mark.parametrize(
+    "shape",
+    [
+        pytest.param((8, 16)),
+    ],
+)
+def test_sfno_init(shape):
+    num_layers = 2
+    sfno_config_data = {
+        "type": "SphericalFourierNeuralOperatorNet",
+        "config": {
+            "num_layers": num_layers,
+            "embed_dim": 3,
+            "scale_factor": 1.0,
+        },
+    }
+    stepper_config_data = {
+        "builder": sfno_config_data,
+        "in_names": ["x"],
+        "out_names": ["x"],
+        "normalization": FromStateNormalizer(
+            state={
+                "means": {"x": torch.randn(1)},
+                "stds": {"x": torch.randn(1)},
+            }
+        ),
+    }
+    area = torch.ones((1, 16, 32)).to(get_device())
+    sigma_coordinates = SigmaCoordinates(ak=torch.arange(7), bk=torch.arange(7)).to(
+        get_device()
+    )
+    stepper_config = SingleModuleStepperConfig.from_state(stepper_config_data)
+    stepper = stepper_config.get_stepper(
+        img_shape=shape,
+        area=area,
+        sigma_coordinates=sigma_coordinates,
+    )
+    assert len(stepper.module.module.blocks) == num_layers
+
+
+@pytest.mark.parametrize(
+    "loaded_shape,built_shape,extra_built_layer,expect_exception",
+    [
+        pytest.param((8, 16), (8, 16), False, False, id="same_shape"),
+        pytest.param((8, 16), (8, 16), True, False, id="extra_layer"),
+        pytest.param((8, 16), (16, 32), False, False, id="larger_model"),
+        pytest.param((16, 32), (8, 16), False, True, id="smaller_model"),
+    ],
+)
+def test_builder_with_weights_sfno_init(
+    tmpdir, loaded_shape, built_shape, extra_built_layer: bool, expect_exception: bool
+):
+    sfno_config_data = {
+        "type": "SphericalFourierNeuralOperatorNet",
+        "config": {
+            "num_layers": 2,
+            "embed_dim": 3,
+            "scale_factor": 1.0,
+        },
+    }
+    stepper_config_data = {
+        "builder": sfno_config_data,
+        "in_names": ["x"],
+        "out_names": ["x"],
+        "normalization": FromStateNormalizer(
+            state={
+                "means": {"x": torch.randn(1)},
+                "stds": {"x": torch.randn(1)},
+            }
+        ),
+    }
+    area = torch.ones((1, 16, 32)).to(get_device())
+    sigma_coordinates = SigmaCoordinates(ak=torch.arange(7), bk=torch.arange(7)).to(
+        get_device()
+    )
+    stepper_config = SingleModuleStepperConfig.from_state(stepper_config_data)
+    stepper = stepper_config.get_stepper(
+        img_shape=loaded_shape,
+        area=area,
+        sigma_coordinates=sigma_coordinates,
+    )
+    built_sfno_config_data = copy.deepcopy(sfno_config_data)
+    if extra_built_layer:
+        built_sfno_config_data["config"]["num_layers"] += 1  # type: ignore
+    torch.save(
+        {
+            "stepper": stepper.get_state(),
+        },
+        str(tmpdir / "weights.ckpt"),
+    )
+    with_builder_config = {
+        "type": "BuilderWithWeights",
+        "config": {
+            "module": built_sfno_config_data,
+            "weights_path": str(tmpdir / "weights.ckpt"),
+            "allow_missing_parameters": False,
+        },
+    }
+    with_builder_stepper_config_data = {
+        "builder": with_builder_config,
+        "in_names": ["x"],
+        "out_names": ["x"],
+        "normalization": FromStateNormalizer(
+            state={
+                "means": {"x": torch.randn(1)},
+                "stds": {"x": torch.randn(1)},
+            }
+        ),
+    }
+    if expect_exception:
+        with pytest.raises(ValueError):
+            with_builder_stepper = SingleModuleStepperConfig.from_state(
+                with_builder_stepper_config_data
+            ).get_stepper(
+                img_shape=built_shape,
+                area=area,
+                sigma_coordinates=sigma_coordinates,
+            )
+    else:
+        with_builder_stepper = SingleModuleStepperConfig.from_state(
+            with_builder_stepper_config_data
+        ).get_stepper(
+            img_shape=built_shape,
+            area=area,
+            sigma_coordinates=sigma_coordinates,
+        )
+        print(with_builder_stepper.module.state_dict().keys())
+        print(stepper.module.state_dict().keys())
+        if extra_built_layer:
+            with pytest.raises(AssertionError):
+                assert_same_state(
+                    with_builder_stepper.module.state_dict(),
+                    stepper.module.state_dict(),
+                    allow_larger=True,
+                    same_keys=True,  # This should fail if there are more parameters
+                )
+        assert_same_state(
+            with_builder_stepper.module.state_dict(),
+            stepper.module.state_dict(),
+            allow_larger=True,
+            same_keys=(not extra_built_layer),
+        )
+
+
+class SimpleLinearModule(torch.nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.linear = torch.nn.Linear(in_features, out_features)
+        self.custom_param = torch.nn.Parameter(torch.randn(5, 5))
+
+
+class NestedModule(torch.nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.linear1 = SimpleLinearModule(in_features, out_features)
+        self.linear2 = SimpleLinearModule(out_features, in_features)
+        self.custom_param = torch.nn.Parameter(torch.randn(5, 5))
+
+
+@pytest.mark.parametrize(
+    "from_module, to_module, expected_exception",
+    [
+        pytest.param(
+            SimpleLinearModule(10, 10),
+            SimpleLinearModule(10, 10),
+            None,
+            id="Matching sizes",
+        ),
+        pytest.param(
+            SimpleLinearModule(10, 10),
+            SimpleLinearModule(20, 10),
+            None,
+            id="to_module larger weights",
+        ),
+        pytest.param(
+            SimpleLinearModule(20, 10),
+            SimpleLinearModule(10, 10),
+            ValueError,
+            id="from_module larger weights",
+        ),
+        pytest.param(
+            NestedModule(10, 20),
+            NestedModule(10, 20),
+            None,
+            id="Complex modules matching sizes",
+        ),
+        pytest.param(
+            NestedModule(10, 20),
+            SimpleLinearModule(10, 20),
+            ValueError,
+            id="Nested modules, mismatched structure",
+        ),
+    ],
+)
+def test_overwrite_weights(from_module, to_module, expected_exception):
+    if expected_exception:
+        with pytest.raises(expected_exception):
+            registry._overwrite_weights(from_module, to_module)
+    else:
+        registry._overwrite_weights(from_module, to_module)
+        for from_param, to_param in zip(
+            from_module.parameters(), to_module.parameters()
+        ):
+            if len(from_param.shape) == 1:
+                assert torch.allclose(
+                    from_param.data, to_param.data[: from_param.data.size(0)]
+                )
+            else:
+                assert torch.allclose(
+                    from_param.data,
+                    to_param.data[: from_param.data.size(0), : from_param.data.size(1)],
+                )
