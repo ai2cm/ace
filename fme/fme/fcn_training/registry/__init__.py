ecad86e103dc8390d5d1db7607d8458d812b6dc0
Jeremy McGibbon <mcgibbon@uw.edu>
2024-02-13 13:26:29 -0500

    Make BuilderWithWeights its own ParameterInitializationConfig (#537)
    BuilderWithWeights is very different from the other builders in how it
    works, and is better represented as its own concept. This PR does that.
    
    Changes:
    - Removed BuilderWithWeights as a ModuleSelector option. Support for
    loading models that used BuilderWithWeights for training is removed.
    - Added ParameterInitializationConfig class that supports overwriting
    weights with values from a checkpoint, the same way as was previously
    done by BuilderWithWeights.
    - Added init_weights option to SingleModuleStepper, which is set to
    False when loading from a checkpoint as those values are about to be
    overwritten
    - Added `parameter_init: ParameterInitializationConfig` option to
    SingleModuleStepper
    
    - [x] Tests added
    
    Resolves #503

--- fme/fme/fcn_training/registry/__init__.py
+++ fme/fme/fcn_training/registry/__init__.py
@@ -1,7 +1,6 @@
 # import modules so they are registered
 from . import prebuilt as _prebuilt
 from . import sfno as _sfno
-from . import with_weights as _with_weights
 from .registry import ModuleSelector, get_from_registry, register
 
-del _prebuilt, _sfno, _with_weights
+del _prebuilt, _sfno
