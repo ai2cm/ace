e7b62edeb73b7f082220ef7640b92a026cc3f5fb
Gideon Dresdner <gideond@allenai.org>
2024-02-15 19:08:55 -0500

    Enable ensemble datasets in downscaling (#554)
    Currently the data loaders in are hard coded to using Xarraydataset
    which does not match the ensemble datasets that we expect in FME.
    
    Changes:
    - uses `get_dataset` instead of hard coding XarrayDataset
    - creates a integration test for data loading in lieu of the old unit
    test that that mocked out the `get_dataset` function. This is more
    effective at testing what we actually need to test here.
    - [x] Tests added

--- fme/fme/downscaling/test_datasets.py
+++ fme/fme/downscaling/test_datasets.py
@@ -1,13 +1,8 @@
-from typing import Dict, Sequence, Tuple
-from unittest.mock import MagicMock, patch
-
 import pytest
 import torch
 import xarray as xr
 
-import fme.core.data_loading.params
-from fme.core.data_loading.requirements import DataRequirements
-from fme.core.typing_ import TensorMapping
+from fme.core.testing.fv3gfs_data import DimSizes, FV3GFSData
 
 from .datasets import BatchData, DataLoaderParams, PairedDataset
 
@@ -96,64 +91,49 @@ def test_downscaling_dataset_validate(
         PairedDataset(highres_data, lowres_data)
 
 
-def mock_xarray_dataset_instance(data: Sequence[Tuple[TensorMapping, xr.DataArray]]):
-    mock_dataset_instance = MagicMock()
-    mock_dataset_instance.__getitem__.side_effect = lambda idx: data[idx]
-    mock_dataset_instance.__len__.return_value = len(data)
-    return mock_dataset_instance
+@pytest.mark.parametrize(
+    "path_extension, data_type",
+    [
+        pytest.param("", "ensemble_xarray", id="ensemble"),
+        pytest.param("data", "xarray", id="xarray"),
+    ],
+)
+def test_dataloader_build(tmp_path, path_extension, data_type):
+    """Integration test that creates test data on disk."""
 
+    highres_path, lowres_path = tmp_path / "highres", tmp_path / "lowres"
+    highres_path.mkdir()
+    lowres_path.mkdir()
 
-def mock_xarray_dataset_selector(paths_to_mocks: Dict[str, MagicMock]):
-    def select_mock_instance(
-        params: fme.core.data_loading.params.DataLoaderParams,
-        requirements: DataRequirements,
-    ):
-        del requirements  # unused
-        try:
-            return paths_to_mocks[params.data_path]
-        except KeyError:
-            raise ValueError(f"Unexpected path {params.data_path}")
-
-    return select_mock_instance
-
-
-@patch("fme.downscaling.datasets.XarrayDataset")
-@pytest.mark.parametrize("n_examples,batch_size", [(1, 1), (4, 4), (4, 2), (5, 2)])
-def test_downscaling_dataloader(mock_xarray_dataset, n_examples, batch_size):
-    n_examples = 2
-    batch_size = 1
-
-    highres_shape = (1, 1, 8, 16)
-    lowres_shape = (1, 1, 4, 8)
-
-    highres_data = [
-        (random_named_tensor(["x"], highres_shape), data_array(0))
-        for _ in range(n_examples)
+    all_names = ["x", "y"]
+    num_timesteps = 10
+    highres_shape, lowres_shape = (64, 32), (32, 16)
+    num_vertical_levels = 1
+    highres_data, lowres_data = [
+        FV3GFSData(
+            path=path,
+            names=all_names,
+            dim_sizes=DimSizes(num_timesteps, h, w, num_vertical_levels),
+            time_varying_values=[float(i) for i in range(num_timesteps)],
+        )
+        for (path, (h, w)) in zip(
+            [highres_path, lowres_path], [highres_shape, lowres_shape]
+        )
     ]
-    lowres_data = [
-        (random_named_tensor(["x"], lowres_shape), data_array(0))
-        for _ in range(n_examples)
-    ]
-
-    mock_xarray_dataset.side_effect = mock_xarray_dataset_selector(
-        {
-            "/path/to/nowhere/highres": mock_xarray_dataset_instance(highres_data),
-            "/path/to/nowhere/lowres": mock_xarray_dataset_instance(lowres_data),
-        }
-    )
-
+    batch_size = 2
     params = DataLoaderParams(
-        "/path/to/nowhere/highres",
-        "/path/to/nowhere/lowres",
-        "xarray",
+        str(highres_data.path / path_extension),
+        str(lowres_data.path / path_extension),
+        data_type,
         batch_size,
         1,
     )
-    loader = params.build(True, ["x"])
 
-    assert len(loader.loader) == n_examples // batch_size
-    assert len(loader.loader.dataset) == n_examples
-    batch = next(iter(loader.loader))
-    assert batch.highres["x"].shape == torch.Size((batch_size,) + highres_shape)
-    assert batch.lowres["x"].shape == torch.Size((batch_size,) + lowres_shape)
+    loader = params.build(True, all_names, None)
+    assert len(loader.loader) == num_timesteps // batch_size
+    assert len(loader.loader.dataset) == num_timesteps  # type: ignore
     assert loader.downscale_factor == 2
+    batch = next(iter(loader.loader))
+    for var_name in all_names:
+        assert batch.highres[var_name].shape == (batch_size, 1, *highres_shape)
+        assert batch.lowres[var_name].shape == (batch_size, 1, *lowres_shape)
