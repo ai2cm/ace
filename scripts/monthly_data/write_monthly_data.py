4b0f8102f0f3ca1741547d7619fdd70ca91e686a
Brian Henn <brianhenn@allenai.org>
2024-03-13 13:37:22 -0700

    Scripts: run monthly test (#611)
    Allows the test of the monthly data coarsening script to run.
    
    Co-authored-by: Jeremy McGibbon <mcgibbon@uw.edu>

--- scripts/monthly_data/write_monthly_data.py
+++ scripts/monthly_data/write_monthly_data.py
@@ -11,8 +11,8 @@ import xarray as xr
 import yaml
 
 from fme.core.data_loading._xarray import XarrayDataset, get_datasets_at_path
+from fme.core.data_loading.config import DataLoaderConfig
 from fme.core.data_loading.data_typing import SigmaCoordinates
-from fme.core.data_loading.params import DataLoaderConfig, XarrayDataConfig
 from fme.core.data_loading.requirements import DataRequirements
 from fme.core.data_loading.utils import BatchData
 from fme.core.device import using_gpu
@@ -205,34 +205,6 @@ def write_ensemble_dataset(
             dim_sizes,
             names,
         )
-
-
-def test_write_monthly_data(tmp_path: pathlib.Path):
-    all_names = ["a", "b"]
-    dim_sizes = DimSizes(
-        n_time=4 * 60,
-        n_lat=4,
-        n_lon=8,
-        nz_interface=2,
-    )
-    write_ensemble_dataset(tmp_path / "data", 3, all_names, dim_sizes)
-    config = Config(
-        experiment_dir=str(tmp_path),
-        data_loader=DataLoaderConfig(
-            dataset=XarrayDataConfig(data_path=str(tmp_path / "data")),
-            batch_size=1,
-            num_data_workers=1,
-            data_type="ensemble_xarray",
-        ),
-        logging=LoggingConfig(
-            log_to_screen=True,
-            log_to_file=False,
-            log_to_wandb=False,
-        ),
-        variable_names=all_names,
-    )
-    run(config)
-    xr.open_dataset(tmp_path / "monthly_binned_data.nc")
 
 
 if __name__ == "__main__":
