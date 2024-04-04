90f9e66909c0a83f04f262c481b63b504275dbc1
Jeremy McGibbon <mcgibbon@uw.edu>
2024-04-04 11:29:48 -0400

    Fix vertical orientation of stitched plots (#684)
    Currently our stitched plots have panels in the opposite vertical
    orientation (flipped top-bottom) from what we expect. This PR fixes that
    issue.

--- fme/fme/core/aggregator/test_plotting.py
+++ fme/fme/core/aggregator/test_plotting.py
@@ -39,10 +39,10 @@ def test_stitch_data_panels():
     ]
     stitched = _stitch_data_panels(data, vmin=1)
     expected = np.array(
-        [
-            [1, 2, 1, 3, 4],
-            [1, 1, 1, 1, 1],
+        [  # vertical orientation is swapped as data starts from bottom-left
             [5, 6, 1, 7, 8],
+            [1, 1, 1, 1, 1],
+            [1, 2, 1, 3, 4],
         ]
     )
     assert np.array_equal(stitched, expected)
