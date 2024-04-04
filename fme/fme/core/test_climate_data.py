95bd4824b9a61fc66534d5708b72bc8f0b485236
Jeremy McGibbon <mcgibbon@uw.edu>
2024-04-04 11:38:42 -0400

    Report variables with missing lower levels as missing (#711)
    Currently ClimateData will always return a value for multi-layer
    variables if any layers are present, leading to exceptions being raised
    down the line when these variables don't have a number of layers
    matching ak and bk. This PR updates ClimateData so that if a layer other
    than the bottom layer is missing, it will raise a KeyError rather than
    return a stacked variable missing some layers. This allows the derived
    metric code to skip computing outputs depending on this data.
    
    Changes:
    - ClimateData will now raise a KeyError if a vertically-resolved
    variable missing layers (other than the bottom-most layer) is accessed
    
    - [x] Tests added

--- fme/fme/core/test_climate_data.py
+++ fme/fme/core/test_climate_data.py
@@ -96,3 +96,37 @@ def test_missing_specific_total_water(has_water_variable):
     else:
         with pytest.raises(KeyError):
             _ = climate_data.specific_total_water
+
+
+@pytest.mark.parametrize("missing_water_layer", [True, False])
+def test_keyerror_when_missing_specific_total_water_layer(missing_water_layer: bool):
+    """Check shape of specific total water and make sure that it returns None
+    when it can't be computed."""
+    n_samples, n_time_steps, nlat, nlon = 2, 3, 4, 8
+
+    def _get_data(missing_water_layer: bool):
+        data = {
+            "x": torch.rand(n_samples, n_time_steps, nlat, nlon),
+            "specific_total_water_1": torch.rand(n_samples, n_time_steps, nlat, nlon),
+            "PRESsfc": torch.rand(n_samples, n_time_steps, nlat, nlon),
+        }
+        if not missing_water_layer:
+            data["specific_total_water_0"] = torch.rand(
+                n_samples, n_time_steps, nlat, nlon
+            )
+        return data
+
+    climate_data = ClimateData(_get_data(missing_water_layer))
+
+    if not missing_water_layer:
+        assert climate_data.specific_total_water is not None
+        assert climate_data.specific_total_water.shape == (
+            n_samples,
+            n_time_steps,
+            nlat,
+            nlon,
+            2,
+        )
+    else:
+        with pytest.raises(KeyError):
+            _ = climate_data.specific_total_water
