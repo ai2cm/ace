a3b8a43952df3833b03bf67e7f1e5b576a05f809
Spencer Clark <spencerkclark@gmail.com>
2024-04-12 13:43:05 -0400

    Broadcast loaded fields to ensure they have full horizontal dimensions (#727)
    This PR adds code to automatically broadcast any loaded variables to
    have a full set of dimensions, ordered (time, latitude, longitude). This
    can be used for instance to load a scalar constant or scalar
    time-varying carbon dioxide concentration.
    
    - [x] Tests added

--- fme/fme/core/data_loading/utils.py
+++ fme/fme/core/data_loading/utils.py
@@ -1,5 +1,6 @@
 import dataclasses
-from typing import List, Sequence, Tuple
+import warnings
+from typing import List, Optional, Sequence, Tuple
 
 import cftime
 import dask
@@ -10,6 +11,82 @@ from torch.utils.data import default_collate
 
 from fme.core.typing_ import TensorMapping
 
+SLICE_NONE = slice(None)
+
+
+def infer_horizontal_dimension_names(ds: xr.Dataset) -> Tuple[str, str]:
+    if "grid_xt" in ds.variables:
+        hdims = "grid_xt", "grid_yt"
+    elif "lon" in ds.variables:
+        hdims = "lon", "lat"
+    elif "longitude" in ds.variables:
+        hdims = "longitude", "latitude"
+    else:
+        reference_da = None
+        for da in ds.data_vars.values():
+            if len(da.dims) == 3:
+                reference_da = da
+                break
+        if reference_da is None:
+            raise ValueError("Could not identify dataset's horizontal dimensions.")
+        else:
+            _, lat_dim, lon_dim = reference_da.dims
+            warnings.warn(
+                f"Familiar latitude and longitude coordinate names could not be "
+                f"found in the dataset. Assuming that the trailing two dimensions, "
+                f"{lat_dim!r} and {lon_dim!r}, represent latitude and longitude "
+                f"respectively."
+            )
+            hdims = lon_dim, lat_dim
+    return hdims
+
+
+def _get_indexers(
+    variable: xr.Variable, dims: Sequence[str]
+) -> Tuple[Optional[slice], ...]:
+    """Returns a tuple of indexers for the dimensions provided.
+
+    Indexers select all data from dimensions that exist in the variable, and
+    create new axes for dimensions that do not exist. The returned tuple will
+    have the same length as the provided sequence of dimensions.
+
+    Inspired by similar code in xarray:
+    https://github.com/pydata/xarray/blob/1d43672574332615f225089d69f95a9f8d81d912/xarray/core/computation.py#L681-L688
+    """
+    indexers: List[Optional[slice]] = []
+    for dim in dims:
+        if dim in variable.dims:
+            indexers.append(SLICE_NONE)
+        else:
+            indexers.append(None)
+    return tuple(indexers)
+
+
+def _load_variable(variable: xr.Variable, time_slice) -> np.ndarray:
+    """Load data from a variable into memory.
+
+    This function leverages xarray's lazy loading to load only the time slice
+    of a variable we need. It assumes that, if present, "time" is the leading
+    dimension of the array.
+    """
+    if "time" in variable.dims:
+        return variable[time_slice, ...].values
+    else:
+        return variable.values
+
+
+def as_broadcasted_tensor(
+    variable: xr.Variable,
+    dims: Sequence[str],
+    shape: Sequence[int],
+    time_slice: slice = SLICE_NONE,
+) -> torch.tensor:
+    """Load data from variable and broadcast to tensor with the given shape."""
+    arr = _load_variable(variable, time_slice)
+    indexers = _get_indexers(variable, dims)
+    tensor = torch.as_tensor(arr[indexers])
+    return torch.broadcast_to(tensor, shape)
+
 
 def load_series_data(
     idx: int,
@@ -18,24 +95,20 @@ def load_series_data(
     names: List[str],
 ):
     time_slice = slice(idx, idx + n_steps)
+    lon_dim, lat_dim = infer_horizontal_dimension_names(ds)
+    dims = ("time", lat_dim, lon_dim)
+    shape = (n_steps, ds.sizes[lat_dim], ds.sizes[lon_dim])
     # disable dask threading to avoid warnings
     with dask.config.set(scheduler="synchronous"):
         arrays = {}
         for n in names:
-            arr = ds.variables[n][time_slice, :, :]
-            arrays[n] = torch.as_tensor(arr.values)
+            variable = ds[n].variable
+            arrays[n] = as_broadcasted_tensor(variable, dims, shape, time_slice)
         return arrays
 
 
 def get_lons_and_lats(ds: xr.Dataset) -> Tuple[np.ndarray, np.ndarray]:
-    if "grid_xt" in ds.variables:
-        hdims = "grid_xt", "grid_yt"
-    elif "lon" in ds.variables:
-        hdims = "lon", "lat"
-    elif "longitude" in ds.variables:
-        hdims = "longitude", "latitude"
-    else:
-        raise ValueError("Could not identify dataset's horizontal dimensions.")
+    hdims = infer_horizontal_dimension_names(ds)
     lons, lats = ds[hdims[0]].values, ds[hdims[1]].values
     return np.array(lons, dtype=np.float32), np.array(lats, dtype=np.float32)
 
