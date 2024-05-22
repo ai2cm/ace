243b95d4bf36606b8b52b7072534c7ea49401123
Jeremy McGibbon <mcgibbon@uw.edu>
2024-05-22 16:41:15 -0400

    Document the Builder pattern (#853)
    This PR adds documentation for the Builder pattern we use for
    configuration.
    
    Changes:
    - Added builder.rst to documentation and linked it in the index.
    
    ---------
    
    Co-authored-by: Gideon Dresdner <gideond@allenai.org>

--- fme/docs/conf.py
+++ fme/docs/conf.py
@@ -45,7 +45,12 @@ with open("available_modules.rst", "w") as f:
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.doctest",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
