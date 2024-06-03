281155b4ac7ebafcc79b99137bc475eb8718587e
Gideon Dresdner <gideond@allenai.org>
2024-06-03 17:22:38 -0400

    Diffusion deterministic generator for downscaling (#854)
    This PR incorporates the diffusion sampling algorithm into our code.
    
    Changes:
    - Implements `DiffusionModel.generate_on_batch` and adds associated
    generation parameters
    - Modifies the sampler code to condition on coarse inputs
    - Makes all noise distribution parameters fields on the
    `DiffusionModelConfig` and threads them through via build methods.
    
    - [x] Tests added
    
    ---------
    
    Co-authored-by: Jeremy McGibbon <mcgibbon@uw.edu>

--- fme/fme/downscaling/samplers.py
+++ fme/fme/downscaling/samplers.py
@@ -23,18 +23,14 @@ import torch
 
 
 def edm_sampler(
-    net, latents, class_labels=None, randn_like=torch.randn_like,
-    num_steps=18, sigma_min=0.002, sigma_max=80, rho=7,
-    S_churn=0, S_min=0, S_max=float('inf'), S_noise=1,
+    net, latents, coarse, randn_like=torch.randn_like,
+    num_steps=18, sigma_min=0.002, sigma_max=80.0, rho=7,
+    S_churn=0.0, S_min=0.0, S_max=float('inf'), S_noise=1,
 ):
-    # Adjust noise levels based on what's supported by the network.
-    sigma_min = max(sigma_min, net.sigma_min)
-    sigma_max = min(sigma_max, net.sigma_max)
-
     # Time step discretization.
     step_indices = torch.arange(num_steps, dtype=torch.float64, device=latents.device)
     t_steps = (sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
-    t_steps = torch.cat([net.round_sigma(t_steps), torch.zeros_like(t_steps[:1])]) # t_N = 0
+    t_steps = torch.cat([torch.tensor(t_steps), torch.zeros_like(t_steps[:1])]) # t_N = 0
 
     # Main sampling loop.
     x_next = latents.to(torch.float64) * t_steps[0]
@@ -43,17 +39,17 @@ def edm_sampler(
 
         # Increase noise temporarily.
         gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
-        t_hat = net.round_sigma(t_cur + gamma * t_cur)
+        t_hat = t_cur + gamma * t_cur
         x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * randn_like(x_cur)
 
         # Euler step.
-        denoised = net(x_hat, t_hat, class_labels).to(torch.float64)
+        denoised = net(x_hat, coarse, t_hat).to(torch.float64)
         d_cur = (x_hat - denoised) / t_hat
         x_next = x_hat + (t_next - t_hat) * d_cur
 
         # Apply 2nd order correction.
         if i < num_steps - 1:
-            denoised = net(x_next, t_next, class_labels).to(torch.float64)
+            denoised = net(x_next, coarse, t_next).to(torch.float64)
             d_prime = (x_next - denoised) / t_next
             x_next = x_hat + (t_next - t_hat) * (0.5 * d_cur + 0.5 * d_prime)
 
