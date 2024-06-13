1c70afd8568d43a3786e2698502e055ffd08f353
Brian Henn <brianhenn@allenai.org>
2024-06-13 10:44:48 -0700

    Inline annual aggregator year alignment (#820)
    This PR revamps the distributed gathering of annual-mean time series to enable the inference GlobalMeanAnnualAggregator to work in inline/distributed mode.
    
    Changes:
    - Adds a `distributed.gather_irregular` method to handle gathering
    annual-mean time series with variable lengths across processes.

--- fme/fme/core/aggregator/inference/test_annual.py
+++ fme/fme/core/aggregator/inference/test_annual.py
@@ -3,20 +3,16 @@ import pathlib
 
 import cftime
 import matplotlib.pyplot as plt
-import numpy as np
+import pytest
 import torch
 import xarray as xr
 
 import fme
 from fme.core.aggregator.inference.annual import GlobalMeanAnnualAggregator
 from fme.core.device import get_device
-from fme.core.testing import DimSizes, MonthlyReferenceData
+from fme.core.testing import DimSizes, MonthlyReferenceData, mock_distributed
 
 TIMESTEP = datetime.timedelta(hours=6)
-
-
-def get_zero_time(shape, dims):
-    return xr.DataArray(np.zeros(shape, dtype="datetime64[ns]"), dims=dims)
 
 
 def test_annual_aggregator(tmpdir):
@@ -48,15 +44,70 @@ def test_annual_aggregator(tmpdir):
         "a": torch.randn(n_sample, n_time, n_lat, n_lon, device=get_device())
     }
     gen_data = {"a": torch.randn(n_sample, n_time, n_lat, n_lon, device=get_device())}
-    time = get_zero_time(shape=[n_sample, n_time], dims=["sample", "time"])
-    time_1d = [
-        cftime.DatetimeProlepticGregorian(2000, 1, 1) + i * datetime.timedelta(hours=6)
-        for i in range(n_time)
-    ]
-    time = xr.DataArray([time_1d for _ in range(n_sample)], dims=["sample", "time"])
+    time = xr.DataArray(
+        [
+            [
+                (
+                    cftime.DatetimeProlepticGregorian(2000, 1, 1)
+                    + i * datetime.timedelta(hours=6)
+                )
+                for i in range(n_time)
+            ]
+            for _ in range(n_sample)
+        ],
+        dims=["sample", "time"],
+    )
     agg.record_batch(time, target_data, gen_data)
     logs = agg.get_logs(label="test")
     assert len(logs) > 0
     assert "test/a" in logs
     assert isinstance(logs["test/a"], plt.Figure)
     assert "test/r2/a_target" in logs
+
+
+@pytest.mark.parametrize("use_mock_distributed", [False, True])
+def test__get_gathered_means(use_mock_distributed):
+    """Test the private _get_gathered_means method, rather than the public
+    get_logs method, because the public method returns some data converted to images,
+    and so that portion can't easily be tested.
+    """
+    n_lat = 16
+    n_lon = 32
+    n_sample = 2
+    n_time = 365 * 4 * 2  # two years, approximately
+    area_weights = torch.ones(n_lat, n_lon).to(fme.get_device())
+    agg = GlobalMeanAnnualAggregator(
+        area_weights=area_weights,
+        timestep=TIMESTEP,
+    )
+    target_data = {
+        "a": torch.randn(n_sample, n_time, n_lat, n_lon, device=get_device())
+    }
+    gen_data = {"a": torch.randn(n_sample, n_time, n_lat, n_lon, device=get_device())}
+    time = xr.DataArray(
+        [
+            [
+                (
+                    cftime.DatetimeProlepticGregorian(
+                        2000 + j, 1, 1
+                    )  # add j so that years are not the same
+                    + i * datetime.timedelta(hours=6)
+                )
+                for i in range(n_time)
+            ]
+            for j in range(n_sample)
+        ],
+        dims=["sample", "time"],
+    )
+    agg.record_batch(time, target_data, gen_data)
+    if use_mock_distributed:
+        world_size = 2
+        with mock_distributed(world_size=world_size):
+            target, gen = agg._get_gathered_means()
+    else:
+        world_size = 1
+        target, gen = agg._get_gathered_means()
+    for dataset in (target, gen):
+        assert set(dataset.dims) == {"sample", "year"}
+        assert list(dataset.year.values) == [2000, 2001, 2002]
+        assert dataset.sizes["sample"] == n_sample * world_size
