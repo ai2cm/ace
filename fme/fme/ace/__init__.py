4f74473f3686f0a78d8ae13ff3041465d286a79b
Jeremy McGibbon <mcgibbon@uw.edu>
2024-06-26 16:49:35 -0400

    Add run_*_from_config functions (#939)
    This PR adds public `run_train_from_config`,
    `run_inference_from_config`, and `run_evaluator_from_config` methods to
    `fme.ace` to allow users to configure and run or run post-processing
    programmatically without exiting Python.
    
    Changes:
    - Added `fme.ace.run_train_from_config`,
    `fme.ace.run_inference_from_config`, and
    `fme.ace.run_evaluator_from_config` functions, each taking an
    appropriate `config` as its only argument.
    
    - [ ] Tests added

--- fme/fme/ace/__init__.py
+++ fme/fme/ace/__init__.py
@@ -6,7 +6,9 @@ from fme.ace.inference.evaluator import (
     InferenceEvaluatorAggregatorConfig,
     InferenceEvaluatorConfig,
     OceanConfig,
+    run_evaluator_from_config,
 )
+from fme.ace.inference.inference import InferenceConfig, run_inference_from_config
 from fme.ace.registry.sfno import SFNO_V0_1_0, SphericalFourierNeuralOperatorBuilder
 from fme.core.corrector import CorrectorConfig
 from fme.core.data_loading.config import TimeSlice, XarrayDataConfig
@@ -16,6 +18,7 @@ from fme.core.normalizer import FromStateNormalizer, NormalizationConfig
 from fme.core.parameter_init import ParameterInitializationConfig
 from fme.core.registry import ModuleSelector, get_available_module_types, register
 
+from .train import run_train_from_config
 from .train_config import (
     CopyWeightsConfig,
     DataLoaderConfig,
