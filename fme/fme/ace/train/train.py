ai2cm/ace

commit 35ec7320e826362a5c5a89731ecf1c142d833771

Prevent corrupted checkpoints during preemption (#1011)
This PR prevents corrupting checkpoints if pre-emption occurs while
checkpoints are written by writing them first to a temporary file, then
moving them to the final location.

Changes:
- No public API changes

- [ ] Tests added

fme/fme/ace/train/train.py
@@ -53,6 +53,7 @@ import dataclasses
 import gc
 import logging
 import os
+import tempfile
 import time
 from typing import Optional
 
@@ -418,18 +419,25 @@ class Trainer:
         return logs
 
     def save_checkpoint(self, checkpoint_path):
-        torch.save(
-            {
-                "num_batches_seen": self.num_batches_seen,
-                "epoch": self._model_epoch,
-                "best_validation_loss": self._best_validation_loss,
-                "best_inference_error": self._best_inference_error,
-                "stepper": self.stepper.get_state(),
-                "optimization": self.optimization.get_state(),
-                "ema": self._ema.get_state(),
-            },
-            checkpoint_path,
-        )
+        # save to a temporary file in case we get pre-empted during save
+        tmp = tempfile.NamedTemporaryFile()
+        try:
+            torch.save(
+                {
+                    "num_batches_seen": self.num_batches_seen,
+                    "epoch": self._model_epoch,
+                    "best_validation_loss": self._best_validation_loss,
+                    "best_inference_error": self._best_inference_error,
+                    "stepper": self.stepper.get_state(),
+                    "optimization": self.optimization.get_state(),
+                    "ema": self._ema.get_state(),
+                },
+                tmp,
+            )
+            os.replace(tmp.name, checkpoint_path)
+        finally:
+            if os.path.exists(tmp.name):
+                tmp.close()
 
     def restore_checkpoint(self, checkpoint_path, ema_checkpoint_path):
         _restore_checkpoint(self, checkpoint_path, ema_checkpoint_path)

commit 1e3f22e0c2ffb20ef93ebe325be9396b3fe5a3a9

use temporary checkpoint on same filesystem as final location (#1014)
Fixes the same issue as #1013 issue introduced by #1011, but retains the
use of `os.replace` to ensure files are being moved within a single
filesystem instead of copied, since a copy operation could be corrupted
by preemption. Will use a temporary filename in the same location as the
output checkpoint.

Changes:
- No public API changes

- [ ] Tests added

fme/fme/ace/train/train.py
@@ -53,8 +53,8 @@ import dataclasses
 import gc
 import logging
 import os
-import tempfile
 import time
+import uuid
 from typing import Optional
 
 import dacite
@@ -420,7 +420,9 @@ class Trainer:
 
     def save_checkpoint(self, checkpoint_path):
         # save to a temporary file in case we get pre-empted during save
-        tmp = tempfile.NamedTemporaryFile()
+        temporary_location = os.path.join(
+            os.path.dirname(checkpoint_path), f".{uuid.uuid4()}.tmp"
+        )
         try:
             torch.save(
                 {
@@ -432,12 +434,12 @@ class Trainer:
                     "optimization": self.optimization.get_state(),
                     "ema": self._ema.get_state(),
                 },
-                tmp,
+                temporary_location,
             )
-            os.replace(tmp.name, checkpoint_path)
+            os.replace(temporary_location, checkpoint_path)
         finally:
-            if os.path.exists(tmp.name):
-                tmp.close()
+            if os.path.exists(temporary_location):
+                os.remove(temporary_location)
 
     def restore_checkpoint(self, checkpoint_path, ema_checkpoint_path):
         _restore_checkpoint(self, checkpoint_path, ema_checkpoint_path)