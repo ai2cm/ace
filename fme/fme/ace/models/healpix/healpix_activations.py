dad52546c341dde57c94354a7fb15d3a2df4f9b1
Jeremy McGibbon <mcgibbon@uw.edu>
2024-07-26 21:07:23 -0400

    Remove circular dependencies in healpix code (#1036)
    This PR removes circular dependencies in the healpix modules. No
    functional changes.
    
    Changes:
    - Deleted HEALPixBlockConfig which was never used as a target type, only
    inherited from (this is the only code that was deleted instead of moved)
    - Moved configuration classes to the files of the Modules they define
    
    - [ ] Tests added

--- fme/fme/ace/models/healpix/healpix_activations.py
+++ fme/fme/ace/models/healpix/healpix_activations.py
@@ -15,8 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import abc
-from typing import Optional, Tuple, Union
+import dataclasses
+from typing import Literal
 
 import torch as th
 import torch.nn as nn
@@ -102,6 +102,74 @@ class AvgPool(nn.Module):
             The averaged values.
         """
         return self.avgpool(x)
+
+
+@dataclasses.dataclass
+class DownsamplingBlockConfig:
+    """
+    Configuration for the downsampling block.
+    Generally, either a pooling block or a striding conv block.
+
+    Attributes:
+        block_type: Type of recurrent block, either "MaxPool" or "AvgPool"
+        pooling: Pooling size
+        enable_nhwc: Flag to enable NHWC data format, default is False.
+        enable_healpixpad: Flag to enable HEALPix padding, default is False.
+
+    """
+
+    block_type: Literal["MaxPool", "AvgPool"]
+    pooling: int = 2
+    enable_nhwc: bool = False
+    enable_healpixpad: bool = False
+
+    def build(self) -> nn.Module:
+        """
+        Builds the recurrent block model.
+
+        Returns:
+            Recurrent block.
+        """
+        if self.block_type == "MaxPool":
+            return MaxPool(
+                pooling=self.pooling,
+                enable_nhwc=self.enable_nhwc,
+                enable_healpixpad=self.enable_healpixpad,
+            )
+
+        elif self.block_type == "AvgPool":
+            return AvgPool(
+                pooling=self.pooling,
+                enable_nhwc=self.enable_nhwc,
+                enable_healpixpad=self.enable_healpixpad,
+            )
+        else:
+            raise ValueError(f"Unsupported block type: {self.block_type}")
+
+
+@dataclasses.dataclass
+class CappedGELUConfig:
+    """
+    Configuration for the CappedGELU activation function.
+
+    Attributes:
+        cap_value: Cap value for the GELU function, default is 10.
+        enable_nhwc: Flag to enable NHWC data format, default is False.
+        enable_healpixpad: Flag to enable HEALPix padding, default is False.
+    """
+
+    cap_value: int = 10
+    enable_nhwc: bool = False
+    enable_healpixpad: bool = False
+
+    def build(self) -> nn.Module:
+        """
+        Builds the CappedGELU activation function.
+
+        Returns:
+            CappedGELU activation function.
+        """
+        return CappedGELU(cap_value=self.cap_value)
 
 
 class CappedGELU(nn.Module):
