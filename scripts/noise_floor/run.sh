6f38148b49880163c0368be4513cca90955e2e24
Jeremy McGibbon <mcgibbon@uw.edu>
2024-08-07 08:47:52 -0400

    Add noise floor stats generation script and updated configs (#1045)
    This PR adds a `scripts/noise_floor/generate_stats.py` which computes
    mean and stdev datasets for the pattern RMSE, as well as a dataset of
    annual means.
    
    Changes:
    - Added `scripts/noise_floor/generate_stats.py`
    
    - [ ] Tests added

--- scripts/noise_floor/run.sh
+++ scripts/noise_floor/run.sh
@@ -2,6 +2,8 @@
 
 timestamp=$(date +%Y-%m-%d)
 
+# You can comment out this first step if the data is already generated
+python3 generate_stats.py fv3gfs-ensemble-4deg-8layer.yaml
 python3 generate_train_report.py \
     fv3gfs-ensemble-4deg-8layer.yaml \
     $timestamp-climsst-4deg \
