6d24b1220da6ae57cb2f85d6e531b141e53ab9c7
James Duncan <jpdunc23@users.noreply.github.com>
2024-10-15 17:04:33 -0700

    Add CoupledDataset (#1223)
    Adds the `fme.coupled` module and
    `fme.coupled.data_loading.data_typing.CoupledDataset`. `CoupledDataset`
    handles alignment of ocean (slow) and atmosphere (fast) timesteps from
    two data sources in a coupled simulation.
    
    Changes:
    - Adds `CoupledDataLoaderConfig` which together with `CoupledDataConfig`
    defines yaml config parameters for coupled data loading via the
    `get_coupled_data_loader` util.
    - Adds `CoupledGriddedData` implementing the `GriddedDataABC` generic
    with `CoupledBatchData` concrete type.
    - Adds the `get_xarray_dataset` and `validate_ensemble` utils to
    `fme.core.data_loading.getters` which are helpful in the coupled data
    loader getters.
    
    - [x] Tests added

---
+++ fme/fme/coupled/data_loading/data_typing.py
@@ -0,0 +1,114 @@
+import dataclasses
+import datetime
+from typing import Dict, Mapping, Tuple
+
+import xarray as xr
+
+from fme.core.data_loading._xarray import XarrayDataset
+from fme.core.data_loading.data_typing import (
+    Dataset,
+    HorizontalCoordinates,
+    SigmaCoordinates,
+    VariableMetadata,
+)
+from fme.core.typing_ import TensorDict
+
+
+@dataclasses.dataclass
+class CoupledDatasetItem:
+    ocean: Tuple[TensorDict, xr.DataArray]
+    atmosphere: Tuple[TensorDict, xr.DataArray]
+
+
+class CoupledDataset(Dataset):
+    def __init__(
+        self,
+        ocean: XarrayDataset,
+        atmosphere: XarrayDataset,
+        ocean_timestep: datetime.timedelta,
+        atmosphere_timestep: datetime.timedelta,
+    ):
+        self.ocean = ocean
+        self.atmosphere = atmosphere
+        for ts, ds in [(ocean_timestep, ocean), (atmosphere_timestep, atmosphere)]:
+            try:
+                timestep = ds.timestep
+            except ValueError as e:
+                raise ValueError(
+                    f"{str(e)} Timesteps must be inferred for CoupledDataset."
+                )
+            if timestep != ts:
+                raise ValueError(
+                    "Loaded dataset had an unexpected timestep. Expected "
+                    f"{ts} but got {timestep}."
+                )
+
+        if atmosphere_timestep > ocean_timestep:
+            raise ValueError(f"Atmosphere timestep must be no larger than the ocean's.")
+        self.timestep = ocean_timestep
+
+        n_steps_fast = ocean_timestep / atmosphere_timestep
+        if n_steps_fast != int(n_steps_fast):
+            raise ValueError(
+                f"Expected atmosphere timestep {atmosphere_timestep} to be a multiple "
+                f"of ocean timestep {ocean_timestep}."
+            )
+        self._n_steps_fast = int(n_steps_fast)
+
+        # check for misconfigured DataRequirements n_timesteps in the atmosphere
+        slow_n_steps = self.ocean.n_steps
+        fast_n_steps = (slow_n_steps - 1) * self._n_steps_fast + 1
+        if self.atmosphere.n_steps != fast_n_steps:
+            raise ValueError(
+                f"Atmosphere dataset timestep is {atmosphere_timestep} and "
+                f"ocean dataset timestep is {ocean_timestep}, "
+                f"so we need {self._n_steps_fast} atmosphere steps for each of the "
+                f"{slow_n_steps - 1} ocean steps, giving {fast_n_steps} total "
+                "timepoints (including IC) per sample, but atmosphere dataset "
+                f"was configured to return {self.atmosphere.n_steps} steps."
+            )
+
+        metadata: Dict[str, VariableMetadata] = {}
+        for ds in [ocean, atmosphere]:
+            metadata.update(ds.metadata)
+        self._is_remote = any(ds.is_remote for ds in [ocean, atmosphere])
+        self._metadata = metadata
+        self._sigma_coordinates = atmosphere.sigma_coordinates
+        self._horizontal_coordinates = atmosphere.horizontal_coordinates
+
+    @property
+    def n_forward_steps(self) -> int:
+        return self.ocean.n_steps - 1
+
+    @property
+    def metadata(self) -> Mapping[str, VariableMetadata]:
+        return self._metadata
+
+    @property
+    def sigma_coordinates(self) -> SigmaCoordinates:
+        return self._sigma_coordinates
+
+    @property
+    def horizontal_coordinates(self) -> HorizontalCoordinates:
+        return self._horizontal_coordinates
+
+    @property
+    def is_remote(self) -> bool:
+        return self._is_remote
+
+    def get_sample_by_time_slice(
+        self, time_slice: slice
+    ) -> Tuple[TensorDict, xr.DataArray]:
+        raise NotImplementedError(
+            "CoupledDataset has no get_sample_by_time_slice method"
+        )
+
+    def __len__(self):
+        return min([len(self.ocean), len(self.atmosphere)])
+
+    def __getitem__(self, idx: int) -> CoupledDatasetItem:
+        fast_idx = idx * self._n_steps_fast
+        ocean = self.ocean[idx]
+        atmosphere = self.atmosphere[fast_idx]
+        assert atmosphere[1].isel(time=0) == ocean[1].isel(time=0)
+        return CoupledDatasetItem(ocean=ocean, atmosphere=atmosphere)
---
+++ fme/fme/coupled/data_loading/test_data_loader.py
@@ -0,0 +1,169 @@
+import dataclasses
+import datetime
+import pathlib
+from typing import List
+
+import cftime
+import numpy as np
+import xarray as xr
+
+from fme.core.data_loading.batch_data import BatchData
+from fme.core.data_loading.config import XarrayDataConfig
+from fme.core.data_loading.requirements import DataRequirements
+from fme.core.data_loading.test_data_loader import _save_netcdf
+
+from .config import CoupledDataConfig, CoupledDataLoaderConfig
+from .getters import get_coupled_data_loader
+from .requirements import CoupledDataRequirements
+
+
+@dataclasses.dataclass
+class _MockCoupledData:
+    ocean: xr.Dataset
+    atmosphere: xr.Dataset
+    ocean_dir: str
+    atmosphere_dir: str
+
+    def __post_init__(self):
+        self.ocean["time"] = cftime.num2date(
+            self.ocean["time"].values,
+            units=self.ocean["time"].units,
+            calendar=self.ocean["time"].calendar,
+        )
+        self.atmosphere["time"] = cftime.num2date(
+            self.atmosphere["time"].values,
+            units=self.atmosphere["time"].units,
+            calendar=self.atmosphere["time"].calendar,
+        )
+
+
+def _create_coupled_data_on_disk(
+    data_dir: pathlib.Path,
+    n_forward_times_ocean: int,
+    n_forward_times_atmosphere: int,
+    ocean_names: List[str],
+    atmosphere_names: List[str],
+) -> _MockCoupledData:
+    np.random.seed(0)
+
+    ocean_dir = data_dir / "ocean"
+    ocean_dir.mkdir()
+    ocean_dim_sizes = {"time": n_forward_times_ocean + 1, "lat": 4, "lon": 8}
+    ocean_timestep_size = n_forward_times_atmosphere / n_forward_times_ocean
+    if ocean_timestep_size != int(ocean_timestep_size):
+        raise ValueError(
+            "n_forward_times_atmosphere should be a multiple of n_forward_times_ocean."
+        )
+    ocean_timestep_size = int(ocean_timestep_size)
+    ocean_ds = _save_netcdf(
+        filename=ocean_dir / "data.nc",
+        dim_sizes=ocean_dim_sizes,
+        variable_names=ocean_names,
+        calendar="proleptic_gregorian",
+        # _save_netcdf has a default timestep of 1 day which we interpret as the
+        # atmosphere timestep, so the ocean data needs
+        timestep_size=ocean_timestep_size,
+    )
+
+    atmos_dir = data_dir / "atmos"
+    atmos_dir.mkdir()
+    atmos_dim_sizes = {"time": n_forward_times_atmosphere + 1, "lat": 4, "lon": 8}
+    atmos_ds = _save_netcdf(
+        filename=atmos_dir / "data.nc",
+        dim_sizes=atmos_dim_sizes,
+        variable_names=atmosphere_names,
+        calendar="proleptic_gregorian",
+        timestep_size=1,
+    )
+
+    return _MockCoupledData(
+        ocean=ocean_ds,
+        atmosphere=atmos_ds,
+        ocean_dir=str(ocean_dir),
+        atmosphere_dir=str(atmos_dir),
+    )
+
+
+def test_coupled_data_loader(tmp_path):
+    """Tests that the coupled loader returns the correct number of timesteps."""
+
+    # Create datasets with fast and slow timesteps.
+    ocean_names = ["bar"]
+    atmos_names = ["foo"]
+
+    n_ics = 2
+    ics = []
+    for i in range(n_ics):
+        # create dataset with 2 samples
+        ic_path = tmp_path / f"ic{i}"
+        ic_path.mkdir()
+        ic = _create_coupled_data_on_disk(
+            ic_path,
+            n_forward_times_ocean=2,
+            n_forward_times_atmosphere=4,
+            ocean_names=ocean_names,
+            atmosphere_names=atmos_names,
+        )
+        ics.append(ic)
+
+    config = CoupledDataLoaderConfig(
+        dataset=[
+            CoupledDataConfig(
+                ocean=XarrayDataConfig(data_path=ics[i].ocean_dir),
+                atmosphere=XarrayDataConfig(data_path=ics[i].atmosphere_dir),
+            )
+            for i in range(n_ics)
+        ],
+        batch_size=1,
+        num_data_workers=0,
+        strict_ensemble=True,
+    )
+    coupled_requirements = CoupledDataRequirements(
+        ocean_timestep=datetime.timedelta(days=2),
+        ocean_requirements=DataRequirements(ocean_names, n_timesteps=2),
+        atmosphere_timestep=datetime.timedelta(days=1),
+        atmosphere_requirements=DataRequirements(atmos_names, n_timesteps=3),
+    )
+    # unshuffled data loader
+    data = get_coupled_data_loader(config, False, coupled_requirements)
+
+    assert data.n_forward_steps == 1
+    assert data.n_batches == 2 * n_ics  # 2 samples per IC
+    for batch in data.loader:
+        ocean_data = batch.ocean_data
+        atmosphere_data = batch.atmosphere_data
+        assert isinstance(ocean_data, BatchData)
+        assert isinstance(atmosphere_data, BatchData)
+        assert len(ocean_data.times.isel(sample=0).values) == 2  # IC + 1 forward step
+        assert (
+            len(atmosphere_data.times.isel(sample=0).values) == 3
+        )  # IC + 2 forward steps
+        assert set(ocean_data.data.keys()) == set(ocean_names)
+        assert set(atmosphere_data.data.keys()) == set(atmos_names)
+        # initial condition times should match:
+        assert ocean_data.times.isel(time=0) == atmosphere_data.times.isel(time=0)
+        # final step times should match:
+        assert ocean_data.times.isel(time=-1) == atmosphere_data.times.isel(time=-1)
+        # check data matches expectations
+        assert ocean_data.data["bar"].shape[1] == 2
+        assert atmosphere_data.data["foo"].shape[1] == 3
+
+    # check that the sample data matches
+    ic_idx = 0
+    sample_idx = 1
+    ocean_ds = ics[ic_idx].ocean
+    atmos_ds = ics[ic_idx].atmosphere
+    sample = data._loader.dataset[sample_idx]
+    ocean_sample_init_time = sample.ocean[1].isel(time=0).item()
+    atmos_sample_init_time = sample.atmosphere[1].isel(time=0).item()
+
+    # we already checked for matching ocean/atmos sample init times above, now
+    # checking that they match times at the right positions in each dataset
+    assert ocean_sample_init_time == ocean_ds.isel(time=1)["time"].item()
+    assert atmos_sample_init_time == atmos_ds.isel(time=2)["time"].item()
+
+    expected_ocean = ocean_ds["bar"].isel(time=slice(1, 3)).values
+    expected_atmos = atmos_ds["foo"].isel(time=slice(2, 5)).values
+    # check that
+    assert np.allclose(sample.ocean[0]["bar"].cpu().numpy(), expected_ocean)
+    assert np.allclose(sample.atmosphere[0]["foo"].cpu().numpy(), expected_atmos)
