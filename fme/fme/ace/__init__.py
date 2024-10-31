ai2cm/ace

commit 125f6f807525cf6c075e800c622e1576876ba91c

Filter if missing required data for normalization (#1292)
Change implementation of normalization to filter out tensors which don't
have corresponding means/stds. Currently the non-normalized data is just
passed through.

API changes:
- remove `exclude_names` from `NormalizationConfig`
- filter out tensors which don't have available normalization statistics
when normalizing or denormalizing
- add `channel_mean_names` argument to relevant aggregator builders. If
not provided, aggregator will attempt to normalize all variables.

Resolves #1286

fme/fme/core/normalizer.py
@@ -21,14 +21,12 @@ class NormalizationConfig:
     Attributes:
         global_means_path: Path to a netCDF file containing global means.
         global_stds_path: Path to a netCDF file containing global stds.
-        exclude_names: Names to exclude from normalization.
         means: Mapping from variable names to means.
         stds: Mapping from variable names to stds.
     """
 
     global_means_path: Optional[str] = None
     global_stds_path: Optional[str] = None
-    exclude_names: Optional[List[str]] = None
     means: Mapping[str, float] = dataclasses.field(default_factory=dict)
     stds: Mapping[str, float] = dataclasses.field(default_factory=dict)
 
@@ -49,8 +47,6 @@ class NormalizationConfig:
             )
 
     def build(self, names: List[str]):
-        if self.exclude_names is not None:
-            names = list(set(names) - set(self.exclude_names))
         using_path = (
             self.global_means_path is not None and self.global_stds_path is not None
         )
@@ -96,12 +92,15 @@ class StandardNormalizer:
     ):
         self.means = move_tensordict_to_device(means)
         self.stds = move_tensordict_to_device(stds)
+        self._names = set(means).intersection(stds)
 
     def normalize(self, tensors: TensorMapping) -> TensorDict:
-        return _normalize(dict(tensors), means=self.means, stds=self.stds)
+        filtered_tensors = {k: v for k, v in tensors.items() if k in self._names}
+        return _normalize(filtered_tensors, means=self.means, stds=self.stds)
 
     def denormalize(self, tensors: TensorMapping) -> TensorDict:
-        return _denormalize(dict(tensors), means=self.means, stds=self.stds)
+        filtered_tensors = {k: v for k, v in tensors.items() if k in self._names}
+        return _denormalize(filtered_tensors, means=self.means, stds=self.stds)
 
     def get_state(self):
         """
@@ -130,10 +129,7 @@ def _normalize(
     means: TensorDict,
     stds: TensorDict,
 ) -> TensorDict:
-    return {
-        k: (t - means[k]) / stds[k] if k in means.keys() else t
-        for k, t in tensors.items()
-    }
+    return {k: (t - means[k]) / stds[k] for k, t in tensors.items()}
 
 
 @torch.jit.script
@@ -142,10 +138,7 @@ def _denormalize(
     means: TensorDict,
     stds: TensorDict,
 ) -> TensorDict:
-    return {
-        k: t * stds[k] + means[k] if k in means.keys() else t
-        for k, t in tensors.items()
-    }
+    return {k: t * stds[k] + means[k] for k, t in tensors.items()}
 
 
 def get_normalizer(

fme/fme/ace/inference/test_evaluator.py
@@ -699,9 +699,11 @@ def test_derived_metrics_run_without_errors(tmp_path: pathlib.Path):
         yaml.dump(dataclasses.asdict(config), f)
 
     with mock_wandb() as _:
-        _ = main(
-            yaml_config=str(config_filename),
-        )
+        inference_logs = main(yaml_config=str(config_filename))
+
+    # derived variables should not have normalized metrics reported
+    assert "inference/mean_norm/weighted_rmse/total_water_path" not in inference_logs[0]
+    assert "inference/time_mean_norm/rmse/total_water_path" not in inference_logs[-1]
 
 
 @pytest.mark.parametrize(

fme/fme/ace/inference/evaluator.py
@@ -222,6 +222,7 @@ def run_evaluator_from_config(config: InferenceEvaluatorConfig):
         n_timesteps=config.n_forward_steps + 1,
         metadata=data.metadata,
         initial_times=initial_times,
+        channel_mean_names=stepper.out_names,
     )
 
     writer = config.get_data_writer(data, stepper.prognostic_names)

fme/fme/ace/train/train.py
@@ -58,7 +58,7 @@ import os
 import time
 import uuid
 from datetime import timedelta
-from typing import Dict, Generic, Mapping, Optional, TypeVar
+from typing import Dict, Generic, Mapping, Optional, Sequence, TypeVar
 
 import dacite
 import dask
@@ -156,6 +156,7 @@ def build_trainer(builder: TrainBuildersABC, config: TrainConfigProtocol) -> "Tr
         n_timesteps=config.inference_n_forward_steps + 1,
         metadata=train_data.metadata,
         loss_scaling=stepper.effective_loss_scaling,
+        channel_mean_names=stepper.out_names,
     )
     return Trainer(
         train_data=train_data,
@@ -228,6 +229,7 @@ class AggregatorBuilder(AggregatorBuilderABC[BatchData, TrainOutput]):
         n_timesteps: int,
         metadata: Optional[Mapping[str, VariableMetadata]] = None,
         loss_scaling: Optional[Dict[str, torch.Tensor]] = None,
+        channel_mean_names: Optional[Sequence[str]] = None,
     ):
         self.inference_config = inference_config
         self.gridded_operations = gridded_operations
@@ -239,6 +241,7 @@ class AggregatorBuilder(AggregatorBuilderABC[BatchData, TrainOutput]):
         self.n_timesteps = n_timesteps
         self.metadata = metadata
         self.loss_scaling = loss_scaling
+        self.channel_mean_names = channel_mean_names
 
     def get_train_aggregator(self) -> TrainAggregator:
         return TrainAggregator()
@@ -262,6 +265,7 @@ class AggregatorBuilder(AggregatorBuilderABC[BatchData, TrainOutput]):
             record_step_20=self.record_step_20,
             n_timesteps=self.n_timesteps,
             metadata=self.metadata,
+            channel_mean_names=self.channel_mean_names,
         )
 
 

fme/fme/core/aggregator/inference/main.py
@@ -1,7 +1,17 @@
 import dataclasses
 import datetime
 import warnings
-from typing import Callable, Dict, Iterable, List, Mapping, Optional, Protocol, Union
+from typing import (
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Mapping,
+    Optional,
+    Protocol,
+    Sequence,
+    Union,
+)
 
 import torch
 import xarray as xr
@@ -151,6 +161,7 @@ class InferenceEvaluatorAggregatorConfig:
         initial_times: xr.DataArray,
         record_step_20: bool = False,
         metadata: Optional[Mapping[str, VariableMetadata]] = None,
+        channel_mean_names: Optional[Sequence[str]] = None,
     ) -> "InferenceEvaluatorAggregator":
         if self.monthly_reference_data is None:
             monthly_reference_data = None
@@ -190,6 +201,7 @@ class InferenceEvaluatorAggregatorConfig:
             time_mean_reference_data=time_mean,
             record_step_20=record_step_20,
             metadata=metadata,
+            channel_mean_names=channel_mean_names,
         )
 
 
@@ -219,6 +231,7 @@ class InferenceEvaluatorAggregator(InferenceEvaluatorAggregatorABC[BatchData]):
         monthly_reference_data: Optional[xr.Dataset] = None,
         log_histograms: bool = False,
         time_mean_reference_data: Optional[xr.Dataset] = None,
+        channel_mean_names: Optional[Sequence[str]] = None,
     ):
         """
         Args:
@@ -243,7 +256,10 @@ class InferenceEvaluatorAggregator(InferenceEvaluatorAggregatorABC[BatchData]):
             log_histograms: Whether to aggregate histograms.
             data_grid: The grid type of the data, used for spherical power spectrum.
             time_mean_reference_data: Reference time means for computing bias stats.
+            channel_mean_names: Names over which to compute channel means. If not
+                provided, all available variables will be used.
         """
+        self._channel_mean_names = channel_mean_names
         self._aggregators: Dict[str, _EvaluatorAggregator] = {}
         self._time_dependent_aggregators: Dict[
             str, _TimeDependentEvaluatorAggregator
@@ -343,9 +359,7 @@ class InferenceEvaluatorAggregator(InferenceEvaluatorAggregatorABC[BatchData]):
         if len(target.data) == 0:
             raise ValueError("No data in target.data")
         target_data = {k: v for k, v in target.data.items() if k in prediction.data}
-        target_data_norm = {
-            k: v for k, v in normalize(target.data).items() if k in prediction.data
-        }
+        target_data_norm = normalize(target_data)
         gen_data_norm = normalize(prediction.data)
         for aggregator in self._aggregators.values():
             aggregator.record_batch(

fme/fme/core/aggregator/inference/test_time_mean.py
@@ -37,6 +37,35 @@ def test_rmse_of_time_mean_all_channels():
     assert logs["time_mean_norm/rmse/channel_mean"] == 1.5
 
 
+def test_custom_channel_mean_names():
+    torch.manual_seed(0)
+    area_weights = torch.ones(1).to(get_device())
+    agg = TimeMeanEvaluatorAggregator(
+        LatLonOperations(area_weights),
+        horizontal_dims=["lat", "lon"],
+        target="norm",
+        channel_mean_names=["a"],
+    )
+    target_data_norm = {
+        "a": torch.ones([2, 3, 4, 4], device=get_device()),
+        "b": torch.ones([2, 3, 4, 4], device=get_device()) * 3,
+    }
+    gen_data_norm = {
+        "a": torch.ones([2, 3, 4, 4], device=get_device()) * 2.0,
+        "b": torch.ones([2, 3, 4, 4], device=get_device()) * 5,
+    }
+    agg.record_batch(
+        target_data=target_data_norm,
+        gen_data=gen_data_norm,
+        target_data_norm=target_data_norm,
+        gen_data_norm=gen_data_norm,
+    )
+    logs = agg.get_logs(label="time_mean_norm")
+    assert logs["time_mean_norm/rmse/a"] == 1.0
+    assert logs["time_mean_norm/rmse/b"] == 2.0
+    assert logs["time_mean_norm/rmse/channel_mean"] == 1.0
+
+
 def test_mean_all_channels_not_in_denorm():
     area_weights = torch.ones(1).to(get_device())
     agg = TimeMeanEvaluatorAggregator(

fme/fme/core/aggregator/inference/time_mean.py
@@ -238,6 +238,7 @@ class TimeMeanEvaluatorAggregator:
         target: Literal["norm", "denorm"] = "denorm",
         metadata: Optional[Mapping[str, VariableMetadata]] = None,
         reference_means: Optional[xr.Dataset] = None,
+        channel_mean_names: Optional[List[str]] = None,
     ):
         """
         Args:
@@ -249,6 +250,8 @@ class TimeMeanEvaluatorAggregator:
                 used in generating logged image captions.
             reference_means: Dataset containing reference time-mean values
                 for bias computation.
+            channel_mean_names: Names of variables whose RMSE will be averaged. If
+                not provided, all available variables will be used.
         """
         self._ops = ops
         self._horizontal_dims = horizontal_dims
@@ -268,6 +271,7 @@ class TimeMeanEvaluatorAggregator:
             metadata=metadata,
             reference_means=reference_means,
         )
+        self._channel_mean_names = channel_mean_names
 
     @torch.no_grad()
     def record_batch(
@@ -330,12 +334,14 @@ class TimeMeanEvaluatorAggregator:
                     }
                 )
         if self._target == "norm":
-            logs.update(
-                {
-                    f"rmse/channel_mean": sum(rmse_all_channels.values())
-                    / len(rmse_all_channels),
-                }
-            )
+            metric_name = "rmse/channel_mean"
+            if self._channel_mean_names is None:
+                values_to_average = list(rmse_all_channels.values())
+            else:
+                values_to_average = [
+                    rmse_all_channels[name] for name in self._channel_mean_names
+                ]
+            logs.update({metric_name: sum(values_to_average) / len(values_to_average)})
 
         if len(label) != 0:
             return {f"{label}/{key}": logs[key] for key in logs}

fme/fme/core/test_normalizer.py
@@ -3,7 +3,6 @@ import torch
 
 from fme.core.device import move_tensordict_to_device
 from fme.core.normalizer import NormalizationConfig, StandardNormalizer
-from fme.core.typing_ import TensorDict
 
 
 def test_normalize_depends_on_mean():
@@ -58,24 +57,7 @@ def test_normalize_and_denormalize_random_tensor():
     assert torch.allclose(denormalized["b"], tensors["b"])
 
 
-def test_normalization_config_exclude_names():
-    torch.manual_seed(0)
-    normalization = NormalizationConfig(
-        means={"a": 1.0, "b": 2.0},
-        stds={"a": 1.0, "b": 1.0},
-        exclude_names=["c"],
-    )
-    normalizer = normalization.build(["a", "b", "c"])
-    tensors = move_tensordict_to_device(
-        {"a": torch.randn(10), "b": torch.randn(10), "c": torch.randn(10)}
-    )
-    normalized: TensorDict = normalizer.normalize(tensors)
-    denormalized = normalizer.denormalize(normalized)
-    assert torch.all(normalized["c"] == tensors["c"])
-    assert torch.all(denormalized["c"] == tensors["c"])
-
-
-def test_missing_normalization_raises_error():
+def test_missing_normalization_build_raises_error():
     normalization = NormalizationConfig(
         means={"a": 1.0, "b": 2.0},
         stds={"a": 1.0, "b": 1.0},
@@ -83,3 +65,18 @@ def test_missing_normalization_raises_error():
     all_names = ["a", "b", "c"]
     with pytest.raises(KeyError):
         normalization.build(all_names)
+
+
+def test_tensors_with_missing_normalization_stats_get_filtered():
+    normalization = NormalizationConfig(
+        means={"a": 1.0, "b": 2.0},
+        stds={"a": 1.0, "b": 1.0},
+    ).build(["a", "b"])
+    sample_input = {"a": torch.zeros(1), "b": torch.zeros(1), "c": torch.zeros(1)}
+    sample_input = move_tensordict_to_device(sample_input)
+
+    normalized = normalization.normalize(sample_input)
+    assert "c" not in normalized
+
+    denormalized = normalization.denormalize(sample_input)
+    assert "c" not in denormalized

commit 11ac74bbceba77dec319da4a74f7b9215281b9d7

Delete `FromStateNormalizer` (#1303)
This class is not actually needed in our code, since
`NormalizationConfig` already directly provides this functionality.
Deleting it makes our code and the stepper configuration simpler to
understand.

This PR also fixes an issue introduced in #1292 where deserializing an
old checkpoint's `SingleModuleStepperConfig` failed due to backwards
incompatibility. Note we don't explicitly have a unit test for config
backwards compatibility, just for the stepper itself.

I verified that with the latest commit on this branch, I load the
original ACE checkpoint:
https://beaker.org/ex/01JBHZ0EWTRP8M0P36EB0SPEZ9/tasks/01JBHZ0EX02461HSS7BQ4JD3SY/job/01JBHZ0F1ZASNVR237AW3S0MAR.
Inference fails later on, but this is for unrelated reasons (see #1308).

Resolves #1086

fme/fme/ace/__init__.py
@@ -42,7 +42,7 @@ from fme.core.data_loading.perturbation import (
 )
 from fme.core.gridded_ops import GriddedOperations
 from fme.core.loss import WeightedMappingLossConfig
-from fme.core.normalizer import FromStateNormalizer, NormalizationConfig
+from fme.core.normalizer import NormalizationConfig
 from fme.core.ocean import SlabOceanConfig
 from fme.core.optimization import SchedulerConfig
 from fme.core.parameter_init import FrozenParameterConfig, ParameterInitializationConfig

fme/fme/core/normalizer.py
@@ -62,24 +62,6 @@ class NormalizationConfig:
             return StandardNormalizer(means=means, stds=stds)
 
 
-@dataclasses.dataclass
-class FromStateNormalizer:
-    """
-    An alternative to NormalizationConfig which provides a normalizer
-    initialized from a serializable state. This is not a public configuration
-    class, but instead allows for loading trained models that have been
-    serialized to disk, using the pre-existing normalization state.
-
-    Attributes:
-        state: State dict of a normalizer.
-    """
-
-    state: Dict[str, Dict[str, float]]
-
-    def build(self, names: List[str]):
-        return StandardNormalizer.from_state(self.state)
-
-
 class StandardNormalizer:
     """
     Responsible for normalizing tensors.

fme/fme/core/stepper.py
@@ -13,7 +13,6 @@ from typing import (
     Optional,
     Tuple,
     TypeVar,
-    Union,
 )
 
 import dacite
@@ -32,7 +31,6 @@ from fme.core.generics.optimization import OptimizationABC
 from fme.core.gridded_ops import GriddedOperations, LatLonOperations
 from fme.core.loss import WeightedMappingLossConfig
 from fme.core.normalizer import (
-    FromStateNormalizer,
     NormalizationConfig,
     StandardNormalizer,
 )
@@ -71,7 +69,7 @@ class SingleModuleStepperConfig:
     builder: ModuleSelector
     in_names: List[str]
     out_names: List[str]
-    normalization: Union[NormalizationConfig, FromStateNormalizer]
+    normalization: NormalizationConfig
     parameter_init: ParameterInitializationConfig = dataclasses.field(
         default_factory=lambda: ParameterInitializationConfig()
     )
@@ -83,10 +81,8 @@ class SingleModuleStepperConfig:
         default_factory=lambda: CorrectorConfig()
     )
     next_step_forcing_names: List[str] = dataclasses.field(default_factory=list)
-    loss_normalization: Optional[Union[NormalizationConfig, FromStateNormalizer]] = None
-    residual_normalization: Optional[
-        Union[NormalizationConfig, FromStateNormalizer]
-    ] = None
+    loss_normalization: Optional[NormalizationConfig] = None
+    residual_normalization: Optional[NormalizationConfig] = None
 
     def __post_init__(self):
         for name in self.next_step_forcing_names:
@@ -212,6 +208,15 @@ class SingleModuleStepperConfig:
                         "SingleModuleStepper being loaded from state cannot be run by "
                         "this version of the code."
                     )
+        if "normalization" in state_copy:
+            if "exclude_names" in state_copy["normalization"]:
+                if state_copy["normalization"]["exclude_names"] is not None:
+                    raise ValueError(
+                        "The exclude_names option in normalization config is no longer "
+                        "supported."
+                    )
+                else:
+                    del state_copy["normalization"]["exclude_names"]
         if "prescriber" in state_copy:
             # want to maintain backwards compatibility for this particular feature
             if state_copy["prescriber"] is not None:
@@ -792,12 +797,13 @@ class SingleModuleStepper(StepperABC[BatchData, TrainOutput]):
             The stepper.
         """
         config = {**state["config"]}  # make a copy to avoid mutating input
-        config["normalization"] = FromStateNormalizer(state["normalizer"])
+        config["normalization"] = state["normalizer"]
 
         # for backwards compatibility with previous steppers created w/o
         # loss_normalization or residual_normalization
         loss_normalizer_state = state.get("loss_normalizer", state["normalizer"])
-        config["loss_normalization"] = FromStateNormalizer(loss_normalizer_state)
+        config["loss_normalization"] = loss_normalizer_state
+
         # Overwrite the residual_normalization key if it exists, since the combined
         # loss scalings are saved in initial training as the loss_normalization
         config["residual_normalization"] = None

fme/fme/ace/inference/test_evaluator.py
@@ -25,7 +25,7 @@ from fme.core.data_loading.inference import (
 from fme.core.device import get_device
 from fme.core.gridded_ops import LatLonOperations
 from fme.core.logging_utils import LoggingConfig
-from fme.core.normalizer import FromStateNormalizer
+from fme.core.normalizer import NormalizationConfig
 from fme.core.ocean import Ocean, OceanConfig
 from fme.core.stepper import (
     SingleModuleStepperConfig,
@@ -60,11 +60,9 @@ def save_plus_one_stepper(
         builder=ModuleSelector(type="prebuilt", config={"module": PlusOne()}),
         in_names=in_names,
         out_names=out_names,
-        normalization=FromStateNormalizer(
-            state={
-                "means": {name: mean for name in all_names},
-                "stds": {name: std for name in all_names},
-            }
+        normalization=NormalizationConfig(
+            means={name: mean for name in all_names},
+            stds={name: std for name in all_names},
         ),
     )
     area = torch.ones(data_shape[-2:], device=get_device())

fme/fme/ace/inference/test_inference.py
@@ -34,7 +34,7 @@ from fme.core.data_loading.inference import (
 )
 from fme.core.gridded_ops import LatLonOperations
 from fme.core.logging_utils import LoggingConfig
-from fme.core.normalizer import FromStateNormalizer
+from fme.core.normalizer import NormalizationConfig
 from fme.core.stepper import SingleModuleStepperConfig
 from fme.core.testing import DimSizes, FV3GFSData, mock_wandb
 
@@ -60,11 +60,9 @@ def save_stepper(
         builder=ModuleSelector(type="prebuilt", config={"module": PlusOne()}),
         in_names=in_names,
         out_names=out_names,
-        normalization=FromStateNormalizer(
-            state={
-                "means": {name: mean for name in all_names},
-                "stds": {name: std for name in all_names},
-            }
+        normalization=NormalizationConfig(
+            means={name: mean for name in all_names},
+            stds={name: std for name in all_names},
         ),
     )
     area = torch.ones(data_shape[-2:], device=fme.get_device())

fme/fme/ace/inference/test_segmented.py
@@ -28,7 +28,7 @@ from fme.core.data_loading.data_typing import DimSize, SigmaCoordinates
 from fme.core.data_loading.inference import ForcingDataLoaderConfig, TimestampList
 from fme.core.gridded_ops import LatLonOperations
 from fme.core.logging_utils import LoggingConfig
-from fme.core.normalizer import FromStateNormalizer
+from fme.core.normalizer import NormalizationConfig
 from fme.core.stepper import SingleModuleStepperConfig
 from fme.core.testing import DimSizes, FV3GFSData
 
@@ -54,11 +54,9 @@ def save_stepper(
         builder=ModuleSelector(type="prebuilt", config={"module": PlusOne()}),
         in_names=in_names,
         out_names=out_names,
-        normalization=FromStateNormalizer(
-            state={
-                "means": {name: mean for name in all_names},
-                "stds": {name: std for name in all_names},
-            }
+        normalization=NormalizationConfig(
+            means={name: mean for name in all_names},
+            stds={name: std for name in all_names},
         ),
     )
     area = torch.ones(data_shape[-2:], device=fme.get_device())

fme/fme/ace/registry/test_hpx.py
@@ -19,7 +19,7 @@ from fme.ace.registry.hpx import UNetDecoderConfig, UNetEncoderConfig
 from fme.core.data_loading.data_typing import SigmaCoordinates
 from fme.core.device import get_device
 from fme.core.gridded_ops import LatLonOperations
-from fme.core.normalizer import FromStateNormalizer
+from fme.core.normalizer import NormalizationConfig
 from fme.core.stepper import SingleModuleStepperConfig
 
 TIMESTEP = datetime.timedelta(hours=6)
@@ -189,11 +189,9 @@ def test_hpx_init(shape):
         "in_names": ["x"],
         "out_names": ["x"],
         "normalization": dataclasses.asdict(
-            FromStateNormalizer(
-                state={
-                    "means": {"x": float(np.random.randn(1).item())},
-                    "stds": {"x": float(np.random.randn(1).item())},
-                }
+            NormalizationConfig(
+                means={"x": float(np.random.randn(1).item())},
+                stds={"x": float(np.random.randn(1).item())},
             )
         ),
     }

fme/fme/ace/registry/test_sfno.py
@@ -8,7 +8,7 @@ import torch
 from fme.core.data_loading.data_typing import SigmaCoordinates
 from fme.core.device import get_device
 from fme.core.gridded_ops import LatLonOperations
-from fme.core.normalizer import FromStateNormalizer
+from fme.core.normalizer import NormalizationConfig
 from fme.core.stepper import SingleModuleStepperConfig
 
 TIMESTEP = datetime.timedelta(hours=6)
@@ -35,11 +35,9 @@ def test_sfno_init(shape):
         "in_names": ["x"],
         "out_names": ["x"],
         "normalization": dataclasses.asdict(
-            FromStateNormalizer(
-                state={
-                    "means": {"x": float(np.random.randn(1).item())},
-                    "stds": {"x": float(np.random.randn(1).item())},
-                }
+            NormalizationConfig(
+                means={"x": float(np.random.randn(1).item())},
+                stds={"x": float(np.random.randn(1).item())},
             )
         ),
     }

fme/fme/core/test_parameter_init.py
@@ -13,7 +13,6 @@ from fme.core import parameter_init
 from fme.core.data_loading.data_typing import SigmaCoordinates
 from fme.core.device import get_device
 from fme.core.gridded_ops import LatLonOperations
-from fme.core.normalizer import FromStateNormalizer
 from fme.core.stepper import SingleModuleStepper, SingleModuleStepperConfig
 from fme.core.typing_ import TensorMapping
 from fme.core.wildcard import wildcard_match
@@ -33,12 +32,10 @@ def test_builder_with_weights_loads_same_state(tmpdir):
         "builder": sfno_config_data,
         "in_names": ["x"],
         "out_names": ["x"],
-        "normalization": FromStateNormalizer(
-            state={
-                "means": {"x": np.random.randn(1)},
-                "stds": {"x": np.random.randn(1)},
-            }
-        ),
+        "normalization": {
+            "means": {"x": np.random.randn(1).item()},
+            "stds": {"x": np.random.randn(1).item()},
+        },
     }
     area = torch.ones((1, 16, 32)).to(get_device())
     sigma_coordinates = SigmaCoordinates(ak=torch.arange(7), bk=torch.arange(7)).to(
@@ -65,12 +62,10 @@ def test_builder_with_weights_loads_same_state(tmpdir):
         "parameter_init": parameter_init_config,
         "in_names": ["x"],
         "out_names": ["x"],
-        "normalization": FromStateNormalizer(
-            state={
-                "means": {"x": np.random.randn(1)},
-                "stds": {"x": np.random.randn(1)},
-            }
-        ),
+        "normalization": {
+            "means": {"x": np.random.randn(1).item()},
+            "stds": {"x": np.random.randn(1).item()},
+        },
     }
     with_builder_stepper = SingleModuleStepperConfig.from_state(
         with_builder_stepper_config_data
@@ -204,12 +199,10 @@ def get_config(
         "builder": sfno_config_data,
         "in_names": ["x"],
         "out_names": ["x"],
-        "normalization": FromStateNormalizer(
-            state={
-                "means": {"x": np.random.randn(1)},
-                "stds": {"x": np.random.randn(1)},
-            }
-        ),
+        "normalization": {
+            "means": {"x": np.random.randn(1).item()},
+            "stds": {"x": np.random.randn(1).item()},
+        },
     }
     area = torch.ones((1, 16, 32)).to(get_device())
     sigma_coordinates = SigmaCoordinates(ak=torch.arange(7), bk=torch.arange(7)).to(
@@ -239,12 +232,10 @@ def get_config(
         "parameter_init": parameter_init_config,
         "in_names": ["x"],
         "out_names": ["x"],
-        "normalization": FromStateNormalizer(
-            state={
-                "means": {"x": np.random.randn(1)},
-                "stds": {"x": np.random.randn(1)},
-            }
-        ),
+        "normalization": {
+            "means": {"x": np.random.randn(1).item()},
+            "stds": {"x": np.random.randn(1).item()},
+        },
     }
     return with_builder_stepper_config_data, area, sigma_coordinates, stepper
 