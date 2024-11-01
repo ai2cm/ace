aa1ea1ec019f8c14355f201e2cf4824ef841ca9b
Jeremy McGibbon <mcgibbon@uw.edu>
2024-11-01 10:37:20 -0400

    Update evaluator aggregator to use PairedData type (#1306)
    This PR updates the evaluator aggregator to use PairedData like the data
    writers. InferenceEvaluatorAggregatorABC is removed because its API is
    now identical to InferenceAggregatorABC (just with a different generic
    type).
    
    Making the writer and aggregator APIs more uniform and removing the
    separate ABC for evaluation will simplify future refactors. Also, the
    update ensures the predicted and target data passed to the aggregator
    will have the same time axis.
    
    Changes:
    - Removed InferenceEvaluatorAggregatorABC
    - Updated InferenceEvaluatorAggregator to take in PairedData instead of
    two separate arguments, and inherit from
    InferenceAggregatorABC[PairedData].
    
    - [ ] Tests added

--- fme/fme/core/generics/aggregator.py
+++ fme/fme/core/generics/aggregator.py
@@ -16,38 +16,11 @@ class AggregatorABC(abc.ABC, Generic[T]):
         pass
 
 
-class InferenceEvaluatorAggregatorABC(abc.ABC, Generic[T]):
-    @abc.abstractmethod
-    def record_batch(
-        self,
-        prediction: T,
-        target: T,
-        normalize: Callable[[TensorMapping], TensorDict],
-        i_time_start: int,
-    ) -> None:
-        pass
-
-    @abc.abstractmethod
-    def get_logs(self, label: str) -> Dict[str, float]:
-        pass
-
-    @abc.abstractmethod
-    def get_inference_logs_slice(
-        self, label: str, step_slice: slice
-    ) -> List[Dict[str, float | int]]:
-        pass
-
-    @property
-    @abc.abstractmethod
-    def log_time_series(self) -> bool:
-        pass
-
-
 class InferenceAggregatorABC(abc.ABC, Generic[T]):
     @abc.abstractmethod
     def record_batch(
         self,
-        prediction: T,
+        data: T,
         normalize: Callable[[TensorMapping], TensorDict],
         i_time_start: int,
     ) -> None:
