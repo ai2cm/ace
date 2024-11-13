1a5c275f786b8e354e8fb2985f796acf4d83fc2c
Oliver Watt-Meyer <oliverwatt@gmail.com>
2024-11-12 16:39:37 -0800

    Add scripts to strip optimization state from 3 ACE models (#1337)
    Convenience script to prepare three ACE checkpoints for upload to
    Hugging Face. We will host the models without the optimizer states so
    that they are smaller. We could separately include the optimizer state
    in the Hugging Face model repos also, but this doesn't seem high
    priority. I also convert the area/sigma coordinates to single precision
    (currently the old ACE checkpoint has them saved in double precision).
    Although we already do this in FME code when deserializing the model,
    this is a bit trickier on MPS which doesn't even support double
    precision, so figured we might as well just distribute the checkpoints
    with single precision coordinates.

---
+++ scripts/checkpoint_publication/process_ckpt.py
@@ -0,0 +1,31 @@
+import argparse
+
+import torch
+
+
+def _get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_path", type=str, help="Path for input checkpoint.")
+    parser.add_argument(
+        "output_path",
+        type=str,
+        help="Where to save checkpoint without optimizer state.",
+    )
+    parser.add_argument("--strip-optimization", action="store_true")
+    parser.add_argument("--cast-coords-to-float32", action="store_true")
+    return parser
+
+
+if __name__ == "__main__":
+    args = _get_parser().parse_args()
+    checkpoint = torch.load(args.input_path, map_location=torch.device("cpu"))
+    if args.strip_optimization:
+        del checkpoint["optimization"]
+    if args.cast_coords_to_float32:
+        if "area" in checkpoint["stepper"]:
+            checkpoint["stepper"]["area"] = checkpoint["stepper"]["area"].float()
+        if "sigma_coordinates" in checkpoint["stepper"]:
+            coords = checkpoint["stepper"]["sigma_coordinates"]
+            coords["ak"] = coords["ak"].float()
+            coords["bk"] = coords["bk"].float()
+    torch.save(checkpoint, args.output_path)
