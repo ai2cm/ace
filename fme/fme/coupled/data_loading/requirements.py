9b57bcfd9bacd28efdd769b4e8fb33f45d83b441
Jeremy McGibbon <mcgibbon@uw.edu>
2024-11-18 15:25:20 -0500

    Do validation of CoupledDataRequirements (#1356)
    This PR puts some validation checks currently done only in
    CoupledDataset intialization onto the CoupledDataRequirements post-init.
    
    Changes:
    - CoupledDataset now takes `n_steps_fast` on init to disallow
    misconfiguration
    - CoupledDataRequirements now provides an `n_steps_fast` attribute, and
    raises exceptions if one cannot be derived because the configuration is
    invalid
    
    - [x] Tests added

--- fme/fme/coupled/data_loading/requirements.py
+++ fme/fme/coupled/data_loading/requirements.py
@@ -1,6 +1,5 @@
 import dataclasses
 import datetime
-from typing import Sequence
 
 from fme.core.data_loading.requirements import DataRequirements
 
@@ -12,6 +11,32 @@ class CoupledDataRequirements:
     atmosphere_timestep: datetime.timedelta
     atmosphere_requirements: DataRequirements
 
+    def __post_init__(self):
+        if self.atmosphere_timestep > self.ocean_timestep:
+            raise ValueError("Atmosphere timestep must be no larger than the ocean's.")
+        n_steps_fast = self.ocean_timestep / self.atmosphere_timestep
+        if n_steps_fast != int(n_steps_fast):
+            raise ValueError(
+                f"Expected atmosphere timestep {self.atmosphere_timestep} to be a "
+                f"multiple of ocean timestep {self.ocean_timestep}."
+            )
+        n_steps_fast = int(n_steps_fast)
+
+        # check for misconfigured DataRequirements n_timesteps in the atmosphere
+        slow_n_steps = self.ocean_requirements.n_timesteps
+        fast_n_steps = (slow_n_steps - 1) * n_steps_fast + 1
+        if self.atmosphere_requirements.n_timesteps != fast_n_steps:
+            raise ValueError(
+                f"Atmosphere dataset timestep is {self.atmosphere_timestep} and "
+                f"ocean dataset timestep is {self.ocean_timestep}, "
+                f"so we need {n_steps_fast} atmosphere steps for each of the "
+                f"{slow_n_steps - 1} ocean steps, giving {fast_n_steps} total "
+                "timepoints (including IC) per sample, but atmosphere dataset "
+                f"was configured to return {self.atmosphere_requirements.n_timesteps} "
+                "steps."
+            )
+        self._n_steps_fast = n_steps_fast
+
     @property
-    def timesteps(self) -> Sequence[datetime.timedelta]:
-        return [self.ocean_timestep, self.atmosphere_timestep]
+    def n_steps_fast(self) -> int:
+        return self._n_steps_fast
