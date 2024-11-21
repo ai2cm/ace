e7faf118c7ae5713e3525477e8022406c971796c
Brian Henn <brianhenn@allenai.org>
2024-11-21 14:27:48 -0800

    Fix validation aggregation device error, add coverage of derived variables in training (#1372)
    Fixes a device error that was occurring in the validation aggregator by
    adding an extra cast of the sigma coordinate. This is a patch until a
    more systematic approach is applied.
    
    Also adds a failing test to cover the device error; does so by adding
    variable names to the mock data in test_train.py that exercise the
    vertical integral computations in the computation of derived variables,
    which aren't run currently in the test with variable names like "foo".
    
    - [X] Tests added
    
    Resolves https://github.com/ai2cm/full-model/issues/1359

--- fme/fme/ace/train/train.py
+++ fme/fme/ace/train/train.py
@@ -259,7 +259,7 @@ class AggregatorBuilder(AggregatorBuilderABC[PrognosticState, TrainOutput, Paire
     def get_validation_aggregator(self) -> OneStepAggregator:
         return OneStepAggregator(
             gridded_operations=self.gridded_operations,
-            sigma_coordinates=self.sigma_coordinates,
+            sigma_coordinates=self.sigma_coordinates.to(device=fme.get_device()),
             variable_metadata=self.variable_metadata,
             loss_scaling=self.loss_scaling,
         )
