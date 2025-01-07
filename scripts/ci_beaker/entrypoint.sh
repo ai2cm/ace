9dba8f1c5b4fc8673485673b7fe53aea62e321ab
Elynn Wu <elynn510@gmail.com>
2025-01-07 12:30:01 -0800

    Add beaker inference test (#1438)
    This PR adds a github action to run a standalone inference job through
    beaker.
    We add a new docker test image using the same base image as fme but
    includes an entrypoint script that clones and installs full-model. A new
    beaker
    [dataset](https://beaker.allen.ai/orgs/ai2/workspaces/ace/datasets/01JFGN943BNB3VHGY3MBGBSES7)
    is mounted for this action which includes everything ace needs to run a
    10 day inference using ace2-era5 checkpoint.
    
    Resolves #1437.
    
    [TODO] add a similar test for training in a different PR

---
+++ scripts/ci_beaker/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Exit script if any commands fail.
+set -e
+set -o pipefail
+
+# Check that the environment variables have been set correctly
+for env_var in "$TMP_GITHUB_TOKEN" "$COMMIT_SHA"; do
+    if [[ -z "$env_var" ]]; then
+        echo >&2 "error: required environment variable $env_var is empty"
+        exit 1
+    fi
+done
+
+export "GITHUB_TOKEN=${TMP_GITHUB_TOKEN}"
+
+# Configure git to use GitHub CLI as a credential helper so that we can clone private repos.
+gh auth setup-git
+
+mkdir fme && cd fme
+gh repo clone ai2cm/full-model .
+git checkout --quiet "$COMMIT_SHA"
+
+pip install --no-deps -e ./fme
+
+yq eval '.experiment_dir="/results" | .n_forward_steps=40 | .forward_steps_in_memory=10 | .checkpoint_path="/test-default/ckpt.tar" | .loader.dataset.data_path="/test-default/data"' fme/docs/evaluator-config.yaml > /workspace/evaluator-config.yaml
+
+mkdir -p /results
+
+# Change direcotry to avoid reading from fme
+cd /results
+
+exec "$@" 2>&1 | tee /results/out.log
