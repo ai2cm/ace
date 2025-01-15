410a9e74152af0e273e4f5a44f7a80df228ea8cd
Jeremy McGibbon <mcgibbon@uw.edu>
2025-01-15 15:00:04 -0500

    Remove padding and clipping from downscaling modules (#1455)
    This PR removes logic from the downscaling top level wrapper modules
    which
    
    Changes:
    - Removed padding and clipping from `UNetRegressionModule` and
    `UNetDiffusionModule` wrappers
    - SongUNet now raises an exception when called if the tensor given is
    not compatible with its downsampling scales.
    
    - [ ] Tests added

--- fme/fme/downscaling/modules/test_diffusion_registry.py
+++ fme/fme/downscaling/modules/test_diffusion_registry.py
@@ -3,13 +3,17 @@ import torch
 
 from fme.core.device import get_device
 from fme.downscaling.modules.diffusion_registry import DiffusionModuleRegistrySelector
+from fme.downscaling.modules.unets import NonDivisibleShapeError
 
 
 @pytest.mark.parametrize("use_topography", [True, False])
 def test_diffusion_unet_shapes(use_topography):
     downscale_factor = 2
-    coarse_shape = (9, 18)
-    fine_shape = (18, 36)
+    coarse_shape = (8, 16)
+    fine_shape = (
+        coarse_shape[0] * downscale_factor,
+        coarse_shape[1] * downscale_factor,
+    )
     fine_topography = torch.ones(*fine_shape)
 
     n_channels = 3
@@ -39,3 +43,40 @@ def test_diffusion_unet_shapes(use_topography):
     outputs = unet(latent, coarse, noise_level)
 
     assert (batch_size, n_channels, *fine_shape) == outputs.shape
+
+
+@pytest.mark.parametrize("use_topography", [True, False])
+def test_diffusion_invalid_shapes(use_topography):
+    downscale_factor = 2
+    coarse_shape = (9, 18)
+    fine_shape = (
+        coarse_shape[0] * downscale_factor,
+        coarse_shape[1] * downscale_factor,
+    )
+    fine_topography = torch.ones(*fine_shape)
+    n_channels = 3
+
+    unet = (
+        DiffusionModuleRegistrySelector(
+            "unet_diffusion_song",
+            dict(
+                model_channels=4,
+            ),
+        )
+        .build(
+            n_in_channels=n_channels,
+            n_out_channels=n_channels,
+            coarse_shape=coarse_shape,
+            downscale_factor=downscale_factor,
+            fine_topography=fine_topography if use_topography else None,
+            sigma_data=1.0,
+        )
+        .to(get_device())
+    )
+
+    batch_size = 2
+    coarse = torch.randn(batch_size, n_channels, *coarse_shape)
+    latent = torch.randn(batch_size, n_channels, *fine_shape)
+    noise_level = torch.randn(batch_size, 1, 1, 1)
+    with pytest.raises(NonDivisibleShapeError):
+        unet(latent, coarse, noise_level)
