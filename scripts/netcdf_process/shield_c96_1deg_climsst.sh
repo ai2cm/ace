b411a54705f9f6cf5daf221123a41fde16dc0fa3
Jeremy McGibbon <mcgibbon@uw.edu>
2025-02-07 15:23:02 -0500

    Submission script to process climSST data to weka (#1515)
    Short description of why the PR is needed and how it satisfies those
    requirements, in sentence form.
    
    Changes:
    - Updated output directories for netCDF files to weka locations
    - Added `make` to docker environment
    - Allow directories to exist already when creating monthly netCDFs, will
    ovewrite output if it exists
    - Update monthly netCDF conversion script to wait on processes before
    exiting, so a submitted job does not exit
    - Added gantry script for converting 1-degree climSST data to netCDF in
    `scripts/netcdf_process`
    
    - [ ] Tests added

---
+++ scripts/netcdf_process/shield_c96_1deg_climsst.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+JOB_NAME="shield-c96-1deg-climsst"  # recommended but not required to change this
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd $REPO_ROOT && gantry run \
+    --name $JOB_NAME \
+    --description 'Convert 1-degree C96 FV3GFS climSST ensemble to monthly netcdfs' \
+    --beaker-image oliverwm/fme-deps-only-2025-01-16 \
+    --workspace ai2/ace \
+    --priority normal \
+    --cluster ai2/phobos-cirrascale \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --gpus 0 \
+    --shared-memory 40GiB \
+    --weka climate-default:/climate-default \
+    --budget ai2/climate \
+    --no-conda \
+    --install "pip install --no-deps ./fme" \
+    -- bash -c "cd /gantry-runtime/scripts/data_process && make fv3gfs_1deg_climSST_monthly_netcdfs"
