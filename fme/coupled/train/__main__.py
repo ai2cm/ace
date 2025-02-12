53686101114f171928c3d110f3a25df850d27000
Oliver Watt-Meyer <oliverwatt@gmail.com>
2025-02-11 16:39:23 -0800

    Add dotlist override to all ACE entrypoints (#1527)
    Currently we only allow overriding config options for `fme.ace.train`
    entrypoint. This adds it for the two other ACE entrypoints and the
    `fme.coupled.train` and `fme.diffusion.train` entrypoints. I needed this
    to more easily launch an ensemble of evaluator runs.
    
    It also deduplicates some of the entrypoint bolierplate by using common
    helper functions where possible.

--- fme/coupled/train/__main__.py
+++ fme/coupled/train/__main__.py
@@ -1,11 +1,7 @@
-import argparse
-
+from fme.core.cli import get_parser
 from fme.coupled.train.train import main
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("yaml_config", type=str)
-
+    parser = get_parser()
     args = parser.parse_args()
-
-    main(yaml_config=args.yaml_config)
+    main(yaml_config=args.yaml_config, override_dotlist=args.override)
