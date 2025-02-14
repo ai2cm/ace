f8d502779567cbb903b2021df0f90e3f8336ccaa
Jeremy McGibbon <mcgibbon@uw.edu>
2025-02-14 14:31:40 -0500

    Move baseline configurations to `configs/baselines` (#1505)
    This PR establishes a directory structure for "baseline" configurations,
    updates our gantry files to work within subdirectories, and provides
    config validation for future baselines. Documentation is updated to
    reflect these changes.
    
    Changes:
    - Gantry example is converted to a directory of baseline configurations
    in `configs/baselines`
    - Gantry scripts are now run from the directory they exist in rather
    than using a top level make target
    
    - [x] Tests added

---
+++ configs/baselines/ace2-era5/run-ace-evaluator.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+JOB_NAME="ace-evaluator"  # recommnended but not required to change this
+EXISTING_RESULTS_DATASET="01J4MT10JPQ8MFA41F2AXGFYJ9"  # this contains the checkpoint to use for inference
+CONFIG_FILENAME="ace-evaluator-config.yaml"
+SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+ # since we use a service account API key for wandb, we use the beaker username to set the wandb username
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+
+python -m fme.ace.validate_config --config_type evaluator $CONFIG_PATH
+
+cd $REPO_ROOT && gantry run \
+    --name $JOB_NAME \
+    --description 'Run ACE evaluator' \
+    --beaker-image oliverwm/fme-deps-only-2025-01-16 \
+    --workspace ai2/ace \
+    --priority normal \
+    --cluster ai2/saturn-cirrascale \
+    --cluster ai2/jupiter-cirrascale-2 \
+    --cluster ai2/ceres-cirrascale \
+    --env WANDB_USERNAME=$BEAKER_USERNAME \
+    --env WANDB_NAME=$JOB_NAME \
+    --env WANDB_JOB_TYPE=inference \
+    --env WANDB_RUN_GROUP= \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $EXISTING_RESULTS_DATASET:training_checkpoints/best_inference_ckpt.tar:/ckpt.tar \
+    --gpus 1 \
+    --shared-memory 20GiB \
+    --weka climate-default:/climate-default \
+    --budget ai2/climate \
+    --no-conda \
+    --install "pip install --no-deps ." \
+    -- python -I -m fme.ace.evaluator $CONFIG_PATH
---
+++ configs/baselines/ace2-era5/run-ace-train.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+JOB_NAME="ace-train"  # recommended but not required to change this
+CONFIG_FILENAME="ace-train-config.yaml"
+SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+ # since we use a service account API key for wandb, we use the beaker username to set the wandb username
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+REPO_ROOT=$(git rev-parse --show-toplevel)
+N_GPUS=8
+
+cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+
+python -m fme.ace.validate_config --config_type train $CONFIG_PATH
+
+gantry run \
+    --name $JOB_NAME \
+    --description 'Run ACE training' \
+    --beaker-image oliverwm/fme-deps-only-2025-01-16 \
+    --workspace ai2/ace \
+    --priority normal \
+    --preemptible \
+    --cluster ai2/saturn-cirrascale \
+    --cluster ai2/jupiter-cirrascale-2 \
+    --cluster ai2/ceres-cirrascale \
+    --env WANDB_USERNAME=$BEAKER_USERNAME \
+    --env WANDB_NAME=$JOB_NAME \
+    --env WANDB_JOB_TYPE=training \
+    --env WANDB_RUN_GROUP= \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset oliverwm/era5-1deg-8layer-stats-1990-2019-v2:/statsdata \
+    --gpus $N_GPUS \
+    --shared-memory 400GiB \
+    --weka climate-default:/climate-default \
+    --budget ai2/climate \
+    --no-conda \
+    --install "pip install --no-deps ." \
+    -- torchrun --nproc_per_node $N_GPUS -m fme.ace.train $CONFIG_PATH
