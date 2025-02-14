4a212d1a20a780ba9aaf7aef0874b9014fd1caed
Jeremy McGibbon <mcgibbon@uw.edu>
2025-02-14 15:30:55 -0500

    Add NoiseConditionedSFNO model option (#1519)
    This PR adds a stochastic NoiseConditionedSFNO model to ACE.
    
    Changes:
    - Moved conditional SFNO model into core namespace
    - Updated conditional SFNO model to allow both scalar and 2D
    conditioning
    - Added NoiseConditionedSFNO as model option for ace training
    
    - [x] Tests added

--- fme/ace/registry/__init__.py
+++ fme/ace/registry/__init__.py
@@ -2,6 +2,7 @@
 from . import m2lines as _m2lines
 from . import prebuilt as _prebuilt
 from . import sfno as _sfno
+from . import stochastic_sfno as _sfno_crps
 from .registry import ModuleSelector
 
 del _prebuilt, _sfno, _m2lines
