6bb92fb59d1ec13505ec3511c18afdc09f1076d9
Jeremy McGibbon <mcgibbon@uw.edu>
2025-02-14 15:30:55 -0500

    Add NoiseConditionedSFNO model option (#1519)
    This PR adds a stochastic NoiseConditionedSFNO model to ACE.
    
    Changes:
    - Moved conditional SFNO model into core namespace
    - Updated conditional SFNO model to allow both scalar and 2D
    conditioning
    - Added NoiseConditionedSFNO as model option for ace training
    
    - [x] Tests added

---
+++ fme/ace/registry/stochastic_sfno.py
@@ -0,0 +1,80 @@
+import dataclasses
+from typing import Literal, Optional, Tuple
+
+import torch
+
+from fme.ace.registry.registry import ModuleConfig, ModuleSelector
+from fme.core.models.conditional_sfno.sfnonet import Context, ContextConfig
+from fme.core.models.conditional_sfno.sfnonet import (
+    SphericalFourierNeuralOperatorNet as ConditionalSFNO,
+)
+
+
+class NoiseConditionedSFNO(torch.nn.Module):
+    def __init__(self, conditional_model: ConditionalSFNO, embed_dim: int):
+        super().__init__()
+        self.conditional_model = conditional_model
+        self.embed_dim = embed_dim
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        noise = torch.randn(
+            [*x.shape[:-3], self.embed_dim, *x.shape[-2:]],
+            device=x.device,
+            dtype=x.dtype,
+        )
+        embedding_scalar = torch.zeros(
+            [*x.shape[:-3], 0], device=x.device, dtype=x.dtype
+        )
+        return self.conditional_model(
+            x, Context(embedding_scalar=embedding_scalar, embedding_2d=noise)
+        )
+
+
+# this is based on the call signature of SphericalFourierNeuralOperatorNet at
+# https://github.com/NVIDIA/modulus/blob/b8e27c5c4ebc409e53adaba9832138743ede2785/modulus/models/sfno/sfnonet.py#L292  # noqa: E501
+@ModuleSelector.register("NoiseConditionedSFNO")
+@dataclasses.dataclass
+class NoiseConditionedSFNOBuilder(ModuleConfig):
+    """
+    Configuration for the SFNO architecture used in FourCastNet-SFNO.
+    """
+
+    spectral_transform: str = "sht"
+    filter_type: str = "non-linear"
+    operator_type: str = "diagonal"
+    embed_dim: int = 256
+    noise_embed_dim: int = 256
+    num_layers: int = 12
+    normalization_layer: str = "instance_norm"
+    use_mlp: bool = True
+    activation_function: str = "gelu"
+    encoder_layers: int = 1
+    pos_embed: bool = True
+    big_skip: bool = True
+    rank: float = 1.0
+    factorization: Optional[str] = None
+    separable: bool = False
+    complex_network: bool = True
+    complex_activation: str = "real"
+    spectral_layers: int = 1
+    checkpointing: int = 0
+    # healpix not supported due to assumptions about number of spatial dims
+    data_grid: Literal["legendre-gauss", "equiangular"] = "legendre-gauss"
+
+    def build(
+        self,
+        n_in_channels: int,
+        n_out_channels: int,
+        img_shape: Tuple[int, int],
+    ):
+        sfno_net = ConditionalSFNO(
+            params=self,
+            in_chans=n_in_channels,
+            out_chans=n_out_channels,
+            img_shape=img_shape,
+            context_config=ContextConfig(
+                embed_dim_scalar=0,
+                embed_dim_2d=self.noise_embed_dim,
+            ),
+        )
+        return NoiseConditionedSFNO(sfno_net, self.noise_embed_dim)
