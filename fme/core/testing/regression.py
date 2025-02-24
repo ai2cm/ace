449078822e258e5f80595d5d1082d7abde6d1d9f
Jeremy McGibbon <mcgibbon@uw.edu>
2025-02-24 12:00:28 -0500

    Add regression tests for predict and train_on_batch (#1585)
    This PR adds basic regression tests for `SingleModuleStepper.predict`
    and `.train_on_batch` using the default SFNO configuration
    
    Changes:
    - Added regression tests for `SingleModuleStepper.predict` and
    `.train_on_batch`
    
    - [x] Tests added

--- fme/core/testing/regression.py
+++ fme/core/testing/regression.py
@@ -3,6 +3,8 @@ import os
 import pytest
 import torch
 
+from fme.core.typing_ import TensorDict
+
 
 def validate_tensor(x: torch.Tensor, filename: str):
     if not os.path.exists(filename):
@@ -11,3 +13,13 @@ def validate_tensor(x: torch.Tensor, filename: str):
     else:
         y = torch.load(filename).to(x.device)
         torch.testing.assert_allclose(x, y)
+
+
+def validate_tensor_dict(x: TensorDict, filename: str):
+    if not os.path.exists(filename):
+        torch.save(x, filename)
+        pytest.fail(f"Regression file {filename} did not exist, so it was created")
+    else:
+        y = torch.load(filename)
+        for k, v in x.items():
+            torch.testing.assert_allclose(v, y[k].to(v.device))
