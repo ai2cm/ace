87543ce63392117500443cdfa48317fc1d891199
James Duncan <jpdunc23@users.noreply.github.com>
2025-02-24 12:09:19 -0800

    Refactor masking to require static mask tensor on init (#1579)
    With https://github.com/ai2cm/full-model/pull/1558 the 3D ocean mask is
    now loaded as part of the ocean `DepthCoordinate`, meaning that we have
    access to the mask when building `OceanCorrector`. This PR refactors
    `StaticMasking` (previously called `Masking`) so that we can directly
    initialize it with the `DepthCoordinate.mask`. As a result, the `mask_*`
    variables are no longer required to be ML forcing inputs in order to
    mask out the land when emulating ocean fields.
    
    Changes:
    - Renames `Masking{Config}` to `StaticMasking{Config}`
    - Removed the `mask_data` argument from `StaticMasking.__call__`.
    - `StaticMaskingConfig.build` now has args `mask_2d` and `mask_3d`.
    - Removed the `StaticMaskingConfig` attributes `mask_name` and
    `surface_mask_name`.
    - Adds `get_mask` and `get_idepth` methods to `DepthCoordinate`
    - `OceanCorrector` has new init args `gridded_operations`,
    `vertical_coordinate` and `timestep`.
    
    - [x] Tests added
    
    Resolves https://github.com/ai2cm/full-model/issues/1555

---
+++ fme/core/corrector/test_ocean.py
@@ -0,0 +1,90 @@
+import datetime
+
+import pytest
+import torch
+
+from fme import get_device
+from fme.core.gridded_ops import LatLonOperations
+from fme.core.masking import StaticMaskingConfig
+
+from .ocean import OceanCorrector, OceanCorrectorConfig
+
+
+def test_ocean_corrector_init_error():
+    ops = LatLonOperations(torch.ones(size=IMG_SHAPE))
+    timestep = datetime.timedelta(seconds=3600)
+
+    config = OceanCorrectorConfig()
+    # no error
+    _ = OceanCorrector(config, ops, None, timestep)
+
+    config = OceanCorrectorConfig(
+        masking=StaticMaskingConfig(
+            variable_names_and_prefixes=["sst", "so_"],
+            mask_value=0,
+            fill_value=float("nan"),
+        ),
+    )
+    with pytest.raises(
+        ValueError,
+        match="OceanCorrector.masking configured but DepthCoordinate missing.",
+    ):
+        _ = OceanCorrector(config, ops, None, timestep)
+
+
+DEVICE = get_device()
+IMG_SHAPE = (5, 5)
+NZ = 2
+
+_MASK = torch.ones(*IMG_SHAPE, NZ, device=DEVICE)
+_LAT, _LON = 2, 2
+_MASK[_LAT, _LON, :] = 0.0
+
+
+class _MockDepth:
+    def __len__(self) -> int:
+        return len(self.get_idepth())
+
+    def get_mask(self) -> torch.Tensor:
+        return _MASK
+
+    def get_mask_level(self, level: int):
+        return _MASK.select(-1, level)
+
+    def get_idepth(self) -> torch.Tensor:
+        return torch.tensor([0, 5, 15], device=DEVICE)
+
+    def integral(self, integrand: torch.Tensor) -> torch.Tensor:
+        thickness = self.get_idepth().diff(dim=-1)
+        return torch.nansum(_MASK * integrand * thickness, dim=-1)
+
+
+_VERTICAL_COORD = _MockDepth()
+
+
+def test_ocean_corrector_integration():
+    """Ensures that OceanCorrector can be called with all methods active
+    but doesn't check results."""
+    torch.manual_seed(0)
+    config = OceanCorrectorConfig(
+        masking=StaticMaskingConfig(
+            variable_names_and_prefixes=["sst", "so_"],
+            mask_value=0,
+            fill_value=float("nan"),
+        ),
+        force_positive_names=["so_0", "so_1"],
+    )
+    ops = LatLonOperations(torch.ones(size=IMG_SHAPE))
+    timestep = datetime.timedelta(seconds=3600)
+    corrector = OceanCorrector(config, ops, _VERTICAL_COORD, timestep)
+    input_data = {f"so_{i}": torch.randn(IMG_SHAPE, device=DEVICE) for i in range(NZ)}
+    input_data["sst"] = torch.randn(IMG_SHAPE, device=DEVICE)
+    gen_data = {f"so_{i}": torch.randn(IMG_SHAPE, device=DEVICE) for i in range(NZ)}
+    gen_data["sst"] = torch.randn(IMG_SHAPE, device=DEVICE)
+    corrected_gen = corrector(input_data, gen_data, {})
+    for name in ["so_0", "so_1", "sst"]:
+        assert corrected_gen[name][_LAT, _LON].isnan().all()
+    for name in ["so_0", "so_1"]:
+        x = corrected_gen[name].clone()
+        x[_LAT, _LON] = 0.0
+        assert torch.all(x >= 0.0)
--- fme/core/masking.py
+++ fme/core/masking.py
@@ -30,27 +30,21 @@ def replace_on_mask(
 
 
 @dataclasses.dataclass
-class MaskingConfig:
+class StaticMaskingConfig:
     """
-    Replace masked regions with a fill value, with handling for both 2D and 3D
+    Replace static masked regions with a fill value, with handling for both 2D and 3D
     variables.
 
     Parameters:
         variable_names_and_prefixes: Names (2D variables) and prefixes (3D variables)
             to mask, which are used to build a variable Stacker.
-        mask_name: The standard name of the mask. May be a prefix (for a 3D masking
-            variable) or a full name (for a 2D masking variable).
         mask_value: Value of the mask variable in masked regions. Either 0 or 1.
         fill_value: The constant fill value to use outside of masked regions.
-        surface_mask_name: (optional) The full name of the surface mask. Only required
-            when mask_name is a prefix and separate 2D surface masking is desired.
     """
 
     variable_names_and_prefixes: List[str]
-    mask_name: str
     mask_value: int
     fill_value: float = 0.0
-    surface_mask_name: Optional[str] = None
 
     def __post_init__(self):
         if self.mask_value not in [0, 1]:
@@ -58,74 +52,88 @@ class MaskingConfig:
                 "mask_value must be either 0 or 1, but got " f"{self.mask_value}"
             )
 
-    def build(self):
-        return Masking(
+    def build(
+        self,
+        mask_2d: Optional[torch.Tensor] = None,
+        mask_3d: Optional[torch.Tensor] = None,
+    ):
+        """
+        Build Masking from config and 2D/3D masks. At least one of mask_2d
+        and mask_3d must be non-null.
+
+        mask_2d: 2D mask with shape (n_lat, n_lon).
+        mask_3d: 3D mask with shape (n_lat, n_lon, n_levels).
+
+        """
+        return StaticMasking(
             variable_names_and_prefixes=self.variable_names_and_prefixes,
-            mask_name=self.mask_name,
             mask_value=self.mask_value,
             fill_value=self.fill_value,
-            surface_mask_name=self.surface_mask_name,
+            mask_2d=mask_2d,
+            mask_3d=mask_3d,
         )
 
 
-class Masking:
+class StaticMasking:
     def __init__(
         self,
         variable_names_and_prefixes: List[str],
-        mask_name: str,
         mask_value: int,
         fill_value: float,
-        surface_mask_name: Optional[str] = None,
+        mask_2d: Optional[torch.Tensor] = None,
+        mask_3d: Optional[torch.Tensor] = None,
     ):
-        self.mask_name = mask_name
-        self.mask_value = mask_value
-        self.fill_value = fill_value
-        self.surface_mask_name = surface_mask_name
-        mask_map = {self.mask_name: [self.mask_name]}
-        if self.surface_mask_name is not None:
-            mask_map[self.surface_mask_name] = [self.surface_mask_name]
+        if mask_2d is None and mask_3d is None:
+            raise ValueError("Either mask_2d or mask_3d must be provided to Masking.")
+        if mask_2d is not None and len(mask_2d.shape) != 2:
+            raise ValueError(
+                f"Expected mask_2d with 2 dimensions, but got shape: {mask_2d.shape}"
+            )
+        if mask_3d is not None and len(mask_3d.shape) != 3:
+            raise ValueError(
+                f"Expected mask_3d with 3 dimensions, but got shape: {mask_3d.shape}"
+            )
+        self._mask_value = mask_value
+        self._fill_value = fill_value
+        # add convenience 3rd dimension to
+        self._mask_2d = None if mask_2d is None else mask_2d.unsqueeze(-1)
+        self._mask_3d = mask_3d
+        self._stacker = Stacker({name: [name] for name in variable_names_and_prefixes})
 
-        self.mask_stacker = Stacker(mask_map)
-        self.stacker = Stacker({name: [name] for name in variable_names_and_prefixes})
-
-    def __call__(
-        self,
-        data: TensorMapping,
-        mask_data: TensorMapping,
-    ) -> TensorDict:
+    def __call__(self, data: TensorMapping) -> TensorDict:
         """
         Apply masking to the data for standard names recognized by a stacker.
 
         Args:
             data: The data to mask.
-            mask_data: The mask data.
 
         """
-        mask = self.mask_stacker(self.mask_name, mask_data)
-        if self.surface_mask_name is not None:
-            surface_mask = self.mask_stacker(self.surface_mask_name, mask_data)
-        else:
-            surface_mask = None
         data_: TensorDict = {**data}
-        for name in self.stacker.standard_names:
-            stacked = self.stacker(name, data)
+        for name in self._stacker.standard_names:
+            stacked = self._stacker(name, data)
+            mask = None
             if stacked.size(-1) > 1:  # 3D masking
-                mask_ = mask
-            elif surface_mask is not None:  # 2D masking with surface mask
-                mask_ = surface_mask
-            elif mask.size(-1) == 1:  # 2D masking with mask
-                mask_ = mask
-            else:
-                raise RuntimeError(
-                    f"Masking surface_mask_name is None but {name} is 2D."
+                mask = self._mask_3d
+                error_msg = (
+                    f"{name[:-1]} is a 3D variable with shape {stacked.shape} "
+                    "but Masking wasn't initialized with a 3D mask."
                 )
-            fill = torch.full_like(stacked, self.fill_value)
+            else:  # 2D masking
+                mask = self._mask_2d
+                stacked = stacked
+                error_msg = (
+                    f"{name} is a 2D variable with shape {stacked.squeeze().shape} "
+                    "but Masking wasn't initialized with a 2D mask."
+                )
+            if mask is None:
+                raise RuntimeError(error_msg)
+            fill = torch.full_like(stacked, self._fill_value)
             masked = replace_on_mask(
                 original=stacked,
                 replacement=fill,
-                mask=mask_,
-                mask_value=self.mask_value,
+                mask=mask,
+                mask_value=self._mask_value,
             )
-            level_names = self.stacker.get_all_level_names(name, data)
+            level_names = self._stacker.get_all_level_names(name, data)
             data_.update(unstack(masked, level_names, dim=-1))
         return data_
