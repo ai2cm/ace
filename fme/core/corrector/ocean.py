b4346a13257998293ee8e4d04c845e568d98556a
James Duncan <jpdunc23@users.noreply.github.com>
2025-02-24 12:09:19 -0800

    Refactor masking to require static mask tensor on init (#1579)
    With https://github.com/ai2cm/full-model/pull/1558 the 3D ocean mask is
    now loaded as part of the ocean `DepthCoordinate`, meaning that we have
    access to the mask when building `OceanCorrector`. This PR refactors
    `StaticMasking` (previously called `Masking`) so that we can directly
    initialize it with the `DepthCoordinate.mask`. As a result, the `mask_*`
    variables are no longer required to be ML forcing inputs in order to
    mask out the land when emulating ocean fields.
    
    Changes:
    - Renames `Masking{Config}` to `StaticMasking{Config}`
    - Removed the `mask_data` argument from `StaticMasking.__call__`.
    - `StaticMaskingConfig.build` now has args `mask_2d` and `mask_3d`.
    - Removed the `StaticMaskingConfig` attributes `mask_name` and
    `surface_mask_name`.
    - Adds `get_mask` and `get_idepth` methods to `DepthCoordinate`
    - `OceanCorrector` has new init args `gridded_operations`,
    `vertical_coordinate` and `timestep`.
    
    - [x] Tests added
    
    Resolves https://github.com/ai2cm/full-model/issues/1555

--- fme/core/corrector/ocean.py
+++ fme/core/corrector/ocean.py
@@ -1,11 +1,14 @@
 import dataclasses
+import datetime
 from typing import Any, List, Mapping, Optional
 
 import dacite
 
 from fme.core.corrector.corrector import force_positive
 from fme.core.corrector.registry import CorrectorABC
-from fme.core.masking import MaskingConfig
+from fme.core.gridded_ops import GriddedOperations
+from fme.core.masking import StaticMaskingConfig
+from fme.core.ocean_data import HasOceanIntegral
 from fme.core.registry.corrector import CorrectorSelector
 from fme.core.typing_ import TensorMapping
 
@@ -13,7 +16,7 @@ from fme.core.typing_ import TensorMapping
 @CorrectorSelector.register("ocean_corrector")
 @dataclasses.dataclass
 class OceanCorrectorConfig:
-    masking: Optional[MaskingConfig] = None
+    masking: Optional[StaticMaskingConfig] = None
     force_positive_names: List[str] = dataclasses.field(default_factory=list)
 
     @classmethod
@@ -27,11 +30,24 @@ class OceanCorrector(CorrectorABC):
     def __init__(
         self,
         config: OceanCorrectorConfig,
+        gridded_operations: GriddedOperations,
+        vertical_coordinate: Optional[HasOceanIntegral],
+        timestep: datetime.timedelta,
     ):
         self._config = config
+        self._gridded_operations = gridded_operations
+        self._vertical_coordinate = vertical_coordinate
+        self._timestep = timestep
 
         if config.masking is not None:
-            self._masking = config.masking.build()
+            if vertical_coordinate is None:
+                raise ValueError(
+                    "OceanCorrector.masking configured but DepthCoordinate missing."
+                )
+            self._masking = config.masking.build(
+                mask_2d=vertical_coordinate.get_mask_level(0),
+                mask_3d=vertical_coordinate.get_mask(),
+            )
         else:
             self._masking = None
 
@@ -42,7 +58,7 @@ class OceanCorrector(CorrectorABC):
         forcing_data: TensorMapping,
     ) -> TensorMapping:
         if self._masking is not None:
-            gen_data = self._masking(gen_data, forcing_data)
+            gen_data = self._masking(gen_data)
         if len(self._config.force_positive_names) > 0:
             gen_data = force_positive(gen_data, self._config.force_positive_names)
         return gen_data
