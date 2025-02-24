8ca8023a52387b3636c3165bb8984d9f931ba16c
Jeremy McGibbon <mcgibbon@uw.edu>
2025-02-24 16:30:41 -0500

    Add a climsst baseline configuration (#1573)
    Our existing ace2-era5 baseline involves training the model on
    historical CO2 and SST forcings, and then testing on an out-of-sample
    inference period. There are some signs we may be overfitting in this
    problem, such as behavior over-indexing on CO2, and in some cases seeing
    the upper atmosphere humidity and temperature validation RMSEs go up
    with training. We also see that in-sample inference loss goes down with
    more training for configurations where out-of-sample inference loss goes
    up. In other words, features which make the model more powerful tend to
    worsen our inference skill.
    
    A main goal we have right now is to align our training objective and
    long-term inference skill. In particular, I am working on changes to our
    model and training objective which might align model training with
    improvements to long-term inference. Even on in-sample forcings, our
    model has issues with long-term and short-term inference behavior which
    we want to rectify, such as muted power spectra or long-term upper
    atmospheric drifts.
    
    The in vs out of sample setup of ace2-era5 makes it difficult to draw
    conclusions for model changes like CRPS loss or diffusion modelling,
    particularly when inference loss increases due to model changes. For
    this reason I'll be evaluating these changes on our older "climSST"
    data, where we believe we can still reproduce (and thus correct) these
    issues.
    
    The configuration here is based mainly on our latest ace2-era5
    configuration, removing inputs and outputs that don't exist in that
    dataset. I have also modified the configuration to use the "layer_norm"
    option and gradient accumulation, which are changes we plan to
    incorporate into the ace2-era5 baseline.
    
    The inference start indices are selected to sample the annual and
    diurnal cycles, while still being near the start of the inference period
    to allow longer inline inference (the validation dataset is limited to
    10 years).
    
    Changes:
    - Added `climsst` baseline configuration

---
+++ configs/baselines/climsst/run-train.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+WANDB_USERNAME=${WANDB_USERNAME:-${BEAKER_USERNAME}}
+REPO_ROOT=$(git rev-parse --show-toplevel)
+N_GPUS=8
+
+cd "$REPO_ROOT"
+
+run_training() {
+  local config_filename="$1"
+  local job_name="$2"
+  local stats_dataset="$3"
+  local CONFIG_PATH="$SCRIPT_PATH/$config_filename"
+
+  python -m fme.ace.validate_config --config_type train "$CONFIG_PATH"
+
+  gantry run \
+    --name "$job_name" \
+    --description 'Run ACE training' \
+    --beaker-image oliverwm/fme-deps-only-2025-01-16 \
+    --workspace ai2/ace \
+    --priority normal \
+    --preemptible \
+    --cluster ai2/saturn-cirrascale \
+    --cluster ai2/jupiter-cirrascale-2 \
+    --cluster ai2/ceres-cirrascale \
+    --env WANDB_USERNAME="$WANDB_USERNAME" \
+    --env WANDB_NAME="$job_name" \
+    --env WANDB_JOB_TYPE=training \
+    --env WANDB_RUN_GROUP= \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset "$stats_dataset:/statsdata" \
+    --gpus "$N_GPUS" \
+    --shared-memory 400GiB \
+    --weka climate-default:/climate-default \
+    --budget ai2/climate \
+    --no-conda \
+    --install "pip install --no-deps ." \
+    -- torchrun --nproc_per_node "$N_GPUS" -m fme.ace.train "$CONFIG_PATH"
+}
+
+base_name="train-climsst"
+
+stats_4deg_dataset="jeremym/2023-08-09-vertically-resolved-4deg-fme-ensemble-dataset-stats"
+stats_1deg_dataset="jeremym/2023-08-09-vertically-resolved-1deg-fme-ensemble-dataset-stats"
+
+# To run a subset of these, comment out the ones you don't want to run
+# and if needed use `--allow-dirty` in the gantry run command above.
+run_training "train-baseline.yaml" "$base_name-baseline" "$stats_1deg_dataset"
+run_training "train-baseline-4deg.yaml" "$base_name-baseline-4deg" "$stats_4deg_dataset"
