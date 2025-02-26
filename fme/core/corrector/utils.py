0896f811034b23d5579ea5447534aeef42940763
Elynn Wu <elynn510@gmail.com>
2025-02-26 08:53:43 -0800

    Samudra predicts sea ice fraction with physical constraints (#1552)
    In this PR, we add the ability to predict sea ice fraction in Samudra.
    We clamp the output to be 0-1 and `sea ice fraction + land fraction +
    ocean fraction = 1`.
    
    TODO: revert config yaml and do not merge
    
    Changes:
    - `OceanCorrectorConfig` has additional option:
    `sea_ice_fraction_correction`
    - [x] Tests added
    
    ---------
    
    Co-authored-by: James Duncan <jpduncan@berkeley.edu>
    Co-authored-by: James Duncan <jpdunc23@users.noreply.github.com>

---
+++ fme/core/corrector/utils.py
@@ -0,0 +1,13 @@
+from typing import List
+
+import torch
+
+from fme.core.typing_ import TensorDict, TensorMapping
+
+
+def force_positive(data: TensorMapping, names: List[str]) -> TensorDict:
+    """Clamp all tensors defined by `names` to be greater than or equal to zero."""
+    out = {**data}
+    for name in names:
+        out[name] = torch.clamp(data[name], min=0.0)
+    return out
