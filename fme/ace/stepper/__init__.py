ec2688c53abd71bd3227d94cb511706b6e8b6af9
Jeremy McGibbon <mcgibbon@uw.edu>
2025-02-26 18:10:32 -0500

    Move stepper into stepper/ directory (#1604)
    This PR simply moves code in `stepper.py` into a subdirectory, for
    easier modification in coming PRs to allow changing the way stepping is
    done.
    
    Changes:
    - Private symbols in `fme.ace.stepper` are moved to
    `fme.ace.stepper.single_module`
    
    - [ ] Tests added

---
+++ fme/ace/stepper/__init__.py
@@ -0,0 +1,9 @@
+from .single_module import (
+    ExistingStepperConfig,
+    SingleModuleStepper,
+    SingleModuleStepperConfig,
+    StepperOverrideConfig,
+    TrainOutput,
+    load_stepper,
+    load_stepper_config,
+)
