0a3cb8135560f21c68e489697c07fc8e375cd543
Elynn Wu <elynn510@gmail.com>
2025-02-26 08:53:43 -0800

    Samudra predicts sea ice fraction with physical constraints (#1552)
    In this PR, we add the ability to predict sea ice fraction in Samudra.
    We clamp the output to be 0-1 and `sea ice fraction + land fraction +
    ocean fraction = 1`.
    
    TODO: revert config yaml and do not merge
    
    Changes:
    - `OceanCorrectorConfig` has additional option:
    `sea_ice_fraction_correction`
    - [x] Tests added
    
    ---------
    
    Co-authored-by: James Duncan <jpduncan@berkeley.edu>
    Co-authored-by: James Duncan <jpdunc23@users.noreply.github.com>

--- fme/core/corrector/ocean.py
+++ fme/core/corrector/ocean.py
@@ -3,14 +3,21 @@ import datetime
 from typing import Any, List, Mapping, Optional
 
 import dacite
+import torch
 
-from fme.core.corrector.corrector import force_positive
 from fme.core.corrector.registry import CorrectorABC
+from fme.core.corrector.utils import force_positive
 from fme.core.gridded_ops import GriddedOperations
 from fme.core.masking import StaticMaskingConfig
 from fme.core.ocean_data import HasOceanDepthIntegral
 from fme.core.registry.corrector import CorrectorSelector
-from fme.core.typing_ import TensorMapping
+from fme.core.typing_ import TensorDict, TensorMapping
+
+
+@dataclasses.dataclass
+class SeaIceFractionConfig:
+    sea_ice_fraction_name: str
+    land_fraction_name: str
 
 
 @CorrectorSelector.register("ocean_corrector")
@@ -18,6 +25,7 @@ from fme.core.typing_ import TensorMapping
 class OceanCorrectorConfig:
     masking: Optional[StaticMaskingConfig] = None
     force_positive_names: List[str] = dataclasses.field(default_factory=list)
+    sea_ice_fraction_correction: Optional[SeaIceFractionConfig] = None
 
     @classmethod
     def from_state(cls, state: Mapping[str, Any]) -> "OceanCorrectorConfig":
@@ -51,6 +59,28 @@ class OceanCorrector(CorrectorABC):
         else:
             self._masking = None
 
+    def _correct_sea_ice_fraction(
+        self, gen_data: TensorMapping, forcing_data: TensorMapping
+    ) -> TensorDict:
+        """Correct predicted sea ice fraction to ensure \
+            sea ice fraction is always 0-1, and \
+            land fraction + sea ice fraction + ocean fraction = 1.
+        """
+        out = {**gen_data}
+        sea_ice_config = self._config.sea_ice_fraction_correction
+        if sea_ice_config is not None:
+            out[sea_ice_config.sea_ice_fraction_name] = torch.clamp(
+                out[sea_ice_config.sea_ice_fraction_name], min=0.0, max=1.0
+            )
+            negative_ocean_fraction = (
+                1
+                - out[sea_ice_config.sea_ice_fraction_name]
+                - forcing_data[sea_ice_config.land_fraction_name]
+            )
+            negative_ocean_fraction = negative_ocean_fraction.clip(max=0)
+            out[sea_ice_config.sea_ice_fraction_name] += negative_ocean_fraction
+        return out
+
     def __call__(
         self,
         input_data: TensorMapping,
@@ -61,4 +91,5 @@ class OceanCorrector(CorrectorABC):
             gen_data = self._masking(gen_data)
         if len(self._config.force_positive_names) > 0:
             gen_data = force_positive(gen_data, self._config.force_positive_names)
+        gen_data = self._correct_sea_ice_fraction(gen_data, forcing_data)
         return gen_data
