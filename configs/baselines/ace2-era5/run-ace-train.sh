45375047643b6a6c4c695dcb83a15cd74023c405
Oliver Watt-Meyer <oliverwatt@gmail.com>
2025-02-27 13:47:37 -0800

    Refactor beaker CI job to separate workflow (#1613)
    This makes it possible to manually launch the beaker evaluator job
    without also rerunning all of the unit tests.
    
    Also updates the "deps-only" image to use commit SHA instead
    of current date for versioning.

--- configs/baselines/ace2-era5/run-ace-train.sh
+++ configs/baselines/ace2-era5/run-ace-train.sh
@@ -18,7 +18,7 @@ python -m fme.ace.validate_config --config_type train $CONFIG_PATH
 gantry run \
     --name $JOB_NAME \
     --description 'Run ACE training' \
-    --beaker-image oliverwm/fme-deps-only-2025-01-16 \
+    --beaker-image oliverwm/fme-deps-only-01cfbdf1 \
     --workspace ai2/ace \
     --priority normal \
     --preemptible \
