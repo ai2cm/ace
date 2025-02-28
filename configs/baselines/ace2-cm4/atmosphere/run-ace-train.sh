34be757a2d1790d23b744b22a9313a01f0a00270
Brian Henn <brianhenn@allenai.org>
2025-02-28 11:03:41 -0800

    Fix ACE2-CM4 atmosphere evaluator submission script (#1617)
    https://github.com/ai2cm/full-model/pull/1595 used an out of date
    submissions script for ACE2-CM4 atmosphere evaluation; this PR brings it
    up to date.

--- configs/baselines/ace2-cm4/atmosphere/run-ace-train.sh
+++ configs/baselines/ace2-cm4/atmosphere/run-ace-train.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-JOB_NAME="ace-cm4-atmosphere-train"
+JOB_NAME="ace2-cm4-atmosphere-train"
 JOB_GROUP="ace2-cm4-atmosphere"
 CONFIG_FILENAME="ace-train-config.yaml"
 SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
-CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+CONFIG_PATH="${SCRIPT_PATH}${CONFIG_FILENAME}"
  # since we use a service account API key for wandb, we use the beaker username to set the wandb username
 BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
 REPO_ROOT=$(git rev-parse --show-toplevel)
