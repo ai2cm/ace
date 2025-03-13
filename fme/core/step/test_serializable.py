952143ec23e4d49088ef49f6df315efb3e622d30
Jeremy McGibbon <mcgibbon@uw.edu>
2025-03-13 10:06:40 -0400

    Add SerializableStep (#1661)
    This PR introduces SerializableStep, which can be saved to or loaded
    from a serialization dictionary.
    
    This change allows Step classes to worry only about internal module
    state, and not about providing arguments needed to construct themselves.
    In the existing modules this reduces duplicated grid serialization
    between each step object. In the upcoming MultiCallStep object
    (https://github.com/ai2cm/full-model/pull/1660), it is very difficult to
    write a `from_state` function without this change.
    
    The idea is that Stepper will depend directly on SerializableStep, which
    will internally contain a StepABC.
    
    StepABC no longer needs a `cls.from_state` with this change.
    
    Changes:
    - Removed `from_state` from StepABC and SeparateRadationStep
    - Added SerializableStep class which wraps a StepABC instance to make it
    serializable
    
    - [x] Tests added
    
    ---------
    
    Co-authored-by: Oliver Watt-Meyer <oliverwatt@gmail.com>

---
+++ fme/core/step/test_serializable.py
@@ -0,0 +1,27 @@
+import datetime
+
+import torch
+
+from fme.core.coordinates import HybridSigmaPressureCoordinate
+from fme.core.gridded_ops import LatLonOperations
+
+from .serializable import SerializableStep
+from .step import StepSelector
+from .test_step_registry import MockStep
+
+
+def test_serializable_step_round_trip():
+    serializable = SerializableStep.build(
+        selector=StepSelector(type="mock", config={}),
+        img_shape=(16, 32),
+        gridded_operations=LatLonOperations(area_weights=torch.ones(16, 32)),
+        vertical_coordinate=HybridSigmaPressureCoordinate(
+            ak=torch.arange(7), bk=torch.arange(7)
+        ),
+        timestep=datetime.timedelta(hours=6),
+    )
+    assert isinstance(serializable._instance, MockStep)
+    state = serializable.to_state()
+    round_tripped = SerializableStep.from_state(state)
+    round_tripped_state = round_tripped.to_state()
+    assert state == round_tripped_state
--- fme/core/step/test_step.py
+++ fme/core/step/test_step.py
@@ -11,59 +11,75 @@ from fme.core.coordinates import HybridSigmaPressureCoordinate
 from fme.core.gridded_ops import LatLonOperations
 from fme.core.normalizer import NormalizationConfig
 from fme.core.registry import ModuleSelector
-from fme.core.step.step import StepConfigABC
+from fme.core.step.serializable import SerializableStep
+from fme.core.step.step import StepSelector
 from fme.core.typing_ import TensorDict
 
 from .radiation import SeparateRadiationStepConfig
 
-CONFIG_CASES = [
+SEPARATE_RADIATION_CONFIG = SeparateRadiationStepConfig(
+    builder=ModuleSelector(
+        type="SphericalFourierNeuralOperatorNet",
+        config={
+            "scale_factor": 1,
+            "embed_dim": 4,
+            "num_layers": 2,
+        },
+    ),
+    radiation_builder=ModuleSelector(
+        type="SphericalFourierNeuralOperatorNet",
+        config={
+            "scale_factor": 1,
+            "embed_dim": 4,
+            "num_layers": 2,
+        },
+    ),
+    main_prognostic_names=["prog_a", "prog_b"],
+    shared_forcing_names=["forcing_shared"],
+    radiation_only_forcing_names=["forcing_rad"],
+    radiation_diagnostic_names=["diagnostic_rad"],
+    main_diagnostic_names=["diagnostic_main"],
+    normalization=NormalizationConfig(
+        means={
+            name: 0.0
+            for name in [
+                "prog_a",
+                "prog_b",
+                "forcing_shared",
+                "forcing_rad",
+                "diagnostic_rad",
+                "diagnostic_main",
+            ]
+        },
+        stds={
+            name: 1.0
+            for name in [
+                "prog_a",
+                "prog_b",
+                "forcing_shared",
+                "forcing_rad",
+                "diagnostic_rad",
+                "diagnostic_main",
+            ]
+        },
+    ),
+)
+
+SELECTOR_CONFIG_CASES = [
     pytest.param(
-        SeparateRadiationStepConfig(
-            builder=ModuleSelector(
-                type="SphericalFourierNeuralOperatorNet",
-                config={
-                    "scale_factor": 1,
-                    "embed_dim": 4,
-                    "num_layers": 2,
-                },
-            ),
-            radiation_builder=ModuleSelector(
-                type="SphericalFourierNeuralOperatorNet",
-                config={
-                    "scale_factor": 1,
-                    "embed_dim": 4,
-                    "num_layers": 2,
-                },
-            ),
-            main_prognostic_names=["prog_a", "prog_b"],
-            shared_forcing_names=["forcing_shared"],
-            radiation_only_forcing_names=["forcing_rad"],
-            radiation_diagnostic_names=["diagnostic_rad"],
-            main_diagnostic_names=["diagnostic_main"],
-            normalization=NormalizationConfig(
-                means={
-                    name: 0.0
-                    for name in [
-                        "prog_a",
-                        "prog_b",
-                        "forcing_shared",
-                        "forcing_rad",
-                        "diagnostic_rad",
-                        "diagnostic_main",
-                    ]
-                },
-                stds={
-                    name: 1.0
-                    for name in [
-                        "prog_a",
-                        "prog_b",
-                        "forcing_shared",
-                        "forcing_rad",
-                        "diagnostic_rad",
-                        "diagnostic_main",
-                    ]
-                },
-            ),
+        StepSelector(
+            type="separate_radiation",
+            config=dataclasses.asdict(SEPARATE_RADIATION_CONFIG),
+        ),
+        id="separate_radiation",
+    ),
+]
+
+HAS_NEXT_STEP_FORCING_NAME_CASES = [
+    pytest.param(
+        StepSelector(
+            type="separate_radiation",
+            config=dataclasses.asdict(SEPARATE_RADIATION_CONFIG),
         ),
         id="separate_radiation",
     ),
@@ -85,24 +101,28 @@ def get_tensor_dict(
     return data_dict
 
 
-def get_step(config: StepConfigABC, img_shape: Tuple[int, int]):
+def get_step(selector: StepSelector, img_shape: Tuple[int, int]) -> SerializableStep:
     device = fme.get_device()
     area = torch.ones(img_shape, device=device)
     vertical_coordinate = HybridSigmaPressureCoordinate(
         ak=torch.arange(7), bk=torch.arange(7)
     )
-    return config.get_step(
-        img_shape, LatLonOperations(area), vertical_coordinate, TIMESTEP
+    return SerializableStep.build(
+        selector=selector,
+        img_shape=img_shape,
+        gridded_operations=LatLonOperations(area),
+        vertical_coordinate=vertical_coordinate,
+        timestep=TIMESTEP,
     )
 
 
-@pytest.mark.parametrize("config", CONFIG_CASES)
-def test_reloaded_step_gives_same_prediction(config: StepConfigABC):
+@pytest.mark.parametrize("config", SELECTOR_CONFIG_CASES)
+def test_reloaded_step_gives_same_prediction(config: StepSelector):
     torch.manual_seed(0)
     img_shape = (5, 5)
     n_samples = 5
     step = get_step(config, img_shape)
-    new_step = step.__class__.from_state(step.get_state())
+    new_step = SerializableStep.from_state(step.to_state())
     input_data = get_tensor_dict(step.input_names, img_shape, n_samples)
     next_step_input_data = get_tensor_dict(
         step.next_step_input_names, img_shape, n_samples
@@ -115,38 +135,38 @@ def test_reloaded_step_gives_same_prediction(config: StepConfigABC):
         torch.testing.assert_close(first_result[k], second_result[k])
 
 
-@pytest.mark.parametrize("config", CONFIG_CASES)
-def test_next_step_forcing_names_is_forcing(config: StepConfigABC):
+@pytest.mark.parametrize("config", HAS_NEXT_STEP_FORCING_NAME_CASES)
+def test_next_step_forcing_names_is_forcing(config: StepSelector):
     data = dataclasses.asdict(config)
     img_shape = (5, 5)
     step = get_step(config, img_shape)
     forcing_names = set(step.input_names).difference(step.output_names)
-    data["next_step_forcing_names"] = [list(forcing_names)[0]]
+    data["config"]["next_step_forcing_names"] = [list(forcing_names)[0]]
     dacite.from_dict(config.__class__, data, config=dacite.Config(strict=True))
 
 
-@pytest.mark.parametrize("config", CONFIG_CASES)
-def test_next_step_forcing_names_is_prognostic(config: StepConfigABC):
+@pytest.mark.parametrize("config", HAS_NEXT_STEP_FORCING_NAME_CASES)
+def test_next_step_forcing_names_is_prognostic(config: StepSelector):
     data = dataclasses.asdict(config)
     img_shape = (5, 5)
     step = get_step(config, img_shape)
     prognostic_names = set(step.output_names).intersection(step.input_names)
     name = list(prognostic_names)[0]
-    data["next_step_forcing_names"] = [name]
+    data["config"]["next_step_forcing_names"] = [name]
     with pytest.raises(ValueError) as err:
         dacite.from_dict(config.__class__, data, config=dacite.Config(strict=True))
     assert "next_step_forcing_name" in str(err.value)
     assert name in str(err.value)
 
 
-@pytest.mark.parametrize("config", CONFIG_CASES)
-def test_next_step_forcing_names_is_diagnostic(config: StepConfigABC):
+@pytest.mark.parametrize("config", HAS_NEXT_STEP_FORCING_NAME_CASES)
+def test_next_step_forcing_names_is_diagnostic(config: StepSelector):
     data = dataclasses.asdict(config)
     img_shape = (5, 5)
     step = get_step(config, img_shape)
     diagnostic_names = set(step.output_names).difference(step.input_names)
     name = list(diagnostic_names)[0]
-    data["next_step_forcing_names"] = [name]
+    data["config"]["next_step_forcing_names"] = [name]
     with pytest.raises(ValueError) as err:
         dacite.from_dict(config.__class__, data, config=dacite.Config(strict=True))
     assert "next_step_forcing_name" in str(err.value)
