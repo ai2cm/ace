d63ce173d0b2dc0cc6c21e205f220dcf83e634ab
James Duncan <jpdunc23@users.noreply.github.com>
2025-03-28 09:35:00 -0700

    Allow config which disables logging validation maps (#1729)
    The `epoch_validation_seconds` can often dominate the
    `epoch_total_seconds`, outpacing `epoch_train_seconds`. This PR attempts
    to reduce the validation burden by allowing configurations which disable
    creation of validation maps (`snapshot` and `mean_map`) during training
    via `OneStepAggregatorConfig`. This could be helpful for doing many
    random seed runs or hyperparameter optimization (HPO).
    
    Changes:
    - Adds the `validation_aggregator: OneStepAggregatorConfig` config
    option to `fme.ace.train.train_config.TrainConfig` and
    `validation_config: OneStepAggregatorConfig` init arg to
    `fme.ace.train.train.AggregatorBuilder`.
    - `AggregatorBuilder.get_validation_aggregator` now builds the
    `OneStepAggregator` via the `OneStepAggregatorConfig.build` method.
    - Adds `include_snapshots` and `include_mean_maps` init args to
    `OneStepAggregator`.
    
    - [x] Tests added
    
    ---------
    
    Co-authored-by: Jeremy McGibbon <mcgibbon@uw.edu>

--- fme/ace/aggregator/__init__.py
+++ fme/ace/aggregator/__init__.py
@@ -1,4 +1,4 @@
 from .inference import InferenceEvaluatorAggregator, InferenceEvaluatorAggregatorConfig
 from .null import NullAggregator
-from .one_step import OneStepAggregator
+from .one_step import OneStepAggregator, OneStepAggregatorConfig
 from .train import TrainAggregator
