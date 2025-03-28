ai2cm/ace

commit 580262438b8a232faf3cd3df474072fbb311c61b

Vertically coarsen land variables (#1679)
LM4 has 20 levels of total moisture in a layer and soil temperature, for
the land model emulator we want to vertically coarsen this to match
ERA5's 4 levels. For the soil temperature, using height thickness-based
weight mean for the coarsening is appropriate and for total moisture of
a soil level using a simple sum over the layers will conserve volumetric
water content. This PR changes `compute_dataset.py` to allow this.

Changes:
- Adds height-based thickness calculation for the vertical coordinates
of the land model in `compute_dataset.py`
- Allows for the passing of a separate list of indices to coarsen over
for the 3D land variables in `compute_dataset.py`
- Adds function to do the vertical coarsening of 3D land variables
- Changes existing config (CM4-piControl-atmosphere-1deg-8layer-200yr.yaml) to allow for CM4 atmosphere + 3D land variables

---------

Co-authored-by: Spencer Clark <spencerkclark@gmail.com>

scripts/data_process/compute_dataset.py
@@ -1,10 +1,6 @@
 # This script is used to compute a training dataset from the "raw"
 # FV3GFS data stored in zarr form on GCS.
 
-# The dependencies of this script are installed in the "fv3net" conda environment
-# which can be installed using fv3net's Makefile. See
-# https://github.com/ai2cm/fv3net/blob/8ed295cf0b8ca49e24ae5d6dd00f57e8b30169ac/Makefile#L310
-
 # The resulting dataset is about 194GB (the input is about 2.5TB). Running this script
 # on my 8-CPU VM takes about 2.5 hours. See "compute_dataset_fv3gfs_argo_workflow.yaml"
 # for a workflow which parallelizes this script across the 11-member ensemble and runs
@@ -84,8 +80,17 @@ class StandardNameMapping(StandardDimMapping):
     land_fraction: str = "land_fraction"
     ocean_fraction: str = "ocean_fraction"
     sea_ice_fraction: str = "sea_ice_fraction"
+    vertical_dim_land: str = "zfull_soil"
+    height_thickness: str = "height_thickness_of_land_layer"
+    total_moisture_content_of_soil_layer: str = "total_moisture_content_of_soil_layer"
     hybrid_level_coeffs: List[str] = dataclasses.field(default_factory=list)
     additional_vertically_resolved_names: List[str] = dataclasses.field(
+        default_factory=list
+    )
+    land_names_to_vertically_coarsen_by_height_weighting: List[str] = dataclasses.field(
+        default_factory=list
+    )
+    land_names_to_vertically_coarsen_by_sum: List[str] = dataclasses.field(
         default_factory=list
     )
 
@@ -105,11 +110,19 @@ class StandardNameMapping(StandardDimMapping):
             self.eastward_wind,
         ] + self.additional_vertically_resolved_names
 
+        self.vertically_resolved_names_land: List[str] = (
+            self.land_names_to_vertically_coarsen_by_height_weighting
+            + self.land_names_to_vertically_coarsen_by_sum
+        )
+
         # variables to drop after all derived variables are computed
         self.dropped_variables: List[str] = (
             self.water_species
             + self.vertically_resolved
             + [self.pressure_thickness, self.vertical_dim]
+            + self.land_names_to_vertically_coarsen_by_height_weighting
+            + self.land_names_to_vertically_coarsen_by_sum
+            + [self.vertical_dim_land]
         )
         for name in [
             self.precipitable_water_path,
@@ -241,6 +254,12 @@ class DatasetComputationConfig:
             with chunks as specified in ``chunking``.
         time_invariant_dir: (optional) path to directory containing time-invariant data
             This option is used for E3SMv2 dataset.
+        vertical_coarsening_indices_land: (optional) list of tuples defining the ranges
+            of reference levels that go into each vertically coarsened layer for the
+            land model variables.
+        reference_vertical_coordinate_file_land: (optional) path to netCDF file
+            containing vertical coordinate definition for the land model of the
+            reference simulation.
     """
 
     reference_vertical_coordinate_file: str
@@ -259,6 +278,8 @@ class DatasetComputationConfig:
         default_factory=lambda: ChunkingConfig(time_dim=360)
     )
     time_invariant_dir: Optional[str] = None
+    vertical_coarsening_indices_land: Optional[Sequence[Tuple[int, int]]] = None
+    reference_vertical_coordinate_file_land: Optional[str] = None
 
 
 @dataclasses.dataclass
@@ -466,6 +487,60 @@ def compute_vertical_coarsening(
     return ds.assign(coarsened_arrays)
 
 
+def compute_vertical_coarsening_land(
+    ds: xr.Dataset,
+    vertically_resolved_names: Sequence[str],
+    interface_indices: Optional[Sequence[Tuple[int, int]]],
+    vertical_coordinate_file: Optional[str],
+    dim: str,
+    height_thickness_name: str,
+    summed_variables: Sequence[str],
+) -> xr.Dataset:
+    """Compute vertical coarsening of 3D land variables by height-weighted mean or
+    unweighted sum. Outputs are saved as new variables in the dataset with the
+    name '{name}_{i}' where i is the new coarse vertical level index. Variables are
+    coarsened by a height-weighted mean by default. Variables listed in
+    summed_variables are coarsened using an unweighted sum. This is useful, for
+    example, for the total_moisture_content_of_soil_layer in LM4, which has units
+    of total moisture (kg / m^2).
+    """
+
+    assert interface_indices is not None, (
+        "Land variables for coarsening are provided, but there"
+        "are not corresponding coarsening indices"
+    )
+
+    assert vertical_coordinate_file is not None, (
+        "Land variables for coarsening are provided, but there"
+        "is not a corresponding reference file"
+    )
+
+    if not vertically_resolved_names:
+        return ds
+
+    with fsspec.open(vertical_coordinate_file) as f:
+        thickness = xr.open_dataset(f).load()
+
+    coarsened_arrays = {}
+    for i, (start, end) in enumerate(interface_indices):
+        height_thickness = thickness[height_thickness_name].isel(
+            {dim: slice(start, end)}
+        )
+        for name in vertically_resolved_names:
+            array_slice = ds[name].isel({dim: slice(start, end)})
+
+            # some land variables are total quantity in layer so just need to sum
+            if name in summed_variables:
+                coarsened_da = array_slice.sum(dim)
+            else:
+                coarsened_da = weighted_mean(array_slice, height_thickness, dim)
+
+            current_long_name = array_slice.long_name
+            coarsened_da.attrs["long_name"] = current_long_name + f" level-{i}"
+            coarsened_arrays[f"{name}_{i}"] = coarsened_da
+    return ds.assign(coarsened_arrays)
+
+
 def compute_tendencies(
     ds: xr.Dataset, time_derivative_names: Sequence[str], dim: str
 ) -> xr.Dataset:
@@ -643,6 +718,18 @@ def construct_lazy_dataset(
         dim=standard_names.vertical_dim,
         pressure_thickness_name=standard_names.pressure_thickness,
     )
+
+    if standard_names.vertically_resolved_names_land:
+        ds = compute_vertical_coarsening_land(
+            ds,
+            vertically_resolved_names=standard_names.vertically_resolved_names_land,
+            interface_indices=config.vertical_coarsening_indices_land,
+            vertical_coordinate_file=config.reference_vertical_coordinate_file_land,
+            dim=standard_names.vertical_dim_land,
+            height_thickness_name=standard_names.height_thickness,
+            summed_variables=standard_names.land_names_to_vertically_coarsen_by_sum,
+        )
+
     ds = compute_column_moisture_integral(
         ds,
         input_name=standard_names.specific_total_water,

scripts/data_process/configs/CM4-piControl-atmosphere-1deg-8layer-200yr.yaml
@@ -1,15 +1,16 @@
 runs:
-  2024-11-15-CM4-piControl-atmosphere-1deg-8layer-200yr: gs://vcm-ml-raw-flexible-retention/2024-11-01-CM4-piControl/regridded-zarrs/gaussian_grid_180_by_360/pre-industrial-control
+  2025-03-21-CM4-piControl-atmosphere-land-1deg-8layer-200yr: gs://vcm-ml-raw-flexible-retention/2024-11-01-CM4-piControl/regridded-zarrs/gaussian_grid_180_by_360/pre-industrial-control
 data_output_directory: gs://vcm-ml-intermediate
 stats:
-  output_directory: gs://vcm-ml-intermediate/2024-11-15-CM4-piControl-atmosphere-1deg-8layer-200yr-stats
+  output_directory: gs://vcm-ml-intermediate/2025-03-21-CM4-piControl-atmosphere-land-1deg-8layer-200yr-stats
   start_date: "0151-01-01T06:00:00"
   end_date: "0351-01-01T00:00:00"
   data_type: CM4
-  beaker_dataset: 2024-11-15-CM4-piControl-atmosphere-1deg-8layer-200yr
+  beaker_dataset: 2025-03-21-CM4-piControl-atmosphere-land-1deg-8layer-200yr
 dataset_computation:
   n_split: 457 # aiming small partitions avoid overloading dask; 200 years * 1461 steps/yr / 160 steps/chunk / 4 chunks/partition ~= 457 partitions
   reference_vertical_coordinate_file: gs://vcm-ml-raw-flexible-retention/2024-08-10-pre-industrial-CM4-simulation/vertical-coordinate-file/fv_core.res.nc
+  reference_vertical_coordinate_file_land: gs://vcm-ml-raw-flexible-retention/2024-11-01-CM4-piControl/land-vertical-coordinate-file/land-vertical-coordinate.nc
   vertical_coarsening_indices:
     # computed here: https://github.com/ai2cm/explore/blob/master/jamesd/2024-08-13-pre-industiral-CM4-eda/2024-08-28-AM4-vertical-indices.ipynb
     - [0, 7]
@@ -20,6 +21,12 @@ dataset_computation:
     - [18, 22]
     - [22, 25]
     - [25, 33]
+  vertical_coarsening_indices_land:
+    # computed here: https://github.com/ai2cm/explore2/blob/main/troya/2025-02-28-LS-evaluation/soil-levels-lm4.ipynb
+    - [0, 2]
+    - [2, 6]
+    - [6, 10]
+    - [10, 15]
   renaming:
     specific_humidity_at_two_meters: Q2m
     air_temperature_at_two_meters: TMP2m
@@ -71,8 +78,8 @@ dataset_computation:
       - downward_longwave_radiative_flux
       - net_surface_longwave_radiative_flux
       # below are vertically resolved soil layer variables -- need to be coarsened
-      # - total_moisture_content_of_soil_layer
-      # - temperature_of_soil_layer
+      - total_moisture_content_of_soil_layer
+      - temperature_of_soil_layer
     land_static.zarr:
       - land_fraction
     ice_6hourly.zarr:
@@ -89,3 +96,7 @@ dataset_computation:
     surface_snow_rate: None
     surface_ice_rate: None
     surface_graupel_rate: None
+    land_names_to_vertically_coarsen_by_height_weighting:
+      - temperature_of_soil_layer
+    land_names_to_vertically_coarsen_by_sum:
+      - total_moisture_content_of_soil_layer

commit 74f7174253fb36ca51ccd12c89320ee8d91a2a6c

Fix data processing error when there is no land vertical coordinate (#1744)
This PR fixes a minor issue introduced in #1679. In datasets where we do
vertically coarsen any land fields, like SHiELD datasets, we do not have
a land vertical coordinate to drop, so we encounter the following error:
```
Traceback (most recent call last):
  File "//script.py", line 892, in <module>
    main()
  File "/usr/local/lib/python3.12/site-packages/click/core.py", line 1161, in __call__
    return self.main(*args, **kwargs)
           ^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/usr/local/lib/python3.12/site-packages/click/core.py", line 1082, in main
    rv = self.invoke(ctx)
         ^^^^^^^^^^^^^^^^
  File "/usr/local/lib/python3.12/site-packages/click/core.py", line 1443, in invoke
    return ctx.invoke(self.callback, **ctx.params)
           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/usr/local/lib/python3.12/site-packages/click/core.py", line 788, in invoke
    return __callback(*args, **kwargs)
           ^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "//script.py", line 861, in main
    ds = ds.drop_vars(standard_names.dropped_variables)
         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/usr/local/lib/python3.12/site-packages/xarray/core/dataset.py", line 6118, in drop_vars
    self._assert_all_in_dataset(names_set)
  File "/usr/local/lib/python3.12/site-packages/xarray/core/dataset.py", line 5982, in _assert_all_in_dataset
    raise ValueError(
ValueError: These variables cannot be found in this dataset: ['zfull_soil']
```
A simple fix is to only specify this as a dropped variable if vertically
resolved land fields exist.

Changes:
- Only specify that we would like to drop the land vertical coordinate
when vertically resolved land fields exist.
- Slightly simplify the logic for defining dropped land variables by
using `self.vertically_resolved_names_land` instead of
`self.land_names_to_vertically_coarsen_by_height_weighting +
self.land_names_to_vertically_coarsen_by_sum`.

scripts/data_process/compute_dataset.py
@@ -120,9 +120,7 @@ class StandardNameMapping(StandardDimMapping):
             self.water_species
             + self.vertically_resolved
             + [self.pressure_thickness, self.vertical_dim]
-            + self.land_names_to_vertically_coarsen_by_height_weighting
-            + self.land_names_to_vertically_coarsen_by_sum
-            + [self.vertical_dim_land]
+            + self.vertically_resolved_names_land
         )
         for name in [
             self.precipitable_water_path,
@@ -132,6 +130,8 @@ class StandardNameMapping(StandardDimMapping):
         ]:
             if name.lower() != "none":
                 self.dropped_variables.append(name)
+        if self.vertically_resolved_names_land:
+            self.dropped_variables.append(self.vertical_dim_land)
 
     @property
     def water_species(self) -> List[str]: