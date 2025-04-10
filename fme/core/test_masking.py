f954e58cee823768793516c3db907c542eeb4f2c
James Duncan <jpdunc23@users.noreply.github.com>
2025-04-10 11:21:18 -0700

    Allow channel-specific fill values in `StaticMasking` (#1766)
    Allow channel-specific fill values in `StaticMasking` (e.g., means).
    
    Changes:
    - `StaticMaskingConfig.fill_value` can now be "means", in which case the
    build method expects a non empty `fill_values: TensorMapping`.
    
    - [x] Tests added

--- fme/core/test_masking.py
+++ fme/core/test_masking.py
@@ -5,7 +5,7 @@ import pytest
 import torch
 
 import fme
-from fme.core.masking import StaticMaskingConfig
+from fme.core.masking import StaticMasking, StaticMaskingConfig
 
 DEVICE = fme.get_device()
 
@@ -22,6 +22,8 @@ class _Mask:
         self.mask_3d = mask_3d
 
     def get_mask_tensor_for(self, name) -> Optional[torch.Tensor]:
+        if name == "mask_ignored":
+            return None
         match = self.LEVEL_PATTERN.search(name)
         if match:
             # 3D variable
@@ -32,13 +34,6 @@ class _Mask:
         else:
             # 2D variable
             return self.mask_2d
-
-        if name in self.mask_2d:
-            return self.mask_2d[name]
-        elif name in self.mask_3d:
-            return self.mask_3d[name]
-        else:
-            return None
 
 
 def test_masking_config():
@@ -58,20 +53,28 @@ def test_masking_config():
             fill_value=0.0,
         )
 
+    config = StaticMaskingConfig(
+        variable_names_and_prefixes=["c"],
+        mask_value=1,
+        fill_value="mean",
+    )
+    with pytest.raises(ValueError, match="fill_values mapping required"):
+        _ = config.build(_Mask(mask_2d=torch.ones(1, 1)))
+
 
 _SIZE = (4, 4)
 
-_MASK_2D = torch.ones(_SIZE, device=fme.get_device())
-_MASK_3D = torch.ones(_SIZE + (2,), device=fme.get_device())
+_MASK_2D = torch.ones(_SIZE, device=DEVICE)
+_MASK_3D = torch.ones(_SIZE + (2,), device=DEVICE)
 _MASK_2D[1, 1] = 0
 _MASK_3D[0, :, 0] = 0
 _MASK_3D[1, :, 1] = 0
 
 
 _DATA = {
-    "PRESsfc": 10.0 + torch.rand(size=_SIZE, device=fme.get_device()),
-    "specific_total_water_0": torch.rand(size=_SIZE, device=fme.get_device()),
-    "specific_total_water_1": torch.rand(size=_SIZE, device=fme.get_device()),
+    "PRESsfc": 10.0 + torch.rand(size=_SIZE, device=DEVICE),
+    "specific_total_water_0": torch.rand(size=_SIZE, device=DEVICE),
+    "specific_total_water_1": torch.rand(size=_SIZE, device=DEVICE),
 }
 
 
@@ -100,6 +103,27 @@ def test_masking(var_names_and_prefixes):
     else:
         assert torch.all(output["specific_total_water_1"][1, :] == 0.0)
     assert torch.all(output["specific_total_water_1"][0, :] != 0.0)
+
+
+def test_masking_with_means():
+    config = StaticMaskingConfig(
+        mask_value=0,
+        fill_value="mean",
+    )
+    mask = config.build(
+        mask=_Mask(_MASK_2D, _MASK_3D),
+        fill_values={
+            "PRESsfc": torch.tensor(1.0, device=DEVICE),
+            "specific_total_water_0": torch.tensor(2.0, device=DEVICE),
+            "specific_total_water_1": torch.tensor(3.0, device=DEVICE),
+        },
+    )
+    output = mask(_DATA)
+    assert output["PRESsfc"][1, 1] == 1.0
+    assert output["PRESsfc"][0, 1] != 1.0
+    assert torch.all(output["specific_total_water_0"][0, :] == 2.0)
+    assert torch.all(output["specific_total_water_1"][1, :] == 3.0)
+    assert torch.all(output["specific_total_water_1"][0, :] != 3.0)
 
 
 def test_masking_no_3d_masking():
@@ -155,3 +179,32 @@ def test_masking_missing_3d_mask():
     assert masked["PRESsfc"][0, 1] != 0.0
     for name in ["specific_total_water_0", "specific_total_water_1"]:
         torch.testing.assert_close(masked[name], _DATA[name])
+
+
+def test_static_masking_error_on_missing_mean():
+    mask = StaticMasking(
+        mask_value=0,
+        fill_value={
+            "specific_total_water_0": torch.tensor(2.0, device=DEVICE),
+            "specific_total_water_1": torch.tensor(3.0, device=DEVICE),
+        },
+        mask=_Mask(_MASK_2D, _MASK_3D),
+    )
+    with pytest.raises(KeyError, match="missing key 'PRESsfc'"):
+        _ = mask(_DATA)
+
+
+def test_static_masking_mask_ignored_name():
+    mask = StaticMasking(
+        mask_value=0,
+        fill_value=float("nan"),
+        mask=_Mask(_MASK_2D, _MASK_3D),
+    )
+    data = {
+        "masked": torch.rand(size=_SIZE, device=DEVICE),
+        "mask_ignored": torch.rand(size=_SIZE, device=DEVICE),
+    }
+    masked = mask(data)
+    assert masked["masked"][1, 1].isnan()
+    # no change to "mask_ignored" because _Mask gives it special treatment
+    torch.testing.assert_close(masked["mask_ignored"], data["mask_ignored"])
