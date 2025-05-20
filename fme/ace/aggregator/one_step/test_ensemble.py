734446ee72c4fdafe775152fe6b517a513329ca6
Jeremy McGibbon <mcgibbon@uw.edu>
2025-05-20 12:23:24 -0400

    Fix bug in implementation of SSR bias (#1911)
    SSR bias has bugs which currently lead it to be 0.5 for an unbiased
    model, and to decrease instead of increase when spread is increased
    (it's the spread skill ratio). This PR fixes these issues and adds unit
    tests.
    
    - [x] Tests added

--- fme/ace/aggregator/one_step/test_ensemble.py
+++ fme/ace/aggregator/one_step/test_ensemble.py
@@ -23,6 +23,7 @@ def test_crps_metric_gives_correct_shape():
 
 
 def test_ssr_metric_gives_correct_shape():
+    torch.manual_seed(0)
     metric = SSRBiasMetric()
     n_batch = 10
     n_sample = 2
@@ -35,3 +36,38 @@ def test_ssr_metric_gives_correct_shape():
     got = metric.get()
     assert isinstance(got, torch.Tensor)
     assert got.shape == (n_y, n_x)
+
+
+def test_ssr_bias_metric_unbiased():
+    torch.manual_seed(0)
+    metric = SSRBiasMetric()
+    n_batch = 5000
+    n_sample = 2
+    n_time = 3
+    n_y = 4
+    n_x = 5
+    # identical distribution for target and gen
+    target = get_tensor((n_batch, 1, n_time, n_y, n_x))
+    gen = get_tensor((n_batch, n_sample, n_time, n_y, n_x))
+    metric.record(target=target, gen=gen)
+    got = metric.get()
+    assert isinstance(got, torch.Tensor)
+    assert got.shape == (n_y, n_x)
+    torch.testing.assert_close(got.mean(), torch.tensor(0.0), atol=1e-2, rtol=0.0)
+
+
+def test_ssr_bias_metric_doubled_spread():
+    torch.manual_seed(0)
+    metric = SSRBiasMetric()
+    n_batch = 5000
+    n_sample = 2
+    n_time = 3
+    n_y = 4
+    n_x = 5
+    target = get_tensor((n_batch, 1, n_time, n_y, n_x))
+    gen = get_tensor((n_batch, n_sample, n_time, n_y, n_x)) * 2
+    metric.record(target=target, gen=gen)
+    got = metric.get()
+    assert isinstance(got, torch.Tensor)
+    assert got.shape == (n_y, n_x)
+    torch.testing.assert_close(got.mean(), torch.tensor(1.0), atol=1e-2, rtol=0.0)
