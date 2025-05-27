04c6abb8ef9bffaa737dd0e079ee81b7aaff160e
Oliver Watt-Meyer <oliverwatt@gmail.com>
2025-05-27 10:57:18 -0700

    Add more logging to ACE data loading benchmark (#1933)
    Add more logging (including to wandb) for the
    `fme.ace.data_loading.benchmark` script.
    
    ---------
    
    Co-authored-by: Spencer Clark <spencerkclark@gmail.com>

---
+++ configs/experiments/2025-05-23-ace-data-loading-benchmark/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+JOB_NAME="ace-data-benchmark-v1-30-steps-16dw"  # recommended but not required to change this
+CONFIG_PATH=configs/experiments/2025-05-23-ace-data-loading-benchmark/config.yaml  # relative to the root of the repository
+
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+WANDB_USERNAME=${WANDB_USERNAME:-${BEAKER_USERNAME}}
+WANDB_PROJECT=ace-data-benchmark
+
+gantry run \
+    --name $JOB_NAME \
+    --description 'Benchmark ACE data loading' \
+    --beaker-image jeremym/fme-deps-only-5a6649b8 \
+    --workspace ai2/ace \
+    --priority normal \
+    --cluster ai2/rhea-cirrascale \
+    --shared-memory 200GiB \
+    --env WANDB_USERNAME=$WANDB_USERNAME \
+    --env WANDB_NAME=$JOB_NAME \
+    --env WANDB_JOB_TYPE=benchmark \
+    --env WANDB_PROJECT=$WANDB_PROJECT \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --weka climate-default:/climate-default \
+    --budget ai2/climate \
+    --no-conda \
+    --install "pip install --no-deps ." \
+    -- python -m fme.ace.data_loading.benchmark $CONFIG_PATH
