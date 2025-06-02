ai2cm/ace

commit 7891d8e929297f6925f9999ed267e349b7948d24

Dont open ds in __get_item__ for zarr case (#1937)
Minor performance enhancement when using `zarr` engine in data loader.
For ACE data loading, I found this gave about a 10-15% increase in
throughput.

fme/core/dataset/xarray.py
@@ -616,11 +616,12 @@ class XarrayDataset(torch.utils.data.Dataset):
         idxs = range(input_file_idx, output_file_idx + 1)
         total_steps = 0
         for i, file_idx in enumerate(idxs):
-            ds = self._open_file(file_idx)
-            ds = ds.isel(**self.isel)
-
             start = input_local_idx if i == 0 else 0
-            stop = output_local_idx if i == len(idxs) - 1 else len(ds["time"]) - 1
+            if i == len(idxs) - 1:
+                stop = output_local_idx
+            else:
+                stop = self.start_indices[i + 1] - self.start_indices[i] - 1
+
             n_steps = stop - start + 1
             shape = [n_steps] + self._shape_excluding_time_after_selection
             total_steps += n_steps
@@ -636,6 +637,8 @@ class XarrayDataset(torch.utils.data.Dataset):
                     nontime_selection=self._isel_tuple,
                 )
             else:
+                ds = self._open_file(file_idx)
+                ds = ds.isel(**self.isel)
                 tensor_dict = load_series_data(
                     idx=start,
                     n_steps=n_steps,
@@ -645,10 +648,10 @@ class XarrayDataset(torch.utils.data.Dataset):
                     final_shape=shape,
                     fill_nans=self.fill_nans,
                 )
+                ds.close()
+                del ds
             for n in self._time_dependent_names:
                 arrays.setdefault(n, []).append(tensor_dict[n])
-            ds.close()
-            del ds
 
         tensors: TensorDict = {}
         for n, tensor_list in arrays.items():

configs/experiments/2025-05-23-ace-data-loading-benchmark/run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-JOB_NAME="ace-data-benchmark-v1-30-steps-16dw"  # recommended but not required to change this
+JOB_NAME="ace-data-benchmark-v2-30-steps-16dw-noopen"  # recommended but not required to change this
 CONFIG_PATH=configs/experiments/2025-05-23-ace-data-loading-benchmark/config.yaml  # relative to the root of the repository
 
 BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')

commit 3f9328087e9f384adc63ce46d6b7f9973d12493c

Fix multi-file data loading bug introduced in #1937 (#1944)
This PR fixes the multi-file data loading bug introduced in #1937. The
test added fails before this PR with this error:

```
_________________________________________________ test_XarrayDataset_monthly[mock_monthly_netcdfs-netcdf4-*.nc-2003_04_30_21 (test for GH #1942)] _________________________________________________

global_idx = 487, mock_data_fixture = 'mock_monthly_netcdfs', engine = 'netcdf4', file_pattern = '*.nc'
request = <FixtureRequest for <Function test_XarrayDataset_monthly[mock_monthly_netcdfs-netcdf4-*.nc-2003_04_30_21 (test for GH #1942)]>>

    @pytest.mark.parametrize(
        "global_idx",
        [
            pytest.param(31 * 8 - 1, id="monthly_XarrayDataset_2003_03_31_21"),
            pytest.param((31 + 30 + 20) * 8 - 1, id="monthly_XarrayDataset_2003_05_20_21"),
            pytest.param((31 + 30) * 8 - 1, id="2003_04_30_21 (test for GH #1942)"),
        ],
    )
    @pytest.mark.parametrize(
        "mock_data_fixture, engine, file_pattern",
        [
            ("mock_monthly_netcdfs", "netcdf4", "*.nc"),
            ("mock_monthly_zarr", "zarr", "*.zarr"),
        ],
    )
    def test_XarrayDataset_monthly(
        global_idx, mock_data_fixture, engine, file_pattern, request
    ):
        mock_data: MockData = request.getfixturevalue(mock_data_fixture)
>       _test_monthly_values(
            mock_data, global_idx, file_pattern=file_pattern, engine=engine
        )

test_xarray.py:450:
_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
test_xarray.py:407: in _test_monthly_values
    arrays, time = dataset[global_idx]
xarray.py:602: in __getitem__
    return self.get_sample_by_time_slice(time_slice)
xarray.py:642: in get_sample_by_time_slice
    tensor_dict = load_series_data(

fme/core/dataset/xarray.py
@@ -620,7 +620,9 @@ class XarrayDataset(torch.utils.data.Dataset):
             if i == len(idxs) - 1:
                 stop = output_local_idx
             else:
-                stop = self.start_indices[i + 1] - self.start_indices[i] - 1
+                stop = (
+                    self.start_indices[file_idx + 1] - self.start_indices[file_idx] - 1
+                )
 
             n_steps = stop - start + 1
             shape = [n_steps] + self._shape_excluding_time_after_selection

fme/core/dataset/test_xarray.py
@@ -433,6 +433,7 @@ def _test_monthly_values(
     [
         pytest.param(31 * 8 - 1, id="monthly_XarrayDataset_2003_03_31_21"),
         pytest.param((31 + 30 + 20) * 8 - 1, id="monthly_XarrayDataset_2003_05_20_21"),
+        pytest.param((31 + 30) * 8 - 1, id="2003_04_30_21 (test for GH #1942)"),
     ],
 )
 @pytest.mark.parametrize(