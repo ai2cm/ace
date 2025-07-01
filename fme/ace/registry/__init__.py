416e3eafaa2d6d694718519c193f7a930cd84f04
Troy Arcomano <troya@allenai.org>
2025-07-01 15:54:31 -0600

    Add the ability to have a column-based LSM emulator in ACE  (#1851)
    Add a column-based (MLP) model to FME with the intent of being used for
    a LSM emulator.
    Changes:
    - Add LandNet and tests to models directory
    - Add last experiment for offline training of LSM emulator
    
    TODOs
    - [x] Tests added
    - [x] Create baseline offline model and run evaluation

--- fme/ace/registry/__init__.py
+++ fme/ace/registry/__init__.py
@@ -1,8 +1,9 @@
 # import modules so they are registered
+from . import land_net as _landnet
 from . import m2lines as _m2lines
 from . import prebuilt as _prebuilt
 from . import sfno as _sfno
 from . import stochastic_sfno as _sfno_crps
 from .registry import ModuleSelector
 
-del _prebuilt, _sfno, _m2lines
+del _prebuilt, _sfno, _m2lines, _landnet
