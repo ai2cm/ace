ae8351aecc0914d2f552222b05446ff422d571a1
James Duncan <jpduncan@berkeley.edu>
2025-08-08 15:49:35 -0700

    Add `atmos/` experiment directory

---
+++ configs/experiments/2025-08-08-jamesd/atmos/train.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+set -e
+
+if [[ "$#" -ne 1 ]]; then
+  echo "Usage: $0 <config_subdirectory>"
+  echo "  - <config_subdirectory>: Subdirectory containing the 'ace-train-config.yaml' to use."
+  exit 1
+fi
+
+# The subdirectory (passed as an argument) that holds the config file.
+CONFIG_SUBDIR=$1
+
+# Get the absolute directory where this script is located.
+SCRIPT_DIR=$(git rev-parse --show-prefix)
+
+# Construct the full path to the specified configuration file.
+CONFIG_FILENAME="ace-train-config.yaml"
+CONFIG_PATH="$SCRIPT_DIR/$CONFIG_SUBDIR/$CONFIG_FILENAME"
+
+# Since we use a service account API key for wandb, we use the beaker username to set the wandb username.
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+REPO_ROOT=$(git rev-parse --show-toplevel)
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+N_GPUS=4
+
+# FIXME: this needs to be per-task configurable
+ATMOS_STATS_DATA=jamesd/2025-08-07-cm4-piControl-200yr-coupled-stats-atmosphere
+
+# Change to the repo root so paths are valid no matter where we run the script from.
+cd "$REPO_ROOT"
+
+while read TRAINING; do
+    GROUP=$(echo "$TRAINING" | cut -d"|" -f1)
+    STATUS=$(echo "$TRAINING" | cut -d"|" -f8)
+    PRIORITY=$(echo "$TRAINING" | cut -d"|" -f9)
+    CLUSTER=$(echo "$TRAINING" | cut -d"|" -f10)
+    RETRIES=$(echo "$TRAINING" | cut -d"|" -f11)
+    OVERRIDE_ARGS=$(echo "$TRAINING" | cut -d"|" -f12)
+    if [[ "$STATUS" != "train" ]]; then
+        continue
+    fi
+    if [[ -z $RETRIES ]]; then
+        RETRIES=0
+    fi
+    JOB_GROUP="${GROUP}"
+    JOB_NAME="${JOB_GROUP}-train"
+    declare -a CLUSTER_ARGS
+    if [[ "$CLUSTER" == "titan" ]];then
+        CLUSTER_ARGS=(
+            --workspace ai2/climate-titan
+            --cluster ai2/titan-cirrascale
+        )
+        N_GPUS=4
+    else
+        CLUSTER_ARGS=(
+            --workspace ai2/climate-ceres
+            --cluster ai2/ceres-cirrascale
+        )
+        N_GPUS=8
+    fi
+
+    # get the template from the config subdir
+    if [[ -n "${OVERRIDE_ARGS}" ]]; then
+        OVERRIDE="--override ${OVERRIDE_ARGS}"
+    else
+        OVERRIDE=""
+    fi
+
+    echo
+    echo "Launching ACE training job:"
+    echo " - Job name: ${JOB_NAME}"
+    echo " - Config: ${CONFIG_PATH}"
+    echo " - Priority: ${PRIORITY}"
+    echo " - Cluster: ${CLUSTER} (${RETRIES} retries)"
+    echo " - Override: ${OVERRIDE_ARGS}"
+
+    python -m fme.ace.validate_config "$CONFIG_PATH" --config_type train $OVERRIDE
+
+    if git status --porcelain "$CONFIG_PATH" | grep -q .; then
+        git add "$CONFIG_PATH"
+        git commit -m"${JOB_NAME}"
+        git push origin "${GIT_BRANCH}"
+    fi
+
+    EXPERIMENT_ID=$(
+        gantry run \
+            --name $JOB_NAME \
+            --description "Run ACE2-CM4 uncoupled pretraining" \
+            --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+            --priority $PRIORITY \
+            --preemptible \
+            --retries $RETRIES \
+            "${CLUSTER_ARGS[@]}" \
+            --weka climate-default:/climate-default \
+            --env WANDB_USERNAME=$BEAKER_USERNAME \
+            --env WANDB_NAME=$JOB_NAME \
+            --env WANDB_JOB_TYPE=training \
+            --env WANDB_RUN_GROUP=$JOB_GROUP \
+            --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+            --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+            --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+            --dataset $ATMOS_STATS_DATA:/statsdata \
+            --gpus $N_GPUS \
+            --shared-memory 800GiB \
+            --budget ai2/climate \
+            --no-conda \
+            --install "pip install --no-deps ." \
+            -- torchrun --nproc_per_node $N_GPUS -m fme.coupled.train "$CONFIG_PATH" $OVERRIDE |
+            tee /dev/tty |
+            grep beaker.org |
+            cut -d/ -f5
+    )
+
+    # remove or change 'training' once completed in order to submit an evaluator job
+    { echo;
+      echo "${JOB_GROUP}|${EXPERIMENT_ID}|training|best_inference_ckpt|normal|--not-preemptible";
+    } >> "${SCRIPT_DIR}/${CONFIG_SUBDIR}/experiments.txt"
+
+    git add "${SCRIPT_DIR}/${CONFIG_SUBDIR}/experiments.txt"
+    git commit -m"Update atmos/${CONFIG_SUBDIR}/experiments.txt"
+    git push origin "${GIT_BRANCH}"
+
+done <"${SCRIPT_DIR}/${CONFIG_SUBDIR}/training.txt"
