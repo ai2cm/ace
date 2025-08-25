2b0ca3321826358a3bd552d0c800e228a248a5ef
James Duncan <jpduncan@berkeley.edu>
2025-08-25 16:21:33 -0700

    Fix test

--- fme/core/generics/test_trainer.py
+++ fme/core/generics/test_trainer.py
@@ -219,6 +219,7 @@ class Config:
     segment_epochs: int | None = None
     evaluate_before_training: bool = False
     save_best_inference_epoch_checkpoints: bool = False
+    resume_results_dir: str | None = None
 
     def __post_init__(self):
         start_epoch = 0 if self.evaluate_before_training else 1
