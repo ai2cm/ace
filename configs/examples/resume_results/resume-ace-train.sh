d4a9202d170307e226477b83483ec6305afdac04
James Duncan <jpduncan@berkeley.edu>
2025-08-28 16:45:32 -0700

    Update resume-ace-train.sh

--- configs/examples/resume_results/resume-ace-train.sh
+++ configs/examples/resume_results/resume-ace-train.sh
@@ -11,8 +11,8 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 # Job configuration
 JOB_GROUP="resume_results"  # Update this to match your experiment group
 JOB_NAME="${JOB_GROUP}-train"
-EXISTING_RESULTS_DATASET="01K32TXSMT883X8M3MW6906HD7"  # Update this with your results dataset ID
-STATS_DATA="jamesd/2025-04-16-cm4-piControl-ocean-atmos-5daily-stats"
+EXISTING_RESULTS_DATASET="01K3SEWJ5S6VFPN54DH4PF36RB"  # Update this with your results dataset ID
+STATS_DATA=jamesd/2025-08-22-cm4-piControl-200yr-coupled-stats-ocean
 N_GPUS=8
 SHARED_MEM="600GiB"
 PRIORITY="urgent" # FIXME
