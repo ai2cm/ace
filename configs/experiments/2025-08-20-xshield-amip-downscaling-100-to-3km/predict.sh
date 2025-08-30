30ecc89da1b9e43530a5eb81160acb6b065f12f5
Anna Kwa <annak@allenai.org>
2025-08-29 21:46:27 -0700

    predict on ace output

---
+++ configs/experiments/2025-08-20-xshield-amip-downscaling-100-to-3km/predict.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# uses the augusta cluster which doesn't have weka access but has GCS access and is
+# typically more available than cirrascale clusters
+
+set -e
+
+JOB_NAME="generate-global-trained-xshield-amip-100km-to-3km-ACE-outputs"
+#JOB_NAME="generate-conus-trained-8x8-100km-to-3km-conus-patched"
+CONFIG_FILENAME="config-generate-on-ace-output-nhem.yaml"
+
+SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+
+ # since we use a service account API key for wandb, we use the beaker username to set the wandb username
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+
+N_NODES=1
+NGPU=4
+
+#IMAGE with B200 pytorch installed
+IMAGE=01JWJ96JMF89D812JS159VF37N
+
+EXISTING_RESULTS_DATASET=01K3W6KD8SP2YD2ZF2SGMF3S5F
+wandb_group=""
+
+gantry run \
+    --name $JOB_NAME \
+    --description 'Run 100km to 3km generation on ACE one step outputs' \
+    --workspace ai2/downscaling \
+    --priority normal \
+    --preemptible \
+    --cluster ai2/titan-cirrascale \
+    --beaker-image $IMAGE \
+    --env WANDB_USERNAME=$BEAKER_USERNAME \
+    --env WANDB_NAME=$JOB_NAME \
+    --env WANDB_JOB_TYPE=inference \
+    --env WANDB_RUN_GROUP=$wandb_group \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $EXISTING_RESULTS_DATASET:/checkpoints \
+    --weka climate-default:/climate-default \
+    --gpus $NGPU \
+    --shared-memory 400GiB \
+    --budget ai2/climate \
+    --no-conda \
+    --install "pip install --no-deps ." \
+    --allow-dirty \
+    -- torchrun --nproc_per_node $NGPU -m fme.downscaling.predict $CONFIG_PATH
