9fb2515350598933e46bcbed8814b32087c108b2
James Duncan <jpduncan@berkeley.edu>
2025-08-30 20:53:09 -0700

    Fix coupled/train.sh

--- configs/experiments/2025-08-08-jamesd/coupled/train.sh
+++ configs/experiments/2025-08-08-jamesd/coupled/train.sh
@@ -154,52 +154,52 @@ while read PRETRAINING; do
 
     python -m fme.coupled.validate_config "$CONFIG_PATH" --config_type train $OVERRIDE
 
-    # if git status --porcelain "$CONFIG_PATH" | grep -q .; then
-    #     git add "$CONFIG_PATH"
-    #     git commit -m"${JOB_NAME}"
-    #     git push origin "${GIT_BRANCH}"
-    # fi
-    # echo
+    if git status --porcelain "$CONFIG_PATH" | grep -q .; then
+        git add "$CONFIG_PATH"
+        git commit -m"${JOB_NAME}"
+        git push origin "${GIT_BRANCH}"
+    fi
+    echo
 
-    # EXPERIMENT_ID=$(
-    #     gantry run \
-    #         --name $JOB_NAME \
-    #         --description "Run coupled training from uncoupled pretraining: ${JOB_GROUP}" \
-    #         --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
-    #         --priority $PRIORITY \
-    #         --preemptible \
-    #         --retries $RETRIES \
-    #         "${CLUSTER_ARGS[@]}" \
-    #         --weka climate-default:/climate-default \
-    #         --env WANDB_USERNAME=$BEAKER_USERNAME \
-    #         --env WANDB_NAME=$JOB_NAME \
-    #         --env WANDB_JOB_TYPE=training \
-    #         --env WANDB_RUN_GROUP=$JOB_GROUP \
-    #         --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
-    #         --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
-    #         --dataset-secret google-credentials:/tmp/google_application_credentials.json \
-    #         --dataset $ATMOS_STATS_DATA:/atmos_stats \
-    #         --dataset $OCEAN_STATS_DATA:/ocean_stats \
-    #         --dataset $EXISTING_RESULTS_ATMOS_DATASET:training_checkpoints/"$ATMOS_CKPT".tar:/atmos_ckpt.tar \
-    #         --dataset $EXISTING_RESULTS_OCEAN_DATASET:training_checkpoints/"$OCEAN_CKPT".tar:/ocean_ckpt.tar \
-    #         --gpus $N_GPUS \
-    #         --shared-memory "${SHARED_MEM}" \
-    #         --budget ai2/climate \
-    #         --no-conda \
-    #         --install "pip install --no-deps ." \
-    #         -- torchrun --nproc_per_node $N_GPUS -m fme.coupled.train "$CONFIG_PATH" $OVERRIDE |
-    #         tee /dev/tty |
-    #         grep beaker.org |
-    #         cut -d/ -f5
-    # )
+    EXPERIMENT_ID=$(
+        gantry run \
+            --name $JOB_NAME \
+            --description "Run coupled training from uncoupled pretraining: ${JOB_GROUP}" \
+            --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+            --priority $PRIORITY \
+            --preemptible \
+            --retries $RETRIES \
+            "${CLUSTER_ARGS[@]}" \
+            --weka climate-default:/climate-default \
+            --env WANDB_USERNAME=$BEAKER_USERNAME \
+            --env WANDB_NAME=$JOB_NAME \
+            --env WANDB_JOB_TYPE=training \
+            --env WANDB_RUN_GROUP=$JOB_GROUP \
+            --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+            --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+            --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+            --dataset $ATMOS_STATS_DATA:/atmos_stats \
+            --dataset $OCEAN_STATS_DATA:/ocean_stats \
+            --dataset $EXISTING_RESULTS_ATMOS_DATASET:training_checkpoints/"$ATMOS_CKPT".tar:/atmos_ckpt.tar \
+            --dataset $EXISTING_RESULTS_OCEAN_DATASET:training_checkpoints/"$OCEAN_CKPT".tar:/ocean_ckpt.tar \
+            --gpus $N_GPUS \
+            --shared-memory "${SHARED_MEM}" \
+            --budget ai2/climate \
+            --no-conda \
+            --install "pip install --no-deps ." \
+            -- torchrun --nproc_per_node $N_GPUS -m fme.coupled.train "$CONFIG_PATH" $OVERRIDE |
+            tee /dev/tty |
+            grep beaker.org |
+            cut -d/ -f5
+    )
 
-    # # remove or change 'training' once completed in order to submit an evaluator job
-    # { echo;
-    #   echo "${JOB_GROUP}|${EXPERIMENT_ID}|training|best_inference_ckpt|normal|--not-preemptible";
-    # } >> "${SCRIPT_DIR}/${CONFIG_SUBDIR}/experiments.txt"
+    # remove or change 'training' once completed in order to submit an evaluator job
+    { echo;
+      echo "${JOB_GROUP}|${EXPERIMENT_ID}|training|best_inference_ckpt|normal|--not-preemptible";
+    } >> "${SCRIPT_DIR}/${CONFIG_SUBDIR}/experiments.txt"
 
-    # git add "${SCRIPT_DIR}/${CONFIG_SUBDIR}/experiments.txt"
-    # git commit -m"Update coupled/${CONFIG_SUBDIR}/experiments.txt"
-    # git push origin "${GIT_BRANCH}"
+    git add "${SCRIPT_DIR}/${CONFIG_SUBDIR}/experiments.txt"
+    git commit -m"Update coupled/${CONFIG_SUBDIR}/experiments.txt"
+    git push origin "${GIT_BRANCH}"
 
 done <"${SCRIPT_DIR}/${CONFIG_SUBDIR}/pretraining.txt"
