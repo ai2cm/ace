9d464ad414367f0a9e827236d3930ca27f0624a9
Troy Arcomano <troya@allenai.org>
2025-09-09 15:15:57 -0700

    Create automatic time coarsening in Zonal Mean Aggregator  (#2064)
    Allow for an automatic time-mean-coarsening of the zonal mean plots
    logged to wandb. Currently we disable the zonal aggregator when there
    are more than 2^15 timesteps due to a limitation in Matplotlib, however,
    well before 2^15 the images logged to Wandb are not very useful due to
    the large aspect ratio. This update automatically coarsens the time
    dimension of the zonal mean images when larger than 2^12 (what looked
    reasonable to me on Wandb) by the smallest amount to keep it from being
    longer than 2^12. There is an override option to allow a user to specify
    if they want longer images up to a length of 2^15 for backwards
    compatibility or if the user wants the highest possible temporal image.
    
    This should also help reduce the training and evaluation runtime as
    during inference `ZonalMeanAggregator` which may currently take up to 8+
    minutes per epoch. This will also reduce the size of
    `zonal_mean_diagnostics.nc` which can be on the order of 1 - 2 GBs for
    some simulations.
    
    [Wandb](https://wandb.ai/ai2cm/ace/runs/u984u6cj?nw=nwusertroya) and
    [beaker
    logs](https://beaker.allen.ai/orgs/ai2/workspaces/ace/work/01K3VJFZPPDCH0STEZAF7MY0XF/logs?jobId=01K3VJFZT5033CM5JF831YJX9W&recentLines=All)
    showing an 81 year with zonal mean metrics plotted (they are coarsened
    by a factor of 29 with a `forward_steps_in_memory=40` mean values).
    During this particular run the 81 year long zonal mean plots only took
    took 1.7 minutes.
    
    Changes:
    - Add `_coarsen_tensor` to `ZonalMeanAggregator` to allow for time
    coarsening
    - Add `zonal_mean_max_size` argument to
    `InferenceEvaluatorAggregatorConfig`
    - Has a buffer in zonal mean to allow for arbitrary time coarsening even
    if that is not divisible by `forward_steps_in_memory `
    
    - [x] Tests added
    
    Resolves #[2009](https://github.com/ai2cm/full-model/issues/2009)

--- fme/ace/aggregator/inference/zonal_mean.py
+++ fme/ace/aggregator/inference/zonal_mean.py
@@ -1,4 +1,5 @@
 import dataclasses
+import logging
 from collections.abc import Callable, Mapping
 
 import matplotlib.pyplot as plt
@@ -57,11 +58,14 @@ class ZonalMeanAggregator:
             "x-axis is time increasing to right, y-axis is latitude increasing upward"
         ),
     }
+    _max_matplotlib_size = 2**15  # max number of timesteps before breaking matplotlib
+    _time_dim = 1
 
     def __init__(
         self,
         zonal_mean: Callable[[torch.Tensor], torch.Tensor],
         n_timesteps: int,
+        zonal_mean_max_size: int,
         variable_metadata: Mapping[str, VariableMetadata] | None = None,
     ):
         """
@@ -70,9 +74,52 @@ class ZonalMeanAggregator:
             n_timesteps: Number of timesteps of inference that will be run.
             variable_metadata: Mapping of variable names their metadata that will
                 used in generating logged image captions.
+            zonal_mean_max_size: Maximum number of timesteps to log zonal mean images.
+                If zonal_mean_max_size > _max_matplotlib_size, it will be set to
+                _max_matplotlib_size.
         """
-        self._n_timesteps = n_timesteps
+        if zonal_mean_max_size > self._max_matplotlib_size:
+            logging.warning(
+                "Zonal mean images support images with a maximum of "
+                f"{self._max_matplotlib_size} timesteps. "
+                f"Coarsening to {self._max_matplotlib_size} timesteps."
+            )
+            zonal_mean_max_size = self._max_matplotlib_size
+
+        if zonal_mean_max_size > n_timesteps:
+            logging.warning(
+                f"Zonal mean max size {zonal_mean_max_size} is greater than "
+                f"the number of timesteps {n_timesteps}. Setting zonal mean max size "
+                "to the number of timesteps."
+            )
+            zonal_mean_max_size = n_timesteps
+
+        self._max_size = zonal_mean_max_size
+
+        if n_timesteps > self._max_size:
+            # warn users that we are automatically coarsening the time dimension of
+            # zonal mean images to avoid memory issues
+            logging.info(
+                f"Automatic time coarsening is enabled for zonal mean images due to "
+                f"timesteps being greater than {self._max_size}. If you want to "
+                f"override this, set log_zonal_mean_images to a value up to "
+                f"{self._max_matplotlib_size}."
+            )
+            self.time_coarsening_factor = np.ceil(n_timesteps / self._max_size).astype(
+                int
+            )
+            self._n_timesteps = n_timesteps // self.time_coarsening_factor
+
+            logging.info(
+                f"Zonal mean time coarsening factor is  "
+                f"{self.time_coarsening_factor} with image size of {self._n_timesteps}."
+            )
+        else:
+            self.time_coarsening_factor = 1
+            self._n_timesteps = n_timesteps
+
         self._dist = Distributed.get_instance()
+
         if variable_metadata is None:
             self._variable_metadata: Mapping[str, VariableMetadata] = {}
         else:
@@ -81,9 +128,13 @@ class ZonalMeanAggregator:
         self._target_data: TensorDict | None = None
         self._gen_data: TensorDict | None = None
         self._n_batches = torch.zeros(
-            n_timesteps, dtype=torch.int32, device=get_device()
+            self._n_timesteps, dtype=torch.int32, device=get_device()
         )[None, :, None]  # sample, time, lat
         self._zonal_mean = zonal_mean
+        self.last_step = 0
+        self.logged_batch_skip = False
+        self._buffer_target: TensorDict | None = None
+        self._buffer_gen: TensorDict | None = None
 
     def record_batch(
         self,
@@ -103,14 +154,71 @@ class ZonalMeanAggregator:
             )
 
         window_steps = next(iter(target_data.values())).shape[1]
-        time_slice = slice(i_time_start, i_time_start + window_steps)
-        # we can average along longitude without area weighting
+
+        if window_steps < self.time_coarsening_factor:
+            # skip this batch if its not possible to coarsen
+            if not self.logged_batch_skip:
+                logging.info(
+                    f"Skipping batch with {window_steps} steps because it is less than "
+                    f"the zonal mean time coarsening factor of "
+                    f"{self.time_coarsening_factor}."
+                )
+                self.logged_batch_skip = True
+            return
+
+        # if we have a buffer that means we didnt record the last batch
+        if self._buffer_gen:
+            start_idx = self.last_step
+        else:
+            start_idx = i_time_start // self.time_coarsening_factor
+
+        time_slice = slice(
+            start_idx,
+            (i_time_start + window_steps) // self.time_coarsening_factor,
+        )
+
+        self.last_step = (i_time_start + window_steps) // self.time_coarsening_factor
+
+        buffer_size = (
+            i_time_start + window_steps
+        ) - self.last_step * self.time_coarsening_factor
+
+        buffer = {}
         for name, tensor in target_data.items():
             if name in self._target_data:
-                self._target_data[name][:, time_slice, :] += self._zonal_mean(tensor)
+                if self._buffer_target:
+                    tensor = torch.cat(
+                        [
+                            self._buffer_target[name],
+                            tensor[:, 0 : window_steps - buffer_size, :],
+                        ],
+                        dim=self._time_dim,
+                    )
+                self._target_data[name][:, time_slice, :] += self._coarsen_tensor(
+                    self._zonal_mean(tensor)
+                )
+                if buffer_size > 0:
+                    buffer[name] = tensor[:, -buffer_size:, :]
+        self._buffer_target = buffer
+
+        buffer = {}
         for name, tensor in gen_data.items():
             if name in self._gen_data:
-                self._gen_data[name][:, time_slice, :] += self._zonal_mean(tensor)
+                if self._buffer_gen:
+                    tensor = torch.cat(
+                        [
+                            self._buffer_gen[name],
+                            tensor[:, 0 : window_steps - buffer_size, :],
+                        ],
+                        dim=self._time_dim,
+                    )
+                self._gen_data[name][:, time_slice, :] += self._coarsen_tensor(
+                    self._zonal_mean(tensor)
+                )
+                if buffer_size > 0:
+                    buffer[name] = tensor[:, -buffer_size:, :]
+        self._buffer_gen = buffer
+
         self._n_batches[:, time_slice, :] += 1
 
     def _get_data(self) -> dict[str, _RawData]:
@@ -184,6 +292,14 @@ class ZonalMeanAggregator:
         caption = self._captions[key].format(name=caption_name, units=units)
         caption += f" vmin={vmin:.4g}, vmax={vmax:.4g}."
         return caption
+
+    def _coarsen_tensor(self, tensor: torch.Tensor) -> torch.Tensor:
+        """Coarsen tensor along a given axis by a given factor."""
+        return tensor.unfold(
+            dimension=self._time_dim,
+            size=self.time_coarsening_factor,
+            step=self.time_coarsening_factor,
+        ).mean(dim=-1)
 
     @staticmethod
     def _initialize_zeros_zonal_mean_from_batch(
