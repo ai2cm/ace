834ddf14778cbc9c2f5525cbf72a7e20d16fc668
Anna Kwa <annak@allenai.org>
2025-09-16 13:24:55 -0700

    Patch refactor predictor (#2160)

--- fme/downscaling/models.py
+++ fme/downscaling/models.py
@@ -1,5 +1,4 @@
 import dataclasses
-import logging
 from collections.abc import Mapping
 from typing import Any
 
@@ -578,7 +577,6 @@ class DiffusionModel:
         )
         latents = torch.randn(outputs_shape).to(device=get_device())
 
-        logging.info("Running EDM sampler...")
         generated_norm, latent_steps = edm_sampler(
             self.module,
             latents,
@@ -588,7 +586,6 @@ class DiffusionModel:
             sigma_max=self.config.sigma_max,
             num_steps=self.config.num_diffusion_generation_steps,
         )
-        logging.info("Done running EDM sampler.")
 
         if self.config.predict_residual:
             base_prediction = interpolate(
