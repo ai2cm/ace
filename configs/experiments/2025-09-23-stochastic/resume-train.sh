94f70d935739e259190a99f8a18eee8f5e683702
Jeremy McGibbon <jeremy.mcgibbon@gmail.com>
2025-10-07 19:00:20 +0000

    fix bug

--- configs/experiments/2025-09-23-stochastic/resume-train.sh
+++ configs/experiments/2025-09-23-stochastic/resume-train.sh
@@ -45,7 +45,7 @@ resume_training() {
         --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
         --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
         --dataset-secret google-credentials:/tmp/google_application_credentials.json \
-        --dataset "$EXISTING_RESULTS_DATASET:/existing-results" \
+        --dataset "$existing_results_dataset:/existing-results" \
         --gpus "$N_GPUS" \
         --shared-memory 400GiB \
         --budget ai2/climate \
