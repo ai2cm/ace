2d01bd2665398119a9c31cafe675821408da6489
Anna Kwa <annak@allenai.org>
2025-10-08 14:58:31 -0700

    try on titan

--- configs/experiments/2025-08-20-xshield-amip-downscaling-100-to-3km/train.sh
+++ configs/experiments/2025-08-20-xshield-amip-downscaling-100-to-3km/train.sh
@@ -28,10 +28,10 @@ IMAGE=01JWJ96JMF89D812JS159VF37N
 gantry run \
     --name $JOB_NAME \
     --description 'Run downscaling 100km to 3km training global' \
-    --workspace ai2/climate-ceres \
-    --priority urgent \
+    --workspace ai2/downscaling \
+    --priority low \
     --preemptible \
-    --cluster ai2/ceres \
+    --cluster ai2/titan \
     --beaker-image $IMAGE \
     --env WANDB_USERNAME=$BEAKER_USERNAME \
     --env WANDB_NAME=$JOB_NAME \
