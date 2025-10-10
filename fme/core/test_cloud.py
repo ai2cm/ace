addf022eda2afe032a0894b23ecd40c137914410
Oliver Watt-Meyer <oliverwatt@gmail.com>
2025-10-10 11:19:47 -0700

    Faster test

--- fme/core/test_cloud.py
+++ fme/core/test_cloud.py
@@ -2,7 +2,6 @@ from .cloud import is_local
 
 
 def test_is_local():
-    assert is_local("/tmp/somefile")
+    assert is_local("/absolute/path/somefile")
     assert is_local("relative/path/somefile")
-    assert not is_local("s3://mybucket/somefile")
     assert not is_local("gs://mybucket/somefile")
