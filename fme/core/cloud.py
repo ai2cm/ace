57db3f8cc658b881ee9a2d94ac505115c496cfaa
Oliver Watt-Meyer <oliverwatt@gmail.com>
2025-10-10 13:04:34 -0700

    is_local works for Path

--- fme/core/cloud.py
+++ fme/core/cloud.py
@@ -1,3 +1,6 @@
-def is_local(path: str) -> bool:
+from pathlib import Path
+
+
+def is_local(path: str | Path) -> bool:
     """Check if a given path is on a local filesystem. Assuming fsspec conventions."""
-    return "://" not in path
+    return "://" not in str(path)
--- fme/core/test_cloud.py
+++ fme/core/test_cloud.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 from .cloud import is_local
 
 
 def test_is_local():
     assert is_local("/absolute/path/somefile")
     assert is_local("relative/path/somefile")
+    assert is_local(Path("/absolute/path/somefile"))
     assert not is_local("gs://mybucket/somefile")
