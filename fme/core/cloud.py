ecd624dba135a502cc741fc031efedc9da691609
Oliver Watt-Meyer <oliverwatt@gmail.com>
2025-10-10 11:33:33 -0700

    Dont use fsspec for check

--- fme/core/cloud.py
+++ fme/core/cloud.py
@@ -1,6 +1,3 @@
-import fsspec
-
-
 def is_local(path: str) -> bool:
-    """Check if a given path is on a local filesystem."""
-    return fsspec.url_to_fs(path)[0] == fsspec.filesystem("file")
+    """Check if a given path is on a local filesystem. Assuming fsspec conventions."""
+    return "://" not in path
