29422684b78df1ffb7c3a3aa8db7356454d532fb
Jeremy McGibbon <jeremy.mcgibbon@gmail.com>
2025-10-16 20:44:06 +0000

    reduce submission priority to high

--- configs/experiments/2025-09-23-stochastic/run-ace-train.sh
+++ configs/experiments/2025-09-23-stochastic/run-ace-train.sh
@@ -28,7 +28,7 @@ run_training() {
     --description 'Run ACE training' \
     --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
     --workspace ai2/climate-ceres \
-    --priority urgent \
+    --priority high \
     --preemptible \
     --cluster ai2/ceres \
     --env WANDB_USERNAME="$WANDB_USERNAME" \
