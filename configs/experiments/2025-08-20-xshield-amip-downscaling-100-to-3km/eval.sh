491dacb013ef14946ca9b865ac0cf51b498a2057
Anna Kwa <annak@allenai.org>
2025-10-20 16:29:38 +0000

    use best ckpt

--- configs/experiments/2025-08-20-xshield-amip-downscaling-100-to-3km/eval.sh
+++ configs/experiments/2025-08-20-xshield-amip-downscaling-100-to-3km/eval.sh
@@ -43,7 +43,7 @@ gantry run \
     --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
     --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
     --dataset-secret google-credentials:/tmp/google_application_credentials.json \
-    --dataset $EXISTING_RESULTS_DATASET:/checkpoints \
+    --dataset $EXISTING_RESULTS_DATASET:checkpoints:/checkpoints \
     --weka climate-default:/climate-default \
     --gpus $NGPU \
     --shared-memory 400GiB \
