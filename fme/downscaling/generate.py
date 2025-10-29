935296796829d71497083cec43e59f5ba904cd60
Andre Perkins <frodre@gmail.com>
2025-10-28 22:10:49 -0700

    Donwscaler implementation

--- fme/downscaling/generate.py
+++ fme/downscaling/generate.py
@@ -8,6 +8,9 @@ from dataclasses import asdict, dataclass, field, replace
 from datetime import datetime, timedelta
 
 import dacite
+import numpy as np
+import torch
+import xarray as xr
 import yaml
 
 from fme.core import logging_utils
@@ -18,11 +21,17 @@ from fme.core.dicts import to_flat_dict
 from fme.core.distributed import Distributed
 from fme.core.logging_utils import LoggingConfig
 from fme.core.typing_ import Slice
+from fme.core.writer import ZarrWriter
 
-from .data import ClosedInterval, DataLoaderConfig, GriddedData
+from .data import BatchData, ClosedInterval, DataLoaderConfig, GriddedData
 from .data.config import XarrayEnsembleDataConfig
 from .models import CheckpointModelConfig, DiffusionModel
-from .predictors import CascadePredictor, CascadePredictorConfig, PatchPredictionConfig
+from .predictors import (
+    CascadePredictor,
+    CascadePredictorConfig,
+    PatchPredictionConfig,
+    PatchPredictor,
+)
 from .requirements import DataRequirements
 from .train import count_parameters
 
@@ -33,6 +42,7 @@ class OutputTarget:
         name: str,
         save_vars: list[str],
         n_ens: int,
+        n_items_per_gpu: int,
         data: GriddedData,
         patch: PatchPredictionConfig,
         chunks: dict[str, int],
@@ -41,10 +51,75 @@ class OutputTarget:
         self.name = name
         self.save_vars = save_vars
         self.n_ens = n_ens
+        self.n_items_per_gpu = n_items_per_gpu
         self.data = data
         self.patch = patch
         self.chunks = chunks
         self.shards = shards
+
+        self.dims = list(data.dims)
+        self.dims.insert(1, "ensemble")  # insert ensemble dim after time
+
+    def get_writer(self, sample_data: BatchData, output_dir: str) -> ZarrWriter:
+        latlon_coords = sample_data.latlon_coordinates[0]
+        ensemble = list(range(self.n_ens))
+        coords = dict(
+            zip(
+                self.dims,
+                [
+                    self.data.all_times.to_numpy(),
+                    np.array(ensemble),
+                    latlon_coords.lat.numpy(),
+                    latlon_coords.lon.numpy(),
+                ],
+            )
+        )
+        dims = tuple(coords.keys())
+
+        return ZarrWriter(
+            path=f"{output_dir}/{self.name}.zarr",
+            dims=dims,
+            coords=coords,
+            data_vars=self.save_vars,
+            chunks=self.chunks,
+            shards=self.shards,
+        )
+
+    def _get_time_slice(self, time: xr.DataArray) -> slice:
+        start_time = time[0].item()
+        end_time = time[-1].item()
+        if start_time == end_time:
+            time_idx = self.data.all_times.get_loc(start_time)
+            time_slice = slice(time_idx, time_idx + 1)
+        else:
+            time_idxs = self.data.all_times.get_indexer(
+                {"time": [start_time, end_time]}
+            )
+            time_slice = slice(time_idxs[0], time_idxs[1] + 1)
+
+        return time_slice
+
+    def _get_ens_slices(self) -> list[slice]:
+        ens_start = 0
+        ens_slices = []
+        while ens_start < self.n_ens:
+            ens_end = min(ens_start + self.n_items_per_gpu, self.n_ens)
+            ens_slices.append(slice(ens_start, ens_end))
+            ens_start = ens_end
+
+        return ens_slices
+
+    def get_insert_slices(self, data: BatchData) -> list[dict[str, slice]]:
+        time_slice = self._get_time_slice(data.time)
+        ens_slices = self._get_ens_slices()
+        # Always storing the entire region
+        lat_slice = slice(None)
+        lon_slice = slice(None)
+
+        return [
+            dict(zip(self.dims, (time_slice, ens_slice, lat_slice, lon_slice)))
+            for ens_slice in ens_slices
+        ]
 
 
 @dataclass
@@ -94,25 +169,40 @@ class OutputTargetConfig(ABC):
 
         return [data_config]
 
-    @staticmethod
     def _replace_loader_config(
-        time, coarse, lat_extent, lon_extent, loader_config
+        self,
+        time,
+        coarse,
+        lat_extent,
+        lon_extent,
+        loader_config: DataLoaderConfig,
+        dist: Distributed,
     ) -> DataLoaderConfig:
         # return a new DataloaderConfig with replaced values
         new_coarse = [replace(coarse[0], subset=time)]
 
+        local_batch_size = dist.local_batch_size(loader_config.batch_size)
+        n_ens = self.n_ens
+        n_items_per_gpu = self.n_item_per_gpu
+
+        n_ens_per_slice = min(n_ens, n_items_per_gpu)
+        n_time_per_slice = max(1, n_items_per_gpu // n_ens_per_slice)
+        local_batch_size = min(local_batch_size, n_time_per_slice)
+        new_batch_size = dist.world_size * local_batch_size
+
+        # TODO: log the replacements for debugging
         new_loader_config = replace(
             loader_config,
             coarse=new_coarse,
             lat_extent=lat_extent,
             lon_extent=lon_extent,
+            batch_size=new_batch_size,
         )
         return new_loader_config
 
     def _get_chunks(
-        self, data_shape: tuple[int, ...], bytes_per_element: int
+        self, dims: list[str], data_shape: tuple[int, ...], bytes_per_element: int
     ) -> dict[str, int]:
-        # time, ensemble, latitude, longitude
         if len(data_shape) != 4:
             raise ValueError(
                 "Data shape must be of length 4 (time, ensemble, latitude, longitude)."
@@ -129,15 +219,9 @@ class OutputTargetConfig(ABC):
 
         def _recursive_chunksize_search(
             shape: tuple[int, ...], bytes_per_element: int, reduce_dim: int
-        ) -> dict[str, int]:
-            chunk = {
-                "time": shape[0],
-                "ensemble": shape[1],
-                "latitude": shape[2],
-                "longitude": shape[3],
-            }
+        ) -> tuple[int, ...]:
             if _total_size_mb(shape, bytes_per_element) <= 20:
-                return chunk
+                return shape
 
             reduce_dim_size = shape[reduce_dim] // 2
             if reduce_dim_size < 1:
@@ -158,7 +242,10 @@ class OutputTargetConfig(ABC):
                 tuple(new_shape), bytes_per_element, reduce_dim
             )
 
-        return _recursive_chunksize_search(data_shape, bytes_per_element, reduce_dim=0)
+        chunk_shape = _recursive_chunksize_search(
+            data_shape, bytes_per_element, reduce_dim=0
+        )
+        return dict(zip(dims, chunk_shape))
 
     def _build(
         self,
@@ -171,15 +258,22 @@ class OutputTargetConfig(ABC):
         coarse: list[XarrayDataConfig],
     ) -> OutputTarget:
         updated_loader_config = self._replace_loader_config(
-            time, coarse, lat_extent, lon_extent, loader_config
+            time,
+            coarse,
+            lat_extent,
+            lon_extent,
+            loader_config,
+            Distributed.get_instance(),
         )
         data = updated_loader_config.build(requirements=requirements)
 
-        sample_dict = next(iter(data.loader))
-        sample_tensor = next(iter(sample_dict.values()))
+        sample_batch: BatchData = next(iter(data.loader))
+        sample_tensor: torch.Tensor = next(iter(sample_batch.data.values()))
+
         if self.zarr_chunks is None:
             chunks = self._get_chunks(
-                data_shape=sample_tensor.shape,
+                dims=data.dims,
+                data_shape=tuple(sample_tensor.shape),
                 bytes_per_element=sample_tensor.element_size(),
             )
         else:
@@ -189,6 +283,7 @@ class OutputTargetConfig(ABC):
             name=self.name,
             save_vars=self.save_vars,
             n_ens=self.n_ens,
+            n_items_per_gpu=self.n_item_per_gpu,
             data=data,
             patch=patch,
             chunks=chunks,
@@ -349,13 +444,41 @@ class Downscaler:
         self,
         model: DiffusionModel | CascadePredictor,
         output_targets: list[OutputTarget],
+        output_dir: str = ".",
     ):
         self.model = model
         self.output_targets = output_targets
+        self.output_dir = output_dir
 
     def run(self):
         for target in self.output_targets:
             logging.info(f"Generating downscaled outputs for target: {target.name}")
+
+            _model: DiffusionModel | PatchPredictor | CascadePredictor
+            if target.patch.needs_patch_predictor:
+                _model = PatchPredictor(
+                    model=self.model,
+                    coarse_horizontal_overlap=target.patch.coarse_horizontal_overlap,
+                )
+
+            insert_slices, writer = None, None
+            for i, (data, topography) in target.data.loader:
+                # TODO: could use some kind of checkpointing to allow restarts
+                if i == 0:
+                    insert_slices = target.get_insert_slices(data)
+                    writer = target.get_writer(
+                        sample_data=data, output_dir=self.output_dir
+                    )
+                else:
+                    assert insert_slices is not None
+                    assert writer is not None
+
+                output = _model.generate_on_batch_no_target(
+                    data, topography=topography, n_samples=target.n_ens
+                )
+                current_insert = insert_slices[i]
+                output_numpy = {k: output[k].cpu().numpy() for k in target.save_vars}
+                writer.record_batch(output_numpy, position_slices=current_insert)
 
 
 @dataclass
