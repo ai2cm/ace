f4c8578229c6a1bb3ff0abb0d24a14137823c68f
Spencer Clark <spencerkclark@gmail.com>
2025-10-30 08:12:14 -0400

    Fix ckpt

--- configs/experiments/2025-10-29-data-only-inference/run-data-only-inference-cm4-frozen-precipitation.sh
+++ configs/experiments/2025-10-29-data-only-inference/run-data-only-inference-cm4-frozen-precipitation.sh
@@ -33,7 +33,7 @@ cd $REPO_ROOT && gantry run \
     --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
     --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
     --dataset-secret google-credentials:/tmp/google_application_credentials.json \
-    --dataset $EXISTING_RESULTS_DATASET:training_checkpoints/best_inference_ckpt.tar:/ckpt.tar \
+    --dataset $EXISTING_RESULTS_DATASET:training_checkpoints/best_ckpt.tar:/ckpt.tar \
     --gpus 1 \
     --shared-memory 50GiB \
     --weka climate-default:/climate-default \
@@ -63,7 +63,7 @@ cd $REPO_ROOT && gantry run \
     --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
     --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
     --dataset-secret google-credentials:/tmp/google_application_credentials.json \
-    --dataset $EXISTING_RESULTS_DATASET:training_checkpoints/best_inference_ckpt.tar:/ckpt.tar \
+    --dataset $EXISTING_RESULTS_DATASET:training_checkpoints/best_ckpt.tar:/ckpt.tar \
     --gpus 1 \
     --shared-memory 50GiB \
     --weka climate-default:/climate-default \
