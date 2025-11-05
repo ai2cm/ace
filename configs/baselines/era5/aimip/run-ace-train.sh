58abeef812dc2ad440bc74223ddf9a5286002cbd
Brian Henn <brianhenn@allenai.org>
2025-11-05 14:18:16 -0800

    AIMIP ACE train 4 random seeds

---
+++ configs/baselines/era5/aimip/run-ace-train.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+JOB_NAME_BASE="ace-aimip-train"
+JOB_GROUP="ace-aimip"
+CONFIG_FILENAME="ace-train-config.yaml"
+SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+BEAKER_USERNAME=bhenn1983
+REPO_ROOT=$(git rev-parse --show-toplevel)
+N_GPUS=4
+
+cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+
+python -m fme.ace.validate_config --config_type train $CONFIG_PATH
+
+launch_job () {
+
+    JOB_NAME=$1
+    shift
+    OVERRIDE="$@"
+
+    gantry run \
+        --name $JOB_NAME \
+        --task-name $JOB_NAME \
+        --description 'Run ACE2-ERA5 training on AIMIP period' \
+        --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+        --workspace ai2/ace \
+        --priority high \
+        --preemptible \
+        --cluster ai2/titan-cirrascale \
+        --env WANDB_USERNAME=$BEAKER_USERNAME \
+        --env WANDB_NAME=$JOB_NAME \
+        --env WANDB_JOB_TYPE=training \
+        --env WANDB_RUN_GROUP=$JOB_GROUP \
+        --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+        --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+        --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+        --dataset oliverwm/era5-1deg-8layer-stats-1990-2019-v2:/statsdata \
+        --gpus $N_GPUS \
+        --shared-memory 400GiB \
+        --weka climate-default:/climate-default \
+        --budget ai2/climate \
+        --system-python \
+        --install "pip install --no-deps ." \
+        -- torchrun --nproc_per_node $N_GPUS -m fme.ace.train $CONFIG_PATH --override $OVERRIDE
+
+}
+
+# random seed ensemble
+for SEED in 0 1 2 3; do
+    JOB_NAME="${JOB_NAME_BASE}-rs${SEED}"
+    OVERRIDE="seed=${SEED}"
+    launch_job "$JOB_NAME" "$OVERRIDE"
+done
