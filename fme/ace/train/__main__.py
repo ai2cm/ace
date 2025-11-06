e446718a43550f6b5fb7cfc95cc1e00e58a8146c
Oscar H. Diaz-Ibarra <odiazib@sandia.gov>
2025-11-06 10:55:17 -0800

    Adding recommendations for PR review and deleting old code.

--- fme/ace/train/__main__.py
+++ fme/ace/train/__main__.py
@@ -4,4 +4,4 @@ from fme.core.cli import get_parser
 if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
-    main(args.yaml_config, override_dotlist=args.override, h_parallel_size =args.h_parallel_size , w_parallel_size=args.w_parallel_size )
+    main(args.yaml_config, override_dotlist=args.override)
