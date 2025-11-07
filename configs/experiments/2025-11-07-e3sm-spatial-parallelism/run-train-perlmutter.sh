7012141ade184c7a4bcfa23b87b2fbeabe0356a4
Elynn Wu <elynn510@gmail.com>
2025-11-07 14:46:09 -0800

    updated hash

--- configs/experiments/2025-11-07-e3sm-spatial-parallelism/run-train-perlmutter.sh
+++ configs/experiments/2025-11-07-e3sm-spatial-parallelism/run-train-perlmutter.sh
@@ -6,7 +6,7 @@ set -x
 export WANDB_NAME=PM-AMIP-EAMv3-train-rs1
 export WANDB_RUN_GROUP=2025-04-01-AMIP-EAMv3
 
-export COMMIT=06e96385f
+export COMMIT=c6016f0e6
 
 # directories for input data (training, validation, inference, stats)
 export FME_TRAIN_DIR=/pscratch/sd/r/rebassoo/fme-preprocess/2025-04-01-e3smv3-1deg/traindata
