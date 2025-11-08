6405bbdb9ecb8e26135157d8314cafc66946e0fa
Oscar H. Diaz-Ibarra <odiazib@sandia.gov>
2025-11-07 19:29:43 -0800

    Fixing test for sfnonet with spatial parallelism.

--- fme/ace/models/modulus/test_sfnonet_spatial_dist.py
+++ fme/ace/models/modulus/test_sfnonet_spatial_dist.py
@@ -1,37 +1,21 @@
 import os
-
+import sys
 import torch
 from fme.core.device import get_device
-from fme.core.testing import validate_tensor
 
-from .sfnonet import SphericalFourierNeuralOperatorNet, SFNO
+from .sfnonet import SFNO
 
 DIR = os.path.abspath(os.path.dirname(__file__))
 
-from .layers import MLP, DropPath, RealFFT2, SpectralAttention2d, InverseRealFFT2
-from .s2convolutions import SpectralAttentionS2, SpectralConvS2
-
-from fme.ace.models.makani_mpu.fft import DistributedRealFFT1, DistributedInverseRealFFT1, DistributedRealFFT2, DistributedInverseRealFFT2, DistributedRealFFT3, DistributedInverseRealFFT3
 from fme.ace.models.makani_mpu.mappings import init_gradient_reduction_hooks
-
-import torch_harmonics as th
-import torch_harmonics.distributed as thd
-
 from fme.core.distributed import Distributed
-
-from fme.core.dataset.test_helper import split_helper_conv, gather_helper_conv, relative_error, init_seed
-
-
-from fme.ace.models.makani_utils import checkpoint_helpers
+from fme.core.dataset.test_helper import gather_helper_conv, relative_error, init_seed
 from fme.ace.models.makani_utils.makani_driver import _save_checkpoint_flexible, _restore_checkpoint_flexible
 from physicsnemo.distributed.mappings import reduce_from_parallel_region
 
-def test_sfnonet_spatial_dist_output_is_unchanged():
-    # torch.manual_seed(0)
-    # fix seed
-    init_seed(333)
-    dist = Distributed()
+def test_sfnonet_without_sp():
     ## without domain decomposition
+    os.environ['H_PARALLEL_SIZE'] = '1'
     verbose=False
     input_channels = 3
     output_channels = 3
@@ -39,6 +23,7 @@ def test_sfnonet_spatial_dist_output_is_unchanged():
     n_samples = 4
     embed_dim=16
     num_layers=2
+
     model = SFNO(
         params=None,
         embed_dim=embed_dim,
@@ -48,7 +33,7 @@ def test_sfnonet_spatial_dist_output_is_unchanged():
         img_shape=img_shape,
         in_chans=input_channels,
         out_chans=output_channels,
-    )
+      )
     # must initialize on CPU to get the same results on GPU
     inp_full = torch.randn(n_samples, input_channels, *img_shape)
     inp_full.requires_grad = True
@@ -62,37 +47,32 @@ def test_sfnonet_spatial_dist_output_is_unchanged():
 
     assert out_full.shape == (n_samples, output_channels, *img_shape)
     tmp_path="testdata"
-    torch.save(out_full, "testdata/test_sfnonet_spatial_dist_output_is_unchanged.pt")
-
-    # get state dict
-    state_dict_full = checkpoint_helpers.gather_model_state_dict(model, grads=False)
-
-
     torch.save(out_full, os.path.join(tmp_path, "out_full.pt"))
-    # torch.save(igrad_full, os.path.join(tmp_path, "igrad_full.pt"))
-    # if mpi_comm_rank == 0:
+    torch.save(inp_full, os.path.join(tmp_path, "inp_full.pt"))
+    torch.save(loss_full, os.path.join(tmp_path, "loss_full.pt"))
+    torch.save(igrad_full, os.path.join(tmp_path, "igrad_full.pt"))
+
     _save_checkpoint_flexible(checkpoint_path=os.path.join(tmp_path, "checkpoint.pt"),
                                           model=model)
-    # delete local model
-    del model
-    print("--------------------------------------------------")
 
-    dist.shutdown()
+def test_sfnonet_with_sp():
+    tmp_path="testdata"
+    os.environ['H_PARALLEL_SIZE'] = '2'
+    verbose=False
+    input_channels = 3
+    output_channels = 3
+    img_shape = (8, 16)
+    n_samples = 4
+    embed_dim=16
+    num_layers=2
 
-    ## with domain decomposition
-    dist = Distributed()
+    dist = Distributed.get_instance()
     mpi_comm_rank = dist.local_rank
 
-    h_parallel_size=2
-    w_parallel_size=2
-    dist._init_distributed(h_parallel_size =  h_parallel_size, w_parallel_size=w_parallel_size)
-    # thd.init(h_parallel_size, w_parallel_size)
+    w_group = dist.comm_get_group("w")
+    h_group = dist.comm_get_group("h")
+    world_rank = dist.rank
 
-
-    comm = dist.get_comm()
-    w_group = comm.get_group("w")
-    h_group = comm.get_group("h")
-    world_rank = comm.get_world_rank()
     device=get_device()
 
     model_dist = SFNO(
@@ -105,7 +85,6 @@ def test_sfnonet_spatial_dist_output_is_unchanged():
         in_chans=input_channels,
         out_chans=output_channels,
     ).to(device)
-
 
     # save reduction hooks
     model_dist = init_gradient_reduction_hooks(
@@ -123,34 +102,38 @@ def test_sfnonet_spatial_dist_output_is_unchanged():
     _restore_checkpoint_flexible(checkpoint_path=os.path.join(tmp_path, "checkpoint.pt"),
                                 model=model_dist)
 
+    # must initialize on CPU to get the same results on GPU
+    # inp_full = torch.randn(n_samples, input_channels, *img_shape)
+    inp_full = torch.load(os.path.join(tmp_path, "inp_full.pt"))
+
     # split input
-    inp_full_device=inp_full.to(device)
-    inp_local= split_helper_conv(inp_full_device, hdim=-2, wdim=-1, w_group=w_group, h_group=h_group)
+    # inputs: ntimes, nsamples, h, w
+    this_shape=(inp_full.shape[-2],inp_full.shape[-1])
+    ## Create a leaf variable
+    inp_local_host = (inp_full[:,:,*dist.get_local_slices(this_shape)]).detach().clone()
+    inp_local=inp_local_host.to(device)
     inp_local.requires_grad = True
     if world_rank == 0:
       print("inp_full", inp_full.shape)
       print("inp_local", inp_local.shape)
 
-    # with torch.no_grad():
     out_local = model_dist(inp_local)
     loss_dist = reduce_from_parallel_region(torch.sum(out_local), "model")
     loss_dist.backward()
     igrad_local = inp_local.grad.clone()
 
-    # get weights and wgrads
-    state_dict_gather_full = checkpoint_helpers.gather_model_state_dict(model_dist, grads=True)
+    out_full = torch.load(os.path.join(tmp_path, "out_full.pt"))
 
-    # output
-    # mpi_comm.Barrier()
     with torch.no_grad():
       out_full_device=out_full.to(device)
       out_gather_full = gather_helper_conv(out_local, hdim=-2, wdim=-1, w_group=w_group, h_group=h_group)
       err = relative_error(out_gather_full, out_full_device)
       if world_rank == 0:
         print(f"final relative error of output: {err.item()}")
-    # mpi_comm.Barrier()
-    assert err < 1e-3
-    # loss
+    assert err < 0.0006
+
+    loss_full=torch.load(os.path.join(tmp_path, "loss_full.pt"))
+
     with torch.no_grad():
       loss_full_device=loss_full.to(device)
       err = relative_error(loss_dist, loss_full)
@@ -158,10 +141,12 @@ def test_sfnonet_spatial_dist_output_is_unchanged():
         print(f"final relative error of loss: {err.item()}")
     # mpi_comm.Barrier()
     assert err < 1e-3
+
     #############################################################
     # evaluate BWD pass
     #############################################################
     # dgrad
+    igrad_full = torch.load(os.path.join(tmp_path, "igrad_full.pt"))
     with torch.no_grad():
       igrad_full_device=igrad_full.to(device)
       igrad_gather_full = gather_helper_conv(igrad_local, hdim=-2, wdim=-1, w_group=w_group, h_group=h_group)
@@ -170,6 +155,3 @@ def test_sfnonet_spatial_dist_output_is_unchanged():
         print(f"final relative error of input gradient: {err.item()}")
       # cleanup
     assert err < 1e-3
-    # mpi_comm.Barrier()
-
-    comm.cleanup()
