1a1eb520ce159764e8373f699ebc28f058f2a9eb
Troy Arcomano <troya@allenai.org>
2025-11-16 20:41:48 +0000

    add ensemble yamlsgi

--- configs/experiments/2025-09-10-downscale-ace-xshield/generate_ensemble.sh
+++ configs/experiments/2025-09-10-downscale-ace-xshield/generate_ensemble.sh
@@ -5,10 +5,8 @@
 set -e
 
 JOB_NAME="downscale-ace-inference-100km-to-3km-ace-1-years-ensemble"
-CONFIG_FILENAME="gen-ace-output.yaml"
 
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
-CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
 
  # since we use a service account API key for wandb, we use the beaker username to set the wandb username
 BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
@@ -23,18 +21,17 @@ NGPU=2
 IMAGE=01JWJ96JMF89D812JS159VF37N
 
 # Checkpoint at ~
-EXISTING_RESULTS_DATASET=01K8XGEEAJRHN8JRZE4WXQRDVDA
+EXISTING_RESULTS_DATASET=01K8XGEEAJRHN8JRZE4WXQRDVD
 
 ACE_DATASET=01KA4CZZP73VC053HST5HFT3BS
 wandb_group=""
 
 run_eval() {
     local ensemble="$1"
-    local JOB_NAME_RUN="${JOB_NAME}-ens${ensemble}"
-    dataset_arg="--dataset \$ACE_DATASET:output_6hourly_predictions_ic${padded}.zarr:/output_6hourly_predictions_ic${padded}.zarr \\"
-    path_arg="data.data_path.file_pattern=output_6hourly_predictions_ic${padded}.zarr \\"
-    echo "$dataset_arg"
-    echo "$test_arg"
+    local JOB_NAME_RUN="${JOB_NAME}-ensemble${ensemble}"
+    local CONFIG_FILENAME="gen-ace-output${ensemble}.yaml"
+    local CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+    dataset_arg="output_6hourly_predictions_ic${padded}.zarr:/output_6hourly_predictions_ic${padded}.zarr"
     gantry run \
         --name $JOB_NAME \
         --description 'Run 100km to 3km generation on ACE data' \
@@ -51,7 +48,7 @@ run_eval() {
         --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
         --dataset-secret google-credentials:/tmp/google_application_credentials.json \
         --dataset $EXISTING_RESULTS_DATASET:checkpoints/best_histogram_tail.ckpt:/ckpt.tar \
-        $dataset_arg
+        --dataset $ACE_DATASET:$dataset_arg \
         --weka climate-default:/climate-default \
         --gpus $NGPU \
         --shared-memory 400GiB \
@@ -59,9 +56,7 @@ run_eval() {
         --system-python \
         --install "pip install --no-deps ." \
         --allow-dirty \
-        -- torchrun --nproc_per_node $NGPU -m fme.downscaling.predict $CONFIG_PATH \
-            --override \
-            $path_arg \
+        -- torchrun --nproc_per_node $NGPU -m fme.downscaling.predict $CONFIG_PATH
 }
 
 n_ensembles_minus_one=1
@@ -71,4 +66,3 @@ for ((i=0; i<=n_ensembles_minus_one; i++)); do
     echo "Running ensemble member $padded"
     run_eval "$padded"
 done
-
