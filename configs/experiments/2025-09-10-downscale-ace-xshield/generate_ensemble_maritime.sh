b3eed8280acafba72941224828e17ddff9857c19
Troy Arcomano <troya@allenai.org>
2025-11-17 19:34:49 +0000

    Maritime downscaling

---
+++ configs/experiments/2025-09-10-downscale-ace-xshield/generate_ensemble_maritime.sh
@@ -0,0 +1,68 @@
+!/bin/bash
+# uses the augusta cluster which doesn't have weka access but has GCS access and is
+# typically more available than cirrascale clusters
+
+set -e
+
+JOB_NAME="downscale-ace-inference-100km-to-3km-ace-maritime-1-years"
+
+SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
+
+ # since we use a service account API key for wandb, we use the beaker username to set the wandb username
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+
+N_NODES=1
+NGPU=2
+
+#IMAGE with B200 pytorch installed
+IMAGE=01JWJ96JMF89D812JS159VF37N
+
+# Checkpoint at ~
+EXISTING_RESULTS_DATASET=01K8XGEEAJRHN8JRZE4WXQRDVD
+
+ACE_DATASET=01KA4CZZP73VC053HST5HFT3BS
+wandb_group=""
+
+run_eval() {
+    local ensemble="$1"
+    local JOB_NAME_RUN="${JOB_NAME}-ensemble${ensemble}"
+    local CONFIG_FILENAME="gen-ace-output-maritime-ic${ensemble}.yaml"
+    local CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+    dataset_arg="output_6hourly_predictions_ic${padded}.zarr:/output_6hourly_predictions_ic${padded}.zarr"
+    gantry run \
+        --name $JOB_NAME_RUN \
+        --description 'Run 100km to 3km generation on ACE data' \
+        --workspace ai2/ace \
+        --priority high \
+        --not-preemptible \
+        --cluster ai2/titan \
+        --beaker-image $IMAGE \
+        --env WANDB_USERNAME=$BEAKER_USERNAME \
+        --env WANDB_NAME=$JOB_NAME_RUN \
+        --env WANDB_JOB_TYPE=inference \
+        --env WANDB_RUN_GROUP=$wandb_group \
+        --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+        --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+        --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+        --dataset $EXISTING_RESULTS_DATASET:checkpoints/best_histogram_tail.ckpt:/ckpt.tar \
+        --dataset $ACE_DATASET:$dataset_arg \
+        --weka climate-default:/climate-default \
+        --gpus $NGPU \
+        --shared-memory 400GiB \
+        --budget ai2/climate \
+        --system-python \
+        --install "pip install --no-deps ." \
+        --allow-dirty \
+        -- torchrun --nproc_per_node $NGPU -m fme.downscaling.predict $CONFIG_PATH
+}
+
+n_ensembles_minus_one=1
+
+for ((i=0; i<=n_ensembles_minus_one; i++)); do
+    padded=$(printf "%04d" "$i")   # produces 0000, 0001, ...
+    echo "Running ensemble member $padded"
+    run_eval "$padded"
+done
