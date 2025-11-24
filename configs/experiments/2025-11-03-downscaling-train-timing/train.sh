a8a625deab29d4ff6c76b135f6011a2743cf7897
Anna Kwa <annak@allenai.org>
2025-11-24 09:36:48 -0800

    train with lwoer sigmin

--- configs/experiments/2025-11-03-downscaling-train-timing/train.sh
+++ configs/experiments/2025-11-03-downscaling-train-timing/train.sh
@@ -6,8 +6,8 @@ set -e
 
 # recommended but not required to change this
 
-JOB_NAME="xshield-downscaling-control-b200-dw3-test"
-CONFIG_FILENAME="train-control.yaml"
+JOB_NAME="xshield-downscaling-control-amp-sigmin8e-3"
+CONFIG_FILENAME="train-amp.yaml"
 
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
 CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
@@ -16,7 +16,7 @@ wandb_group=""
  # since we use a service account API key for wandb, we use the beaker username to set the wandb username
 BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
 REPO_ROOT=$(git rev-parse --show-toplevel)
-N_GPUS=2 # TODO: change to 8 after testing
+N_GPUS=8 # TODO: change to 8 after testing
 
 cd $REPO_ROOT  # so config path is valid no matter where we are running this script
 
@@ -27,10 +27,9 @@ gantry run \
     --name $JOB_NAME \
     --description 'Run downscaling 100km to 3km training global' \
     --workspace ai2/ace \
-    --priority high \
+    --priority low \
     --preemptible \
-    --cluster ai2/ceres \
-    --cluster ai2/jupiter \
+    --cluster ai2/titan \
     --beaker-image $IMAGE \
     --env WANDB_USERNAME=$BEAKER_USERNAME \
     --env WANDB_NAME=$JOB_NAME \
