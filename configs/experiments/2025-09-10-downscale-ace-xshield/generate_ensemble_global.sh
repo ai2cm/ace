f2c4878fdc2001a38f6928f8ba000f5f053f0f0f
Troy Arcomano <troya@allenai.org>
2025-11-28 19:44:38 +0000

    try again

--- configs/experiments/2025-09-10-downscale-ace-xshield/generate_ensemble_global.sh
+++ configs/experiments/2025-09-10-downscale-ace-xshield/generate_ensemble_global.sh
@@ -1,4 +1,3 @@
-!/bin/bash
 # uses the augusta cluster which doesn't have weka access but has GCS access and is
 # typically more available than cirrascale clusters
 
@@ -6,7 +5,10 @@ set -e
 
 JOB_NAME="downscale-ace-inference-100km-to-3km-ace-1-year-global"
 
+CONFIG_FILENAME="gen-ace-output-global-ic0000.yaml"
+
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
 
  # since we use a service account API key for wandb, we use the beaker username to set the wandb username
 BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
@@ -14,53 +16,35 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 cd $REPO_ROOT  # so config path is valid no matter where we are running this script
 
-N_NODES=1
 NGPU=2
+IMAGE="$(cat $REPO_ROOT/latest_deps_only_image.txt)"
 
-#IMAGE with B200 pytorch installed
-IMAGE=01JWJ96JMF89D812JS159VF37N
-
-# Checkpoint at ~
+#EXISTING_RESULTS_DATASET=01K8P3P5205396WR50FCMZR6P7 # best crps checkpoint from job using global validation
 EXISTING_RESULTS_DATASET=01K8XGEEAJRHN8JRZE4WXQRDVD
 
-ACE_DATASET=01KA4CZZP73VC053HST5HFT3BS
 wandb_group=""
 
-run_eval() {
-    local ensemble="$1"
-    local JOB_NAME_RUN="${JOB_NAME}-ensemble${ensemble}"
-    local CONFIG_FILENAME="gen-ace-output-global-ic${ensemble}.yaml"
-    local CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
-    gantry run \
-        --name $JOB_NAME \
-        --description 'Run 100km to 3km generation on ACE data' \
-        --workspace ai2/ace \
-        --priority high \
-        --not-preemptible \
-        --cluster ai2/titan \
-        --beaker-image $IMAGE \
-        --env WANDB_USERNAME=$BEAKER_USERNAME \
-        --env WANDB_NAME=$JOB_NAME_RUN \
-        --env WANDB_JOB_TYPE=inference \
-        --env WANDB_RUN_GROUP=$wandb_group \
-        --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
-        --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
-        --dataset-secret google-credentials:/tmp/google_application_credentials.json \
-        --dataset $EXISTING_RESULTS_DATASET:checkpoints/best_histogram_tail.ckpt:/ckpt.tar \
-        --weka climate-default:/climate-default \
-        --gpus $NGPU \
-        --shared-memory 400GiB \
-        --budget ai2/climate \
-        --system-python \
-        --install "pip install --no-deps ." \
-        --allow-dirty \
-        -- torchrun --nproc_per_node $NGPU -m fme.downscaling.inference $CONFIG_PATH
-}
-
-n_ensembles_minus_one=0
-
-for ((i=0; i<=n_ensembles_minus_one; i++)); do
-    padded=$(printf "%04d" "$i")   # produces 0000, 0001, ...
-    echo "Running ensemble member $padded"
-    run_eval "$padded"
-done
+gantry run \
+    --name $JOB_NAME \
+    --description 'Run 100km to 3km generation on ACE' \
+    --workspace ai2/ace \
+    --priority high \
+    --preemptible \
+    --cluster ai2/titan \
+    --beaker-image $IMAGE \
+    --env WANDB_USERNAME=$BEAKER_USERNAME \
+    --env WANDB_NAME=$JOB_NAME \
+    --env WANDB_JOB_TYPE=inference \
+    --env WANDB_RUN_GROUP=$wandb_group \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $EXISTING_RESULTS_DATASET:checkpoints:/checkpoints \
+    --weka climate-default:/climate-default \
+    --gpus $NGPU \
+    --shared-memory 400GiB \
+    --budget ai2/climate \
+    --no-conda \
+    --install "pip install --no-deps ." \
+    --allow-dirty \
+    -- torchrun --nproc_per_node $NGPU -m fme.downscaling.inference $CONFIG_PATH
