a37dc242f28c04bca1cd3ecb8626d6fdb465213c
Elynn Wu <elynnw@allenai.org>
2025-12-02 22:18:05 -0800

    flags

--- configs/experiments/2025-11-05-e3smv3-piControl-100yr/atmosphere/gantry/run-ace-train.sh
+++ configs/experiments/2025-11-05-e3smv3-piControl-100yr/atmosphere/gantry/run-ace-train.sh
@@ -59,6 +59,8 @@ for RS in $(seq 1 $N_RANDOM_SEED_RUNS); do
           --env WANDB_JOB_TYPE=training \
           --env WANDB_RUN_GROUP="${JOB_GROUP}" \
           --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+          --env NCCL_DEBUG=WARN \
+          --env NCCL_DEBUG_FILE=/results/nccl_debug.log \
           --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
           --dataset-secret google-credentials:/tmp/google_application_credentials.json \
           --dataset $STATS_DATA:/statsdata \
