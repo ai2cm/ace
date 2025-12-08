e654a06ff89c6e3910de19afced16fe0af039f10
Anna Kwa <annak@allenai.org>
2025-12-08 09:51:30 -0800

    Allow `ZarrWriterAdapter` to write data with n_timestep=1 (#2357)

--- fme/ace/inference/data_writer/zarr.py
+++ fme/ace/inference/data_writer/zarr.py
@@ -31,20 +31,28 @@ def _variable_metadata_to_dict(
 
 
 def _get_ace_time_coords(batch_time: xr.DataArray, n_timesteps: int):
-    dt_timedelta = _get_timestep(batch_time.isel(sample=0).values)
-    dt_microseconds = encode_timestep(dt_timedelta)
+    if n_timesteps > 1:
+        dt_timedelta = _get_timestep(batch_time.isel(sample=0).values)
+        dt_microseconds = encode_timestep(dt_timedelta)
+        lead_times_microseconds = dt_microseconds * np.arange(n_timesteps)
+        lead_times_coord = xr.DataArray(
+            lead_times_microseconds,
+            dims=["time"],
+            attrs={"units": "microseconds", "dtype": "timedelta64[us]"},
+        )
+    elif n_timesteps == 1:
+        lead_times_coord = xr.DataArray(
+            np.array([0], dtype=np.int64),
+            dims=["time"],
+            attrs={"units": "microseconds", "dtype": "timedelta64[us]"},
+        )
     init_times_datetime = batch_time.isel(time=0).values
     init_times_numeric = cftime.date2num(
         init_times_datetime,
         units=DATETIME_ENCODING_UNITS,
         calendar=batch_time.dt.calendar,
     )
-    lead_times_microseconds = dt_microseconds * np.arange(n_timesteps)
-    lead_times_coord = xr.DataArray(
-        lead_times_microseconds,
-        dims=["time"],
-        attrs={"units": "microseconds", "dtype": "timedelta64[us]"},
-    )
+
     init_times_coord = xr.DataArray(
         init_times_numeric,
         dims=["sample"],
