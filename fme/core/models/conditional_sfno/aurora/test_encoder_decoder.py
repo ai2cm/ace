d162d123a38c1629a2dfcbce0fa675a96c244f81
Jeremy McGibbon <jeremy.mcgibbon@gmail.com>
2026-01-05 22:39:02 +0000

    convert lists to tuples

--- fme/core/models/conditional_sfno/aurora/test_encoder_decoder.py
+++ fme/core/models/conditional_sfno/aurora/test_encoder_decoder.py
@@ -1,9 +1,11 @@
-from fme.core.device import get_device
-from fme.core.models.conditional_sfno.aurora.encoder import Perceiver3DEncoder
-from fme.core.models.conditional_sfno.aurora.decoder import Perceiver3DDecoder
-from fme.core.models.conditional_sfno.aurora.batch import Batch, Metadata
-import torch
 from datetime import datetime, timedelta
+
+import torch
+
+from fme.core.device import get_device
+from fme.core.models.conditional_sfno.aurora.batch import Batch, Metadata
+from fme.core.models.conditional_sfno.aurora.decoder import Perceiver3DDecoder
+from fme.core.models.conditional_sfno.aurora.encoder import Perceiver3DEncoder
 
 
 def get_batch(
@@ -36,26 +38,26 @@ def get_batch(
     )
 
     for name in surf_names:
-        surf_vars[name] = torch.randn(
-            batch_size, n_time, n_lat, n_lon, device=device
-        )
+        surf_vars[name] = torch.randn(batch_size, n_time, n_lat, n_lon, device=device)
     for name in static_names:
-        static_vars[name] = torch.randn(
-            batch_size, n_time, n_lat, n_lon, device=device
-        )
+        static_vars[name] = torch.randn(batch_size, n_time, n_lat, n_lon, device=device)
     for name in atmos_names:
         atmos_vars[name] = torch.randn(
             batch_size, n_time, n_levels, n_lat, n_lon, device=device
         )
 
     return Batch(
-        surf_vars=surf_vars, static_vars=static_vars, atmos_vars=atmos_vars, metadata=metadata
+        surf_vars=surf_vars,
+        static_vars=static_vars,
+        atmos_vars=atmos_vars,
+        metadata=metadata,
     )
 
+
 def test_can_call_round_trip_encoder_decoder():
-    surf_names = ["surface_temp", "surface_pressure"]
-    static_names = ["orography"]
-    atmos_names = ["temp", "humidity", "u_wind", "v_wind"]
+    surf_names = ("surface_temp", "surface_pressure")
+    static_names = ("orography",)
+    atmos_names = ("temp", "humidity", "u_wind", "v_wind")
     n_lat = 16
     n_lon = 32
     n_time = 2
@@ -75,9 +77,9 @@ def test_can_call_round_trip_encoder_decoder():
     lead_time = timedelta(hours=6)
 
     encoder = Perceiver3DEncoder(
-        surf_vars = surf_names,
-        static_vars = static_names,
-        atmos_vars = atmos_names,
+        surf_vars=surf_names,
+        static_vars=static_names,
+        atmos_vars=atmos_names,
         patch_size=2,
         latent_levels=4,
         embed_dim=16,
@@ -123,4 +125,3 @@ def test_can_call_round_trip_encoder_decoder():
             n_lat,
             n_lon,
         )
-
