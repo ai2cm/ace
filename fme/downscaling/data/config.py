2540de5f862b193a836bdaba6b69f6e9db618416
Anna Kwa <annak@allenai.org>
2026-01-07 15:14:07 -0800

    data configs take optional static_inputs arg in build methods

--- fme/downscaling/data/config.py
+++ fme/downscaling/data/config.py
@@ -22,6 +22,7 @@ from fme.downscaling.data.datasets import (
     PairedGriddedData,
 )
 from fme.downscaling.data.topography import (
+    StaticInputs,
     Topography,
     get_normalized_topography,
     get_topography_downscale_factor,
@@ -161,16 +162,25 @@ class DataLoaderConfig:
         )
 
     def build_topography(
-        self, coarse_coords: LatLonCoordinates, requires_topography: bool
+        self,
+        coarse_coords: LatLonCoordinates,
+        requires_topography: bool,
+        static_inputs_from_checkpoint: StaticInputs | None = None,
     ) -> Topography | None:
         if requires_topography is False:
             return None
-        if self.topography is None:
-            raise ValueError(
-                "Topography is required for this model, but no topography "
-                "dataset was specified in the configuration."
-            )
-        topography = get_normalized_topography(self.topography)
+        if static_inputs_from_checkpoint is not None:
+            # TODO: change to use full static inputs list
+            topography = static_inputs_from_checkpoint[0]
+        else:
+            if self.topography is None:
+                raise ValueError(
+                    "Topography is required for this model, but no topography "
+                    "dataset was specified in the configuration nor provided "
+                    "in model checkpoint."
+                )
+            topography = get_normalized_topography(self.topography)
+
         # Fine grid boundaries are adjusted to exactly match the coarse grid
         fine_lat_interval = adjust_fine_coord_range(
             self.lat_extent,
@@ -218,6 +228,7 @@ class DataLoaderConfig:
         self,
         requirements: DataRequirements,
         dist: Distributed | None = None,
+        static_inputs_from_checkpoint: StaticInputs | None = None,
     ) -> GriddedData:
         xr_dataset, properties = self.get_xarray_dataset(
             names=requirements.coarse_names, n_timesteps=1
@@ -256,6 +267,7 @@ class DataLoaderConfig:
         subset_topography = self.build_topography(
             coarse_coords=latlon_coords,
             requires_topography=requirements.use_fine_topography,
+            static_inputs_from_checkpoint=static_inputs_from_checkpoint,
         )
         return GriddedData(
             _loader=dataloader,
@@ -358,6 +370,7 @@ class PairedDataLoaderConfig:
         train: bool,
         requirements: DataRequirements,
         dist: Distributed | None = None,
+        static_inputs_from_checkpoint: StaticInputs | None = None,
     ) -> PairedGriddedData:
         if dist is None:
             dist = Distributed.get_instance()
@@ -412,7 +425,10 @@ class PairedDataLoaderConfig:
         )
 
         if requirements.use_fine_topography:
-            if self.topography is None:
+            if static_inputs_from_checkpoint is not None:
+                # TODO: change to use full static inputs list
+                fine_topography = static_inputs_from_checkpoint[0]
+            elif self.topography is None:
                 data_path = self.fine[0].data_path
                 file_pattern = self.fine[0].file_pattern
                 raw_paths = get_raw_paths(data_path, file_pattern)
@@ -423,6 +439,7 @@ class PairedDataLoaderConfig:
                 fine_topography = get_normalized_topography(raw_paths[0])
             else:
                 fine_topography = get_normalized_topography(self.topography)
+
             fine_topography = fine_topography.to_device()
             if (
                 get_topography_downscale_factor(
@@ -435,6 +452,7 @@ class PairedDataLoaderConfig:
                     f"Fine topography shape {fine_topography.shape} does not match "
                     f"fine data shape {properties_fine.horizontal_coordinates.shape}."
                 )
+
             fine_topography = fine_topography.subset_latlon(
                 lat_interval=fine_lat_extent, lon_interval=fine_lon_extent
             )
