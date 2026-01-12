951f146faa9a16d2823ac4c2599b1ac961a65000
Anna Kwa <annak@allenai.org>
2026-01-12 23:40:46 +0000

    final script

--- scripts/checkpoint_publication/add_static_inputs.py
+++ scripts/checkpoint_publication/add_static_inputs.py
@@ -8,8 +8,7 @@ scratch_dir = "/climate-default/home/annak/scratch/checkpoints/hiro"
 topography_path = "/climate-default/2025-09-25-downscaling-data-X-SHiELD-AMIP-downscaling/3km.zarr"
 checkpoint_path = f"{scratch_dir}/checkpoints/best_histogram_tail.ckpt"
 
-topography = get_normalized_topography(topography_path).to_device()
-import pdb; pdb.set_trace()
+topography = get_normalized_topography(topography_path)
 
 config = CheckpointModelConfig(checkpoint_path=checkpoint_path)
 model = config.build()
@@ -18,4 +17,4 @@ model.static_inputs = StaticInputs([topography])
 new_checkpoint = config._checkpoint
 new_checkpoint.update(model=model.get_state())  # type: ignore
 
-torch.save(new_checkpoint, f"{scratch_dir}/HiRO.ckpt")
+torch.save(new_checkpoint, f"{scratch_dir}/hiro-public-ckpt.tar")
