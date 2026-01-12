e44e5ee28301019e040600466705ae7677de2d7d
Anna Kwa <annak@allenai.org>
2026-01-12 23:26:07 +0000

    update paths to weka

--- scripts/checkpoint_publication/add_static_inputs.py
+++ scripts/checkpoint_publication/add_static_inputs.py
@@ -3,12 +3,13 @@ import torch
 from fme.downscaling.data import StaticInputs, get_normalized_topography
 from fme.downscaling.models import CheckpointModelConfig
 
-scratch_dir = "/Users/annak/Code/scratch/checkpoints/hiro-ckpt"
+scratch_dir = "/climate-default/home/annak/scratch/checkpoints/hiro"
 
-topography_path = "gs://vcm-ml-raw-flexible-retention/2025-08-12-X-SHiELD-AMIP-downscaling/regridded-zarrs/gaussian_grid_180_by_360_refined_to_5760_by_11520/control/static.zarr"
-checkpoint_path = f"{scratch_dir}/hiro-0.ckpt"
+topography_path = "/climate-default/2025-09-25-downscaling-data-X-SHiELD-AMIP-downscaling/3km.zarr"
+checkpoint_path = f"{scratch_dir}/checkpoints/best_histogram_tail.ckpt"
 
-topography = get_normalized_topography(topography_path)
+topography = get_normalized_topography(topography_path).to_device()
+import pdb; pdb.set_trace()
 
 config = CheckpointModelConfig(checkpoint_path=checkpoint_path)
 model = config.build()
