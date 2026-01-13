c6ecc028bfae86b1380192e25a4710b78f4c173f
Anna Kwa <annak@allenai.org>
2026-01-13 00:06:59 +0000

    add test script

---
+++ configs/baselines/downscaling-hiro-global/inference.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+# for now, just using a single node
+NGPU=2
+
+
+JOB_NAME="test-release-model-with-serialized-data"  # recommended but not required to change this
+CONFIG_FILENAME="inference.yaml"
+
+EXISTING_RESULTS_DATASET="01KET96PB1RJMVJDADX04NDQJH"  # 25km-to-3km https://beaker.allen.ai/orgs/ai2/workspaces/downscaling/work/01JS04B56S6AS5X96H8AMB7FE7
+
+SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+
+ # since we use a service account API key for wandb, we use the beaker username to set the wandb username
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+DEPS_ONLY_IMAGE="$(cat latest_deps_only_image.txt)"
+
+gantry run \
+    --name $JOB_NAME \
+    --description 'Run downscaling inference' \
+    --workspace ai2/ace \
+    --priority high  \
+    --preemptible \
+    --cluster ai2/jupiter \
+    --gpus $NGPU \
+    --budget ai2/climate \
+    --beaker-image $DEPS_ONLY_IMAGE \
+    --env WANDB_USERNAME=$BEAKER_USERNAME \
+    --env WANDB_NAME=$JOB_NAME \
+    --env WANDB_JOB_TYPE=inference \
+    --env WANDB_RUN_GROUP= \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $EXISTING_RESULTS_DATASET:hiro-public-ckpt.tar:/hiro-public-ckpt.tar \
+    --system-python \
+    --install "pip install --no-deps ." \
+    -- torchrun --nproc_per_node $NGPU -m fme.downscaling.inference $CONFIG_PATH
