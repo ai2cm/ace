728823b2430f1c4399a5c54df92ce425c8582dc7
Troy Arcomano <troya@allenai.org>
2026-01-20 20:04:09 +0000

    rename ace config and add more documentation

--- configs/experiments/2026-01-15-hiro-ace/run-hiro-ace.sh
+++ configs/experiments/2026-01-15-hiro-ace/run-hiro-ace.sh
@@ -1,7 +1,19 @@
-# First run coarse ACE2S
-python -m fme.ace.inference inference_config.yaml
+# Example script to show the steps needed to downscale ACE
+# using HiRO
 
-# Downscale ACE2S using HiRO
+# Provided are two inference configurations used in the
+# manuscript:
+# 1.) Global inference with ACE for 2023 then downscaled
+# for most of the global (-65S to 65N) using HiRO
+#
+# 2.) Global inference with ACE for 2014 - 2023 then
+# the Pacific NorthWest (PNW) only downscaled with HiRO
+
+
+# First run coarse ACE2S
+python -m fme.ace.inference ace2s_inference_config_global.yaml
+
+# Second downscale ACE2S using HiRO
 
 # HiRO is more computationally costly then ACE2S
 # for faster through put more GPUs may be required
