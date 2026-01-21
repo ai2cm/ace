a58046c974530bd0a9caae8b0a993f2c850ac832
Oscar <odiazib@sandia.gov>
2026-01-21 16:59:56 -0700

    Remove duplicate code.

--- fme/core/distributed/spatial_torch_distributed.py
+++ fme/core/distributed/spatial_torch_distributed.py
@@ -14,6 +14,7 @@ from fme.core.device import get_device, using_gpu, using_srun
 from .base import DistributedBackend
 from .non_distributed import DummyWrapper
 from fme.core.distributed import comm
+from .torch_distributed import _gather_irregular, _pad_tensor_at_end, _unpad_tensor_at_end
 
 logger = logging.getLogger(__name__)
 
@@ -139,106 +140,3 @@ class SpatialTorchDistributed(DistributedBackend):
         self.barrier()
         logger.debug(f"Shutting down rank {self.rank}")
         torch.distributed.destroy_process_group()
-
-
-def _gather_irregular(
-    tensor: torch.Tensor,
-    reduce_max: Callable[[torch.Tensor], torch.tensor],
-    gather: Callable[[torch.Tensor], list[torch.Tensor] | None],
-    fill_value: float | int = 0.0,
-) -> list[torch.Tensor] | None:
-    """
-    Gather a tensor from all processes to the root process. The rank tensors
-    may have different dimension lengths, but must have the same number of dimensions.
-
-    To accomplish this, the tensor is temporarily padded with `fill_value` where
-    its dimension length is smaller than the maximum dimension length for the purpose of
-    communication, and the padding is removed prior to returning the gathered tensors.
-
-    Args:
-        tensor: The tensor to gather.
-        reduce_max: The reduction function to use for each dimension length.
-        gather: The gather function to use.
-        fill_value: The value to fill each tensor with.
-
-    Returns:
-        A list of tensors, where the i-th element is the tensor from the i-th process.
-    """
-    output_tensor_size = []
-    tensor_size = list(tensor.size())
-    for dim_len in tensor_size:
-        dimension_length = torch.tensor(dim_len, dtype=torch.int32, device=get_device())
-        reduce_max(dimension_length)
-        output_tensor_size.append(int(dimension_length.item()))
-    dimension_difference = [
-        output - input for output, input in zip(output_tensor_size, tensor_size)
-    ]
-    regular_tensor = _pad_tensor_at_end(tensor, dimension_difference, fill_value)
-    gathered_regular_tensors = gather(regular_tensor)
-    gathered_dimension_differences = gather(
-        torch.tensor(dimension_difference, device=get_device())
-    )
-    if gathered_regular_tensors is None or gathered_dimension_differences is None:
-        return None
-    else:
-        return [
-            _unpad_tensor_at_end(regular_tensor, dimension_difference)
-            for regular_tensor, dimension_difference in zip(
-                gathered_regular_tensors, gathered_dimension_differences
-            )
-        ]
-
-
-def _pad_tensor_at_end(
-    tensor: torch.Tensor,
-    dimension_difference: list[int],
-    fill_value: float | int = 0.0,
-):
-    """Pad tensor by specified amount at end of each dimension.
-    Note that `pad` format is in reverse dimension order.
-
-    Args:
-        tensor: The tensor to pad
-        dimension_difference: The amount to pad each dimension
-        fill_value: The value to fill the padding with
-
-    Returns:
-        The padded tensor
-    """
-    assert len(dimension_difference) == len(tensor.size()), "Dimension mismatch"
-    pad_dimensions = tuple(
-        [
-            val
-            for pair in zip(
-                [0 for _ in tensor.size()],
-                [diff for diff in dimension_difference[::-1]],
-            )
-            for val in pair
-        ]
-    )
-    padded_tensor = pad(tensor, pad_dimensions, mode="constant", value=fill_value).to(
-        get_device()
-    )
-    return padded_tensor
-
-
-def _unpad_tensor_at_end(
-    tensor: torch.Tensor, dimension_difference: torch.Tensor
-) -> torch.Tensor:
-    """Remove padding from tensor.
-
-    Args:
-        tensor: The tensor to remove padding from
-        dimension_difference: The amount of padding to remove from each dimension
-
-    Returns:
-        The tensor with padding removed
-    """
-    assert len(dimension_difference) == len(tensor.size()), "Dimension mismatch"
-    slice_dimensions = tuple(
-        [
-            slice(0, tensor.size()[i] - dimension_difference[i])
-            for i in range(len(tensor.size()))
-        ]
-    )
-    return tensor[slice_dimensions]
