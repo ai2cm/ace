e0dbe796e15a6c554977bc4e1447dffa5316ceb0
James Duncan <jpduncan@berkeley.edu>
2026-01-21 20:24:20 -0800

    Add cm4_piControl_v20250603 experiment
    * prediction_loader

---
+++ configs/experiments/cm4_piControl_v20250603/prediction_loader/atmos/run-ace-evaluator.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+JOB_NAME="cm4_piControl_v20250603_reference-ic0311_40yrs"
+JOB_GROUP="cm4_piControl_v20250603_reference"
+EXISTING_RESULTS_DATASET="01JXXESTVASYBEKBM1VAWCRV87"  # determines variables
+CONFIG_FILENAME="ace-evaluator-config.yaml"
+SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
+CONFIG_PATH="${SCRIPT_PATH}${CONFIG_FILENAME}"
+ # since we use a service account API key for wandb, we use the beaker username to set the wandb username
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+
+python -m fme.ace.validate_config --config_type evaluator $CONFIG_PATH
+
+gantry run \
+    --name $JOB_NAME \
+    --task-name $JOB_NAME \
+    --description 'Run prediction loader eval for CM4 atmosphere data' \
+    --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+    --workspace ai2/ace \
+    --priority high \
+    --not-preemptible \
+    --cluster ceres \
+    --cluster jupiter \
+    --cluster saturn \
+    --env WANDB_USERNAME=$BEAKER_USERNAME \
+    --env WANDB_NAME=$JOB_NAME \
+    --env WANDB_JOB_TYPE=inference \
+    --env WANDB_RUN_GROUP=$JOB_GROUP \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $EXISTING_RESULTS_DATASET:training_checkpoints/best_inference_ckpt.tar:/ckpt.tar \
+    --gpus 1 \
+    --shared-memory 50GiB \
+    --weka climate-default:/climate-default \
+    --budget ai2/climate \
+    --system-python \
+    --install "pip install --no-deps ." \
+    -- python -I -m fme.ace.evaluator $CONFIG_PATH
+
+
+
