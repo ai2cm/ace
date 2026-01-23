433410a69e91a9dc3bec5d320886d91d23001901
Anna Kwa <annak@allenai.org>
2026-01-23 14:56:44 -0800

    reduce val area

--- configs/experiments/2026-01-23-downscaling-train-timing/train.sh
+++ configs/experiments/2026-01-23-downscaling-train-timing/train.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-JOB_NAME="xshield-downscaling-convert-to-channels-last-apex-gnorm-updated-unet"
-CONFIG_FILENAME="train_channels_last.yaml"
+JOB_NAME="xshield-downscaling-torch-gnorm-updated-unet"
+CONFIG_FILENAME="train_control.yaml"
 
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
 CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
