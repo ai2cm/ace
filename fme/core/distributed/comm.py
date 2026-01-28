71963c4e8461a7abb105b5183f1f49149297fdf8
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2026-01-28 14:19:18 -0700

    Add information about the comm.py file.

--- fme/core/distributed/comm.py
+++ fme/core/distributed/comm.py
@@ -1,3 +1,6 @@
+"""
+https://github.com/NVIDIA/makani/blob/dbcf2c1dc82cdbc544c81193eecd8ac4a6be337c/makani/utils/comm.py.
+"""
 # SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,16 +16,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import time
-import logging
 import math
-from typing import Union
-import numpy as np
+
+from physicsnemo.distributed.config import ProcessGroupConfig, ProcessGroupNode
 
 # we are using the distributed manager from physicsnemo
 from physicsnemo.distributed.manager import DistributedManager
-from physicsnemo.distributed.config import ProcessGroupNode, ProcessGroupConfig
 
 # we need this
 _DM = None
@@ -90,15 +89,17 @@ def get_local_rank():
 def get_comm_names():
     global _DM
     if _DM is not None:
-        return [name for name in _DM.group_names
-                if (not name.startswith("__orthogonal_to"))]
+        return [
+            name for name in _DM.group_names if (not name.startswith("__orthogonal_to"))
+        ]
     else:
         return []
 
 
 def get_model_comm_names():
-    return [x for x in get_comm_names()
-            if x not in ["world", "data", "ensemble", "batch"]]
+    return [
+        x for x in get_comm_names() if x not in ["world", "data", "ensemble", "batch"]
+    ]
 
 
 def is_distributed(name: str):
@@ -118,11 +119,13 @@ def cleanup():
 
 
 # initialization routine
-def init(model_parallel_sizes=[1, 1, 1, 1],
-         model_parallel_names=["h", "w", "fin", "fout"],
-         data_parallel_sizes=[1, -1],
-         data_parallel_names=["ensemble", "batch"], verbose=False):
-
+def init(
+    model_parallel_sizes=[1, 1, 1, 1],
+    model_parallel_names=["h", "w", "fin", "fout"],
+    data_parallel_sizes=[1, -1],
+    data_parallel_names=["ensemble", "batch"],
+    verbose=False,
+):
     # call basic init first
     DistributedManager.initialize()
 
@@ -200,8 +203,10 @@ def init(model_parallel_sizes=[1, 1, 1, 1],
             if rank == _DM.rank:
                 print(f"{rank}: groups:")
                 for gname in get_comm_names():
-                    print(f"\t{gname}: {_DM._group_ranks[gname]},"
-                          f"root={_COMM_ROOTS[gname]}")
+                    print(
+                        f"\t{gname}: {_DM._group_ranks[gname]},"
+                        f"root={_COMM_ROOTS[gname]}"
+                    )
             torch.distributed.barrier(device_ids=[_DM.local_rank])
 
     return get_size("model")
