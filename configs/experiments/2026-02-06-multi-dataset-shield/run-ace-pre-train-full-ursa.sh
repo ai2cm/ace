0c1885906140e0276ef32e0b564724001f55cbae
Spencer Clark <spencerkclark@gmail.com>
2026-02-06 16:59:02 -0500

    Submit one-step pre-training jobs

---
+++ configs/experiments/2026-02-06-multi-dataset-shield/run-ace-pre-train-full-ursa.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+# https://askubuntu.com/questions/893911/when-writing-a-bash-script-how-do-i-get-the-absolute-path-of-the-location-of-th
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+COMMIT=3d05b879ffb9a8208a4db845e140db2f3bde376e
+URSA_CONDA_DIR=$HOME/software/vcm-workflow-control/examples/ace/ursa-conda
+TRAIN_SUBMISSION_SCRIPT=$URSA_CONDA_DIR/run-train-ursa.sh
+SCRATCH=/scratch4/GFDL/gfdlhires/$USER
+FME_VENV=$($URSA_CONDA_DIR/make-venv.sh $COMMIT $SCRATCH/fme-env $SCRATCH | tail -n 1)
+
+# If resuming a failed job, provide its slurm job ID below and uncomment;
+# note that information entered above should be consistent with that of
+# the failed job.
+# export RESUME_JOB_ID=12345678
+
+CONFIG_FILENAME="one-step-pre-train-config-full-ursa.yaml"
+CONFIG_PATH=$SCRIPT_DIR/$CONFIG_FILENAME
+export WANDB_RUN_GROUP=ace-shield
+for seed in 0 1 2 3
+do
+    override="seed=${seed}"
+    wandb_name=ace-shield-one-step-pre-train-full-rs${seed}
+    conda run --prefix $FME_VENV \
+	    python -m fme.ace.validate_config --config_type train $CONFIG_PATH --override $override
+    $TRAIN_SUBMISSION_SCRIPT \
+        $FME_VENV \
+        $CONFIG_PATH \
+        $URSA_CONDA_DIR \
+        $SCRATCH \
+        $wandb_name \
+        $WANDB_USERNAME \
+        $override
+done
---
+++ configs/experiments/2026-02-06-multi-dataset-shield/run-ace-pre-train-no-random-co2-beaker.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+CONFIG_FILENAME="one-step-pre-train-config-no-random-co2-beaker.yaml"
+SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+WANDB_USERNAME=spencerc_ai2
+WANDB_GROUP=ace-shield
+REPO_ROOT=$(git rev-parse --show-toplevel)
+N_GPUS=8
+STATS_DATASET=andrep/2026-02-06-vertically-resolved-1deg-fme-c96-shield-som-ensemble-dataset-ic_0001-stats
+
+cd $REPO_ROOT  # so config path is valid no matter where we are running this
+
+for seed in 0 1
+do
+    job_name="ace-shield-one-step-pre-train-no-random-co2-rs${seed}"
+    override="seed=${seed}"
+    python -m fme.ace.validate_config --config_type train $CONFIG_PATH --override $override
+    gantry run \
+        --name $job_name \
+        --description 'Run ACE training' \
+        --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+        --workspace ai2/climate-titan \
+        --priority urgent \
+        --preemptible \
+        --cluster ai2/jupiter \
+        --env WANDB_USERNAME=$WANDB_USERNAME \
+        --env WANDB_JOB_TYPE=training \
+        --env WANDB_RUN_GROUP=$WANDB_GROUP \
+        --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+        --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+        --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+        --dataset $STATS_DATASET:/statsdata \
+        --gpus $N_GPUS \
+        --shared-memory 400GiB \
+        --weka climate-default:/climate-default \
+        --budget ai2/climate \
+        --system-python \
+        --install "pip install --no-deps ." \
+        -- torchrun --nproc_per_node $N_GPUS -m fme.ace.train $CONFIG_PATH --override $override
+done
