9a2f9b734436b3a0779fbef45a6c2346ddcd6f59
Oscar Diaz-Ibarra <73365179+odiazib@users.noreply.github.com>
2026-02-10 14:30:15 -0700

    Update fme/core/testing/distributed.py
    Co-authored-by: Naser Mahfouz <naser.mahfouz@pnnl.gov>

--- fme/core/testing/distributed.py
+++ fme/core/testing/distributed.py
@@ -14,8 +14,8 @@ class MockDistributed:
     def local_batch_size(self, batch_size: int) -> int:
         return batch_size
 
-    def get_local_slices(self, crop_shape):
-        return tuple(slice(None, None) for _ in crop_shape)
+    def get_local_slices(self, tensor_shape):
+        return tuple(slice(None, None) for _ in tensor_shape)
 
     def reduce_mean(self, tensor: torch.Tensor) -> torch.Tensor:
         tensor.fill_(self.fill_value)
