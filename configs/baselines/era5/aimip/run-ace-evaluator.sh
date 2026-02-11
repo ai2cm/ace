46f3f86d289cc1692e57be9a303f52fcb7f71dbe
Troy Arcomano <troya@allenai.org>
2026-02-11 03:13:04 +0000

    pinned stratosphere out of sample only

--- configs/baselines/era5/aimip/run-ace-evaluator.sh
+++ configs/baselines/era5/aimip/run-ace-evaluator.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-JOB_NAME="ace-aimip-inference-all-prog-pinned-val-eval-period"
+JOB_NAME="ace-aimip-inference-level0-vars-pinned-val-eval-period"
 JOB_GROUP="ace-aimip"
 EXISTING_RESULTS_DATASET="01K9B1MXD6V26S8BQH5CKY514C"  # this contains the checkpoint to use for inference
-CONFIG_FILENAME="ace-evaluator-config-override-all-prog-vars-out-sample.yaml"
+CONFIG_FILENAME="ace-evaluator-config-override-level0-vars-out-sample.yaml"
 SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
 CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
  # since we use a service account API key for wandb, we use the beaker username to set the wandb username
