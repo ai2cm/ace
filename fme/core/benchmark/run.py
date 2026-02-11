2354ef837d42555c17b50f1bfaf3a74d7a620cd0
Jeremy McGibbon <jeremy.mcgibbon@gmail.com>
2026-02-11 15:00:41 +0000

    use logging.info, save json as well

--- fme/core/benchmark/run.py
+++ fme/core/benchmark/run.py
@@ -1,7 +1,11 @@
 import argparse
+import dataclasses
+import json
+import logging
 import os
 import pathlib
 import subprocess
+import sys
 
 import torch
 
@@ -29,18 +33,19 @@ def get_device_name() -> str:
         return "CPU"
 
 
-def main(name: str | None, iters: int, child: str | None = None) -> None:
+def main(name: str | None, iters: int, child: str | None = None) -> int:
     RESULTS_PATH.mkdir(exist_ok=True)
     device_name = get_device_name()
 
-    print(f"Running benchmarks on device: {device_name}")
+    logging.info(f"Running benchmarks on device: {device_name}")
     benchmarks = get_benchmarks()
     if name is not None:
         if name not in benchmarks:
-            print(f"Specified benchmark {name} not found. Available benchmarks:")
-            for benchmark_name in benchmarks:
-                print(f"  - {benchmark_name}")
-            return
+            logging.error(
+                f"Specified benchmark {name} not found. "
+                f"Available benchmarks: {', '.join(benchmarks.keys())}"
+            )
+            return 1
         benchmarks_to_run = {name: benchmarks[name]}
     else:
         benchmarks_to_run = benchmarks
@@ -48,21 +53,30 @@ def main(name: str | None, iters: int, child: str | None = None) -> None:
     def get_label(name):
         return f"{name} on {device_name} at commit {get_git_commit()}"
 
-    def get_filename(name) -> pathlib.Path:
+    def get_filename(name, extension) -> pathlib.Path:
         safe_name = name.replace("/", "_").replace(".", "_").lower()
         safe_device_name = device_name.replace(" ", "_").replace("/", "_").lower()
-        return RESULTS_PATH / f"{safe_name}_{safe_device_name}_{get_git_commit()}.png"
+        return (
+            RESULTS_PATH
+            / f"{safe_name}_{safe_device_name}_{get_git_commit()}.{extension}"
+        )
 
     for name, cls in benchmarks_to_run.items():
-        print(f"Running benchmark: {name}")
+        logging.info(f"Running benchmark: {name}")
         result = cls.run_benchmark(iters=iters)
-        result.to_png(get_filename(name), label=get_label(name))
+        result.to_png(get_filename(name, "png"), label=get_label(name))
         if child is not None:
             child_name = f"{name}.{child}"
             child_label = get_label(child_name)
-            print(f"  Generating report for child timer: {child_label}")
-            result.to_png(get_filename(child_name), label=child_label, child=child)
-        print(f"  Result: {result}")
+            logging.info(f"Generating report for child timer: {child_label}")
+            result.to_png(
+                get_filename(child_name, "png"), label=child_label, child=child
+            )
+        result_data = json.dumps(dataclasses.asdict(result), indent=2)
+        with open(get_filename(name, "json"), "w") as f:
+            f.write(result_data)
+        logging.info(f"Result: {result_data}")
+    return 0
 
 
 def get_benchmark_label(name):
@@ -71,6 +85,9 @@ def get_benchmark_label(name):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
     parser = argparse.ArgumentParser(description="Run registered benchmarks.")
     parser.add_argument(
         "--name",
@@ -99,8 +116,10 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    main(
-        name=args.name,
-        iters=args.iters,
-        child=args.child,
+    sys.exit(
+        main(
+            name=args.name,
+            iters=args.iters,
+            child=args.child,
+        )
     )
