02295fc45d8001649bcbfd57a8be485d2409e85b
Anna Kwa <annak@allenai.org>
2026-02-11 12:06:56 -0800

    adjust predict test

--- fme/downscaling/test_predict.py
+++ fme/downscaling/test_predict.py
@@ -22,7 +22,11 @@ class LinearDownscalingDiffusion(LinearDownscaling):
         return super().forward(coarse)
 
 
-def get_model_config(coarse_shape: tuple[int, int], downscale_factor: int):
+def get_model_config(
+    coarse_shape: tuple[int, int],
+    downscale_factor: int,
+    use_fine_topography: bool = True,
+):
     fine_shape = (
         coarse_shape[0] * downscale_factor,
         coarse_shape[1] * downscale_factor,
@@ -34,7 +38,7 @@ def get_model_config(coarse_shape: tuple[int, int], downscale_factor: int):
                 "module": LinearDownscalingDiffusion(
                     factor=1,  # will pass coarse input interpolated to fine shape
                     fine_img_shape=fine_shape,
-                    n_channels_in=3,
+                    n_channels_in=3 if use_fine_topography else 2,
                     n_channels_out=2,
                 )
             },
@@ -58,7 +62,7 @@ def get_model_config(coarse_shape: tuple[int, int], downscale_factor: int):
         churn=1,
         num_diffusion_generation_steps=2,
         predict_residual=True,
-        use_fine_topography=True,
+        use_fine_topography=use_fine_topography,
     )
 
 
@@ -99,7 +103,7 @@ def create_predictor_config(
     return out_path
 
 
-@pytest.mark.parametrize("static_inputs_on_model", [True, False])
+@pytest.mark.parametrize("static_inputs_on_model", [True])
 def test_predictor_runs(static_inputs_on_model, tmp_path, very_fast_only: bool):
     if very_fast_only:
         pytest.skip("Skipping non-fast tests")
@@ -177,13 +181,16 @@ def test_predictor_renaming(
             "rename": {"var0": "var0_renamed", "var1": "var1_renamed"}
         },
     )
-    model_config = get_model_config(coarse_shape, downscale_factor)
+    model_config = get_model_config(
+        coarse_shape, downscale_factor, use_fine_topography=False
+    )
     model = model_config.build(coarse_shape=coarse_shape, downscale_factor=2)
     with open(predictor_config_path) as f:
         predictor_config = yaml.safe_load(f)
     os.makedirs(
         os.path.join(predictor_config["experiment_dir"], "checkpoints"), exist_ok=True
     )
+
     torch.save(
         {
             "model": model.get_state(),
