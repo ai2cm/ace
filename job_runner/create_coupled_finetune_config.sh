e6b28051edb14a139e3ea713635ee9b9a7d6aa70
James Duncan <jpduncan@berkeley.edu>
2026-02-19 12:11:28 -0800

    job_runner: Update coupled config creation scripts

--- job_runner/create_coupled_finetune_config.sh
+++ job_runner/create_coupled_finetune_config.sh
@@ -12,10 +12,10 @@ cp $TEMPLATE_CONFIG_PATH $CONFIG_PATH
 
 # NOTE: requires yq >= 4
 
-# update the coupled stepper config, without modifying the template config's parameter_init
+# update the coupled stepper config, removing training-specific fields from each component
 yq -i '.stepper *= (load("config.yaml").stepper | del(.ocean) | del(.atmosphere))' $CONFIG_PATH
-yq -i '.stepper.ocean.stepper *= (load("config.yaml").stepper.ocean.stepper | del(.parameter_init))' $CONFIG_PATH
-yq -i '.stepper.atmosphere.stepper *= (load("config.yaml").stepper.atmosphere.stepper | del(.parameter_init))' $CONFIG_PATH
+yq -i '.stepper.ocean.stepper *= (load("config.yaml").stepper.ocean.stepper | del(.loss, .optimize_last_step_only, .n_ensemble, .parameter_init, .train_n_forward_step))' $CONFIG_PATH
+yq -i '.stepper.atmosphere.stepper *= (load("config.yaml").stepper.atmosphere.stepper | del(.loss, .optimize_last_step_only, .n_ensemble, .parameter_init, .train_n_forward_step))' $CONFIG_PATH
 
 # cleanup
 rm ./config.yaml
