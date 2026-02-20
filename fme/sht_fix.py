9fb9c85f7640c17f35c03379ead23348589a28ac
Jeremy McGibbon <jeremy.mcgibbon@gmail.com>
2026-02-20 20:08:46 +0000

    go from 100k down to 10k

--- fme/sht_fix.py
+++ fme/sht_fix.py
@@ -227,7 +227,7 @@ torch_harmonics.RealSHT = RealSHT
 torch_harmonics.InverseRealSHT = InverseRealSHT
 
 
-@register_benchmark("sht_100k")
+@register_benchmark("sht_10k")
 class RealSHTBenchmark(BenchmarkABC):
 
     def __init__(self, sht: RealSHT, x: torch.Tensor):
@@ -240,7 +240,7 @@ class RealSHTBenchmark(BenchmarkABC):
         Initialize any state needed for the benchmark.
         This will be called once before the benchmark is run.
         """
-        return cls.new_from_shape(batch_size=100_000, nlat=180, nlon=360)
+        return cls.new_from_shape(batch_size=10_000, nlat=180, nlon=360)
 
     @classmethod
     def new_for_regression(cls: type[Self]) -> Self | None:
@@ -275,7 +275,7 @@ class RealSHTBenchmark(BenchmarkABC):
         return {"output": result}
 
 
-@register_benchmark("inverse_sht_100k")
+@register_benchmark("inverse_sht_10k")
 class InverseRealSHTBenchmark(BenchmarkABC):
 
     def __init__(self, isht: InverseRealSHT, x_hat: torch.Tensor):
@@ -288,7 +288,7 @@ class InverseRealSHTBenchmark(BenchmarkABC):
         Initialize any state needed for the benchmark.
         This will be called once before the benchmark is run.
         """
-        return cls.new_from_shape(batch_size=100_000, nlat=180, nlon=360)
+        return cls.new_from_shape(batch_size=10_000, nlat=180, nlon=360)
 
     @classmethod
     def new_for_regression(cls: type[Self]) -> Self | None:
