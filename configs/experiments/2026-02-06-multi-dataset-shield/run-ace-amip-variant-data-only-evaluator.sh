a35ea55307a5a0764888dc4e399617b2b52512b6
Spencer Clark <spencerkclark@gmail.com>
2026-02-21 10:31:03 -0500

    Run AMIP variant data only inference

---
+++ configs/experiments/2026-02-06-multi-dataset-shield/run-ace-amip-variant-data-only-evaluator.sh
@@ -0,0 +1,89 @@
+#!/opt/homebrew/bin/bash
+
+set -e
+
+DATE="2026-02-21"
+CONFIG_FILENAME="ace-amip-data-only-evaluator-config.yaml"
+SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+ # since we use a service account API key for wandb, we use the beaker username to set the wandb username
+WANDB_USERNAME=spencerc_ai2
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+REFERENCE_MODEL="01KHGDAMB2BDZQS8JFF65A2YDR"
+CHECKPOINT_PATH=training_checkpoints/best_inference_ckpt.tar
+
+cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+
+AMIP_VARIANT_DATA_ROOT="/climate-default/2025-04-29-c96-1deg-shield-amip-p4k-dataset"
+AMIP_VARIANT_FILE_PATTERN="ic_0001.zarr"
+override="\
+    loader.dataset.data_path=${AMIP_VARIANT_DATA_ROOT} \
+    loader.dataset.file_pattern=${AMIP_VARIANT_FILE_PATTERN} \
+    prediction_loader.dataset.data_path=${AMIP_VARIANT_DATA_ROOT} \
+    prediction_loader.dataset.file_pattern=${AMIP_VARIANT_FILE_PATTERN} \
+"
+python -m fme.ace.validate_config --config_type evaluator $CONFIG_PATH --override $override
+job_name="${DATE}-amip-plus-4K-data-only-evaluator"
+gantry run \
+    --name $job_name \
+    --description 'Run ACE AMIP +4 K data-only evaluator' \
+    --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+    --workspace ai2/ace \
+    --priority high \
+    --preemptible \
+    --cluster ai2/jupiter \
+    --cluster ai2/ceres \
+    --cluster ai2/titan \
+    --env WANDB_USERNAME=$WANDB_USERNAME \
+    --env WANDB_NAME=$job_name \
+    --env WANDB_JOB_TYPE=inference \
+    --env WANDB_RUN_GROUP= \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $REFERENCE_MODEL:$CHECKPOINT_PATH:/ckpt.tar \
+    --gpus 1 \
+    --shared-memory 20GiB \
+    --weka climate-default:/climate-default \
+    --budget ai2/climate \
+    --system-python \
+    --install "pip install --no-deps ." \
+    -- python -I -m fme.ace.evaluator $CONFIG_PATH --override $override
+
+
+AMIP_VARIANT_DATA_ROOT="/climate-default/2025-12-17-vertically-resolved-c96-1deg-shield-amip-constant-co2-data"
+AMIP_VARIANT_FILE_PATTERN="AMIP-constant-CO2.zarr"
+override="\
+    loader.dataset.data_path=${AMIP_VARIANT_DATA_ROOT} \
+    loader.dataset.file_pattern=${AMIP_VARIANT_FILE_PATTERN} \
+    prediction_loader.dataset.data_path=${AMIP_VARIANT_DATA_ROOT} \
+    prediction_loader.dataset.file_pattern=${AMIP_VARIANT_FILE_PATTERN} \
+"
+python -m fme.ace.validate_config --config_type evaluator $CONFIG_PATH --override $override
+job_name="${DATE}-amip-constant-CO2-data-only-evaluator"
+gantry run \
+    --name $job_name \
+    --description 'Run ACE AMIP constant CO2 data-only evaluator' \
+    --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+    --workspace ai2/ace \
+    --priority high \
+    --preemptible \
+    --cluster ai2/jupiter \
+    --cluster ai2/ceres \
+    --cluster ai2/titan \
+    --env WANDB_USERNAME=$WANDB_USERNAME \
+    --env WANDB_NAME=$job_name \
+    --env WANDB_JOB_TYPE=inference \
+    --env WANDB_RUN_GROUP= \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $REFERENCE_MODEL:$CHECKPOINT_PATH:/ckpt.tar \
+    --gpus 1 \
+    --shared-memory 20GiB \
+    --weka climate-default:/climate-default \
+    --budget ai2/climate \
+    --system-python \
+    --install "pip install --no-deps ." \
+    -- python -I -m fme.ace.evaluator $CONFIG_PATH --override $override
