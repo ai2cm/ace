cf5f7d7af0bca6ebabee1d492aa75a52112eb7ed
Spencer Clark <spencerkclark@gmail.com>
2026-02-21 05:42:46 -0500

    Run two-day ensemble inference with new models

--- configs/experiments/2026-02-06-multi-dataset-shield/run-two-day-1xCO2-and-abrupt-4xCO2-inference-ensemble.sh
+++ configs/experiments/2026-02-06-multi-dataset-shield/run-two-day-1xCO2-and-abrupt-4xCO2-inference-ensemble.sh
@@ -14,13 +14,15 @@ CHECKPOINT_PATH=training_checkpoints/best_inference_ckpt.tar
 cd $REPO_ROOT  # so config path is valid no matter where we are running this script
 
 declare -A MODELS=( \
-    [published-baseline-rs3]="01J4BR6J5AW32ZDQ77VZ60P4KT" \
-    [no-random-co2-rs0]="01KHGDAMB2BDZQS8JFF65A2YDR" \
-    [no-random-co2-rs1]="01KH4SDCYN1NF2RP2JXZS0WZ1Y" \
-    [no-random-co2-energy-conserving-rs0]="01KHGDA8TVGP9JKWVJ1N0SMHCN" \
-    [no-random-co2-energy-conserving-rs1]="01KH4SDT1Q5246GZ307W8AW4M3" \
-    [full-rs0]="01KHKJ02SQM8S8T4B6030F94CV" \
-    [full-energy-conserving-rs1]="01KHCXABVNA3TJW0ZT5F4YDDQT" \
+    # [published-baseline-rs3]="01J4BR6J5AW32ZDQ77VZ60P4KT" \
+    # [no-random-co2-rs0]="01KHGDAMB2BDZQS8JFF65A2YDR" \
+    # [no-random-co2-rs1]="01KH4SDCYN1NF2RP2JXZS0WZ1Y" \
+    # [no-random-co2-energy-conserving-rs0]="01KHGDA8TVGP9JKWVJ1N0SMHCN" \
+    # [no-random-co2-energy-conserving-rs1]="01KH4SDT1Q5246GZ307W8AW4M3" \
+    # [full-rs0]="01KHKJ02SQM8S8T4B6030F94CV" \
+    [full-rs1]="01KHJ5EQ04XTFG46QCKX3TTAHF" \
+    [full-energy-conserving-rs0]="01KHJ5F1M6YKVZESPZAAVVD6G8" \
+    # [full-energy-conserving-rs1]="01KHCXABVNA3TJW0ZT5F4YDDQT" \
 )
 
 for name in "${!MODELS[@]}"; do
@@ -31,10 +33,12 @@ for name in "${!MODELS[@]}"; do
         --name $job_name \
         --description 'Run two-day ACE 1xCO2 ensemble inference' \
         --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
-        --workspace ai2/climate-titan \
-        --priority urgent \
-        --not-preemptible \
+        --workspace ai2/ace \
+        --priority high \
+        --preemptible \
         --cluster ai2/jupiter \
+        --cluster ai2/titan \
+        --cluster ai2/ceres \
         --env WANDB_USERNAME=$WANDB_USERNAME \
         --env WANDB_NAME=$job_name \
         --env WANDB_JOB_TYPE=inference \
