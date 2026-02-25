3035f9ebd7815676060be3a402b4e0cc1c492817
Andre Perkins <frodre@gmail.com>
2026-02-24 23:16:10 -0800

    Run plus4k

--- configs/experiments/2026-02-23-plus4k-evaluation/run.sh
+++ configs/experiments/2026-02-23-plus4k-evaluation/run.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-JOB_BASE="evaluate-HiRO-100km-to-3km-generate"
+JOB_BASE="evaluate-HiRO-xshield-amip-plus4K-100km-to-3km-conus-events-generate"
 
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
+CONFIG_PATH="$SCRIPT_PATH/config-plus4k.yaml"
 BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
@@ -18,33 +19,29 @@ EXISTING_RESULTS_DATASET=01K8RWE83W8BEEAT2KRS94FVCD # best hist checkpoint from 
 
 wandb_group=""
 
-run_config() {
-    local JOB_NAME="$JOB_BASE"
-    local CONFIG_PATH="$SCRIPT_PATH/config.yaml"
+JOB_NAME="$JOB_BASE"
+CONFIG_PATH="$SCRIPT_PATH/config-plus4k.yaml"
 
-    gantry run \
-        --name $JOB_NAME \
-        --description 'Run 100km to 3km evaluation on coarsened X-SHiELD' \
-        --workspace ai2/climate-titan \
-        --priority urgent \
-        --preemptible \
-        --cluster ai2/jupiter \
-        --beaker-image $IMAGE \
-        --env WANDB_USERNAME=$BEAKER_USERNAME \
-        --env WANDB_NAME=$JOB_NAME \
-        --env WANDB_JOB_TYPE=inference \
-        --env WANDB_RUN_GROUP=$wandb_group \
-        --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
-        --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
-        --dataset-secret google-credentials:/tmp/google_application_credentials.json \
-        --dataset $EXISTING_RESULTS_DATASET:checkpoints:/checkpoints \
-        --weka climate-default:/climate-default \
-        --gpus $NGPU \
-        --shared-memory 400GiB \
-        --budget ai2/climate \
-        --no-conda \
-        --install "pip install --no-deps ." \
-        -- torchrun --nproc_per_node $NGPU -m fme.downscaling.evaluator $CONFIG_PATH
-}
-
-run_config
+gantry run \
+    --name $JOB_NAME \
+    --description 'Run 100km to 3km evaluation on coarsened X-SHiELD' \
+    --workspace ai2/climate-titan \
+    --priority urgent \
+    --preemptible \
+    --cluster ai2/jupiter \
+    --beaker-image $IMAGE \
+    --env WANDB_USERNAME=$BEAKER_USERNAME \
+    --env WANDB_NAME=$JOB_NAME \
+    --env WANDB_JOB_TYPE=inference \
+    --env WANDB_RUN_GROUP=$wandb_group \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $EXISTING_RESULTS_DATASET:checkpoints:/checkpoints \
+    --weka climate-default:/climate-default \
+    --gpus $NGPU \
+    --shared-memory 400GiB \
+    --budget ai2/climate \
+    --no-conda \
+    --install "pip install --no-deps ." \
+    -- torchrun --nproc_per_node $NGPU -m fme.downscaling.evaluator $CONFIG_PATH
