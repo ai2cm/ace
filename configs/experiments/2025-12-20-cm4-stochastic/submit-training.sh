ffe0fc4e7a7684197e6a4a05af76957b87f6eb12
Spencer Clark <spencerkclark@gmail.com>
2026-02-27 14:34:50 +0000

    Update script and config for re-submitting multi-step fine-tuning jobs

--- configs/experiments/2025-12-20-cm4-stochastic/submit-training.sh
+++ configs/experiments/2025-12-20-cm4-stochastic/submit-training.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-SCRATCH=/scratch4/GFDL/gfdlscr/$USER
+SCRATCH=/scratch4/GFDL/gfdlhires/$USER
 WANDB_USERNAME=spencerc_ai2
-COMMIT=ba6cc3709ddd98ef2d1ea278804e0009f8c70276
+COMMIT=43de3254a21bfe2727e5232ff1f8484d66bf7dbc
 
 # https://askubuntu.com/questions/893911/when-writing-a-bash-script-how-do-i-get-the-absolute-path-of-the-location-of-th
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-URSA_CONDA_DIR=$HOME/software/vcm-workflow-control-repo-switch/examples/ace/ursa-conda
+URSA_CONDA_DIR=$HOME/software/vcm-workflow-control/examples/ace/ursa-conda
 TRAIN_SUBMISSION_SCRIPT=$URSA_CONDA_DIR/run-train-ursa.sh
-FME_VENV=$($URSA_CONDA_DIR/make-venv.sh $COMMIT $SCRATCH/fme-env | tail -n 1)
+FME_VENV=$($URSA_CONDA_DIR/make-venv.sh $COMMIT $SCRATCH/fme-env $SCRATCH | tail -n 1)
 CHECKPOINT_ROOT="/home/Spencer.Clark/scratch/fme-output"
 
 # If resuming a failed job, provide its slurm job ID below and uncomment;
@@ -24,11 +24,11 @@ CONFIG_FILENAME="ursa-stochastic-train.yaml"
 CONFIG_PATH=$SCRIPT_DIR/$CONFIG_FILENAME
 CHECKPOINT="best_ckpt.tar"
 export WANDB_RUN_GROUP=cm4_like_am4_random_co2_stochastic_ec_train
-for seed in 0
+for seed in 0 1
 do
     weights_id=${weights_ids[$seed]}
     weights=${CHECKPOINT_ROOT}/${weights_id}/training_checkpoints/${CHECKPOINT}
-    override="stepper.parameter_init.weights_path=$weights"
+    override="stepper_training.parameter_init.weights_path=$weights"
     wandb_name=cm4_like_am4_random_co2_stochastic_ec-rs${seed}-train
     conda run --prefix $FME_VENV \
 	  python -m fme.ace.validate_config --config_type train $CONFIG_PATH --override $override
@@ -41,26 +41,3 @@ do
         $WANDB_USERNAME \
         $override
 done
-
-# declare -A weights_ids=( [0]="6552401" [1]="6552402" )
-# CONFIG_FILENAME="ursa-stochastic-train-limited.yaml"
-# CONFIG_PATH=$SCRIPT_DIR/$CONFIG_FILENAME
-# CHECKPOINT="best_ckpt.tar"
-# export WANDB_RUN_GROUP=cm4_like_am4_random_co2_stochastic_limited_ec_train
-# for seed in 0 1
-# do
-#     weights_id=${weights_ids[$seed]}
-#     weights=${CHECKPOINT_ROOT}/${weights_id}/training_checkpoints/${CHECKPOINT}
-#     override="stepper.parameter_init.weights_path=$weights"
-#     wandb_name=cm4_like_am4_random_co2_stochastic_limited_ec-rs${seed}-train
-#     conda run --prefix $FME_VENV \
-# 	  python -m fme.ace.validate_config --config_type train $CONFIG_PATH --override $override
-#     $TRAIN_SUBMISSION_SCRIPT \
-#         $FME_VENV \
-#         $CONFIG_PATH \
-#         $URSA_CONDA_DIR \
-#         $SCRATCH \
-#         $wandb_name \
-#         $WANDB_USERNAME \
-#         $override
-# done
