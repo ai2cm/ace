1aee96f90d2c1be42b717a54e3de9a332a47e3f6
Anna Kwa <annak@allenai.org>
2026-02-27 13:24:07 -0800

    18 steps

--- configs/experiments/2026-02-10-downsc-add-pressfc/eval.sh
+++ configs/experiments/2026-02-10-downsc-add-pressfc/eval.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-JOB_NAME="eval-xshield-amip-100km-to-3km-prmsl-loguni-conus"
-CONFIG_FILENAME="eval-100-to-3km-prmsl-conus.yaml"
+JOB_NAME="eval-xshield-amip-100km-to-3km-prmsl-loguni-conus-18st"
+CONFIG_FILENAME="eval-100-to-3km-prmsl-output.yaml"
 
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
 CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
