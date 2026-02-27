4880591989e5c686846821d0bd870ac74bad105b
Spencer Clark <spencerkclark@gmail.com>
2026-02-27 18:57:29 -0500

    Submit one-year runs with data logged to WandB

--- configs/experiments/2026-02-06-multi-dataset-shield/run-ace-amip-plus-4K-spinup.sh
+++ configs/experiments/2026-02-06-multi-dataset-shield/run-ace-amip-plus-4K-spinup.sh
@@ -32,8 +32,60 @@ SPIN_UP_EXPERIMENT_DIR="/results"
 SPIN_UP_N_FORWARD_STEPS=4
 AMIP_DATA_ROOT="/climate-default/2026-01-28-vertically-resolved-c96-1deg-shield-amip-ensemble-dataset"
 
+# for name in "${!MODELS[@]}"; do
+#     job_name="${DATE}-${name}-amip-plus-4K-spinup-inference"
+#     existing_results_dataset=${MODELS[$name]}
+
+#     spin_up_override="\
+#         experiment_dir=${SPIN_UP_EXPERIMENT_DIR} \
+#         n_forward_steps=${SPIN_UP_N_FORWARD_STEPS} \
+#         initial_condition.start_indices.times=[1979-01-01T00:00:00] \
+#         initial_condition.path=${AMIP_DATA_ROOT}/${ENSEMBLE_ID}.zarr \
+#         initial_condition.engine=zarr \
+#         forcing_loader.dataset.data_path=${AMIP_DATA_ROOT} \
+#         forcing_loader.dataset.file_pattern=${ENSEMBLE_ID}.zarr \
+#         logging.log_to_wandb=true \
+#         stepper_override.ocean.surface_temperature_name=surface_temperature \
+#         stepper_override.ocean.ocean_fraction_name=ocean_fraction \
+#         stepper_override.ocean.interpolate=true \
+#         stepper_override.ocean.slab=null \
+#         data_writer.save_prediction_files=true \
+#     "
+#     python -m fme.ace.validate_config --config_type inference $CONFIG_PATH --override $spin_up_override
+
+#     gantry run \
+#         --name $job_name \
+#         --description 'Run ACE AMIP +4 K spinup inference' \
+#         --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+#         --workspace ai2/ace \
+#         --priority low \
+#         --preemptible \
+#         --cluster ai2/jupiter \
+#         --cluster ai2/titan \
+#         --cluster ai2/ceres \
+#         --env WANDB_USERNAME=$WANDB_USERNAME \
+#         --env WANDB_NAME=$job_name \
+#         --env WANDB_JOB_TYPE=inference \
+#         --env WANDB_RUN_GROUP= \
+#         --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+#         --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+#         --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+#         --dataset $existing_results_dataset:$CHECKPOINT_PATH:/ckpt.tar \
+#         --gpus 1 \
+#         --shared-memory 20GiB \
+#         --weka climate-default:/climate-default \
+#         --budget ai2/climate \
+#         --system-python \
+#         --install "pip install --no-deps ." \
+#         -- python -I -m fme.ace.inference $CONFIG_PATH --override $spin_up_override
+# done
+
+SPIN_UP_EXPERIMENT_DIR="/results"
+SPIN_UP_N_FORWARD_STEPS=1460
+AMIP_DATA_ROOT="/climate-default/2026-01-28-vertically-resolved-c96-1deg-shield-amip-ensemble-dataset"
+
 for name in "${!MODELS[@]}"; do
-    job_name="${DATE}-${name}-amip-plus-4K-spinup-inference"
+    job_name="${DATE}-${name}-amip-plus-4K-one-year-spinup-inference"
     existing_results_dataset=${MODELS[$name]}
 
     spin_up_override="\
@@ -49,7 +101,6 @@ for name in "${!MODELS[@]}"; do
         stepper_override.ocean.ocean_fraction_name=ocean_fraction \
         stepper_override.ocean.interpolate=true \
         stepper_override.ocean.slab=null \
-        data_writer.save_prediction_files=true \
     "
     python -m fme.ace.validate_config --config_type inference $CONFIG_PATH --override $spin_up_override
 
@@ -58,7 +109,7 @@ for name in "${!MODELS[@]}"; do
         --description 'Run ACE AMIP +4 K spinup inference' \
         --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
         --workspace ai2/ace \
-        --priority low \
+        --priority high \
         --preemptible \
         --cluster ai2/jupiter \
         --cluster ai2/titan \
