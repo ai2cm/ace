1f573a0aaf5410f2ec8054d7b9219d1fffd1ee8d
James Duncan <jpduncan@berkeley.edu>
2026-03-01 00:08:23 -0800

    job_runner: Update create_finetune_config.sh

--- job_runner/create_finetune_config.sh
+++ job_runner/create_finetune_config.sh
@@ -12,9 +12,11 @@ cp $TEMPLATE_CONFIG_PATH $CONFIG_PATH
 
 # NOTE: requires yq >= 4
 
-# merge checkpoint stepper as base, then overlay with template stepper so that
-# any values explicitly set in the template take precedence
-yq -i '.stepper = load("config.yaml").stepper * .stepper' $CONFIG_PATH
+# Remove training-specific fields from loaded stepper configs
+yq -i 'del(.stepper.loss, .stepper.optimize_last_step_only, .stepper.n_ensemble, .stepper.parameter_init, .stepper.train_n_forward_step)' ./config.yaml
+
+# update stepper config, preserving template values on conflict
+yq -i '.stepper *=n load("config.yaml").stepper' $CONFIG_PATH
 
 # cleanup
 rm ./config.yaml
