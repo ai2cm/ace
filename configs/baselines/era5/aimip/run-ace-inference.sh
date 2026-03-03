04ff40f7965456c2768311ccac276d5348b2c477
Troy Arcomano <troya@allenai.org>
2026-03-03 13:35:04 -0800

    p2k p4k for inference skin temp only

--- configs/baselines/era5/aimip/run-ace-inference.sh
+++ configs/baselines/era5/aimip/run-ace-inference.sh
@@ -2,25 +2,22 @@
 
 set -e
 
-JOB_NAME_BASE="ace-aimip-inference-oct-1978-2024"
+JOB_NAME_BASE="ace-aimip-inference-aimip-skin-temp-only"
 JOB_GROUP="ace-aimip"
 # this is from ace-aimip-fine-tune-decoder-pressure-levels-separate-decoder-lr-warmup-RS0
-EXISTING_RESULTS_DATASET="01KAKXY0EK24K7BZK2N8SPJ5SJ"
-BEAKER_USERNAME=bhenn1983
+EXISTING_RESULTS_DATASET="01KJRJXX19R0NGR8BBY1BPT7DK"
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
 
 SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-AIMIP_INFERENCE_CONFIG_FILENAME="ace-aimip-inference-config.yaml"
-AIMIP_INFERENCE_BASE_CONFIG_PATH=$SCRIPT_PATH/$AIMIP_INFERENCE_CONFIG_FILENAME
-AIMIP_INFERENCE_P2K_CONFIG_FILENAME="ace-aimip-inference-p2k-config.yaml"
+AIMIP_INFERENCE_P2K_CONFIG_FILENAME="ace-aimip-inference-p2k-skin-temp-only.yaml"
 AIMIP_INFERENCE_BASE_P2K_CONFIG_PATH=$SCRIPT_PATH/$AIMIP_INFERENCE_P2K_CONFIG_FILENAME
-AIMIP_INFERENCE_P4K_CONFIG_FILENAME="ace-aimip-inference-p4k-config.yaml"
+AIMIP_INFERENCE_P4K_CONFIG_FILENAME="ace-aimip-inference-p4k-skin-temp-only.yaml"
 AIMIP_INFERENCE_BASE_P4K_CONFIG_PATH=$SCRIPT_PATH/$AIMIP_INFERENCE_P4K_CONFIG_FILENAME
 
 cd $REPO_ROOT  # so config path is valid no matter where we are running this script
 
-python -m fme.ace.validate_config --config_type inference $AIMIP_INFERENCE_BASE_CONFIG_PATH
 python -m fme.ace.validate_config --config_type inference $AIMIP_INFERENCE_BASE_P2K_CONFIG_PATH
 python -m fme.ace.validate_config --config_type inference $AIMIP_INFERENCE_BASE_P4K_CONFIG_PATH
 
@@ -39,10 +36,10 @@ launch_job () {
         --workspace ai2/ace \
         --priority high \
         --not-preemptible \
-        --cluster ai2/ceres-cirrascale \
-        --cluster ai2/titan-cirrascale \
-        --cluster ai2/saturn-cirrascale \
-        --cluster ai2/jupiter-cirrascale-2 \
+        --cluster ai2/ceres \
+        --cluster ai2/titan \
+        --cluster ai2/saturn \
+        --cluster ai2/jupiter \
         --env WANDB_USERNAME=$BEAKER_USERNAME \
         --env WANDB_NAME=$JOB_NAME \
         --env WANDB_JOB_TYPE=inference \
@@ -57,41 +54,12 @@ launch_job () {
         --budget ai2/climate \
         --system-python \
         --install "pip install --no-deps ." \
-        -- python -I -m fme.ace.inference $CONFIG_PATH --override $OVERRIDE
+        -- python -I -m fme.ace.inference $CONFIG_PATH
 
 }
 
-# # generate config files (for commiting only)
-# for IC in {1..5}; do
-#     OUTPUT_CONFIG_PATH="${SCRIPT_PATH}/$(basename $AIMIP_INFERENCE_BASE_CONFIG_PATH -config.yaml)-IC${IC}-config.yaml"
-#     yq '(.data_writer.files[].label |= sub("_r[0-9]i", '"\"_r${IC}i\"))" $AIMIP_INFERENCE_BASE_CONFIG_PATH > $OUTPUT_CONFIG_PATH
-#     P2K_OUTPUT_CONFIG_PATH="${SCRIPT_PATH}/$(basename $AIMIP_INFERENCE_BASE_CONFIG_PATH -config.yaml)-p2k-IC${IC}-config.yaml"
-#     yq '(.data_writer.files[].label |= sub("_r[0-9]i", '"\"_r${IC}i\"))" $AIMIP_INFERENCE_BASE_P2K_CONFIG_PATH > $P2K_OUTPUT_CONFIG_PATH
-#     P4K_OUTPUT_CONFIG_PATH="${SCRIPT_PATH}/$(basename $AIMIP_INFERENCE_BASE_CONFIG_PATH -config.yaml)-p4k-IC${IC}-config.yaml"
-#     yq '(.data_writer.files[].label |= sub("_r[0-9]i", '"\"_r${IC}i\"))" $AIMIP_INFERENCE_BASE_P4K_CONFIG_PATH > $P4K_OUTPUT_CONFIG_PATH
-# done
-
-# launch 46-year (1979-2024) with spinup from 1978-10-01
-# use 5 different initial conditions files
-for IC in {1..5}; do
-    JOB_NAME="${JOB_NAME_BASE}-IC${IC}"
-    IC_PATH="/climate-default/2025-09-12-aimip-evaluation/aimip-evaluation-ics-v3/1978-09-30_IC$(( IC - 1 )).nc" # files are 0-indexed
-    OUTPUT_PATH="/climate-default/2025-11-25-ace-aimip-inference-results/${JOB_NAME}"
-    IC_CONFIG_PATH="${SCRIPT_PATH}/$(basename $AIMIP_INFERENCE_BASE_CONFIG_PATH -config.yaml)-IC${IC}-config.yaml"
-    OVERRIDE="initial_condition.path=${IC_PATH} experiment_dir=${OUTPUT_PATH}"
-    echo "Launching job $JOB_NAME with override: $OVERRIDE"
-    launch_job "$JOB_NAME" "$IC_CONFIG_PATH" "$OVERRIDE"
-done
-
 # same as above but use SST perturbed by +2K and +4K
-for PERTURBATION in p2k p4k; do
-    for IC in {1..5}; do
-        JOB_NAME="${JOB_NAME_BASE}-${PERTURBATION}-IC${IC}"
-        IC_PATH="/climate-default/2025-09-12-aimip-evaluation/aimip-evaluation-ics-v3/1978-09-30_IC$(( IC - 1 )).nc" # files are 0-indexed
-        OUTPUT_PATH="/climate-default/2025-11-25-ace-aimip-inference-results/${JOB_NAME}"
-        IC_PERTURBATION_CONFIG_PATH="${SCRIPT_PATH}/$(basename $AIMIP_INFERENCE_BASE_CONFIG_PATH -config.yaml)-${PERTURBATION}-IC${IC}-config.yaml"
-        OVERRIDE="initial_condition.path=${IC_PATH} experiment_dir=${OUTPUT_PATH}"
-        echo "Launching job: $JOB_NAME with perturbation: $PERTURBATION and IC: $IC"
-        launch_job "$JOB_NAME" "$IC_PERTURBATION_CONFIG_PATH" "$OVERRIDE"
-    done
-done
+JOB_NAME="${JOB_NAME_BASE}-p2k"
+OVERRIDE=""
+echo "Launching job: $JOB_NAME"
+launch_job "$JOB_NAME" "$AIMIP_INFERENCE_P2K_CONFIG_FILENAME" "$OVERRIDE"
