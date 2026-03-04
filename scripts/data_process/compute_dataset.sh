1ccab7e8d16a5733989d34398b7945840fa0ecc9
Jeremy McGibbon <jeremy.mcgibbon@gmail.com>
2026-03-04 15:52:40 +0000

    refactor the way data_process steps are selected

--- scripts/data_process/compute_dataset.sh
+++ scripts/data_process/compute_dataset.sh
@@ -2,15 +2,17 @@
 
 set -e
 
-COMPUTE_DATASET=true
+COMPUTE_DATASET=false
 COMPUTE_TIME_COARSEN=false
+COMPUTE_STATS=false
 
 while [[ "$#" -gt 0 ]]
 do case $1 in
     --config) CONFIG="$2"
     shift;;
-    --stats-only) COMPUTE_DATASET=false;;
+    --dataset) COMPUTE_DATASET=true;;
     --time-coarsen) COMPUTE_TIME_COARSEN=true;;
+    --stats) COMPUTE_STATS=true;;
     *) echo "Unknown parameter passed: $1"
     exit 1;;
 esac
@@ -20,6 +22,12 @@ done
 if [[ -z "${CONFIG}" ]]
 then
     echo "Option --config missing"
+    exit 1;
+fi
+
+if [[ "${COMPUTE_DATASET}" = false && "${COMPUTE_STATS}" = false && "${COMPUTE_TIME_COARSEN}" = false ]]
+then
+    echo "At least one of --dataset, --stats, or --time-coarsen must be specified"
     exit 1;
 fi
 
@@ -33,6 +41,7 @@ runs_count_minus_one=$(($runs_count - 1))
 output=$(argo submit compute_dataset_argo_workflow.yaml \
     -p compute_dataset=${COMPUTE_DATASET} \
     -p compute_time_coarsen=${COMPUTE_TIME_COARSEN} \
+    -p compute_stats=${COMPUTE_STATS} \
     -p python_script="$(< compute_dataset.py)" \
     -p get_stats_script="$(< get_stats.py)" \
     -p combine_stats_script="$(< combine_stats.py)" \
