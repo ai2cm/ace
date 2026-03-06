33030be5b8161353800328f6f1e8cc4a6f529b40
Anna Kwa <annak@allenai.org>
2026-03-06 15:45:07 -0800

    up gpus

--- configs/experiments/2026-02-10-downsc-add-pressfc/train.sh
+++ configs/experiments/2026-02-10-downsc-add-pressfc/train.sh
@@ -6,7 +6,7 @@ set -e
 
 # recommended but not required to change this
 
-JOB_NAME="xshield-downscaling-100km-to-3km-zeroweight-prate"
+JOB_NAME="xshield-downscaling-100km-to-3km-0weight-prate"
 CONFIG_FILENAME="train-100-to-3km-prmsl-output-loguni.yaml"
 
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
@@ -16,7 +16,7 @@ wandb_group=""
  # since we use a service account API key for wandb, we use the beaker username to set the wandb username
 BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
 REPO_ROOT=$(git rev-parse --show-toplevel)
-N_GPUS=4 # TODO: change to 8 after testing
+N_GPUS=8 # TODO: change to 8 after testing
 
 cd $REPO_ROOT  # so config path is valid no matter where we are running this script
 
