e1a63dccd71e66eb48e69a7ddea0645c57a0a41b
Anna Kwa <annak@allenai.org>
2026-03-10 08:30:10 -0700

    update scripts

--- configs/experiments/2026-02-10-downsc-add-pressfc/eval.sh
+++ configs/experiments/2026-02-10-downsc-add-pressfc/eval.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-JOB_NAME="eval-xshield-amip-100km-to-3km-0weight-prate-events"
+JOB_NAME="eval-xshield-amip-100km-to-3km-0.5sigmaexp-tropics-events"
 CONFIG_FILENAME="eval-100-to-3km-prmsl-output.yaml"
 
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
@@ -19,7 +19,7 @@ NGPU=2
 
 IMAGE="$(cat latest_deps_only_image.txt)"
 
-EXISTING_RESULTS_DATASET=01KK4SG3X0VBJ7MG7JMT2J7TJX
+EXISTING_RESULTS_DATASET=01KKAPG0J6DMHN471DH96FAY2V
 wandb_group=""
 
 #--not-preemptible \
--- configs/experiments/2026-02-10-downsc-add-pressfc/resume.sh
+++ configs/experiments/2026-02-10-downsc-add-pressfc/resume.sh
@@ -6,8 +6,7 @@ set -e
 
 # recommended but not required to change this
 
-JOB_NAME="
-xshield-downscaling-100km-to-3km-winds-prmsl-only-0.5sigmaexp-tropics-resume"
+JOB_NAME="xshield-downscaling-100km-to-3km-winds-prmsl-only-0.5sigmaexp-tropics-resume"
 CONFIG_FILENAME="train-100-to-3km-prmsl-winds-only.yaml"
 
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
