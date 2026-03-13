bb0ef156791df1337d11ca3b8eb3ffc7a1dccd42
Andre Perkins <frodre@gmail.com>
2026-03-13 15:12:27 -0700

    Fine coords required for paired data

--- fme/downscaling/data/datasets.py
+++ fme/downscaling/data/datasets.py
@@ -337,7 +337,7 @@ class PairedGriddedData:
     dims: list[str]
     variable_metadata: Mapping[str, VariableMetadata]
     all_times: xr.CFTimeIndex
-    fine_coords: LatLonCoordinates | None = None
+    fine_coords: LatLonCoordinates
 
     @property
     def loader(self) -> DataLoader[PairedBatchItem]:
