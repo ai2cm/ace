320cf99c02fdc3aa6ce1b1fac8c741524d82654e
Anna Kwa <annak@allenai.org>
2026-03-18 13:05:54 -0700

    update sig range untuned multivar

--- configs/experiments/2026-02-10-downsc-add-pressfc/eval.sh
+++ configs/experiments/2026-02-10-downsc-add-pressfc/eval.sh
@@ -3,9 +3,9 @@
 set -e
 
 #JOB_NAME="eval-xshield-amip-100km-to-3km-0.5sigmaexp-tropics-events"
-JOB_NAME="eval-xshield-amip-100km-to-3km-winds-prmsl-only-0.75sigmaexp-events"
+JOB_NAME="eval-xshield-amip-100km-to-3km-winds-multivar-untuned-tropics"
 
-CONFIG_FILENAME="eval-100-to-3km-prmsl-output.yaml"
+CONFIG_FILENAME="eval-tropic-pac.yaml"
 
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
 CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
@@ -21,7 +21,7 @@ NGPU=2
 
 IMAGE="$(cat latest_deps_only_image.txt)"
 
-EXISTING_RESULTS_DATASET=01KKC6TPBFKV4MC5HDNFD3P0HB
+EXISTING_RESULTS_DATASET=01KK08S4JXARPPH88S22Q0X83C
 wandb_group=""
 
 #--not-preemptible \
