c0bce2da6d12974a172de085463841045dcfad4d
Spencer Clark <spencerkclark@gmail.com>
2026-03-23 16:04:37 -0400

    Run data-only abrupt 4xCO2 inference with monthly diagnostics

---
+++ configs/experiments/2026-02-06-multi-dataset-shield/run-ace-abrupt-4xCO2-data-only-evaluator.sh
@@ -0,0 +1,44 @@
+#!/opt/homebrew/bin/bash
+
+set -e
+
+DATE="2026-03-23"
+CONFIG_FILENAME="ace-abrupt-4xCO2-data-only-evaluator-config.yaml"
+SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+WANDB_USERNAME=spencerc_ai2
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+CHECKPOINT_PATH=training_checkpoints/best_inference_ckpt.tar
+
+cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+
+existing_results_dataset="01KHJ5F1M6YKVZESPZAAVVD6G8"
+
+python -m fme.ace.validate_config --config_type evaluator $CONFIG_PATH
+job_name="${DATE}-abrupt-4xCO2-data-only-evaluator"
+gantry run \
+    --name $job_name \
+    --description 'Run ACE abrupt 4xCO2 data-only evaluator' \
+    --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+    --workspace ai2/ace \
+    --priority high \
+    --preemptible \
+    --cluster ai2/jupiter \
+    --cluster ai2/titan \
+    --cluster ai2/ceres \
+    --env WANDB_USERNAME=$WANDB_USERNAME \
+    --env WANDB_NAME=$job_name \
+    --env WANDB_JOB_TYPE=inference \
+    --env WANDB_RUN_GROUP= \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $existing_results_dataset:$CHECKPOINT_PATH:/ckpt.tar \
+    --gpus 1 \
+    --shared-memory 20GiB \
+    --weka climate-default:/climate-default \
+    --budget ai2/climate \
+    --system-python \
+    --install "pip install --no-deps ." \
+    -- python -I -m fme.ace.evaluator $CONFIG_PATH
