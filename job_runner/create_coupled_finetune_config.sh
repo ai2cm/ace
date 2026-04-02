e39ffa44ae36732ae46afbc9f1ea5cd49e168b6f
James Duncan <jpduncan@berkeley.edu>
2026-04-02 14:02:26 -0700

    Fix typo

--- job_runner/create_coupled_finetune_config.sh
+++ job_runner/create_coupled_finetune_config.sh
@@ -14,8 +14,8 @@ cp $TEMPLATE_CONFIG_PATH $CONFIG_PATH
 
 # update the coupled stepper config, removing training-specific fields from each component
 yq -i '.stepper *= (load("config.yaml").stepper | del(.ocean) | del(.atmosphere))' $CONFIG_PATH
-yq -i '.stepper.ocean.stepper *= (load("config.yaml").stepper.ocean.stepper | del(.loss, .optimize_last_step_only, .n_ensemble, .parameter_init, .train_n_forward_step))' $CONFIG_PATH
-yq -i '.stepper.atmosphere.stepper *= (load("config.yaml").stepper.atmosphere.stepper | del(.loss, .optimize_last_step_only, .n_ensemble, .parameter_init, .train_n_forward_step))' $CONFIG_PATH
+yq -i '.stepper.ocean.stepper *= (load("config.yaml").stepper.ocean.stepper | del(.loss, .optimize_last_step_only, .n_ensemble, .parameter_init, .train_n_forward_steps))' $CONFIG_PATH
+yq -i '.stepper.atmosphere.stepper *= (load("config.yaml").stepper.atmosphere.stepper | del(.loss, .optimize_last_step_only, .n_ensemble, .parameter_init, .train_n_forward_steps))' $CONFIG_PATH
 
 # cleanup
 rm ./config.yaml
