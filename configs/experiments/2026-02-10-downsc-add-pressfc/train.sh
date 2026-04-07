f5789618496f0504fe097d55f292720ed20dd916
Anna Kwa <annak@allenai.org>
2026-04-07 16:00:58 -0700

    update train

--- configs/experiments/2026-02-10-downsc-add-pressfc/train.sh
+++ configs/experiments/2026-02-10-downsc-add-pressfc/train.sh
@@ -6,8 +6,8 @@ set -e
 
 # recommended but not required to change this
 
-JOB_NAME="xshield-downscaling-100km-to-3km-multivar-prate-weight0-till600-exp0.75-tropics"
-CONFIG_FILENAME="train-100-to-3km-multivar-prate-weight0-exp0.75.yaml"
+JOB_NAME="xshield-downscaling-100km-to-3km-multivar-coarse-prmsl-tropics"
+CONFIG_FILENAME="train-prmsl-multivar.yaml"
 
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
 CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
