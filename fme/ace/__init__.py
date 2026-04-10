f61fdba6d7ac133e025f334c889f9b4f4de3ee9a
Will Gregory <William.gregory@outlook.com>
2026-04-10 13:24:13 -0400

    register ice corrector

--- fme/ace/__init__.py
+++ fme/ace/__init__.py
@@ -69,6 +69,7 @@ from fme.ace.train.train_config import WeatherEvaluationConfig
 from fme.core.cli import ResumeResultsConfig
 from fme.core.corrector.atmosphere import AtmosphereCorrectorConfig
 from fme.core.corrector.ocean import OceanCorrectorConfig
+from fme.core.corrector.ice import IceCorrectorConfig
 from fme.core.dataset.concat import ConcatDatasetConfig
 from fme.core.dataset.merged import MergeDatasetConfig, MergeNoConcatDatasetConfig
 from fme.core.dataset.time import RepeatedInterval, TimeSlice
