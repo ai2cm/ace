23c560060ad2f20ffe1c4b77ed4fd872891e0866
Anna Kwa <annak@allenai.org>
2026-04-17 14:28:24 -0700

    reduce model sizes

--- configs/experiments/2026-04-14-noise-schedule-moe/train.sh
+++ configs/experiments/2026-04-14-noise-schedule-moe/train.sh
@@ -6,8 +6,8 @@ set -e
 
 # recommended but not required to change this
 
-JOB_NAME="xshield-downscaling-100km-to-3km-multivar-downweight-prate-small-model"
-CONFIG_FILENAME="train-prmsl-multivar-single.yaml"
+JOB_NAME="xshield-downscaling-100km-to-3km-multivar-downweight-prate-small-model-high-sigma"
+CONFIG_FILENAME="train-prmsl-multivar-high-sigma.yaml"
 
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
 CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
