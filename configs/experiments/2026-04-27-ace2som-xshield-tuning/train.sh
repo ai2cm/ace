372033a498b5aaeea1f89f6e86c5d789a2972ef3
Anna Kwa <annak@allenai.org>
2026-04-28 11:07:48 -0700

    add configs

---
+++ configs/experiments/2026-04-27-ace2som-xshield-tuning/train.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -e
+
+job_name="ace2som-xshield-tune-1yr-even-split"
+CONFIG_FILENAME="tune-xshield-1yr-even-split.yaml"
+
+
+SCRIPT_PATH=$(git rev-parse --show-prefix)
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+WANDB_USERNAME=${WANDB_USERNAME:-${BEAKER_USERNAME}}
+WANDB_GROUP=ace
+REPO_ROOT=$(git rev-parse --show-toplevel)
+N_GPUS=4
+STATS_DATASET=spencerc/2025-09-11-X-SHiELD-AMIP-1deg-8layer-11yr-stats
+PRE_TRAINED_WEIGHTS_PATH=/pre-trained-weights/training_checkpoints/best_ckpt.tar
+SEED_OFFSET=10
+
+cd $REPO_ROOT
+
+# two different ACE2SOM seeds
+# declare -A PRE_TRAINED_WEIGHTS_DATASETS=( \
+#     [0]="01KHJ5F1M6YKVZESPZAAVVD6G8" \
+#     [1]="01KHCEF1SBYCZCGDM78N1CJC3H" \
+# )
+
+PRE_TRAINED_WEIGHTS_DATASET=01KHJ5F1M6YKVZESPZAAVVD6G8
+
+
+fine_tune_seed=$((seed + SEED_OFFSET))
+
+override="seed=${fine_tune_seed} \
+stepper_training.parameter_init.weights_path=${PRE_TRAINED_WEIGHTS_PATH} \
+optimization.lr=${lr}"
+
+python -m fme.ace.validate_config --config_type train $CONFIG_PATH --override $override
+
+gantry run \
+    --name $job_name \
+    --description 'Run ACE training' \
+    --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+    --workspace ai2/climate-titan \
+    --priority urgent \
+    --preemptible \
+    --cluster ai2/titan \
+    --env WANDB_NAME=$job_name \
+    --env WANDB_USERNAME=$WANDB_USERNAME \
+    --env WANDB_JOB_TYPE=training \
+    --env WANDB_RUN_GROUP=$WANDB_GROUP \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $STATS_DATASET:/statsdata \
+    --dataset $PRE_TRAINED_WEIGHTS_DATASET:/pre-trained-weights \
+    --gpus $N_GPUS \
+    --shared-memory 400GiB \
+    --weka climate-default:/climate-default \
+    --budget ai2/climate \
+    --system-python \
+    --install "pip install --no-deps ." \
+    -- torchrun --nproc_per_node $N_GPUS -m fme.ace.train $CONFIG_PATH --override $override
+
