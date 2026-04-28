02047e3cb857298f734dd7e1dd6c2403d4e028a9
Troy Arcomano <troya@allenai.org>
2026-04-28 02:09:00 +0000

    train on ERA5 1940 - 2020

--- configs/experiments/2026-03-20-ACE3-on-ERA5/run-ace-train-multi-step-ft-era5.sh
+++ configs/experiments/2026-03-20-ACE3-on-ERA5/run-ace-train-multi-step-ft-era5.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CONFIG_FILENAME="multi-step-fine-tune-config-era5-energy-corrector.yaml"
+CONFIG_FILENAME="multi-step-fine-tune-config-era5-1940-2020.yaml"
 SCRIPT_PATH=$(git rev-parse --show-prefix)
 CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
 BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
@@ -27,7 +27,7 @@ LEARNING_RATES=(1e-4)  #1e-6)
 for seed in 0; do
     for lr in "${LEARNING_RATES[@]}"; do
 
-        job_name="ace-era5-pt-multi-step-shield-ft-energy-correctors-on-lr${lr}-rs${seed}-sampler"
+        job_name="ace-era5-pt-multi-step-shield-ft-1940-2020-lr${lr}-rs${seed}"
 
         fine_tune_seed=$((seed + SEED_OFFSET))
 
