fc5682470cbdf335cd3bf3615f890314bface1fa
Anna Kwa <annak@allenai.org>
2026-04-29 14:49:46 -0700

    evaluator configs

--- configs/experiments/2026-04-27-ace2som-xshield-tuning/evaluate.sh
+++ configs/experiments/2026-04-27-ace2som-xshield-tuning/evaluate.sh
@@ -3,7 +3,7 @@
 set -e
 
 
-CONFIG_FILENAME="tune-xshield-1yr-even-split.yaml"
+CONFIG_FILENAME="ace-evaluator-4k.yaml"
 
 SCRIPT_PATH=$(git rev-parse --show-prefix)
 CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
@@ -11,31 +11,14 @@ BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
 WANDB_USERNAME=${WANDB_USERNAME:-${BEAKER_USERNAME}}
 WANDB_GROUP=ace
 REPO_ROOT=$(git rev-parse --show-toplevel)
-N_GPUS=4
-STATS_DATASET=annak/2026-04-27-vertically-resolved-1deg-c96-shield-ramped-climSST-random-CO2-ensemble-xshield-prmsl-stats
-PRE_TRAINED_WEIGHTS_PATH=/pre-trained-weights/training_checkpoints/best_ckpt.tar
-SEED_OFFSET=10
 
 cd $REPO_ROOT
 
-# two different ACE2SOM seeds
-PRE_TRAINED_WEIGHTS_DATASETS=("01KHJ5F1M6YKVZESPZAAVVD6G8" "01KHCEF1SBYCZCGDM78N1CJC3H")
+MODEL_CHECKPOINT_DATASETS=("01KQD8NF9HQD1QY2X0S132YH72" "01KQD8NMEYCVQ835WQV751MNYP")
 
-# ACE2S ckpt used in paper
-ACE2S_CKPT=01KQDG7X72D4E2JJTGQ0ZF9J9T
 
-# tuned on XSHiELD, seed 0
-TUNED_DATASET=01KQAZ8HBV7K6XZWGPMF159VV2
-
-#       --dataset $TUNED_DATASET:/pre-trained-weights \
-#        --dataset ${PRE_TRAINED_WEIGHTS_DATASETS[$seed]}:/pre-trained-weights \
 for seed in {0..0}; do
-    #job_name="ace2som-xshield-tune-1yr-even-split-single-decoder-seed${seed}"
-    job_name="ace2s-ckpt-xshield-tune-1yr-even-split-single-decoder-seed${seed}"
-    fine_tune_seed=$((seed + SEED_OFFSET))
-    override="seed=${fine_tune_seed}"
-    python -m fme.ace.validate_config --config_type train $CONFIG_PATH --override $override
-
+    job_name="evaluate-4k-ace2som-xshield-tune-1yr-even-split-single-decoder-seed${seed}"
     gantry run \
         --name $job_name \
         --description 'Run ACE training' \
@@ -44,6 +27,7 @@ for seed in {0..0}; do
         --priority urgent \
         --preemptible \
         --cluster ai2/titan \
+        --cluster ai2/jupiter \
         --env WANDB_NAME=$job_name \
         --env WANDB_USERNAME=$WANDB_USERNAME \
         --env WANDB_JOB_TYPE=training \
@@ -52,8 +36,8 @@ for seed in {0..0}; do
         --env-secret WANDB_API_KEY=wandb-api-key-annak \
         --dataset-secret google-credentials:/tmp/google_application_credentials.json \
         --dataset $STATS_DATASET:/statsdata \
-        --dataset $ACE2S_CKPT:training_checkpoints/ACE2S.ckpt:/pre-trained-weights/training_checkpoints/best_ckpt.tar \
-        --gpus $N_GPUS \
+        --dataset ${MODEL_CHECKPOINT_DATASETS[$seed]}:training_checkpoints/best_inference_ckpt.tar:/ckpt.tar \
+        --gpus 1 \
         --shared-memory 400GiB \
         --weka climate-default:/climate-default \
         --budget ai2/climate \
