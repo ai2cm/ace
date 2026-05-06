2cbcee73c6137a46894b66555b7e81c19b0d59ab
James Duncan <jpduncan@berkeley.edu>
2026-05-05 21:44:32 -0700

    Reduce coupled training data I/O

--- fme/coupled/train/train_config.py
+++ fme/coupled/train/train_config.py
@@ -16,13 +16,18 @@ from fme.core.typing_ import Slice
 from fme.core.weight_ops import CopyWeightsConfig
 from fme.coupled.aggregator import InferenceEvaluatorAggregatorConfig
 from fme.coupled.data_loading.config import CoupledDataLoaderConfig
-from fme.coupled.data_loading.getters import get_gridded_data, get_inference_data
+from fme.coupled.data_loading.getters import (
+    get_gridded_data,
+    get_gridded_train_data,
+    get_inference_data,
+)
 from fme.coupled.data_loading.gridded_data import GriddedData, InferenceGriddedData
 from fme.coupled.data_loading.inference import InferenceDataLoaderConfig
 from fme.coupled.dataset_info import CoupledDatasetInfo
 from fme.coupled.requirements import (
     CoupledDataRequirements,
     CoupledPrognosticStateDataRequirements,
+    CoupledTrainDataRequirements,
 )
 from fme.coupled.stepper import (
     CoupledStepperConfig,
@@ -253,7 +258,12 @@ class TrainBuilders:
     def __init__(self, config: TrainConfig):
         self.config = config
 
-    def _get_train_window_data_requirements(self) -> CoupledDataRequirements:
+    def _get_train_window_data_requirements(self) -> CoupledTrainDataRequirements:
+        return self.config.stepper_training.get_train_window_data_requirements(
+            self.config.stepper
+        )
+
+    def _get_valid_window_data_requirements(self) -> CoupledDataRequirements:
         return self.config.stepper.get_evaluation_window_data_requirements(
             self.config.n_coupled_steps
         )
@@ -270,14 +280,13 @@ class TrainBuilders:
 
     def get_train_data(self) -> GriddedData:
         data_requirements = self._get_train_window_data_requirements()
-        return get_gridded_data(
+        return get_gridded_train_data(
             self.config.train_loader,
             requirements=data_requirements,
-            train=True,
         )
 
     def get_validation_data(self) -> GriddedData:
-        data_requirements = self._get_train_window_data_requirements()
+        data_requirements = self._get_valid_window_data_requirements()
         return get_gridded_data(
             self.config.validation_loader,
             requirements=data_requirements,
