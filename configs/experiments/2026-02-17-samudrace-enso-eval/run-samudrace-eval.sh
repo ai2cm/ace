ec479e9bcef47ac1c98b1b326100f2f15a201375
Troy Arcomano <troya@allenai.org>
2026-05-11 17:42:04 +0000

    use best_ckpt

--- configs/experiments/2026-02-17-samudrace-enso-eval/run-samudrace-eval.sh
+++ configs/experiments/2026-02-17-samudrace-enso-eval/run-samudrace-eval.sh
@@ -41,7 +41,7 @@ run_eval() {
     --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
     --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
     --dataset-secret google-credentials:/tmp/google_application_credentials.json \
-    --dataset "${ckpt_dataset}:training_checkpoints/best_inference_ckpt.tar:/ckpt.tar" \
+    --dataset "${ckpt_dataset}:training_checkpoints/best_ckpt.tar:/ckpt.tar" \
     --gpus 1 \
     --shared-memory 200GiB \
     --weka climate-default:/climate-default \
@@ -59,6 +59,6 @@ for year in $(seq 311 320); do
     year_str=$(printf "%04d" "$year")
 
     run_eval "./configs/evaluator-config-updated-picontrol-yr${year_str}.yaml" \
-             "${base_name}-ft-50-50-piControl-fto_stochastic-1pctCO2_0256to0350-hfds_prescribed-gqmfjs2j_cbj61w53-rs0-no-pinned-atmo-yr${year_str}" \
+             "${base_name}-ft-50-50-piControl-fto_stochastic-1pctCO2_0256to0350-hfds_prescribed-gqmfjs2j_cbj61w53-rs0-best-ckpt-yr${year_str}" \
              "01KR273RJZGE851QCES6V905FP"
 done
