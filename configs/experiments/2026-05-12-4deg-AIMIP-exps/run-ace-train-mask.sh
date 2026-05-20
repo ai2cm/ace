3bb1af1af38fb75e7ac11798091f01423fcf4900
Alexey Yermakov <alexeyy@allenai.org>
2026-05-20 10:40:51 -0700

    Added 80% variable masking config

--- configs/experiments/2026-05-12-4deg-AIMIP-exps/run-ace-train-mask.sh
+++ configs/experiments/2026-05-12-4deg-AIMIP-exps/run-ace-train-mask.sh
@@ -6,12 +6,12 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TRAIN_SCRIPT="$SCRIPT_DIR/run-ace-train.sh"
 
 TAGS=(
-    masked
+    variable-masking-0.80
 )
 
 for tag in "${TAGS[@]}"; do
     bash "$TRAIN_SCRIPT" \
         "ace-train-config-4deg-AIMIP-${tag}.yaml" \
-        "ace2-era5-train-4deg-AIMIP-${tag}" \
+        "ace2-era5-train-4deg-${tag}" \
         "ace2-era5-masked"
 done
