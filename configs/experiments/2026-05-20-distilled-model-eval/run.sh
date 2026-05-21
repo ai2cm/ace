a16204686ec38f460aef644cd2034cf118b12e98
Andre Perkins <frodre@gmail.com>
2026-05-21 11:45:38 -0700

    Fix checkpoint path

--- configs/experiments/2026-05-20-distilled-model-eval/run.sh
+++ configs/experiments/2026-05-20-distilled-model-eval/run.sh
@@ -14,7 +14,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 cd $REPO_ROOT
 
-NGPU=8
+NGPU=4
 IMAGE="$(cat $REPO_ROOT/latest_deps_only_image.txt)"
 DATASET=01KRYPVQ3Z5YWQWND9X680GBMD
 
@@ -45,7 +45,7 @@ run_eval() {
         --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
         --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
         --dataset-secret google-credentials:/tmp/google_application_credentials.json \
-        --dataset "$DATASET":checkpoints:/checkpoints \
+        --dataset "$DATASET":/checkpoints \
         --weka climate-default:/climate-default \
         --gpus $NGPU \
         --shared-memory 400GiB \
