0d9b162b8122e6bc4f57d38bf1bf28754d51475d
Alexey Yermakov <yyexela@disroot.org>
2026-05-29 09:30:41 -0700

    Added swin bash run

---
+++ configs/experiments/2026-05-28-swin-transformer/run-ace-train-swin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+JOB_GROUP="2026-05-28-swin-transformer"
+export WANDB_PROJECT=${WANDB_PROJECT:-swin-transformer}
+
+declare -A JOBS=(
+  ["ace-train-config-4deg-AIMIP-nc-swin.yaml"]="ace2-era5-train-4deg-AIMIP-nc-swin"
+  ["ace-train-config-4deg-AIMIP-sfno.yaml"]="ace2-era5-train-4deg-AIMIP-sfno"
+  ["ace-train-config-4deg-AIMIP-swin.yaml"]="ace2-era5-train-4deg-AIMIP-swin"
+)
+
+for config in "${!JOBS[@]}"; do
+  bash "$SCRIPT_DIR/run-ace-train.sh" "$config" "${JOBS[$config]}" "$JOB_GROUP"
+done
