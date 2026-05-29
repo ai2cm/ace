ea256262e30d587190b8f7588de12f82bf2af323
Troy Arcomano <troya@allenai.org>
2026-05-29 19:52:12 +0000

    update stats

--- configs/experiments/2026-05-27-test-ufs-samudra-training-1-month-data/train.sh
+++ configs/experiments/2026-05-27-test-ufs-samudra-training-1-month-data/train.sh
@@ -10,7 +10,7 @@ CONFIG_PATH="${SCRIPT_PATH}${CONFIG_FILENAME}"
 BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
 REPO_ROOT=$(git rev-parse --show-toplevel)
 N_GPUS=1
-STATS_DATA=jamesd/2025-06-03-cm4-piControl-200yr-coupled-stats-ocean
+STATS_DATA=01KSTMKQ80R5EBQTJZY0H54KKX
 
 cd $REPO_ROOT  # so config path is valid no matter where we are running this script
 
