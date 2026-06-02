9d86fc89900540f22e2bca429d3783d7da5fc7a7
Troy Arcomano <troya@allenai.org>
2026-06-02 23:13:35 +0000

    test training from CM4-based weights

---
+++ configs/experiments/2026-05-27-test-ufs-samudra-training-1-month-data/train-ft-from-cm4.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+JOB_NAME="test-ft-from-cm4-samudra-1pct-ocean-train-using-ufs-dataset"
+JOB_GROUP="ufs-replay-ocean"
+CONFIG_FILENAME="train_ft_from_cm4_to_ufs_test.yaml"
+SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
+CONFIG_PATH="${SCRIPT_PATH}${CONFIG_FILENAME}"
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+REPO_ROOT=$(git rev-parse --show-toplevel)
+N_GPUS=4
+STATS_DATA="troya/2026-06-01-ufs-replay-ocean-1deg-19level-1994-2023-stats"
+CKPT_DATASET="01KT54731WCRPKAPMZ6Y47P6D6"
+cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+
+python -m fme.ace.validate_config --config_type train $CONFIG_PATH
+
+gantry run \
+    --name $JOB_NAME \
+    --task-name $JOB_NAME \
+    --description "SamudraI UFS pre-training" \
+    --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+    --workspace ai2/climate-titan \
+    --priority urgent \
+    --preemptible \
+    --cluster ai2/titan \
+    --weka climate-default:/climate-default \
+    --env WANDB_USERNAME=$BEAKER_USERNAME \
+    --env WANDB_NAME=$JOB_NAME \
+    --env WANDB_JOB_TYPE=training \
+    --env WANDB_RUN_GROUP=$JOB_GROUP \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $STATS_DATA:/ocean_stats \
+    --dataset $CKPT_DATASET:/weights \
+    --gpus $N_GPUS \
+    --shared-memory 400GiB \
+    --budget ai2/atec-climate \
+    --allow-dirty \
+    --system-python \
+    --install "pip install --no-deps ." \
+    -- torchrun --nproc_per_node $N_GPUS -m fme.ace.train $CONFIG_PATH
