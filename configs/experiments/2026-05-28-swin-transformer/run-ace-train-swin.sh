7813c1ed820f24ac109c995c440a3d46aa09fdbd
Alexey Yermakov <yyexela@disroot.org>
2026-06-02 15:10:35 -0700

    Updated train dispatch shell script

--- configs/experiments/2026-05-28-swin-transformer/run-ace-train-swin.sh
+++ configs/experiments/2026-05-28-swin-transformer/run-ace-train-swin.sh
@@ -7,6 +7,7 @@ JOB_GROUP="swin-transformer-comparison"
 export WANDB_PROJECT=${WANDB_PROJECT:-SwinTransformer}
 export BEAKER_WORKSPACE=${BEAKER_WORKSPACE:-ai2/ace}
 export BEAKER_CLUSTER=${BEAKER_CLUSTER:-ai2/titan}
+export BEAKER_PRIORITY=${BEAKER_PRIORITY:-high}
 
 declare -A JOBS=(
   ["ace-train-config-4deg-AIMIP-swin-gmron.yaml"]="ace2-era5-train-4deg-AIMIP-swin-v1-gmron"
