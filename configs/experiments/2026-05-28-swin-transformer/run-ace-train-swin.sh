b1978197f6ce20364391e41f9305b395e50da0f7
Alexey Yermakov <yyexela@disroot.org>
2026-06-03 09:48:10 -0700

    Starting nc-swin-v1 on climate-titan

--- configs/experiments/2026-05-28-swin-transformer/run-ace-train-swin.sh
+++ configs/experiments/2026-05-28-swin-transformer/run-ace-train-swin.sh
@@ -4,14 +4,14 @@ set -e
 
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 JOB_GROUP="swin-transformer-comparison"
-export WANDB_PROJECT=${WANDB_PROJECT:-SwinTransformer}
-export BEAKER_WORKSPACE=${BEAKER_WORKSPACE:-ai2/ace}
-export BEAKER_CLUSTER=${BEAKER_CLUSTER:-"ai2/titan ai2/saturn ai2/jupiter ai2/ceres"}
-export BEAKER_PRIORITY=${BEAKER_PRIORITY:-high}
+export WANDB_PROJECT=${WANDB_PROJECT:-"SwinTransformer"}
+export BEAKER_WORKSPACE=${BEAKER_WORKSPACE:-"ai2/climate-titan"}
+export BEAKER_CLUSTER=${BEAKER_CLUSTER:-"ai2/titan"}
+export BEAKER_PRIORITY=${BEAKER_PRIORITY:-"urgent"}
 
 declare -A JOBS=(
-  ["ace-train-config-4deg-AIMIP-swin-gmron.yaml"]="ace2-era5-train-4deg-AIMIP-swin-v1-gmron"
-  ["ace-train-config-4deg-AIMIP-swin-gmroff.yaml"]="ace2-era5-train-4deg-AIMIP-swin-v1-gmroff"
+  #["ace-train-config-4deg-AIMIP-swin-gmron.yaml"]="ace2-era5-train-4deg-AIMIP-swin-v1-gmron"
+  #["ace-train-config-4deg-AIMIP-swin-gmroff.yaml"]="ace2-era5-train-4deg-AIMIP-swin-v1-gmroff"
   ["ace-train-config-4deg-AIMIP-nc-swin-gmron.yaml"]="ace2-era5-train-4deg-AIMIP-nc-swin-v1-gmron"
   ["ace-train-config-4deg-AIMIP-nc-swin-gmroff.yaml"]="ace2-era5-train-4deg-AIMIP-nc-swin-v1-gmroff"
 )
