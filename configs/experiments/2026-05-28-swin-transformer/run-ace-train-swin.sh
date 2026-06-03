82004f476c9b4860688f1c6301a81c952b16302d
Alexey Yermakov <yyexela@disroot.org>
2026-06-03 13:34:42 -0700

    More runs

--- configs/experiments/2026-05-28-swin-transformer/run-ace-train-swin.sh
+++ configs/experiments/2026-05-28-swin-transformer/run-ace-train-swin.sh
@@ -5,14 +5,18 @@ set -e
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 JOB_GROUP="swin-transformer-comparison"
 export WANDB_PROJECT=${WANDB_PROJECT:-SwinTransformer}
-export BEAKER_WORKSPACE=${BEAKER_WORKSPACE:-ai2/climate-titan}
-export BEAKER_CLUSTER=${BEAKER_CLUSTER:-"ai2/titan"}
-export BEAKER_PRIORITY=${BEAKER_PRIORITY:-urgent}
+export BEAKER_WORKSPACE=${BEAKER_WORKSPACE:-ai2/ace}
+export BEAKER_CLUSTER=${BEAKER_CLUSTER:-"ai2/titan ai2/jupiter ai2/saturn ai2/ceres"}
+export BEAKER_PRIORITY=${BEAKER_PRIORITY:-high}
 
 declare -A JOBS=(
+  ["ace-train-config-4deg-AIMIP-nc-sfno-gmron-bs16-schedon.yaml"]="ace2-era5-train-4deg-AIMIP-nc-sfno-gmron-bs16-schedon"
+  ["ace-train-config-4deg-AIMIP-nc-sfno-gmron-bs16-schedoff.yaml"]="ace2-era5-train-4deg-AIMIP-nc-sfno-gmron-bs16-schedoff"
+  ["ace-train-config-4deg-AIMIP-nc-sfno-gmron-bs8-schedon.yaml"]="ace2-era5-train-4deg-AIMIP-nc-sfno-gmron-bs8-schedon"
+  ["ace-train-config-4deg-AIMIP-nc-sfno-gmron-bs8-schedoff.yaml"]="ace2-era5-train-4deg-AIMIP-nc-sfno-gmron-bs8-schedoff"
   #["ace-train-config-4deg-AIMIP-swin-gmron.yaml"]="ace2-era5-train-4deg-AIMIP-swin-v2-gmron"
   #["ace-train-config-4deg-AIMIP-swin-gmroff.yaml"]="ace2-era5-train-4deg-AIMIP-swin-v2-gmroff"
-  ["ace-train-config-4deg-AIMIP-nc-swin-gmron.yaml"]="ace2-era5-train-4deg-AIMIP-nc-swin-v2-gmron"
+  #["ace-train-config-4deg-AIMIP-nc-swin-gmron.yaml"]="ace2-era5-train-4deg-AIMIP-nc-swin-v2-gmron"
   #["ace-train-config-4deg-AIMIP-nc-swin-gmroff.yaml"]="ace2-era5-train-4deg-AIMIP-nc-swin-v2-gmroff"
   #["ace-train-config-4deg-AIMIP-sfno-gmron.yaml"]="ace2-era5-train-4deg-AIMIP-sfno-gmron"
   #["ace-train-config-4deg-AIMIP-sfno-gmroff.yaml"]="ace2-era5-train-4deg-AIMIP-sfno-gmroff"
