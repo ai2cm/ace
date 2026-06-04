48c82aeb558c207e9ccdf07e5d0db87969d71ddb
Troy Arcomano <troya@allenai.org>
2026-06-04 22:24:45 +0000

    run full eval

--- configs/baselines/era5/run-ace-evaluator.sh
+++ configs/baselines/era5/run-ace-evaluator.sh
@@ -2,42 +2,73 @@
 
 set -e
 
-JOB_NAME="ace2-era5-evaluator"
-JOB_GROUP="ace2-era5"
-EXISTING_RESULTS_DATASET="01K013CYF8HX12KJK91YJ8MM92"  # this contains the checkpoint to use for inference
-CONFIG_FILENAME="ace-evaluator-config.yaml"
 SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
-CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
- # since we use a service account API key for wandb, we use the beaker username to set the wandb username
 BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+# since we use a service account API key for wandb, we use the beaker username to set the wandb username by default
+WANDB_USERNAME=${WANDB_USERNAME:-${BEAKER_USERNAME}}
 REPO_ROOT=$(git rev-parse --show-toplevel)
+N_GPUS=1
 
-cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+cd "$REPO_ROOT"  # so config path is valid no matter where we are running this script
 
-python -m fme.ace.validate_config --config_type evaluator $CONFIG_PATH
+run_evaluator() {
+  local config_filename="$1"
+  local job_name="$2"
+  local job_group="$3"
+  local beaker_dataset_id="$4"
+  local checkpoint_relpath="${5:-training_checkpoints/best_inference_ckpt.tar}"
+  local CONFIG_PATH="$SCRIPT_PATH/$config_filename"
 
-cd $REPO_ROOT && gantry run \
-    --name $JOB_NAME \
-    --task-name $JOB_NAME \
-    --description 'Run ACE2-ERA5 evaluator' \
-    --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+  python -m fme.ace.validate_config --config_type evaluator "$CONFIG_PATH"
+
+  # Extract additional args from config header
+  local extra_args=()
+  while IFS= read -r line; do
+    [[ "$line" =~ ^#\ arg:\ (.*) ]] && extra_args+=(${BASH_REMATCH[1]})
+  done < "$CONFIG_PATH"
+
+  gantry run \
+    --name "$job_name" \
+    --task-name "$job_name" \
+    --description 'Run ACE2S-ERA5 evaluator' \
+    --beaker-image "$(cat "$REPO_ROOT/latest_deps_only_image.txt")" \
     --workspace ai2/ace \
     --priority normal \
     --not-preemptible \
     --cluster ai2/saturn-cirrascale \
     --cluster ai2/ceres-cirrascale \
-    --env WANDB_USERNAME=$BEAKER_USERNAME \
-    --env WANDB_NAME=$JOB_NAME \
+    --cluster ai2/titan \
+    --env WANDB_USERNAME="$WANDB_USERNAME" \
+    --env WANDB_NAME="$job_name" \
     --env WANDB_JOB_TYPE=inference \
-    --env WANDB_RUN_GROUP=$JOB_GROUP \
+    --env WANDB_RUN_GROUP="$job_group" \
     --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
     --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
     --dataset-secret google-credentials:/tmp/google_application_credentials.json \
-    --dataset $EXISTING_RESULTS_DATASET:training_checkpoints/best_inference_ckpt.tar:/ckpt.tar \
-    --gpus 1 \
+    --dataset "${beaker_dataset_id}:${checkpoint_relpath}:/ckpt.tar" \
+    --gpus "$N_GPUS" \
     --shared-memory 50GiB \
     --weka climate-default:/climate-default \
     --budget ai2/atec-climate \
     --system-python \
     --install "pip install --no-deps ." \
-    -- python -I -m fme.ace.evaluator $CONFIG_PATH
+    "${extra_args[@]}" \
+    -- python -I -m fme.ace.evaluator "$CONFIG_PATH"
+}
+
+base_name="ace2s"
+job_group="${base_name}-era5"
+
+# Beaker result dataset ID from the training job (beaker experiment get ... | jq ...)
+
+
+run_evaluator "ace-evaluator-config.yaml" \
+  "$base_name-era5-eval-multi-step-ft-inverse-ace2-channel-weightings-rs0" \
+  "$job_group" \
+  "01KT9RWQH2C3JQ4EY4N3M5FYBJ"
+
+run_evaluator "ace-evaluator-config.yaml" \
+  "$base_name-era5-eval-multi-step-ft-no-var-weighting-rs0" \
+  "$job_group" \
+  "01KSVC6YS7C18SGYV4VPZYZ232"
+
