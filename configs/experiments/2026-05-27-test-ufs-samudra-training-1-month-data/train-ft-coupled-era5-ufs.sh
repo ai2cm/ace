b1de54bec8763ea729337078a232ec930c89a1f9
Troy Arcomano <troya@allenai.org>
2026-06-07 19:28:45 -0700

    use urgent

--- configs/experiments/2026-05-27-test-ufs-samudra-training-1-month-data/train-ft-coupled-era5-ufs.sh
+++ configs/experiments/2026-05-27-test-ufs-samudra-training-1-month-data/train-ft-coupled-era5-ufs.sh
@@ -25,8 +25,8 @@ gantry run \
     --task-name $JOB_NAME \
     --description "Coupled ERA5+UFS ensemble fine-tuning" \
     --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
-    --workspace ai2/ace \
-    --priority high \
+    --workspace ai2/climate-titan \
+    --priority urgent \
     --preemptible \
     --cluster ai2/titan \
     --weka climate-default:/climate-default \
@@ -34,7 +34,6 @@ gantry run \
     --env WANDB_NAME=$JOB_NAME \
     --env WANDB_JOB_TYPE=training \
     --env WANDB_RUN_GROUP=$JOB_GROUP \
-    --env FME_LOG_NAN_DIAGNOSTICS=1 \
     --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
     --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
     --dataset-secret google-credentials:/tmp/google_application_credentials.json \
