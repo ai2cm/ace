46d521b6c37069a42392e8c87cd4b54b3296e52f
Troy Arcomano <troya@allenai.org>
2026-06-10 07:21:42 -0700

    preliminary eval of coupled FT UFS ERA5

---
+++ configs/experiments/2026-05-27-test-ufs-samudra-training-1-month-data/evaluate-ft-coupled-era5-ufs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+JOB_NAME="ft-coupled-era5-ufs-stochastic-5day-ocean-evaluator"
+JOB_GROUP="ufs-replay-ocean"
+EXISTING_RESULTS_DATASET="01KTMD1TRMSFN4FHP19CZAST62"
+CONFIG_FILENAME="evaluator-ft-coupled-era5-ufs.yaml"
+SCRIPT_PATH=$(git rev-parse --show-prefix)  # relative to the root of the repository
+CONFIG_PATH="${SCRIPT_PATH}${CONFIG_FILENAME}"
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd "$REPO_ROOT"  # so config path is valid no matter where we are running this script
+
+python -m fme.coupled.validate_config --config_type evaluator "$CONFIG_PATH"
+
+gantry run \
+    --name "$JOB_NAME" \
+    --task-name "$JOB_NAME" \
+    --description "Coupled ERA5+UFS ensemble fine-tuning evaluator" \
+    --beaker-image "$(cat "$REPO_ROOT/latest_deps_only_image.txt")" \
+    --workspace ai2/climate-titan \
+    --priority normal \
+    --preemptible \
+    --cluster ai2/titan \
+    --weka climate-default:/climate-default \
+    --env WANDB_USERNAME="$BEAKER_USERNAME" \
+    --env WANDB_NAME="$JOB_NAME" \
+    --env WANDB_JOB_TYPE=inference \
+    --env WANDB_RUN_GROUP="$JOB_GROUP" \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-ai2cm-sa \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset "$EXISTING_RESULTS_DATASET:training_checkpoints/best_inference_ckpt.tar:/ckpt.tar" \
+    --gpus 1 \
+    --shared-memory 50GiB \
+    --budget ai2/atec-climate \
+    --system-python \
+    --install "pip install --no-deps ." \
+    -- python -I -m fme.coupled.evaluator "$CONFIG_PATH"
