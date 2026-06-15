9b2e7261b2aaf092df8093793e6490a620bf2770
Alexey Yermakov <yyexela@disroot.org>
2026-06-15 14:45:25 -0700

    Added Apache 2.0 License to boundary_padding

--- fme/core/models/swin_transformer/boundary_padding.py
+++ fme/core/models/swin_transformer/boundary_padding.py
@@ -5,6 +5,21 @@
 # AI atmospheric prediction models.
 # Reference: Schreck et al., npj Clim Atmos Sci 8, 239 (2025).
 #   https://doi.org/10.1038/s41612-025-01125-6
+#
+# Copyright [2025] [NSF NCAR MILES group]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import dataclasses
 from typing import Literal
 
