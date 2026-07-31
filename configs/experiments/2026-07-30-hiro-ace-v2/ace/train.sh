43aeb6b15447f095afbcc5bf0ccdf5f29287e199
Anna Kwa <annak@allenai.org>
2026-07-31 11:33:04 -0700

    retune ace2s

---
+++ configs/experiments/2026-07-30-hiro-ace-v2/ace/train.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+
+
+CONFIG_FILENAME="fine-tune-xshield.yaml"
+
+SCRIPT_PATH=$(git rev-parse --show-prefix)
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+WANDB_USERNAME=${WANDB_USERNAME:-${BEAKER_USERNAME}}
+WANDB_GROUP=ace
+REPO_ROOT=$(git rev-parse --show-toplevel)
+N_GPUS=4
+STATS_DATASET=andrep/2026-06-08-vertically-resolved-1deg-c96-shield-ramped-climSST-random-CO2-ensemble-fme-dataset-stats
+#STATS_DATASET=andrep/2025-09-11-X-SHiELD-AMIP-1deg-8layer-11yr-stats
+SEED_OFFSET=10
+
+cd $REPO_ROOT
+
+# two different ACE2SOM seeds
+PRE_TRAINED_WEIGHTS_DATASETS=("01KHJ5F1M6YKVZESPZAAVVD6G8" "01KHCEF1SBYCZCGDM78N1CJC3H")
+PRMSL_SHIELDPLUS_TUNED=("01KV5ZF269W3T9WPVENGEWZZYW")
+
+# tuned on XSHiELD, 1-1, seed 0
+#TUNED_DATASET=01KQD8NF9HQD1QY2X0S132YH72
+
+# tuned on 5-1 split, seed 0
+#PRE_TRAINED_WEIGHTS_DATASETS_5_1=("01KQG2R8RCWH1ZJS0FK3P9Z8C4" "01KQG2RE1A9BC3G0JD46T3HM18")
+
+
+# --dataset ${PRE_TRAINED_WEIGHTS_DATASETS[$seed]}:training_checkpoints/best_ckpt.tar:/ckpt.tar \
+
+#       --dataset $TUNED_DATASET:/pre-trained-weights \
+#        --dataset ${PRE_TRAINED_WEIGHTS_DATASETS[$seed]}:/pre-trained-weights \
+for seed in {0..0}; do
+    #job_name="ace2som-xshield-tune-1yr-even-split-single-decoder-seed${seed}"
+    job_name="ace2s-shieldplus-tune-xshield-10yr-2014val-seed${seed}"
+    fine_tune_seed=$((seed + SEED_OFFSET))
+    override="seed=${fine_tune_seed}"
+    python -m fme.ace.validate_config --config_type train $CONFIG_PATH --override $override
+
+    gantry run \
+        --name $job_name \
+        --description 'Run ACE training' \
+        --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
+        --workspace ai2/ace \
+        --priority high \
+        --preemptible \
+        --cluster ai2/titan \
+        --env WANDB_NAME=$job_name \
+        --env WANDB_USERNAME=$WANDB_USERNAME \
+        --env WANDB_JOB_TYPE=training \
+        --env WANDB_RUN_GROUP=$WANDB_GROUP \
+        --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+        --env-secret WANDB_API_KEY=wandb-api-key-annak \
+        --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+        --dataset ${PRMSL_SHIELDPLUS_TUNED[$seed]}:training_checkpoints/best_inference_ckpt.tar:/ckpt.tar \
+        --dataset $STATS_DATASET:/statsdata \
+        --gpus $N_GPUS \
+        --shared-memory 400GiB \
+        --weka climate-default:/climate-default \
+        --budget ai2/atec-climate \
+        --no-python \
+        --install "pip install --no-deps ." \
+        --allow-dirty \
+        -- torchrun --nproc_per_node $N_GPUS -m fme.ace.train $CONFIG_PATH --override $override
+done
