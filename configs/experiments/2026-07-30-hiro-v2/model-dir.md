88294f289beaf7d5ef2868a2ff91cc4dc48fefd4
Anna Kwa <annak@allenai.org>
2026-07-31 09:58:52 -0700

    25 3 km pred tracks

---
+++ configs/experiments/2026-07-30-hiro-v2/model-dir.md
@@ -0,0 +1,15 @@
+### 100 to 3 km
+#### Training
+
+- training low sigma, +/-30 oversample x8 [01KRBYGNYJ6FD7PGNF3VVHQ5V1](https://beaker.org/orgs/ai2/workspaces/climate-titan/datasets/01KRBYGNYJ6FD7PGNF3VVHQ5V1)
+
+- high sigma, +/- oversample x8 [01KRGZT4X2QCW2RFH7WN7X8BYA](https://beaker.org/orgs/ai2/workspaces/climate-titan/datasets/01KRGZT4X2QCW2RFH7WN7X8BYA)[beaker job](https://beaker.org/orgs/ai2/workspaces/climate-titan/work/01KRGZT4PGD6RF76C6492AXD28)
+
+- eval tropics low sigma, +/-30 oversample x8 [01KREFYC0EJ91KX1YKGGCK05JJ](https://beaker.org/orgs/ai2/workspaces/climate-titan/work/01KREFYC0EJ91KX1YKGGCK05JJ)
+
+- eval events low sigma, +/-30 oversample x8 [01KREN3NCRCTFYF0KDXJTZ31AS](https://beaker.org/orgs/ai2/workspaces/climate-titan/work/01KREN3NCRCTFYF0KDXJTZ31AS)
+
+### 25 to 3 km
+#### Training:
+- dataset [01KYT6C60VZHDM1AFQJ42PTNRK](https://beaker.org/orgs/ai2/workspaces/climate-titan/datasets/01KYT6C60VZHDM1AFQJ42PTNRK)
+- [beaker link](https://beaker.org/orgs/ai2/workspaces/ace/work/01KYT6C60N27GQEMTTT5BYGX1R?taskId=01KYT6C60S62S99N96TWBH2ZCS&jobId=01KYT6C6432XEFQ6SHBZ72GMXS)
---
+++ configs/experiments/2026-07-30-hiro-v2/predict.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+JOB_NAME="predict-25-3km-perfect-pred-xshield-2023-tc-tracks"
+#JOB_NAME="eval-global-trained-denoising-moe-events"
+
+CONFIG_FILENAME="pred-25-3km-xshield-2023-tc-tracks.yaml"
+
+SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+
+ # since we use a service account API key for wandb, we use the beaker username to set the wandb username
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+
+N_NODES=1
+NGPU=4
+
+IMAGE="$(cat latest_deps_only_image.txt)"
+
+
+EXISTING_RESULTS_DATASET=01KYT6C60VZHDM1AFQJ42PTNRK
+wandb_group=""
+
+#--not-preemptible \
+#     --dataset $EXISTING_RESULTS_DATASET:checkpoints:/checkpoints \
+
+#    --dataset $EXISTING_RESULTS_DATASET:bundled_moe_multivariate.ckpt:/ckpt.tar \
+
+    # --dataset $EXISTING_RESULTS_DATASET_HIGH_SIGMA:checkpoints:/checkpoints_high_sigma  \
+    # --dataset $EXISTING_RESULTS_DATASET_LOW_SIGMA:checkpoints:/checkpoints_low_sigma  \
+gantry run \
+    --name $JOB_NAME \
+    --description 'Run 25km to 3km evaluation on ACE2S-SHiELD' \
+    --workspace ai2/climate-titan \
+    --priority urgent \
+    --cluster ai2/titan \
+    --beaker-image $IMAGE \
+    --env WANDB_USERNAME=$BEAKER_USERNAME \
+    --env WANDB_NAME=$JOB_NAME \
+    --env WANDB_JOB_TYPE=inference \
+    --env WANDB_RUN_GROUP=$wandb_group \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-annak \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $EXISTING_RESULTS_DATASET:checkpoints:/checkpoints  \
+    --weka climate-default:/climate-default \
+    --gpus $NGPU \
+    --shared-memory 400GiB \
+    --budget ai2/atec-climate \
+    --no-python \
+    --install "pip install --no-deps ." \
+    --allow-dirty \
+    -- torchrun --nproc_per_node $NGPU -m fme.downscaling.predict $CONFIG_PATH
