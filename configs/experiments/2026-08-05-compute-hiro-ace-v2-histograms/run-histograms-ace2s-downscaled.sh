81d86c3bc6980a5e433a8fb75977d8f7d2cad08c
Anna Kwa <annak@allenai.org>
2026-08-07 14:27:00 -0700

    wind events eval

---
+++ configs/experiments/2026-08-05-compute-hiro-ace-v2-histograms/run-histograms-ace2s-downscaled.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+cd "$REPO_ROOT"
+
+DEPS_ONLY_IMAGE="$(cat latest_deps_only_image.txt)"
+
+HISTOGRAM_SCRIPT="scripts/downscaling/histogram.py"
+ZARR_PATH="/climate-default/2026-08-03-hiro-ace-v2-outputs/2026-08-05-hiro-ace2s-downscale-global-2023-ic0000/global_ace2s_downscaled_2023.zarr"
+OUTPUT_DIR="/climate-default/2026-08-03-hiro-ace-v2-outputs/2026-08-03-hiro-ace-global-2023/prediction_histograms"
+
+submit_histogram_job() {
+    local job_name="$1"
+    shift
+    local variables=("$@")
+
+    gantry run \
+        --name "$job_name" \
+        --description "Compute histograms over 1 year of global 2023 HiRO-ACE output" \
+        --workspace ai2/ace \
+        --priority normal \
+        --cluster ai2/phobos \
+        --gpus 0 \
+        --budget ai2/atec-climate \
+        --beaker-image "$DEPS_ONLY_IMAGE" \
+        --weka climate-default:/climate-default \
+        --system-python \
+        --allow-dirty \
+        --install "pip install --no-deps ." \
+        -- bash -c "pip install 'dask[array]' && python '$HISTOGRAM_SCRIPT' '$ZARR_PATH' \
+            --progress-interval 120 \
+            --output-dir '$OUTPUT_DIR' \
+            --lat-range -65 65 \
+            --variables ${variables[*]}"
+}
+
+submit_histogram_job "hiro-histograms-precip-hiro-ace-2023" PRATEsfc
+submit_histogram_job "hiro-histograms-prmsl-hiro-ace-2023" PRMSL
+submit_histogram_job "hiro-histograms-wind-hiro-ace-2023" UGRD10m VGRD10m wind_speed
