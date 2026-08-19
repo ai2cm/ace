5fa69476b3d186f8dd458181a1c833bb4bcab7e6
Anna Kwa <annak@allenai.org>
2026-08-19 13:48:31 -0700

    move files

---
+++ configs/experiments/2026-07-30-hiro-ace-v2/ace/predict.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -e
+
+JOB_NAME="predict-all-ace2s-tcs-max-snapshot"
+#JOB_NAME="eval-global-trained-denoising-moe-events"
+
+CONFIG_FILENAME="pred-all-ace2s-tcs-max-snapshot.yaml"
+
+SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
+CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
+
+ # since we use a service account API key for wandb, we use the beaker username to set the wandb username
+BEAKER_USERNAME=$(beaker account whoami --format=json | jq -r '.[0].name')
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd $REPO_ROOT  # so config path is valid no matter where we are running this script
+
+N_NODES=1
+NGPU=4
+
+IMAGE="$(cat latest_deps_only_image.txt)"
+
+EXISTING_RESULTS_DATASET_HIGH_SIGMA=01KRGZT4X2QCW2RFH7WN7X8BYA
+EXISTING_RESULTS_DATASET_LOW_SIGMA=01KRBYGNYJ6FD7PGNF3VVHQ5V1
+#EXISTING_RESULTS_DATASET=01KYT6C60VZHDM1AFQJ42PTNRK
+wandb_group=""
+
+#--not-preemptible \
+#     --dataset $EXISTING_RESULTS_DATASET:checkpoints:/checkpoints \
+
+#    --dataset $EXISTING_RESULTS_DATASET:bundled_moe_multivariate.ckpt:/ckpt.tar \
+
+    # --dataset $EXISTING_RESULTS_DATASET_HIGH_SIGMA:checkpoints:/checkpoints_high_sigma  \
+    # --dataset $EXISTING_RESULTS_DATASET_LOW_SIGMA:checkpoints:/checkpoints_low_sigma  \
+gantry run \
+    --name $JOB_NAME \
+    --description 'Run 100km to 3km evaluation on ACE2S-SHiELD' \
+    --workspace ai2/ace \
+    --priority urgent \
+    --cluster ai2/titan \
+    --beaker-image $IMAGE \
+    --env WANDB_USERNAME=$BEAKER_USERNAME \
+    --env WANDB_NAME=$JOB_NAME \
+    --env WANDB_JOB_TYPE=inference \
+    --env WANDB_RUN_GROUP=$wandb_group \
+    --env CM_PRIORITY=HIGH \
+    --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/google_application_credentials.json \
+    --env-secret WANDB_API_KEY=wandb-api-key-annak \
+    --dataset-secret google-credentials:/tmp/google_application_credentials.json \
+    --dataset $EXISTING_RESULTS_DATASET_HIGH_SIGMA:checkpoints:/checkpoints_high_sigma  \
+    --dataset $EXISTING_RESULTS_DATASET_LOW_SIGMA:checkpoints:/checkpoints_low_sigma  \
+    --weka climate-default:/climate-default \
+    --gpus $NGPU \
+    --shared-memory 400GiB \
+    --budget ai2/atec-climate \
+    --no-python \
+    --install "pip install --no-deps ." \
+    --allow-dirty \
+    -- torchrun --nproc_per_node $NGPU -m fme.downscaling.predict $CONFIG_PATH
