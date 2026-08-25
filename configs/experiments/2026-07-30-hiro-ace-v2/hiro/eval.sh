f1fcbc376f3d986801049307ba942dc20fbbeb85
Anna Kwa <annak@allenai.org>
2026-08-25 10:51:08 -0700

    test with default patch config

--- configs/experiments/2026-07-30-hiro-ace-v2/hiro/eval.sh
+++ configs/experiments/2026-07-30-hiro-ace-v2/hiro/eval.sh
@@ -3,9 +3,9 @@
 set -e
 
 #JOB_NAME="eval-tmp2m-prate-events"
-JOB_NAME="eval-Phl-32x"
+JOB_NAME="eval-Phl-no-composite"
 
-CONFIG_FILENAME="eval-perfect-pred-example-Phl-tc-max-snapshot.yaml"
+CONFIG_FILENAME="eval-perfect-pred-example-Phl-tc-max-snapshot-test.yaml"
 
 SCRIPT_PATH=$(echo "$(git rev-parse --show-prefix)" | sed 's:/*$::')
 CONFIG_PATH=$SCRIPT_PATH/$CONFIG_FILENAME
@@ -17,7 +17,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 cd $REPO_ROOT  # so config path is valid no matter where we are running this script
 
 N_NODES=1
-NGPU=4
+NGPU=2
 
 IMAGE="$(cat latest_deps_only_image.txt)"
 #EXISTING_RESULTS_DATASET=01KTCHVDHY0SATWH9E0AW2PDS6
