7b14e8c088756b20b52afb144944357f19dcf5be
Spencer Clark <spencerkclark@gmail.com>
2026-09-17 21:25:44 -0400

    Submit runs with non-energy-conserving no-random-co2 stochastic model

--- configs/experiments/2026-07-14-investigate-crashes/run-ace-1000-year-equilibrium-climate-segmented-inference.sh
+++ configs/experiments/2026-07-14-investigate-crashes/run-ace-1000-year-equilibrium-climate-segmented-inference.sh
@@ -13,9 +13,9 @@ INITIAL_CONDITION_TIME=2032-01-01T00:00:00
 
 declare -A INITIAL_CONDITION_DATASETS
 INITIAL_CONDITION_DATASETS=( \
-    # ["1xCO2"]="${INITIAL_CONDITION_ROOT}/1xCO2-ic_0005.zarr" \
-    # ["2xCO2"]="${INITIAL_CONDITION_ROOT}/2xCO2-ic_0005.zarr" \
-    # ["3xCO2"]="${INITIAL_CONDITION_ROOT}/3xCO2-ic_0002.zarr" \
+    ["1xCO2"]="${INITIAL_CONDITION_ROOT}/1xCO2-ic_0005.zarr" \
+    ["2xCO2"]="${INITIAL_CONDITION_ROOT}/2xCO2-ic_0005.zarr" \
+    ["3xCO2"]="${INITIAL_CONDITION_ROOT}/3xCO2-ic_0002.zarr" \
     ["4xCO2"]="${INITIAL_CONDITION_ROOT}/4xCO2-ic_0005.zarr" \
 )
 
@@ -29,9 +29,9 @@ CO2_CONCENTRATIONS=( \
 
 declare -A MODELS=( \
     # [published-baseline-rs3]="01J4BR6J5AW32ZDQ77VZ60P4KT" \
-    # [no-random-co2-rs0]="01KHGDAMB2BDZQS8JFF65A2YDR" \
+    [no-random-co2-rs0]="01KHGDAMB2BDZQS8JFF65A2YDR" \
     # [no-random-co2-rs1]="01KH4SDCYN1NF2RP2JXZS0WZ1Y" \
-    [no-random-co2-energy-conserving-rs0]="01KHGDA8TVGP9JKWVJ1N0SMHCN" \
+    # [no-random-co2-energy-conserving-rs0]="01KHGDA8TVGP9JKWVJ1N0SMHCN" \
     # [no-random-co2-energy-conserving-rs1]="01KH4SDT1Q5246GZ307W8AW4M3" \
     # [full-rs0]="01KHKJ02SQM8S8T4B6030F94CV" \
     # [full-rs1]="01KHJ5EQ04XTFG46QCKX3TTAHF" \
@@ -43,7 +43,7 @@ declare -A MODELS=( \
 REPO_ROOT=$(git rev-parse --show-toplevel)
 cd $REPO_ROOT  # so config path is valid no matter where we are running this script
 
-for seed in {0..6}; do
+for seed in {0..0}; do
     for model in "${!MODELS[@]}"; do
         dataset_id="${MODELS[$model]}"
 
@@ -66,8 +66,7 @@ for seed in {0..6}; do
                 --beaker-image "$(cat $REPO_ROOT/latest_deps_only_image.txt)" \
                 --workspace ai2/ace \
                 --priority high \
-                --cluster ai2/titan \
-                --env CM_PRIORITY=high \
+                --cluster ai2/jupiter \
                 --env WANDB_USERNAME=$WANDB_USERNAME \
                 --env WANDB_NAME=$job_name \
                 --env WANDB_JOB_TYPE=inference \
