944a5bb083a6d3a4bb670a38656170559e963944
adam-subel <adam.subel@nyu.edu>
2026-09-25 11:15:02 -0400

    Fix typesetting, remove unused import

--- fme/core/ocean_derived_variables.py
+++ fme/core/ocean_derived_variables.py
@@ -196,7 +196,7 @@ def HI(
 ) -> torch.Tensor:
     """Recover sea ice thickness (HI) in meters from sea ice volume in m^3.
 
-    HI = sea_ice_volume/ (area_weights_m2 * sea_ice_frac)
+    HI = sea_ice_volume / (area_weights_m2 * sea_ice_frac)
     """
     return data.sea_ice_thickness
 
